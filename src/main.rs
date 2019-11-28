98ab15397879d7e31b6057679bd5d112abf95891
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-11-28 19:32:32 +0000

    Sending packet :)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-mod directory;
-mod mix;
+mod clients;
 
-fn main() {
-    let _ = "Hello, Sphinx!".as_bytes().to_vec();
+use tokio::prelude::*;
+
+use crate::clients::mix::MixClient;
+use crate::clients::directory::DirectoryClient;
+
+#[tokio::main]
+async fn main() {
+    let message = "Hello, Sphinx!".as_bytes().to_vec();
+
+    // set up the route
+    let directory = DirectoryClient::new();
+    let route = directory.get_mixes();
+    let destination = directory.get_destination();
+
+    // build the packet
+    let packet = sphinx::SphinxPacket::new(message, &route[..], &destination);
+
+    // send to mixnet
+    let mix_client = MixClient::new();
+    let result = mix_client.send(packet, route.first().unwrap()).await;
 }
 
 

39bbecb55d4187d7053f30a495a0eff9e0d01eaa
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-12-11 16:42:25 +0000

    Closing socket to server on being done

--- src/clients/provider.rs
+++ src/clients/provider.rs
@@ -2,6 +2,7 @@ use sphinx::route::Node as MixNode;
 use sphinx::SphinxPacket;
 use tokio::prelude::*;
 use sfw_provider_requests::*;
+use std::net::Shutdown;
 
 pub struct ProviderClient {}
 
@@ -19,8 +20,20 @@ impl ProviderClient {
         let pull_request = PullRequest::new(address);
         let bytes = pull_request.to_bytes();
 
-        let mut stream = tokio::net::TcpStream::connect("127.0.0.1:9000").await?;
-        stream.write_all(&bytes[..]).await?;
+        let mut socket = tokio::net::TcpStream::connect("127.0.0.1:9000").await?;
+        socket.write_all(&bytes[..]).await?;
+        if let Err(e) = socket.shutdown(Shutdown::Write) {
+            eprintln!("failed to close write part of the socket; err = {:?}", e)
+        }
+
+        let mut response = Vec::new();
+        socket.read_to_end(&mut response).await?;
+        if let Err(e) = socket.shutdown(Shutdown::Read) {
+            eprintln!("failed to close read part of the socket; err = {:?}", e)
+        }
+
+        println!("Received the following response: {:?}", response);
+
         Ok(())
     }
 }

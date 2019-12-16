265ef228383a21a0dec1b836ac12cadddae83dc8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-12-16 12:20:48 +0000

    main: can now be started with a local directory server for testing purposes

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,12 @@ fn main() {
                         .long("keyfile")
                         .help("Optional path to the persistent keyfile of the node")
                         .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("local")
+                        .long("local")
+                        .help("Flag to indicate whether the client is expected to run on a local deployment.")
+                        .takes_value(false),
                 ),
         )
         .get_matches();

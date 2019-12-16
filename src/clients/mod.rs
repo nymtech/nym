a472c23b48819271f50d568e01a7dea50259a054
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-12-16 10:59:11 +0000

    Managed to combine cover traffic stream with out queue

--- src/clients/mod.rs
+++ src/clients/mod.rs
@@ -3,9 +3,14 @@ use tokio::runtime::Runtime;
 use futures::channel::mpsc;
 use std::time::Duration;
 use crate::utils;
-use futures::{future, StreamExt, SinkExt};
+use futures::{StreamExt, SinkExt, Stream, TryStreamExt};
 use crate::clients::mix::MixClient;
 use sphinx::SphinxPacket;
+use futures::Future;
+use futures::future;
+use futures::task::{Context, Poll};
+use std::pin::Pin;
+use futures::select;
 
 pub mod directory;
 pub mod mix;
@@ -34,22 +39,19 @@ struct MixMessage(NodeAddressBytes, SphinxPacket);
 struct MixTrafficController;
 
 
-
 impl MixTrafficController {
     // this was way more difficult to implement than what this code may suggest...
     async fn run(mut rx: mpsc::UnboundedReceiver<MixMessage>) {
         let mix_client = MixClient::new();
         while let Some(mix_message) = rx.next().await {
-            println!("got a mix_message for {:?}", mix_message.0);
-
-            println!("here i will be sending sphinx packet to a mixnode ({:?}!", mix_message.0);
+            println!("[MIX TRAFFIC CONTROL] - got a mix_message for {:?}", mix_message.0);
             // here NodeAddressBytes would be transformed into a SocketAddr with SOME library call...
             let node_net_address = "127.0.0.1:8080";
-            let send_res = mix_client.send(mix_message.1, node_net_address.parse().unwrap()).await;
-            match send_res {
-                Ok(_) => println!("We successfully sent the message!"),
-                Err(e) => eprintln!("We failed to send the message :( - {:?}", e),
-            };
+//            let send_res = mix_client.send(mix_message.1, node_net_address.parse().unwrap()).await;
+//            match send_res {
+//                Ok(_) => println!("We successfully sent the message!"),
+//                Err(e) => eprintln!("We failed to send the message :( - {:?}", e),
+//            };
         }
     }
 }
@@ -57,16 +59,38 @@ impl MixTrafficController {
 pub struct NymClient {
     // to be replaced by something else I guess
     address: DestinationAddressBytes,
-    pub input_tx: mpsc::UnboundedSender<Vec<u8>>,
+    pub input_tx: mpsc::UnboundedSender<InputMessage>,
     // to be used by "send" function or socket, etc
-    input_rx: mpsc::UnboundedReceiver<Vec<u8>>,
+    input_rx: mpsc::UnboundedReceiver<InputMessage>,
 }
 
 type TripleFutureResult = (Result<(), Box<dyn std::error::Error>>, Result<(), Box<dyn std::error::Error>>, Result<(), Box<dyn std::error::Error>>);
 
+//struct OutQueueFuture {
+//
+//}
+//
+//impl OutQueueFuture {
+//    fn poll_data(&mut self, cx: &mut Context) -> Poll<()> {
+//        use Poll::*;
+//
+//    }
+//}
+//
+//impl Future for OutQueueFuture {
+//    type Output = ();
+//
+//    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+//        unimplemented!()
+//    }
+//}
+
+#[derive(Debug)]
+pub struct InputMessage(Destination, Vec<u8>);
+
 impl NymClient {
     pub fn new(address: DestinationAddressBytes) -> Self {
-        let (input_tx, input_rx) = mpsc::unbounded::<Vec<u8>>();
+        let (input_tx, input_rx) = mpsc::unbounded::<InputMessage>();
 
         NymClient {
             address,
@@ -77,20 +101,35 @@ impl NymClient {
 
     async fn start_loop_cover_traffic_stream(mut tx: mpsc::UnboundedSender<MixMessage>, our_info: Destination) -> Result<(), Box<dyn std::error::Error>> {
         loop {
+            println!("[LOOP COVER TRAFFIC STREAM] - next cover message!");
             let delay = utils::poisson::sample(LOOP_COVER_AVERAGE_DELAY);
             let delay_duration = Duration::from_secs_f64(delay);
-            println!("waiting for {:?}", delay_duration);
             tokio::time::delay_for(delay_duration).await;
             let cover_message = utils::sphinx::loop_cover_message(our_info.address, our_info.identifier);
-            println!("waited {:?} - time to send cover message!", delay_duration);
             tx.send(MixMessage(cover_message.0, cover_message.1)).await?;
         }
     }
 
-    async fn control_out_queue(mut mix_tx: mpsc::UnboundedSender<MixMessage>, mut input_rx: mpsc::UnboundedReceiver<Vec<u8>>) -> Result<(), Box<dyn std::error::Error>> {
+    async fn control_out_queue(mut mix_tx: mpsc::UnboundedSender<MixMessage>, mut input_rx: mpsc::UnboundedReceiver<InputMessage>, our_info: Destination) -> Result<(), Box<dyn std::error::Error>> {
         loop {
-            println!("here I will be sending real traffic (or loop cover if nothing is available)");
-            let delay_duration = Duration::from_secs_f64(10.0);
+            println!("[OUT QUEUE] here I will be sending real traffic (or loop cover if nothing is available)");
+            select! {
+                real_message = input_rx.next() => {
+                    println!("[OUT QUEUE] - we got a real message!");
+                    let real_message = real_message.expect("The channel must have closed! - if the client hasn't crashed, it should have!");
+                    println!("real: {:?}", real_message);
+                    let encapsulated_message = utils::sphinx::encapsulate_message(real_message.0, real_message.1);
+                    mix_tx.send(MixMessage(encapsulated_message.0, encapsulated_message.1)).await?;
+                },
+
+                default => {
+                    println!("[OUT QUEUE] - no real message - going to send extra loop cover");
+                    let cover_message = utils::sphinx::loop_cover_message(our_info.address, our_info.identifier);
+                    mix_tx.send(MixMessage(cover_message.0, cover_message.1)).await?;
+                }
+            };
+
+            let delay_duration = Duration::from_secs_f64(MESSAGE_SENDING_AVERAGE_DELAY);
             tokio::time::delay_for(delay_duration).await;
         }
     }
@@ -98,7 +137,7 @@ impl NymClient {
 
     async fn start_provider_polling() -> Result<(), Box<dyn std::error::Error>> {
         loop {
-            println!("here I will be polling provider for messages");
+//            println!("here I will be polling provider for messages");
             let delay_duration = Duration::from_secs_f64(10.0);
             tokio::time::delay_for(delay_duration).await;
         }
@@ -109,16 +148,24 @@ impl NymClient {
         println!("starting nym client");
 
         let (mix_tx, mix_rx) = mpsc::unbounded();
-        let our_info = Destination::new(self.address, Default::default());
 
         let mut rt = Runtime::new()?;
-
         rt.spawn(MixTrafficController::run(mix_rx));
+
+        let tmp_dest = Destination::new(self.address, Default::default());
+        let mut foo_tx = self.input_tx.clone();
+//         just to send something after a delay to the input channel
+        rt.spawn(async move {
+            tokio::time::delay_for(Duration::from_secs(8)).await;
+            let tmp_message = vec![5, 23, 62, 6, 236];
+            println!("SENDING TMP MESSAGE!");
+            foo_tx.send(InputMessage(tmp_dest, tmp_message)).await.unwrap();
+        });
 
         rt.block_on(async {
             let future_results = futures::future::join3(
-                NymClient::start_loop_cover_traffic_stream(mix_tx.clone(), our_info),
-                NymClient::control_out_queue(mix_tx, self.input_rx),
+                NymClient::start_loop_cover_traffic_stream(mix_tx.clone(), Destination::new(self.address, Default::default())),
+                NymClient::control_out_queue(mix_tx, self.input_rx, Destination::new(self.address, Default::default())),
                 NymClient::start_provider_polling()).await;
             assert!(future_results.0.is_ok() && future_results.1.is_ok() && future_results.2.is_ok());
         });
--- src/utils/sphinx.rs
+++ src/utils/sphinx.rs
@@ -8,6 +8,16 @@ pub fn loop_cover_message(
     our_address: DestinationAddressBytes,
     surb_id: SURBIdentifier,
 ) -> (NodeAddressBytes, SphinxPacket) {
+    let destination = Destination::new(our_address, surb_id);
+
+    encapsulate_message(destination, LOOP_COVER_MESSAGE_PAYLOAD.to_vec())
+}
+
+pub fn encapsulate_message(
+    recipient: Destination,
+    message: Vec<u8>,
+) -> (NodeAddressBytes, SphinxPacket) {
+    // here we would be getting topology, etc
     let first_node_address = bytes::zero_pad_to_32("127.0.0.1:8080".as_bytes().to_vec());
     let dummy_route = vec![
         Node::new(first_node_address, Default::default()),
@@ -17,17 +27,10 @@ pub fn loop_cover_message(
         ),
     ];
 
-    let destination = Destination::new(our_address, surb_id);
     let delays = sphinx::header::delays::generate(dummy_route.len());
 
     // build the packet
-    let packet = sphinx::SphinxPacket::new(
-        LOOP_COVER_MESSAGE_PAYLOAD.to_vec(),
-        &dummy_route[..],
-        &destination,
-        &delays,
-    )
-    .unwrap();
+    let packet = sphinx::SphinxPacket::new(message, &dummy_route[..], &recipient, &delays).unwrap();
 
     (first_node_address, packet)
 }

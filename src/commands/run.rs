127f6b4047a48c709974b79b733a1f1292a33f0d
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2019-12-17 14:48:44 +0000

    run: reading the client's keypair from disk whenever it's run

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,13 +1,23 @@
 use crate::clients::NymClient;
+use crate::identity::mixnet;
+use crate::persistence::pathfinder::Pathfinder;
+use crate::persistence::pemstore::PemStore;
 use clap::ArgMatches;
 
 pub fn execute(matches: &ArgMatches) {
     let is_local = matches.is_present("local");
+
+    let id = matches.value_of("id").unwrap().to_string();
     println!("Starting client, local: {:?}", is_local);
 
-    // TODO: to be taken from config or something
-    let my_address = [42u8; 32];
-    let is_local = true;
-    let client = NymClient::new(my_address, is_local);
+    let keypair = read_keypair_from_disk(id);
+    let client = NymClient::new(keypair.public_bytes(), is_local);
     client.start().unwrap();
+}
+
+fn read_keypair_from_disk(id: String) -> mixnet::KeyPair {
+    let pathfinder = Pathfinder::new(id);
+    let pem_store = PemStore::new(pathfinder);
+    let keypair = pem_store.read();
+    keypair
 }

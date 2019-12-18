812a0fe1446023d989898e6f3a86f91d3a29a421
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-12-18 10:25:32 +0000

    Deprecated run command

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -5,12 +5,13 @@ use clap::ArgMatches;
 
 pub fn execute(matches: &ArgMatches) {
     println!("{}", banner());
-
-    let is_local = matches.is_present("local");
-    let id = matches.value_of("id").unwrap().to_string();
-    println!("Starting client...");
-
-    let keypair = pemstore::read_keypair_from_disk(id);
-    let client = NymClient::new(keypair.public_bytes(), is_local);
-    client.start("127.0.0.1:9000".parse().unwrap()).unwrap();
+    panic!("For time being this command is deprecated! Please use 'websocket' instead");
+    //
+    //    let is_local = matches.is_present("local");
+    //    let id = matches.value_of("id").unwrap().to_string();
+    //    println!("Starting client...");
+    //
+    //    let keypair = pemstore::read_keypair_from_disk(id);
+    //    let client = NymClient::new(keypair.public_bytes(), is_local);
+    //    client.start("127.0.0.1:9000".parse().unwrap()).unwrap();
 }

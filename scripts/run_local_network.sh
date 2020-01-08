67a900701e926cf02b5dd00c8ff3ea949f3a4ca8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-01-08 15:56:01 +0000

    Updated script copyright date

--- scripts/run_local_network.sh
+++ scripts/run_local_network.sh
@@ -1,4 +1,4 @@
-#// Copyright 2018 The Nym Mixnet Authors
+#// Copyright 2020 The Nym Mixnet Authors
 #//
 #// Licensed under the Apache License, Version 2.0 (the "License");
 #// you may not use this file except in compliance with the License.

db2f80302336845bb6840c6aa4690ee0a6b3aea4
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-09 15:03:24 +0000

    Making use of the code simplification

--- clients/nym-client/src/utils/sphinx.rs
+++ clients/nym-client/src/utils/sphinx.rs
@@ -1,7 +1,5 @@
-use crate::utils::bytes;
 use addressing;
-use curve25519_dalek::montgomery::MontgomeryPoint;
-use sphinx::route::{Destination, DestinationAddressBytes, Node, SURBIdentifier};
+use sphinx::route::{Destination, DestinationAddressBytes, SURBIdentifier};
 use sphinx::SphinxPacket;
 use std::net::SocketAddr;
 use topology::NymTopology;
@@ -23,17 +21,8 @@ pub fn encapsulate_message<T: NymTopology>(
     message: Vec<u8>,
     topology: &T,
 ) -> (SocketAddr, SphinxPacket) {
-    let providers = topology.get_mix_provider_nodes();
-    let first_provider = providers.first().unwrap();
-    let decoded_key_bytes =
-        base64::decode_config(&first_provider.pub_key, base64::URL_SAFE).unwrap();
-    let key_bytes = bytes::zero_pad_to_32(decoded_key_bytes);
-    let key = MontgomeryPoint(key_bytes);
-
-    let provider = Node::new(
-        addressing::encoded_bytes_from_socket_address(first_provider.mixnet_listener.clone()),
-        key,
-    );
+    let mut providers = topology.get_mix_provider_nodes();
+    let provider = providers.pop().unwrap().into();
 
     let route = topology.route_to(provider).unwrap();
 

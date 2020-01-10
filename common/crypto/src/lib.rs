73028f85a98e8c2e4675fd21b9add7c5ac6e142f
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-10 13:49:00 +0000

    Defined trait for returning key pem type

--- common/crypto/src/lib.rs
+++ common/crypto/src/lib.rs
@@ -1,2 +1,9 @@
 pub mod encryption;
 pub mod identity;
+
+// TODO: this trait will need to be moved elsewhere, probably to some 'persistence' crate
+// but since it will need to be used by all identities, it's not really appropriate if it lived in nym-client
+
+pub trait PemStorable {
+    fn pem_type(&self) -> String;
+}

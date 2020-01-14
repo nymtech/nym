164ebf47be708b0e5231385f5c8f68187829506f
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-14 10:26:54 +0000

    From nymtopology nodes for prence topology

--- common/clients/directory-client/src/presence.rs
+++ common/clients/directory-client/src/presence.rs
@@ -1,7 +1,7 @@
 use crate::requests::presence_topology_get::PresenceTopologyGetRequester;
 use crate::{Client, Config, DirectoryClient};
 use serde::{Deserialize, Serialize};
-use topology::NymTopology;
+use topology::{NymTopology, MixProviderNode, MixNode, CocoNode};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -19,6 +19,17 @@ impl Into<topology::CocoNode> for CocoPresence {
             pub_key: self.pub_key,
             last_seen: self.last_seen,
             version: self.version,
+        }
+    }
+}
+
+impl From<topology::CocoNode> for CocoPresence {
+    fn from(cn: CocoNode) -> Self {
+        CocoPresence {
+            host: cn.host,
+            pub_key: cn.pub_key,
+            last_seen: cn.last_seen,
+            version: cn.version,
         }
     }
 }
@@ -44,6 +55,19 @@ impl Into<topology::MixNode> for MixNodePresence {
         }
     }
 }
+
+impl From<topology::MixNode> for MixNodePresence {
+    fn from(mn: MixNode) -> Self {
+        MixNodePresence {
+            host: mn.host.to_string(),
+            pub_key: mn.pub_key,
+            layer: mn.last_seen,
+            last_seen: mn.last_seen,
+            version: mn.version,
+        }
+    }
+}
+
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -73,6 +97,19 @@ impl Into<topology::MixProviderNode> for MixProviderPresence {
     }
 }
 
+impl From<topology::MixProviderNode> for MixProviderPresence {
+    fn from(mpn: MixProviderNode) -> Self {
+        MixProviderPresence {
+            client_listener: mpn.client_listener.to_string(),
+            mixnet_listener: mpn.mixnet_listener.to_string(),
+            pub_key: mpn.pub_key,
+            registered_clients: mpn.registered_clients.into_iter().map(|c| c.into()).collect(),
+            last_seen: mpn.last_seen,
+            version: mpn.version,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MixProviderClient {
@@ -83,6 +120,14 @@ impl Into<topology::MixProviderClient> for MixProviderClient {
     fn into(self) -> topology::MixProviderClient {
         topology::MixProviderClient {
             pub_key: self.pub_key,
+        }
+    }
+}
+
+impl From<topology::MixProviderClient> for MixProviderClient {
+    fn from(mpc: topology::MixProviderClient) -> Self {
+        MixProviderClient {
+            pub_key: mpc.pub_key,
         }
     }
 }

551d0eb161e59494bb109c2db4591841c8eecfdb
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-21 17:16:22 +0000

    More comments regarding uses of specific channels

--- nym-client/src/client/mod.rs
+++ nym-client/src/client/mod.rs
@@ -127,8 +127,19 @@ impl NymClient {
         let mut rt = Runtime::new()?;
 
         // channels for inter-component communication
+
+        // mix_tx is the transmitter for any component generating sphinx packets that are to be sent to the mixnet
+        // they are used by cover traffic stream and real traffic stream
+        // mix_rx is the receiver used by MixTrafficController that sends the actual traffic
         let (mix_tx, mix_rx) = mpsc::unbounded();
+
+        // poller_input_tx is the transmitter of messages fetched from the provider - used by ProviderPoller
+        // poller_input_rx is the receiver for said messages - used by ReceivedMessagesBuffer
         let (poller_input_tx, poller_input_rx) = mpsc::unbounded();
+
+        // received_messages_buffer_output_tx is the transmitter for *REQUESTS* for messages contained in ReceivedMessagesBuffer - used by sockets
+        // the requests contain a oneshot channel to send a reply on
+        // received_messages_buffer_output_rx is the received for the said requests - used by ReceivedMessagesBuffer
         let (received_messages_buffer_output_tx, received_messages_buffer_output_rx) =
             mpsc::unbounded();
 

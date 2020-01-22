566e17719c8e8691a0da7e7bb76488ccec0fe5ee
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-22 12:22:05 +0000

    added PartialEq restriction on NymTopology for convenience sake

--- common/topology/src/lib.rs
+++ common/topology/src/lib.rs
@@ -83,7 +83,7 @@ pub enum NymTopologyError {
     MissingLayerError(Vec<u64>),
 }
 
-pub trait NymTopology: Sized {
+pub trait NymTopology: Sized + PartialEq {
     fn new(directory_server: String) -> Self;
     fn new_from_nodes(
         mix_nodes: Vec<MixNode>,

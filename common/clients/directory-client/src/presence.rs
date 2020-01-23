e5b3a29c5f1b29bc0d9a7d172edf5f56465f7cae
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-23 16:53:02 +0000

    Defined ability to compare two 'Topology' instances
    The comparison ignores 'last-seen' field or ordering of nodes and
    clients

--- common/clients/directory-client/src/presence.rs
+++ common/clients/directory-client/src/presence.rs
@@ -1,12 +1,19 @@
 use crate::requests::presence_topology_get::PresenceTopologyGetRequester;
 use crate::{Client, Config, DirectoryClient};
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::convert::TryInto;
 use std::io;
 use std::net::ToSocketAddrs;
 use topology::{CocoNode, MixNode, MixProviderNode, NymTopology};
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+// special version of 'PartialEq' that does not care about last_seen field (where applicable)
+// or order of elements in vectors
+trait PresenceEq {
+    fn presence_eq(&self, other: &Self) -> bool;
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CocoPresence {
     pub host: String,
@@ -37,7 +44,7 @@ impl From<topology::CocoNode> for CocoPresence {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MixNodePresence {
     pub host: String,
@@ -81,7 +88,7 @@ impl From<topology::MixNode> for MixNodePresence {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MixProviderPresence {
     pub client_listener: String,
@@ -90,6 +97,32 @@ pub struct MixProviderPresence {
     pub registered_clients: Vec<MixProviderClient>,
     pub last_seen: u64,
     pub version: String,
+}
+
+impl PresenceEq for MixProviderPresence {
+    fn presence_eq(&self, other: &Self) -> bool {
+        if self.registered_clients.len() != other.registered_clients.len() {
+            return false;
+        }
+
+        if self.client_listener != other.client_listener
+            || self.mixnet_listener != other.mixnet_listener
+            || self.pub_key != other.pub_key
+            || self.version != other.version
+        {
+            return false;
+        }
+
+        let clients_self_set: HashSet<_> =
+            self.registered_clients.iter().map(|c| &c.pub_key).collect();
+        let clients_other_set: HashSet<_> = other
+            .registered_clients
+            .iter()
+            .map(|c| &c.pub_key)
+            .collect();
+
+        clients_self_set == clients_other_set
+    }
 }
 
 impl Into<topology::MixProviderNode> for MixProviderPresence {
@@ -126,7 +159,7 @@ impl From<topology::MixProviderNode> for MixProviderPresence {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MixProviderClient {
     pub pub_key: String,
@@ -148,8 +181,102 @@ impl From<topology::MixProviderClient> for MixProviderClient {
     }
 }
 
+impl PresenceEq for Vec<CocoPresence> {
+    fn presence_eq(&self, other: &Self) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+
+        // we can't take the whole thing into set as it does not implement 'Eq' and we can't
+        // derive it as we don't want to take 'last_seen' into consideration
+        let self_set: HashSet<_> = self
+            .iter()
+            .map(|c| (&c.host, &c.pub_key, &c.version))
+            .collect();
+        let other_set: HashSet<_> = other
+            .iter()
+            .map(|c| (&c.host, &c.pub_key, &c.version))
+            .collect();
+
+        self_set == other_set
+    }
+}
+
+impl PresenceEq for Vec<MixNodePresence> {
+    fn presence_eq(&self, other: &Self) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+
+        // we can't take the whole thing into set as it does not implement 'Eq' and we can't
+        // derive it as we don't want to take 'last_seen' into consideration
+        let self_set: HashSet<_> = self
+            .iter()
+            .map(|m| (&m.host, &m.pub_key, &m.version, &m.layer))
+            .collect();
+        let other_set: HashSet<_> = other
+            .iter()
+            .map(|m| (&m.host, &m.pub_key, &m.version, &m.layer))
+            .collect();
+
+        self_set == other_set
+    }
+}
+
+impl PresenceEq for Vec<MixProviderPresence> {
+    fn presence_eq(&self, other: &Self) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+
+        // we can't take the whole thing into set as it does not implement 'Eq' and we can't
+        // derive it as we don't want to take 'last_seen' into consideration.
+        // We also don't care about order of registered_clients
+
+        // since we're going to be getting rid of this very soon anyway, just clone registered
+        // clients vector and sort it
+
+        let self_set: HashSet<_> = self
+            .iter()
+            .map(|p| {
+                (
+                    &p.client_listener,
+                    &p.mixnet_listener,
+                    &p.pub_key,
+                    &p.version,
+                    p.registered_clients
+                        .iter()
+                        .cloned()
+                        .map(|c| c.pub_key)
+                        .collect::<Vec<_>>()
+                        .sort(),
+                )
+            })
+            .collect();
+        let other_set: HashSet<_> = other
+            .iter()
+            .map(|p| {
+                (
+                    &p.client_listener,
+                    &p.mixnet_listener,
+                    &p.pub_key,
+                    &p.version,
+                    p.registered_clients
+                        .iter()
+                        .cloned()
+                        .map(|c| c.pub_key)
+                        .collect::<Vec<_>>()
+                        .sort(),
+                )
+            })
+            .collect();
+
+        self_set == other_set
+    }
+}
+
 // Topology shows us the current state of the overall Nym network
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Topology {
     pub coco_nodes: Vec<CocoPresence>,
@@ -157,9 +284,25 @@ pub struct Topology {
     pub mix_provider_nodes: Vec<MixProviderPresence>,
 }
 
+impl PartialEq for Topology {
+    // we need a custom implementation as the order of nodes does not matter
+    // also we do not care about 'last_seen' when comparing topologies
+    fn eq(&self, other: &Self) -> bool {
+        if !self.coco_nodes.presence_eq(&other.coco_nodes)
+            || !self.mix_nodes.presence_eq(&other.mix_nodes)
+            || !self
+                .mix_provider_nodes
+                .presence_eq(&other.mix_provider_nodes)
+        {
+            return false;
+        }
+
+        true
+    }
+}
+
 impl NymTopology for Topology {
     fn new(directory_server: String) -> Self {
-        println!("Using directory server: {:?}", directory_server);
         let directory_config = Config {
             base_url: directory_server,
         };

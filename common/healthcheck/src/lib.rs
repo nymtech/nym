c662dad57f70d6a1be942393d6d0677b12c69247
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-01-27 17:10:29 +0000

    Healthchecker getting keypair from outside rather than generating new one every run

--- common/healthcheck/src/lib.rs
+++ common/healthcheck/src/lib.rs
@@ -1,8 +1,10 @@
 use crate::result::HealthCheckResult;
+use crypto::identity::{MixnetIdentityKeyPair, MixnetIdentityPrivateKey, MixnetIdentityPublicKey};
 use directory_client::requests::presence_topology_get::PresenceTopologyGetRequester;
 use directory_client::DirectoryClient;
 use log::{debug, error, info, trace};
 use std::fmt::{Error, Formatter};
+use std::marker::PhantomData;
 use std::time::Duration;
 use topology::NymTopologyError;
 
@@ -34,15 +36,29 @@ impl From<topology::NymTopologyError> for HealthCheckerError {
     }
 }
 
-pub struct HealthChecker {
+pub struct HealthChecker<IDPair, Priv, Pub>
+where
+    IDPair: MixnetIdentityKeyPair<Priv, Pub>,
+    Priv: MixnetIdentityPrivateKey,
+    Pub: MixnetIdentityPublicKey,
+{
     directory_client: directory_client::Client,
     interval: Duration,
     num_test_packets: usize,
     resolution_timeout: Duration,
+    identity_keypair: IDPair,
+
+    _phantom_private: PhantomData<Priv>,
+    _phantom_public: PhantomData<Pub>,
 }
 
-impl HealthChecker {
-    pub fn new(config: config::HealthCheck) -> Self {
+impl<IDPair, Priv, Pub> HealthChecker<IDPair, Priv, Pub>
+where
+    IDPair: crypto::identity::MixnetIdentityKeyPair<Priv, Pub>,
+    Priv: crypto::identity::MixnetIdentityPrivateKey,
+    Pub: crypto::identity::MixnetIdentityPublicKey,
+{
+    pub fn new(config: config::HealthCheck, identity_keypair: IDPair) -> Self {
         debug!(
             "healthcheck will be using the following directory server: {:?}",
             config.directory_server
@@ -53,11 +69,16 @@ impl HealthChecker {
             interval: Duration::from_secs_f64(config.interval),
             resolution_timeout: Duration::from_secs_f64(config.resolution_timeout),
             num_test_packets: config.num_test_packets,
+            identity_keypair,
+
+            _phantom_private: PhantomData,
+            _phantom_public: PhantomData,
         }
     }
 
     pub async fn do_check(&self) -> Result<HealthCheckResult, HealthCheckerError> {
         trace!("going to perform a healthcheck!");
+
         let current_topology = match self.directory_client.presence_topology.get() {
             Ok(topology) => topology,
             Err(err) => {
@@ -71,6 +92,7 @@ impl HealthChecker {
             current_topology,
             self.num_test_packets,
             self.resolution_timeout,
+            &self.identity_keypair,
         )
         .await;
         healthcheck_result.sort_scores();

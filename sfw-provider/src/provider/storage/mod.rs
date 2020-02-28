3cb813d056147591f7ab823c11040a375a410f31
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-02-28 11:48:08 +0000

    Updated Storage to work with async io

--- sfw-provider/src/provider/storage/mod.rs
+++ sfw-provider/src/provider/storage/mod.rs
@@ -1,22 +1,30 @@
+use futures::lock::Mutex;
+use futures::StreamExt;
 use log::*;
 use rand::Rng;
 use sfw_provider_requests::DUMMY_MESSAGE_CONTENT;
 use sphinx::route::{DestinationAddressBytes, SURBIdentifier};
-use std::fs::File;
 use std::io;
-use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::fs;
+use tokio::fs::File;
+use tokio::prelude::*;
 
-pub enum StoreError {
-    ClientDoesntExistError,
-    FileIOFailure,
+const DUMMY_MESSAGE: ClientFile = ClientFile {
+    content: DUMMY_MESSAGE_CONTENT.to_vec(),
+    path: Default::default(),
+};
+
+#[derive(Clone)]
+pub struct ClientFile {
+    content: Vec<u8>,
+    path: PathBuf,
 }
 
-impl From<std::io::Error> for StoreError {
-    fn from(_: std::io::Error) -> Self {
-        use StoreError::*;
-
-        FileIOFailure
+impl ClientFile {
+    fn new(content: Vec<u8>, path: PathBuf) -> Self {
+        ClientFile { content, path }
     }
 }
 
@@ -25,7 +33,6 @@ pub struct StoreData {
     #[allow(dead_code)]
     client_surb_id: SURBIdentifier,
     message: Vec<u8>,
-    filename_length: u16,
 }
 
 impl StoreData {
@@ -33,21 +40,29 @@ impl StoreData {
         client_address: DestinationAddressBytes,
         client_surb_id: SURBIdentifier,
         message: Vec<u8>,
-        filename_length: u16,
     ) -> Self {
         StoreData {
             client_address,
             client_surb_id,
             message,
-            filename_length,
         }
     }
 }
 
-// TODO: replace with database
-pub struct ClientStorage(());
+// TODO: replace with proper database...
+pub struct ClientStorage {
+    inner: Arc<Mutex<ClientStorageInner>>,
+}
 
-// TODO: change it to some generic implementation to inject fs
+// even though the data inside is extremely cheap to copy, we have to have a single mutex,
+// so might as well store the data behind it
+pub struct ClientStorageInner {
+    message_retrieval_limit: usize,
+    filename_length: u16,
+    main_store_path_dir: PathBuf,
+}
+
+// TODO: change it to some generic implementation to inject fs (or even better - proper database)
 impl ClientStorage {
     pub(crate) fn generate_random_file_name(length: usize) -> String {
         rand::thread_rng()
@@ -56,16 +71,14 @@ impl ClientStorage {
             .collect::<String>()
     }
 
-    fn dummy_message() -> Vec<u8> {
-        // TODO: should it be padded to constant length?
-        DUMMY_MESSAGE_CONTENT.to_vec()
-    }
+    pub(crate) async fn store_processed_data(&self, store_data: StoreData) -> io::Result<()> {
+        let inner_data = self.inner.lock().await;
 
-    pub fn store_processed_data(store_data: StoreData, store_dir: &Path) -> io::Result<()> {
+        // TODO: to replace with store_data.to_b58_string() once PR on sphinx is merged
         let client_dir_name = bs58::encode(store_data.client_address).into_string();
-        let full_store_dir = store_dir.join(client_dir_name);
-        let full_store_path = full_store_dir.join(ClientStorage::generate_random_file_name(
-            store_data.filename_length as usize,
+        let full_store_dir = inner_data.main_store_path_dir.join(client_dir_name);
+        let full_store_path = full_store_dir.join(Self::generate_random_file_name(
+            inner_data.filename_length as usize,
         ));
         debug!(
             "going to store: {:?} in file: {:?}",
@@ -74,44 +87,61 @@ impl ClientStorage {
 
         // TODO: what to do with surbIDs??
 
-        // we can use normal io here, no need for tokio as it's all happening in one thread per connection
-        let mut file = File::create(full_store_path)?;
-        file.write_all(store_data.message.as_ref())?;
-
-        Ok(())
+        let mut file = File::create(full_store_path).await?;
+        file.write_all(store_data.message.as_ref()).await
     }
 
-    pub fn retrieve_client_files(
+    pub(crate) async fn retrieve_client_files(
+        &self,
         client_address: DestinationAddressBytes,
-        store_dir: &Path,
-        message_retrieval_limit: u16,
-    ) -> Result<Vec<Vec<u8>>, StoreError> {
+    ) -> io::Result<Vec<ClientFile>> {
+        let inner_data = self.inner.lock().await;
+
+        // TODO: to replace with store_data.to_b58_string() once PR on sphinx is merged
         let client_dir_name = bs58::encode(client_address).into_string();
-        let full_store_dir = store_dir.join(client_dir_name);
+        let full_store_dir = inner_data.main_store_path_dir.join(client_dir_name);
 
         trace!("going to lookup: {:?}!", full_store_dir);
         if !full_store_dir.exists() {
-            return Err(StoreError::ClientDoesntExistError);
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "Target client does not exist",
+            ));
         }
 
-        let msgs: Vec<_> = std::fs::read_dir(full_store_dir)?
-            .filter_map(|entry| entry.ok())
-            .filter(|entry| ClientStorage::is_valid_file(entry))
-            .map(|entry| {
-                // Not yet sure how to exactly get rid of those unwraps
-                let content = std::fs::read(entry.path()).unwrap();
-                ClientStorage::delete_file(entry.path()).unwrap();
-                content
-            }) // TODO: THIS MAP IS UNSAFE (BOTH FOR READING AND DELETING)!! - in the case where there are multiple requests from the same client being processed in parallel
-            .chain(std::iter::repeat(ClientStorage::dummy_message()))
-            .take(message_retrieval_limit as usize)
-            .collect();
+        let mut msgs = Vec::new();
+        let read_dir = fs::read_dir(full_store_dir).await?;
+
+        while let Some(dir_entry) = read_dir.next().await {
+            if let Ok(dir_entry) = dir_entry {
+                if !Self::is_valid_file(&dir_entry).await {
+                    continue;
+                }
+                // Do not delete the file itself here!
+                // Only do it after client has received it
+                let client_file =
+                    ClientFile::new(fs::read(dir_entry.path()).await?, dir_entry.path());
+                msgs.push(client_file)
+            }
+            if msgs.len() == inner_data.message_retrieval_limit {
+                break;
+            }
+        }
+
+        // make sure we always return as many messages as we need
+        if msgs.len() != inner_data.message_retrieval_limit as usize {
+            msgs = msgs
+                .into_iter()
+                .chain(std::iter::repeat(DUMMY_MESSAGE))
+                .take(inner_data.message_retrieval_limit)
+                .collect();
+        }
 
         Ok(msgs)
     }
 
-    fn is_valid_file(entry: &std::fs::DirEntry) -> bool {
-        let metadata = match entry.metadata() {
+    async fn is_valid_file(entry: &fs::DirEntry) -> bool {
+        let metadata = match entry.metadata().await {
             Ok(meta) => meta,
             Err(e) => {
                 error!(
@@ -134,10 +164,13 @@ impl ClientStorage {
         is_file
     }
 
-    // TODO: THIS NEEDS A LOCKING MECHANISM!!! (or a db layer on top - basically 'ClientStorage' on steroids)
-    // TODO 2: This should only be called AFTER we sent the reply. Because if client's connection failed after sending request
+    fn delete_files(&self, files: Vec<ClientFile>) -> io::Result<()> {
+        unimplemented!()
+    }
+
+    // TODO: This should only be called AFTER we sent the reply. Because if client's connection failed after sending request
     // the messages would be deleted but he wouldn't have received them
-    fn delete_file(path: PathBuf) -> io::Result<()> {
+    fn delete_file(&self, path: PathBuf) -> io::Result<()> {
         trace!("Here {:?} will be deleted!", path);
         std::fs::remove_file(path) // another argument for db layer -> remove_file is NOT guaranteed to immediately get rid of the file
     }

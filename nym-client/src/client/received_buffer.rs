7ea3c688208c126a9ae864759cec6b45cff1a133
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-03-04 14:37:27 +0000

    Refactored received buffer controller

--- nym-client/src/client/received_buffer.rs
+++ nym-client/src/client/received_buffer.rs
@@ -1,89 +1,124 @@
+use crate::client::provider_poller::PolledMessagesReceiver;
 use futures::channel::{mpsc, oneshot};
-use futures::lock::Mutex as FMutex;
+use futures::lock::Mutex;
 use futures::StreamExt;
-use log::{error, info, trace};
+use log::*;
 use std::sync::Arc;
+use tokio::runtime::Handle;
+use tokio::task::JoinHandle;
 
-pub type BufferResponse = oneshot::Sender<Vec<Vec<u8>>>;
+pub(crate) type ReceivedBufferResponse = oneshot::Sender<Vec<Vec<u8>>>;
+pub(crate) type ReceivedBufferRequestSender = mpsc::UnboundedSender<ReceivedBufferResponse>;
+pub(crate) type ReceivedBufferRequestReceiver = mpsc::UnboundedReceiver<ReceivedBufferResponse>;
 
-pub(crate) struct ReceivedMessagesBuffer {
-    inner: Arc<FMutex<Inner>>,
-}
-
-impl ReceivedMessagesBuffer {
-    pub(crate) fn new() -> Self {
-        ReceivedMessagesBuffer {
-            inner: Arc::new(FMutex::new(Inner::new())),
-        }
-    }
-
-    pub(crate) async fn start_controllers(
-        self,
-        poller_rx: mpsc::UnboundedReceiver<Vec<Vec<u8>>>, // to receive new messages
-        query_receiver: mpsc::UnboundedReceiver<BufferResponse>, // to receive requests to acquire all stored messages
-    ) {
-        let input_controller_future = tokio::spawn(Self::run_poller_input_controller(
-            self.inner.clone(),
-            poller_rx,
-        ));
-        let output_controller_future = tokio::spawn(Self::run_query_output_controller(
-            self.inner,
-            query_receiver,
-        ));
-
-        futures::future::select(input_controller_future, output_controller_future).await;
-        panic!("One of the received buffer controllers failed!")
-    }
-
-    pub(crate) async fn run_poller_input_controller(
-        buf: Arc<FMutex<Inner>>,
-        mut poller_rx: mpsc::UnboundedReceiver<Vec<Vec<u8>>>,
-    ) {
-        info!("Started Received Messages Buffer Input Controller");
-
-        while let Some(new_messages) = poller_rx.next().await {
-            Inner::add_new_messages(&*buf, new_messages).await;
-        }
-    }
-
-    pub(crate) async fn run_query_output_controller(
-        buf: Arc<FMutex<Inner>>,
-        mut query_receiver: mpsc::UnboundedReceiver<BufferResponse>,
-    ) {
-        info!("Started Received Messages Buffer Output Controller");
-
-        while let Some(request) = query_receiver.next().await {
-            let messages = Inner::acquire_and_empty(&*buf).await;
-            if let Err(failed_messages) = request.send(messages) {
-                error!(
-                    "Failed to send the messages to the requester. Adding them back to the buffer"
-                );
-                Inner::add_new_messages(&*buf, failed_messages).await;
-            }
-        }
-    }
-}
-
-pub(crate) struct Inner {
+struct ReceivedMessagesBufferInner {
     messages: Vec<Vec<u8>>,
 }
 
-impl Inner {
+#[derive(Debug, Clone)]
+// Note: you should NEVER create more than a single instance of this using 'new()'.
+// You should always use .clone() to create additional instances
+struct ReceivedMessagesBuffer {
+    inner: Arc<Mutex<ReceivedMessagesBufferInner>>,
+}
+
+impl ReceivedMessagesBuffer {
     fn new() -> Self {
-        Inner {
-            messages: Vec::new(),
+        ReceivedMessagesBuffer {
+            inner: Arc::new(Mutex::new(ReceivedMessagesBufferInner {
+                messages: Vec::new(),
+            })),
         }
     }
 
-    async fn add_new_messages(buf: &FMutex<Self>, msgs: Vec<Vec<u8>>) {
+    async fn add_new_messages(&mut self, msgs: Vec<Vec<u8>>) {
         trace!("Adding new messages to the buffer! {:?}", msgs);
-        let mut unlocked = buf.lock().await;
-        unlocked.messages.extend(msgs);
+        self.inner.lock().await.messages.extend(msgs)
     }
 
-    async fn acquire_and_empty(buf: &FMutex<Self>) -> Vec<Vec<u8>> {
+    async fn acquire_and_empty(&mut self) -> Vec<Vec<u8>> {
         trace!("Emptying the buffer and returning all messages");
-        let mut unlocked = buf.lock().await;
-        std::mem::replace(&mut unlocked.messages, Vec::new())
+        let mutex_guard = self.inner.lock().await;
+        std::mem::replace(&mut mutex_guard.messages, Vec::new())
+    }
+}
+
+struct RequestReceiver {
+    received_buffer: ReceivedMessagesBuffer,
+    query_receiver: ReceivedBufferRequestReceiver,
+}
+
+impl RequestReceiver {
+    fn new(
+        received_buffer: ReceivedMessagesBuffer,
+        query_receiver: ReceivedBufferRequestReceiver,
+    ) -> Self {
+        RequestReceiver {
+            received_buffer,
+            query_receiver,
+        }
+    }
+
+    fn start(mut self, handle: &Handle) -> JoinHandle<()> {
+        handle.spawn(async move {
+            while let Some(request) = self.query_receiver.next().await {
+                let messages = self.received_buffer.acquire_and_empty().await;
+                if let Err(failed_messages) = request.send(messages) {
+                    error!(
+                        "Failed to send the messages to the requester. Adding them back to the buffer"
+                    );
+                    self.received_buffer.add_new_messages(failed_messages).await;
+                }
+            }
+        })
+    }
+}
+
+struct MessageReceiver {
+    received_buffer: ReceivedMessagesBuffer,
+    poller_receiver: PolledMessagesReceiver,
+}
+
+impl MessageReceiver {
+    fn new(
+        received_buffer: ReceivedMessagesBuffer,
+        poller_receiver: PolledMessagesReceiver,
+    ) -> Self {
+        MessageReceiver {
+            received_buffer,
+            poller_receiver,
+        }
+    }
+    fn start(mut self, handle: &Handle) -> JoinHandle<()> {
+        handle.spawn(async move {
+            while let Some(new_messages) = self.poller_receiver.next().await {
+                self.received_buffer.add_new_messages(new_messages).await;
+            }
+        })
+    }
+}
+
+pub(crate) struct ReceivedMessagesBufferController {
+    messsage_receiver: MessageReceiver,
+    request_receiver: RequestReceiver,
+}
+
+impl ReceivedMessagesBufferController {
+    fn new(
+        query_receiver: ReceivedBufferRequestReceiver,
+        poller_receiver: PolledMessagesReceiver,
+    ) -> Self {
+        let received_buffer = ReceivedMessagesBuffer::new();
+
+        ReceivedMessagesBufferController {
+            messsage_receiver: MessageReceiver::new(received_buffer.clone(), poller_receiver),
+            request_receiver: RequestReceiver::new(received_buffer, query_receiver),
+        }
+    }
+
+    fn start(mut self, handle: &Handle) {
+        // TODO: should we do anything with JoinHandle(s) returned by start methods?
+        self.messsage_receiver.start(handle);
+        self.request_receiver.start(handle);
     }
 }

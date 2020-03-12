8279efb684c732918f3b5c2eb99a48755fb796d8
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2020-03-12 16:22:40 +0000

    Exposed methods

--- common/clients/multi-tcp-client/src/error_reader.rs
+++ common/clients/multi-tcp-client/src/error_reader.rs
@@ -6,26 +6,27 @@ use std::net::SocketAddr;
 use tokio::runtime::Handle;
 use tokio::task::JoinHandle;
 
-type ConnectionErrorResponse = (SocketAddr, io::Result<()>);
-type ConnectionErrorSender = mpsc::UnboundedSender<ConnectionErrorResponse>;
-type ConnectionErrorReceiver = mpsc::UnboundedReceiver<ConnectionErrorResponse>;
+pub(crate) type ConnectionErrorResponse = (SocketAddr, io::Result<()>);
+pub(crate) type ConnectionErrorSender = mpsc::UnboundedSender<ConnectionErrorResponse>;
+pub(crate) type ConnectionErrorReceiver = mpsc::UnboundedReceiver<ConnectionErrorResponse>;
 
-struct ConnectionErrorReader {
+pub(crate) struct ConnectionErrorReader {
     error_rx: ConnectionErrorReceiver,
 }
 
 impl ConnectionErrorReader {
-    fn new(error_rx: ConnectionErrorReceiver) -> Self {
+    pub(crate) fn new(error_rx: ConnectionErrorReceiver) -> Self {
         ConnectionErrorReader { error_rx }
     }
 
-    fn start(mut self, handle: &Handle) -> JoinHandle<()> {
+    pub(crate) fn start(mut self, handle: &Handle) -> JoinHandle<()> {
         handle.spawn(async move {
             while let Some(err_res) = self.error_rx.next().await {
                 let (source, err) = err_res;
                 match err {
-                    Ok(_) => trace!("packet to {} was sent successfully!", source.to_string()),
+                    // Ok(_) => trace!("packet to {} was sent successfully!", source.to_string()),
                     Err(e) => warn!("failed to send packet to {} - {:?}", source.to_string(), e),
+                    Ok(_) => (), // right now we're not expecting to receive any 'Ok' responses
                 }
             }
         })

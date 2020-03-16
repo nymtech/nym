2bd837a1e4357924f34ca0ef06ecef9596d02f26
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-16 18:05:21 +0000

    validator: fixing unused variable and spelling warnings

--- validator/src/network/rest/routes/topology.rs
+++ validator/src/network/rest/routes/topology.rs
@@ -1,12 +1,12 @@
 use super::*;
 use crate::network::rest::models::presence;
 
-pub fn get(req: &mut Request) -> IronResult<Response> {
+pub fn get(_req: &mut Request) -> IronResult<Response> {
     let topology = presence::Topology {
         mix_nodes: Vec::<presence::MixNode>::new(),
         service_providers: Vec::<presence::ServiceProvider>::new(),
         validators: Vec::<presence::Validator>::new(),
     };
-    let resp = serde_json::to_string_pretty(&topology).unwrap();
-    Ok(Response::with((status::Ok, resp)))
+    let response = serde_json::to_string_pretty(&topology).unwrap();
+    Ok(Response::with((status::Ok, response)))
 }

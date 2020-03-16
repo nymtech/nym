d8e4c8d24d994f3bb8caa4b36656774233d49ef1
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-16 13:22:15 +0000

    Extracting logging setup for all binaries

--- nym-client/src/main.rs
+++ nym-client/src/main.rs
@@ -8,23 +8,7 @@ pub mod sockets;
 
 fn main() {
     dotenv::dotenv().ok();
-
-    let mut log_builder = pretty_env_logger::formatted_timed_builder();
-    if let Ok(s) = ::std::env::var("RUST_LOG") {
-        log_builder.parse_filters(&s);
-    } else {
-        // default to 'Info'
-        log_builder.filter(None, log::LevelFilter::Info);
-    }
-
-    log_builder
-        .filter_module("hyper", log::LevelFilter::Warn)
-        .filter_module("tokio_reactor", log::LevelFilter::Warn)
-        .filter_module("reqwest", log::LevelFilter::Warn)
-        .filter_module("mio", log::LevelFilter::Warn)
-        .filter_module("want", log::LevelFilter::Warn)
-        .init();
-
+    setup_logging();
     println!("{}", banner());
 
     let arg_matches = App::new("Nym Client")
@@ -65,4 +49,22 @@ fn banner() -> String {
     "#,
         built_info::PKG_VERSION
     )
+}
+
+fn setup_logging() {
+    let mut log_builder = pretty_env_logger::formatted_timed_builder();
+    if let Ok(s) = ::std::env::var("RUST_LOG") {
+        log_builder.parse_filters(&s);
+    } else {
+        // default to 'Info'
+        log_builder.filter(None, log::LevelFilter::Info);
+    }
+
+    log_builder
+        .filter_module("hyper", log::LevelFilter::Warn)
+        .filter_module("tokio_reactor", log::LevelFilter::Warn)
+        .filter_module("reqwest", log::LevelFilter::Warn)
+        .filter_module("mio", log::LevelFilter::Warn)
+        .filter_module("want", log::LevelFilter::Warn)
+        .init();
 }

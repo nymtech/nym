e6ac9667577d9a72c19bf18e5913581775788e2f
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-17 09:42:52 +0000

    validator: using fully-qualified struct names to eliminate a bit of noise

--- validator/src/network/rest/routes/topology.rs
+++ validator/src/network/rest/routes/topology.rs
@@ -1,11 +1,11 @@
 use super::*;
-use crate::network::rest::models::presence;
+use crate::network::rest::models::presence::{MixNode, ServiceProvider, Topology, Validator};
 
 pub fn get(_req: &mut Request) -> IronResult<Response> {
-    let topology = presence::Topology {
-        mix_nodes: Vec::<presence::MixNode>::new(),
-        service_providers: Vec::<presence::ServiceProvider>::new(),
-        validators: Vec::<presence::Validator>::new(),
+    let topology = Topology {
+        mix_nodes: Vec::<MixNode>::new(),
+        service_providers: Vec::<ServiceProvider>::new(),
+        validators: Vec::<Validator>::new(),
     };
     let response = serde_json::to_string_pretty(&topology).unwrap();
     Ok(Response::with((status::Ok, response)))

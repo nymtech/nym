660943911cddafdd53161a4b47c84c9ba32bddc7
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-17 14:12:46 +0000

    validator: removed presence announcements from persistence

--- validator/migrations/2020-03-17-121545_create_presence_announcements/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE presence_announcements
-(
-    id INTEGER PRIMARY KEY NOT NULL,
-    host VARCHAR NOT NULL,
-    public_key VARCHAR NOT NULL,
-    node_type VARCHAR NOT NULL
-)
--- validator/src/schema.rs
+++ validator/src/schema.rs
@@ -1,8 +1,1 @@
-table! {
-    presence_announcements (id) {
-        id -> Integer,
-        host -> Text,
-        public_key -> Text,
-        node_type -> Text,
-    }
-}
+

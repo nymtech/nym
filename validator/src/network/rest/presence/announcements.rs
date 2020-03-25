1343af015392a17c98cade8a46b3cac69efd743d
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-25 14:27:20 +0000

    validator: further announcement HTTP progress

--- validator/src/network/rest/presence/announcements.rs
+++ validator/src/network/rest/presence/announcements.rs
@@ -1,25 +1,27 @@
 use super::*;
 use crate::services::mixmining;
+use bodyparser::Struct;
 use iron::status;
-
-use params::{Params, Value};
 
 pub fn post(req: &mut Request) -> IronResult<Response> {
     // the part where we check if the worst possible handler code can work.
     let db = mixmining::db::MixminingDb::new();
     let service = mixmining::Service::new(db);
-    let m = mixmining::Mixnode {
-        public_key: "foo".to_string(),
-        stake: 6,
-    };
-    service.add(m);
 
-    // the actual params handling part.
-    let map = req.get_ref::<Params>().unwrap();
-    match map.find(&["user", "name"]) {
-        Some(&Value::String(ref name)) if name == "Marie" => {
-            Ok(Response::with((status::Ok, "Welcome back, Marie!")))
-        }
-        _ => Ok(Response::with(status::NotFound)),
+    let maybe_mixnode = match req.get::<Struct<mixmining::Mixnode>>() {
+        Ok(Some(mixnode)) => Ok(mixnode),
+        Ok(None) => Err("JSON parsing error"),
+        Err(_) => Err("JSON parsing error"),
+    };
+
+    if maybe_mixnode.is_ok() {
+        let mixnode = maybe_mixnode.unwrap();
+        service.add(mixnode);
+        Ok(Response::with(status::Created))
+    } else {
+        Ok(Response::with((
+            status::BadRequest,
+            maybe_mixnode.unwrap_err(),
+        )))
     }
 }

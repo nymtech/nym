bcac889fc443c9cda8734d6cb867994c90b7974f
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-03-26 16:01:13 +0000

    validator: moved models into their own file.

--- validator/src/network/rest/presence/mod.rs
+++ validator/src/network/rest/presence/mod.rs
@@ -1,42 +1,6 @@
 use super::*;
-use serde::{Deserialize, Serialize};
 
 mod conversions;
 pub mod mixnode;
+mod models;
 pub mod topology;
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Mixnode {
-    host: String,
-    public_key: String,
-    version: String,
-    last_seen: u64,
-    location: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ServiceProvider {
-    host: String,
-    public_key: String,
-    version: String,
-    last_seen: u64,
-    location: String,
-}
-
-/// Topology shows us the current state of the overall Nym network
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Topology {
-    pub mix_nodes: Vec<Mixnode>,
-    pub service_providers: Vec<ServiceProvider>,
-    pub validators: Vec<Validator>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Validator {
-    host: String,
-    public_key: String,
-    version: String,
-    last_seen: u64,
-    location: String,
-}
---
+++ validator/src/network/rest/presence/models.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Mixnode {
+    pub host: String,
+    pub public_key: String,
+    pub version: String,
+    pub location: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ServiceProvider {
+    host: String,
+    public_key: String,
+    version: String,
+    last_seen: u64,
+    location: String,
+}
+
+/// Topology shows us the current state of the overall Nym network
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Topology {
+    pub mix_nodes: Vec<Mixnode>,
+    pub service_providers: Vec<ServiceProvider>,
+    pub validators: Vec<Validator>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Validator {
+    host: String,
+    public_key: String,
+    version: String,
+    last_seen: u64,
+    location: String,
+}

66bda28b3db364773c1f748b4cde19a9d8705a7a
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-04-03 14:14:16 +0100

    validator: initial topology retrieval working

--- validator/src/network/rest/presence/topology.rs
+++ validator/src/network/rest/presence/topology.rs
@@ -16,7 +16,7 @@ impl Handler for GetTopology {
     fn handle(&self, _req: &mut Request) -> IronResult<Response> {
         println!("Getting topology!...");
         let service_topology = self.service.lock().unwrap().topology();
-        let topology = service_topology; //models::Topology::from(service_topology);
+        let topology = models::Topology::from(service_topology);
         let response = serde_json::to_string_pretty(&topology).unwrap();
         Ok(Response::with((status::Ok, response)))
     }

e43b9500ac0b825e951fab536ff796daa460faa3
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-04-03 13:41:07 +0100

    validator: service mixnode and rest mixnode + topology conversions + tests

--- validator/src/network/rest/presence/conversions.rs
+++ validator/src/network/rest/presence/conversions.rs
@@ -1,10 +1,12 @@
-use crate::network::rest::presence::models::Mixnode as PresenceMixnode;
-use crate::services::mixmining::Mixnode as ServiceMixnode;
+use crate::network::rest::presence::models::Mixnode as RestMixnode;
+use crate::network::rest::presence::models::Topology as RestTopology;
+use crate::services::mixmining::models::Mixnode as ServiceMixnode;
+use crate::services::mixmining::models::Topology as ServiceTopology;
 use std::convert::From;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-impl From<PresenceMixnode> for ServiceMixnode {
-    fn from(value: PresenceMixnode) -> ServiceMixnode {
+impl From<RestMixnode> for ServiceMixnode {
+    fn from(value: RestMixnode) -> ServiceMixnode {
         let now = SystemTime::now();
         let timestamp = now.duration_since(UNIX_EPOCH).unwrap().as_millis() as u64;
         ServiceMixnode {
@@ -18,27 +20,79 @@ impl From<PresenceMixnode> for ServiceMixnode {
     }
 }
 
+impl From<RestTopology> for ServiceTopology {
+    fn from(value: RestTopology) -> ServiceTopology {
+        let mut converted_mixnodes: Vec<ServiceMixnode> = Vec::new();
+        for mixnode in value.mixnodes {
+            converted_mixnodes.push(mixnode.into());
+        }
+        ServiceTopology {
+            mixnodes: converted_mixnodes.to_vec(),
+            service_providers: vec![], // add these when they exist
+            validators: vec![],        // add these when they exist
+        }
+    }
+}
+
 #[cfg(test)]
 mod test_presence_conversions_for_mixmining_service {
-    use super::*;
 
-    #[test]
-    fn test_converting_presence_mixnode_to_mixmining_service_mixnode() {
-        let presence_mixnode = PresenceMixnode {
+    fn mixnode_fixture() -> RestMixnode {
+        RestMixnode {
             host: "foo.org".to_owned(),
             public_key: "abc".to_owned(),
             location: "London".to_owned(),
             version: "1.0.0".to_owned(),
-        };
+        }
+    }
 
-        let result: ServiceMixnode = presence_mixnode.clone().into();
-        assert_eq!(result.host, presence_mixnode.host);
-        assert_eq!(result.public_key, presence_mixnode.public_key);
-        assert_eq!(result.location, presence_mixnode.location);
+    use super::*;
+
+    #[test]
+    fn test_converting_rest_mixnode_to_mixmining_service_mixnode() {
+        let rest_mixnode = mixnode_fixture();
+        let result: ServiceMixnode = rest_mixnode.clone().into();
+        assert_eq!(result.host, rest_mixnode.host);
+        assert_eq!(result.public_key, rest_mixnode.public_key);
+        assert_eq!(result.location, rest_mixnode.location);
         assert_eq!(result.stake, 0);
-        assert_eq!(result.version, presence_mixnode.version);
+        assert_eq!(result.version, rest_mixnode.version);
         // I'm not going to test the last_seen timestamp as I can't be bothered
         // setting up a fake clock right now.
         // The behaviour is: it should set time to SystemTime::now().
+    }
+
+    #[test]
+    fn test_building_service_mixnode_from_rest_mixnode() {
+        let rest_mixnode = RestMixnode {
+            host: "foo.org".to_owned(),
+            // last_seen: 1234,
+            location: "London".to_owned(),
+            public_key: "abc".to_owned(),
+            // stake: 0,
+            version: "1.0.0".to_owned(),
+        };
+        let result = ServiceMixnode::from(rest_mixnode.clone());
+        assert_eq!(result.host, rest_mixnode.host);
+        assert_eq!(result.public_key, rest_mixnode.public_key);
+        assert_eq!(result.location, rest_mixnode.location);
+        assert_eq!(result.stake, 0);
+        assert_eq!(result.version, rest_mixnode.version);
+        // I'm not going to test the last_seen timestamp as I can't be bothered
+        // setting up a fake clock right now.
+        // The behaviour is: it should set time to SystemTime::now().
+    }
+
+    #[test]
+    fn test_converting_service_topology_into_rest_topology() {
+        let rest_topology = RestTopology {
+            mixnodes: vec![mixnode_fixture()],
+            service_providers: vec![],
+            validators: vec![],
+        };
+
+        let service_topology: ServiceTopology = rest_topology.into();
+        let service_mixnode: ServiceMixnode = mixnode_fixture().into();
+        assert_eq!(service_mixnode, service_topology.mixnodes[0]);
     }
 }

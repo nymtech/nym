d7d8be234bc944f3cbb981b7c120cf78f274627e
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-04-14 10:41:13 +0100

    New way of serialization provider requests/responses

--- sfw-provider/sfw-provider-requests/src/requests.rs
+++ sfw-provider/sfw-provider-requests/src/requests.rs
@@ -1,59 +1,204 @@
-use crate::AuthToken;
+use crate::auth_token::{AuthToken, AUTH_TOKEN_SIZE};
+use sphinx::constants::DESTINATION_ADDRESS_LENGTH;
 use sphinx::route::DestinationAddressBytes;
-
-const PULL_REQUEST_MESSAGE_PREFIX: [u8; 2] = [1, 0];
-const REGISTER_MESSAGE_PREFIX: [u8; 2] = [0, 1];
-
-// TODO: how to do it more nicely, considering all sfw-provider-requests implement same trait that is exercised here?
-#[derive(Debug)]
-pub enum ProviderRequests {
-    PullMessages(PullRequest),
-    Register(RegisterRequest),
-}
-
-impl ProviderRequests {
-    pub fn to_bytes(&self) -> Vec<u8> {
-        use ProviderRequests::*;
-        match self {
-            PullMessages(pr) => pr.to_bytes(),
-            Register(pr) => pr.to_bytes(),
-        }
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError> {
-        use ProviderRequests::*;
-        if bytes.len() < 2 {
-            return Err(ProviderRequestError::UnmarshalError);
-        }
-        let mut received_prefix = [0; 2];
-        received_prefix.copy_from_slice(&bytes[..2]);
-        match received_prefix {
-            PULL_REQUEST_MESSAGE_PREFIX => Ok(PullMessages(PullRequest::from_bytes(bytes)?)),
-            REGISTER_MESSAGE_PREFIX => Ok(Register(RegisterRequest::from_bytes(bytes)?)),
-            _ => Err(ProviderRequestError::UnmarshalErrorIncorrectPrefix),
-        }
-    }
-}
+use std::convert::TryFrom;
+use std::io;
+use std::io::Error;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 #[derive(Debug)]
 pub enum ProviderRequestError {
     MarshalError,
     UnmarshalError,
-    UnmarshalErrorIncorrectPrefix,
+    UnmarshalErrorInvalidKind,
+    UnmarshalErrorInvalidLength,
+    TooLongRequestError,
+    TooShortRequestError,
+    IOError(io::Error),
+    RemoteConnectionClosed,
 }
 
-pub trait ProviderRequest
-where
-    Self: Sized,
-{
-    fn get_prefix() -> [u8; 2];
-    fn to_bytes(&self) -> Vec<u8>;
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError>;
+impl From<io::Error> for ProviderRequestError {
+    fn from(e: Error) -> Self {
+        ProviderRequestError::IOError(e)
+    }
 }
 
-#[derive(Debug)]
+impl<'a, R: AsyncRead + Unpin> Drop for TokioAsyncRequestReader<'a, R> {
+    fn drop(&mut self) {
+        println!("request reader drop");
+    }
+}
+
+impl<'a, R: AsyncWrite + Unpin> Drop for TokioAsyncRequestWriter<'a, R> {
+    fn drop(&mut self) {
+        println!("request writer drop");
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum RequestKind {
+    Pull = 1,
+    Register = 2,
+}
+
+impl TryFrom<u8> for RequestKind {
+    type Error = ProviderRequestError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            _ if value == (RequestKind::Pull as u8) => Ok(Self::Pull),
+            _ if value == (RequestKind::Register as u8) => Ok(Self::Register),
+            _ => Err(Self::Error::UnmarshalErrorInvalidKind),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ProviderRequest {
+    Pull(PullRequest),
+    Register(RegisterRequest),
+}
+
+// Ideally I would have used futures::AsyncRead for even more generic approach, but unfortunately
+// tokio::io::AsyncRead differs from futures::AsyncRead
+pub struct TokioAsyncRequestReader<'a, R: AsyncRead + Unpin> {
+    max_allowed_len: usize,
+    reader: &'a mut R,
+}
+
+impl<'a, R: AsyncRead + Unpin> TokioAsyncRequestReader<'a, R> {
+    pub fn new(reader: &'a mut R, max_allowed_len: usize) -> Self {
+        TokioAsyncRequestReader {
+            reader,
+            max_allowed_len,
+        }
+    }
+
+    pub async fn try_read_request(&mut self) -> Result<ProviderRequest, ProviderRequestError> {
+        let req_len = self.reader.read_u32().await?;
+        if req_len == 0 {
+            return Err(ProviderRequestError::RemoteConnectionClosed);
+        }
+        if req_len as usize > self.max_allowed_len {
+            // TODO: should reader be drained?
+            return Err(ProviderRequestError::TooLongRequestError);
+        }
+
+        let mut req_buf = Vec::with_capacity(req_len as usize);
+        let mut chunk = self.reader.take(req_len as u64);
+
+        if let Err(_) = chunk.read_to_end(&mut req_buf).await {
+            return Err(ProviderRequestError::TooShortRequestError);
+        };
+
+        let parse_res = RequestDeserializer::new_with_len(req_len, &req_buf)?.try_to_parse();
+
+        parse_res
+    }
+}
+
+pub struct RequestDeserializer<'a> {
+    kind: RequestKind,
+    data: &'a [u8],
+}
+
+impl<'a> RequestDeserializer<'a> {
+    // perform initial parsing
+    pub fn new(raw_bytes: &'a [u8]) -> Result<Self, ProviderRequestError> {
+        if raw_bytes.len() < 1 + 4 {
+            Err(ProviderRequestError::UnmarshalErrorInvalidLength)
+        } else {
+            let data_len =
+                u32::from_be_bytes([raw_bytes[0], raw_bytes[1], raw_bytes[2], raw_bytes[3]]);
+            let kind = RequestKind::try_from(raw_bytes[4])?;
+            let data = &raw_bytes[4..];
+
+            if data.len() != data_len as usize {
+                Err(ProviderRequestError::UnmarshalErrorInvalidLength)
+            } else {
+                Ok(RequestDeserializer { kind, data })
+            }
+        }
+    }
+
+    pub fn new_with_len(len: u32, raw_bytes: &'a [u8]) -> Result<Self, ProviderRequestError> {
+        if raw_bytes.len() != len as usize {
+            Err(ProviderRequestError::UnmarshalErrorInvalidLength)
+        } else {
+            let kind = RequestKind::try_from(raw_bytes[0])?;
+            let data = &raw_bytes[1..];
+            Ok(RequestDeserializer { kind, data })
+        }
+    }
+
+    pub fn get_kind(&self) -> RequestKind {
+        self.kind
+    }
+
+    pub fn get_data(&self) -> &'a [u8] {
+        self.data
+    }
+
+    pub fn try_to_parse(self) -> Result<ProviderRequest, ProviderRequestError> {
+        match self.get_kind() {
+            RequestKind::Pull => Ok(ProviderRequest::Pull(PullRequest::try_from_bytes(
+                self.data,
+            )?)),
+            RequestKind::Register => Ok(ProviderRequest::Register(
+                RegisterRequest::try_from_bytes(self.data)?,
+            )),
+        }
+    }
+}
+
+// Ideally I would have used futures::AsyncWrite for even more generic approach, but unfortunately
+// tokio::io::AsyncWrite differs from futures::AsyncWrite
+pub struct TokioAsyncRequestWriter<'a, W: AsyncWrite + Unpin> {
+    writer: &'a mut W,
+}
+
+impl<'a, W: AsyncWrite + Unpin> TokioAsyncRequestWriter<'a, W> {
+    pub fn new(writer: &'a mut W) -> Self {
+        TokioAsyncRequestWriter { writer }
+    }
+
+    pub async fn try_write_request(&mut self, res: ProviderRequest) -> io::Result<()> {
+        let res_bytes = RequestSerializer::new(res).into_bytes();
+        self.writer.write_all(&res_bytes).await
+    }
+}
+
+pub struct RequestSerializer {
+    req: ProviderRequest,
+}
+
+impl RequestSerializer {
+    pub fn new(req: ProviderRequest) -> Self {
+        RequestSerializer { req }
+    }
+
+    /// Serialized requests in general have the following structure:
+    /// follows: 4 byte len (be u32) || 1-byte kind prefix || request-specific data
+    pub fn into_bytes(self) -> Vec<u8> {
+        let (kind, req_bytes) = match self.req {
+            ProviderRequest::Pull(req) => (req.get_kind(), req.to_bytes()),
+            ProviderRequest::Register(req) => (req.get_kind(), req.to_bytes()),
+        };
+        let req_len = req_bytes.len() as u32 + 1; // 1 is to accommodate for 'kind'
+        let req_len_bytes = req_len.to_be_bytes();
+        req_len_bytes
+            .iter()
+            .cloned()
+            .chain(std::iter::once(kind as u8))
+            .chain(req_bytes.into_iter())
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct PullRequest {
-    // TODO: public keys, signatures, tokens, etc. basically some kind of authentication bs
     pub auth_token: AuthToken,
     pub destination_address: sphinx::route::DestinationAddressBytes,
 }
@@ -68,38 +213,30 @@ impl PullRequest {
             destination_address,
         }
     }
-}
 
-impl ProviderRequest for PullRequest {
-    fn get_prefix() -> [u8; 2] {
-        PULL_REQUEST_MESSAGE_PREFIX
+    pub fn get_kind(&self) -> RequestKind {
+        RequestKind::Pull
     }
 
     fn to_bytes(&self) -> Vec<u8> {
-        Self::get_prefix()
-            .to_vec()
-            .into_iter()
-            .chain(self.destination_address.to_bytes().iter().cloned())
-            .chain(self.auth_token.0.iter().cloned())
+        self.destination_address
+            .to_bytes()
+            .iter()
+            .cloned()
+            .chain(self.auth_token.as_bytes().iter().cloned())
             .collect()
     }
 
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError> {
-        if bytes.len() != 2 + 32 + 32 {
-            return Err(ProviderRequestError::UnmarshalError);
+    fn try_from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError> {
+        if bytes.len() != DESTINATION_ADDRESS_LENGTH + AUTH_TOKEN_SIZE {
+            return Err(ProviderRequestError::UnmarshalErrorInvalidLength);
         }
 
-        let mut received_prefix = [0u8; 2];
-        received_prefix.copy_from_slice(&bytes[..2]);
-        if received_prefix != Self::get_prefix() {
-            return Err(ProviderRequestError::UnmarshalErrorIncorrectPrefix);
-        }
+        let mut destination_address = [0u8; DESTINATION_ADDRESS_LENGTH];
+        destination_address.copy_from_slice(&bytes[..DESTINATION_ADDRESS_LENGTH]);
 
-        let mut destination_address = [0u8; 32];
-        destination_address.copy_from_slice(&bytes[2..34]);
-
-        let mut auth_token = [0u8; 32];
-        auth_token.copy_from_slice(&bytes[34..]);
+        let mut auth_token = [0u8; AUTH_TOKEN_SIZE];
+        auth_token.copy_from_slice(&bytes[DESTINATION_ADDRESS_LENGTH..]);
 
         Ok(PullRequest {
             auth_token: AuthToken::from_bytes(auth_token),
@@ -108,7 +245,7 @@ impl ProviderRequest for PullRequest {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct RegisterRequest {
     pub destination_address: DestinationAddressBytes,
 }
@@ -119,34 +256,26 @@ impl RegisterRequest {
             destination_address,
         }
     }
-}
 
-impl ProviderRequest for RegisterRequest {
-    fn get_prefix() -> [u8; 2] {
-        REGISTER_MESSAGE_PREFIX
+    pub fn get_kind(&self) -> RequestKind {
+        RequestKind::Register
     }
 
     fn to_bytes(&self) -> Vec<u8> {
-        Self::get_prefix()
-            .to_vec()
-            .into_iter()
-            .chain(self.destination_address.to_bytes().iter().cloned())
+        self.destination_address
+            .to_bytes()
+            .iter()
+            .cloned()
             .collect()
     }
 
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError> {
-        if bytes.len() != 2 + 32 {
-            return Err(ProviderRequestError::UnmarshalError);
+    fn try_from_bytes(bytes: &[u8]) -> Result<Self, ProviderRequestError> {
+        if bytes.len() != DESTINATION_ADDRESS_LENGTH {
+            return Err(ProviderRequestError::UnmarshalErrorInvalidLength);
         }
 
-        let mut received_prefix = [0u8; 2];
-        received_prefix.copy_from_slice(&bytes[..2]);
-        if received_prefix != Self::get_prefix() {
-            return Err(ProviderRequestError::UnmarshalErrorIncorrectPrefix);
-        }
-
-        let mut destination_address = [0u8; 32];
-        destination_address.copy_from_slice(&bytes[2..]);
+        let mut destination_address = [0u8; DESTINATION_ADDRESS_LENGTH];
+        destination_address.copy_from_slice(&bytes[..DESTINATION_ADDRESS_LENGTH]);
 
         Ok(RegisterRequest {
             destination_address: DestinationAddressBytes::from_bytes(destination_address),
@@ -164,33 +293,12 @@ mod creating_pull_request {
             1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
             0, 1, 2,
         ]);
-        let auth_token = [1u8; 32];
-        let pull_request = PullRequest::new(address.clone(), AuthToken(auth_token));
+        let auth_token = [1u8; AUTH_TOKEN_SIZE];
+        let pull_request = PullRequest::new(address.clone(), AuthToken::from_bytes(auth_token));
         let bytes = pull_request.to_bytes();
 
-        let recovered = PullRequest::from_bytes(&bytes).unwrap();
-        assert_eq!(address, recovered.destination_address);
-        assert_eq!(AuthToken(auth_token), recovered.auth_token);
-    }
-
-    #[test]
-    fn it_is_possible_to_recover_it_from_bytes_with_enum_wrapper() {
-        let address = DestinationAddressBytes::from_bytes([
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-            0, 1, 2,
-        ]);
-        let auth_token = [1u8; 32];
-        let pull_request = PullRequest::new(address.clone(), AuthToken(auth_token));
-        let bytes = pull_request.to_bytes();
-
-        let recovered = ProviderRequests::from_bytes(&bytes).unwrap();
-        match recovered {
-            ProviderRequests::PullMessages(req) => {
-                assert_eq!(address, req.destination_address);
-                assert_eq!(AuthToken(auth_token), req.auth_token);
-            }
-            _ => panic!("expected to recover pull request!"),
-        }
+        let recovered = PullRequest::try_from_bytes(&bytes).unwrap();
+        assert_eq!(recovered, pull_request);
     }
 }
 
@@ -207,25 +315,7 @@ mod creating_register_request {
         let register_request = RegisterRequest::new(address.clone());
         let bytes = register_request.to_bytes();
 
-        let recovered = RegisterRequest::from_bytes(&bytes).unwrap();
-        assert_eq!(address, recovered.destination_address);
-    }
-
-    #[test]
-    fn it_is_possible_to_recover_it_from_bytes_with_enum_wrapper() {
-        let address = DestinationAddressBytes::from_bytes([
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-            0, 1, 2,
-        ]);
-        let register_request = RegisterRequest::new(address.clone());
-        let bytes = register_request.to_bytes();
-
-        let recovered = ProviderRequests::from_bytes(&bytes).unwrap();
-        match recovered {
-            ProviderRequests::Register(req) => {
-                assert_eq!(address, req.destination_address);
-            }
-            _ => panic!("expected to recover pull request!"),
-        }
+        let recovered = RegisterRequest::try_from_bytes(&bytes).unwrap();
+        assert_eq!(recovered, register_request);
     }
 }
--- sfw-provider/sfw-provider-requests/src/responses.rs
+++ sfw-provider/sfw-provider-requests/src/responses.rs
@@ -1,74 +1,234 @@
-use crate::AuthToken;
-use std::convert::TryInto;
+use crate::auth_token::{AuthToken, AUTH_TOKEN_SIZE};
+use std::convert::{TryFrom, TryInto};
+use std::io;
+use std::io::Error;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 #[derive(Debug)]
 pub enum ProviderResponseError {
     MarshalError,
     UnmarshalError,
+    UnmarshalErrorInvalidKind,
     UnmarshalErrorInvalidLength,
+    TooShortResponseError,
+    TooLongResponseError,
+    IOError(io::Error),
+    RemoteConnectionClosed,
 }
 
-pub trait ProviderResponse
-where
-    Self: Sized,
-{
-    fn to_bytes(&self) -> Vec<u8>;
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError>;
+impl From<io::Error> for ProviderResponseError {
+    fn from(e: Error) -> Self {
+        ProviderResponseError::IOError(e)
+    }
 }
 
-#[derive(Debug)]
+impl<'a, R: AsyncRead + Unpin> Drop for TokioAsyncResponseReader<'a, R> {
+    fn drop(&mut self) {
+        println!("response reader drop");
+    }
+}
+
+impl<'a, R: AsyncWrite + Unpin> Drop for TokioAsyncResponseWriter<'a, R> {
+    fn drop(&mut self) {
+        println!("response writer drop");
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum ResponseKind {
+    Failure = 0, // perhaps Error would have been a better name, but it'd clash with Self::TryFrom<u8>::Error
+    Pull = 1,
+    Register = 2,
+}
+
+impl TryFrom<u8> for ResponseKind {
+    type Error = ProviderResponseError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            _ if value == (ResponseKind::Failure as u8) => Ok(Self::Register),
+            _ if value == (ResponseKind::Pull as u8) => Ok(Self::Pull),
+            _ if value == (ResponseKind::Register as u8) => Ok(Self::Register),
+            _ => Err(Self::Error::UnmarshalErrorInvalidKind),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ProviderResponse {
+    Failure(FailureResponse),
+    Pull(PullResponse),
+    Register(RegisterResponse),
+}
+
+// Ideally I would have used futures::AsyncRead for even more generic approach, but unfortunately
+// tokio::io::AsyncRead differs from futures::AsyncRead
+pub struct TokioAsyncResponseReader<'a, R: AsyncRead + Unpin> {
+    max_allowed_len: usize,
+    reader: &'a mut R,
+}
+
+impl<'a, R: AsyncRead + Unpin> TokioAsyncResponseReader<'a, R> {
+    pub fn new(reader: &'a mut R, max_allowed_len: usize) -> Self {
+        TokioAsyncResponseReader {
+            reader,
+            max_allowed_len,
+        }
+    }
+
+    pub async fn try_read_response(&mut self) -> Result<ProviderResponse, ProviderResponseError> {
+        let res_len = self.reader.read_u32().await?;
+        if res_len == 0 {
+            return Err(ProviderResponseError::RemoteConnectionClosed);
+        }
+        if res_len as usize > self.max_allowed_len {
+            // TODO: should reader be drained?
+            return Err(ProviderResponseError::TooLongResponseError);
+        }
+
+        let mut res_buf = Vec::with_capacity(res_len as usize);
+        let mut chunk = self.reader.take(res_len as u64);
+
+        if let Err(_) = chunk.read_to_end(&mut res_buf).await {
+            return Err(ProviderResponseError::TooShortResponseError);
+        };
+
+        ResponseDeserializer::new_with_len(res_len, &res_buf)?.try_to_parse()
+    }
+}
+
+pub struct ResponseDeserializer<'a> {
+    kind: ResponseKind,
+    data: &'a [u8],
+}
+
+impl<'a> ResponseDeserializer<'a> {
+    // perform initial parsing
+    pub fn new(raw_bytes: &'a [u8]) -> Result<Self, ProviderResponseError> {
+        if raw_bytes.len() < 1 + 4 {
+            Err(ProviderResponseError::UnmarshalErrorInvalidLength)
+        } else {
+            let data_len =
+                u32::from_be_bytes([raw_bytes[0], raw_bytes[1], raw_bytes[2], raw_bytes[3]]);
+            let kind = ResponseKind::try_from(raw_bytes[4])?;
+            let data = &raw_bytes[4..];
+
+            if data.len() != data_len as usize {
+                Err(ProviderResponseError::UnmarshalErrorInvalidLength)
+            } else {
+                Ok(ResponseDeserializer { kind, data })
+            }
+        }
+    }
+
+    pub fn new_with_len(len: u32, raw_bytes: &'a [u8]) -> Result<Self, ProviderResponseError> {
+        if raw_bytes.len() != len as usize {
+            Err(ProviderResponseError::UnmarshalErrorInvalidLength)
+        } else {
+            let kind = ResponseKind::try_from(raw_bytes[0])?;
+            let data = &raw_bytes[1..];
+            Ok(ResponseDeserializer { kind, data })
+        }
+    }
+
+    pub fn get_kind(&self) -> ResponseKind {
+        self.kind
+    }
+
+    pub fn get_data(&self) -> &'a [u8] {
+        self.data
+    }
+
+    pub fn try_to_parse(self) -> Result<ProviderResponse, ProviderResponseError> {
+        match self.get_kind() {
+            ResponseKind::Failure => Ok(ProviderResponse::Failure(
+                FailureResponse::try_from_bytes(self.data)?,
+            )),
+            ResponseKind::Pull => Ok(ProviderResponse::Pull(PullResponse::try_from_bytes(
+                self.data,
+            )?)),
+            ResponseKind::Register => Ok(ProviderResponse::Register(
+                RegisterResponse::try_from_bytes(self.data)?,
+            )),
+        }
+    }
+}
+
+// Ideally I would have used futures::AsyncWrite for even more generic approach, but unfortunately
+// tokio::io::AsyncWrite differs from futures::AsyncWrite
+pub struct TokioAsyncResponseWriter<'a, W: AsyncWrite + Unpin> {
+    writer: &'a mut W,
+}
+
+impl<'a, W: AsyncWrite + Unpin> TokioAsyncResponseWriter<'a, W> {
+    pub fn new(writer: &'a mut W) -> Self {
+        TokioAsyncResponseWriter { writer }
+    }
+
+    pub async fn try_write_response(&mut self, res: ProviderResponse) -> io::Result<()> {
+        let res_bytes = ResponseSerializer::new(res).into_bytes();
+        self.writer.write_all(&res_bytes).await
+    }
+}
+
+pub struct ResponseSerializer {
+    res: ProviderResponse,
+}
+
+impl ResponseSerializer {
+    pub fn new(res: ProviderResponse) -> Self {
+        ResponseSerializer { res }
+    }
+
+    /// Serialized responses in general have the following structure:
+    /// follows: 4 byte len (be u32) || 1-byte kind prefix || response-specific data
+    pub fn into_bytes(self) -> Vec<u8> {
+        let (kind, res_bytes) = match self.res {
+            ProviderResponse::Failure(res) => (res.get_kind(), res.to_bytes()),
+            ProviderResponse::Pull(res) => (res.get_kind(), res.to_bytes()),
+            ProviderResponse::Register(res) => (res.get_kind(), res.to_bytes()),
+        };
+        let res_len = res_bytes.len() as u32 + 1; // 1 is to accommodate for 'kind'
+        let res_len_bytes = res_len.to_be_bytes();
+        res_len_bytes
+            .iter()
+            .cloned()
+            .chain(std::iter::once(kind as u8))
+            .chain(res_bytes.into_iter())
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct PullResponse {
-    pub messages: Vec<Vec<u8>>,
-}
-
-#[derive(Debug)]
-pub struct RegisterResponse {
-    pub auth_token: AuthToken,
-}
-
-pub struct ErrorResponse {
-    pub message: String,
+    messages: Vec<Vec<u8>>,
 }
 
 impl PullResponse {
     pub fn new(messages: Vec<Vec<u8>>) -> Self {
         PullResponse { messages }
     }
-}
 
-impl RegisterResponse {
-    pub fn new(auth_token: AuthToken) -> Self {
-        RegisterResponse { auth_token }
+    pub fn extract_messages(self) -> Vec<Vec<u8>> {
+        self.messages
     }
-}
 
-impl ErrorResponse {
-    pub fn new<S: Into<String>>(message: S) -> Self {
-        ErrorResponse {
-            message: message.into(),
-        }
+    pub fn get_kind(&self) -> ResponseKind {
+        ResponseKind::Pull
     }
-}
 
-// TODO: This should go into some kind of utils module/crate
-fn read_be_u16(input: &mut &[u8]) -> u16 {
-    let (int_bytes, rest) = input.split_at(std::mem::size_of::<u16>());
-    *input = rest;
-    u16::from_be_bytes(int_bytes.try_into().unwrap())
-}
-
-// TODO: currently this allows for maximum 64kB payload -
-// if we go over that in sphinx we need to update this code.
-impl ProviderResponse for PullResponse {
+    // TODO: currently this allows for maximum 64kB payload (max value of u16) -
+    // if we go over that in sphinx we need to update this code.
     // num_msgs || len1 || len2 || ... || msg1 || msg2 || ...
-    fn to_bytes(&self) -> Vec<u8> {
+    pub fn to_bytes(&self) -> Vec<u8> {
         let num_msgs = self.messages.len() as u16;
         let msgs_lens: Vec<u16> = self.messages.iter().map(|msg| msg.len() as u16).collect();
 
         num_msgs
             .to_be_bytes()
-            .to_vec()
-            .into_iter()
+            .iter()
+            .cloned()
             .chain(
                 msgs_lens
                     .into_iter()
@@ -78,7 +238,7 @@ impl ProviderResponse for PullResponse {
             .collect()
     }
 
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
+    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
         // can we read number of messages?
         if bytes.len() < 2 {
             return Err(ProviderResponseError::UnmarshalErrorInvalidLength);
@@ -119,36 +279,77 @@ impl ProviderResponse for PullResponse {
     }
 }
 
-impl ProviderResponse for RegisterResponse {
-    fn to_bytes(&self) -> Vec<u8> {
-        self.auth_token.0.to_vec()
+#[derive(Debug, Clone, PartialEq)]
+pub struct RegisterResponse {
+    auth_token: AuthToken,
+}
+
+impl RegisterResponse {
+    pub fn new(auth_token: AuthToken) -> Self {
+        RegisterResponse { auth_token }
     }
 
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
-        match bytes.len() {
-            32 => {
-                let mut auth_token = [0u8; 32];
-                auth_token.copy_from_slice(&bytes[..32]);
-                Ok(RegisterResponse {
-                    auth_token: AuthToken(auth_token),
-                })
-            }
-            _ => Err(ProviderResponseError::UnmarshalErrorInvalidLength),
+    pub fn get_token(&self) -> AuthToken {
+        self.auth_token
+    }
+
+    pub fn get_kind(&self) -> ResponseKind {
+        ResponseKind::Register
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        self.auth_token.to_bytes().to_vec()
+    }
+
+    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
+        if bytes.len() != AUTH_TOKEN_SIZE {
+            return Err(ProviderResponseError::UnmarshalErrorInvalidLength);
+        }
+
+        let mut auth_token = [0u8; AUTH_TOKEN_SIZE];
+        auth_token.copy_from_slice(&bytes[..AUTH_TOKEN_SIZE]);
+        Ok(RegisterResponse {
+            auth_token: AuthToken::from_bytes(auth_token),
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FailureResponse {
+    message: String,
+}
+
+impl FailureResponse {
+    pub fn new<S: Into<String>>(message: S) -> Self {
+        FailureResponse {
+            message: message.into(),
+        }
+    }
+
+    pub fn get_message(&self) -> &str {
+        &self.message
+    }
+
+    pub fn get_kind(&self) -> ResponseKind {
+        ResponseKind::Failure
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        self.message.clone().into_bytes()
+    }
+
+    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
+        match String::from_utf8(bytes.to_vec()) {
+            Err(_) => Err(ProviderResponseError::UnmarshalError),
+            Ok(message) => Ok(FailureResponse { message }),
         }
     }
 }
 
-impl ProviderResponse for ErrorResponse {
-    fn to_bytes(&self) -> Vec<u8> {
-        self.message.clone().into_bytes()
-    }
-
-    fn from_bytes(bytes: &[u8]) -> Result<Self, ProviderResponseError> {
-        match String::from_utf8(bytes.to_vec()) {
-            Err(_) => Err(ProviderResponseError::UnmarshalError),
-            Ok(message) => Ok(ErrorResponse { message }),
-        }
-    }
+fn read_be_u16(input: &mut &[u8]) -> u16 {
+    let (int_bytes, rest) = input.split_at(std::mem::size_of::<u16>());
+    *input = rest;
+    u16::from_be_bytes(int_bytes.try_into().unwrap())
 }
 
 #[cfg(test)]
@@ -172,7 +373,7 @@ mod creating_pull_response {
         let pull_response = PullResponse::new(msgs.clone());
         let bytes = pull_response.to_bytes();
 
-        let recovered = PullResponse::from_bytes(&bytes).unwrap();
+        let recovered = PullResponse::try_from_bytes(&bytes).unwrap();
         assert_eq!(msgs, recovered.messages);
     }
 }

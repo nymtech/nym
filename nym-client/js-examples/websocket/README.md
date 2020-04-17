2d78dff54297e45f83a3b08512c9d492ee3816a8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-04-17 18:08:55 +0100

    nym-client: more webpacking

---
+++ nym-client/js-examples/websocket/README.md
@@ -0,0 +1,10 @@
+### Prerequisites
+
+* Reasonably up to date Node + `npm`
+
+### Running it
+
+```
+npm install 
+npm run start # starts a webserver on port 8888
+```
---
+++ nym-client/js-examples/websocket/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8888
+    }
+};

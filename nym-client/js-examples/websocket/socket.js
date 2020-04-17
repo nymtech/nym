88c9b4f2540a1214e9a7802aae3a67dbdb7c46ea
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-04-17 17:03:27 +0100

    nym-client: refactoring js example code a bit.

--- nym-client/js-examples/websocket/socket.js
+++ nym-client/js-examples/websocket/socket.js
@@ -1,39 +1,74 @@
-display("Initialising...");
+async function main() {
+    display("Initialising...");
 
-var port = '9001' // client websocket listens on 9001 by default, change if yours is different
-var connection = new WebSocket('ws://127.0.0.1:' + port);
+    var port = '9001' // client websocket listens on 9001 by default, change if yours is different
+    var localClientUrl = "ws://127.0.0.1:" + port;
 
-// When the connection is open, send some data to the server
-connection.onopen = function () {
-    display('Opened connection to Nym client websocket.');
-};
+    var connection = await connectWebsocket(localClientUrl).then(function (c) {
+        return c;
+    }).catch(function (err) {
+        console.log("Websocket ERROR: " + err);
+    })
 
-// Log errors
-connection.onerror = function (error) {
-    display('WebSocket Error ' + error);
-};
+    connection.onmessage = function (e) {
+        handleMessage(e);
+    };
+    var ownDetails = {
+        type: "ownDetails"
+    }
 
-// Log messages from the server
-connection.onmessage = function (e) {
+    connection.send(JSON.stringify(ownDetails));
+}
+
+// Let's get this private party started!
+main();
+
+function handleMessage(e) {
     let response = JSON.parse(e.data);
     if (response.type == "error") {
-        display('Server responded with error: ' + response.message);
+        display("Server responded with error: " + response.message);
     } else if (response.type == "fetch") {
-        displayFetch(response.messages);
+        display(response.messages);
+    } else {
+        display("response: " + JSON.stringify(response));
     }
-};
+}
 
 function display(message) {
     document.getElementById("output").innerHTML = message;
 }
 
-function displayFetch(message) {
-    document.getElementById("fetch").innerHTML = message;
+function print(name, obj) {
+    console.log(name + ": " + JSON.stringify(obj));
 }
 
-display("Nym client demo started.");
+function connectWebsocket(url) {
+    return new Promise(function (resolve, reject) {
+        var server = new WebSocket(url);
+        server.onopen = function () {
+            resolve(server);
+        };
+        server.onerror = function (err) {
+            reject(err);
+        };
 
-var sequenceNum = 0;
+    });
+}
+
+
+    // setInterval(() => {
+    //     var message = {
+    //         type: "send",
+    //         message: "FOOMP " + sequenceNum,
+    //         recipient_address: "2ub7f2s5en4Pn2nhY69uyWqGSMLZwhtPASjePq4gLxQs"
+    //     }
+    //     connection.send(JSON.stringify(message));
+    //     display("Sent message: " + message.message + " to " + message.recipient_address);
+    //     sequenceNum += 1;
+    // }, 3000);
+
+
+// var sequenceNum = 0;
 
 // window.setInterval(function () {
 //     var message = {
@@ -46,11 +81,13 @@ var sequenceNum = 0;
 //     sequenceNum += 1;
 // }, 500);
 
-window.setInterval(function () {
-    var message = {
-        type: "fetch",
-    }
-    // displayFetch("fetching...");
-    connection.send(JSON.stringify(message));
-    sequenceNum += 1;
-}, 1000);
+// window.setInterval(function () {
+//     var message = {
+//         type: "fetch",
+//     }
+//     // displayFetch("fetching...");
+//     connection.send(JSON.stringify(message));
+//     sequenceNum += 1;
+// }, 1000);
+
+

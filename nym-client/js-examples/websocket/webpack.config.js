e74748756e6d153b09e96f69732f2eb36e0fbcd6
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-04-20 14:23:35 +0100

    nym-client: removed lots of webpack config

--- nym-client/js-examples/websocket/webpack.config.js
+++ nym-client/js-examples/websocket/webpack.config.js
@@ -9,46 +9,6 @@ module.exports = {
         filename: 'bundle.js',
         // path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'bundle.css',
-                        },
-                    },
-                    { loader: 'extract-loader' },
-                    { loader: 'css-loader' },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()]
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            // Prefer Dart Sass
-                            implementation: require('sass'),
-                            sassOptions: {
-                                includePaths: ['./node_modules']
-                            },
-                        }
-                    },
-                ]
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env'],
-                },
-            }
-        ]
-    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({

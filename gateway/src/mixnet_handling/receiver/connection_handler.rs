27db08b08135eb7151c656e35e0abc8ce501f7aa
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-04-24 15:35:16 +0100

    Missing license notices

--- gateway/src/mixnet_handling/receiver/connection_handler.rs
+++ gateway/src/mixnet_handling/receiver/connection_handler.rs
@@ -1,12 +1,19 @@
-use crate::client_handling::clients_handler::{
-    ClientsHandlerRequest, ClientsHandlerRequestSender, ClientsHandlerResponse,
-};
-use crate::client_handling::websocket::message_receiver::MixMessageSender;
+// Copyright 2020 Nym Technologies SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::mixnet_handling::receiver::packet_processing::PacketProcessor;
-use futures::channel::{mpsc, oneshot};
 use log::*;
-use nymsphinx::DestinationAddressBytes;
-use std::collections::HashMap;
 use std::net::SocketAddr;
 use tokio::{io::AsyncReadExt, prelude::*};
 

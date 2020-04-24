72b8b7acd302e166f32de53633caa4f82690d004
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-04-24 15:30:31 +0100

    Everything put into main

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -15,9 +15,12 @@
 use crate::client_handling::clients_handler::ClientsHandler;
 use crate::client_handling::websocket;
 use crate::mixnet_handling::receiver::packet_processing::PacketProcessor;
+use crate::mixnet_handling::sender::PacketForwarder;
 use crate::storage::ClientStorage;
 use log::*;
+use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
 
 mod client_handling;
 mod mixnet_client;
@@ -28,22 +31,43 @@ pub(crate) mod storage;
 async fn main() {
     dotenv::dotenv().ok();
     setup_logging();
-    let addr = "127.0.0.1:1793".parse().unwrap();
-    info!("Listening on: {}", addr);
+    // TODO: assume config is parsed here, keys are loaded, etc
+    // ALL OF BELOW WILL BE DONE VIA CONFIG
+    let keypair = crypto::encryption::KeyPair::new();
+    let clients_addr = "127.0.0.1:9000".parse().unwrap();
+    let mix_addr = "127.0.0.1:1789".parse().unwrap();
+    let inbox_store_dir: PathBuf = "foomp".into();
+    let ledger_path: PathBuf = "foomp2".into();
+    let message_retrieval_limit = 1000;
+    let filename_len = 16;
+    let initial_reconnection_backoff = Duration::from_millis(10_000);
+    let maximum_reconnection_backoff = Duration::from_millis(300_000);
+    let initial_connection_timeout = Duration::from_millis(1500);
+    // ALL OF ABOVE WILL HAVE BEEN DONE VIA CONFIG
 
-    let (dummy_clients_handler_tx, dummy_clients_handler_rx) = futures::channel::mpsc::unbounded();
-    let client_storage = ClientStorage::new(42, 42, "foomp".into());
-    let dummy_keypair = crypto::encryption::KeyPair::new();
-    let arced_sk = Arc::new(dummy_keypair.private_key().to_owned());
-    let dummy_mix_packet_processor = PacketProcessor::new(
-        Arc::clone(&arced_sk),
-        dummy_clients_handler_tx.clone(),
-        client_storage,
-    );
+    let arced_sk = Arc::new(keypair.private_key().to_owned());
 
-    ClientsHandler::new(dummy_clients_handler_rx, arced_sk).start();
-    websocket::Listener::new(addr, dummy_clients_handler_tx.clone()).start();
-    mixnet_handling::Listener::new(addr).start(dummy_mix_packet_processor);
+    // TODO: this should really be a proper DB, right now it will be most likely a bottleneck,
+    // due to possible frequent independent writes
+    let client_storage = ClientStorage::new(message_retrieval_limit, filename_len, inbox_store_dir);
+
+    let (_, forwarding_channel) = PacketForwarder::new(
+        initial_reconnection_backoff,
+        maximum_reconnection_backoff,
+        initial_connection_timeout,
+    )
+    .start();
+
+    let (_, clients_handler_sender) =
+        ClientsHandler::new(Arc::clone(&arced_sk), ledger_path, client_storage.clone()).start();
+
+    let packet_processor =
+        PacketProcessor::new(arced_sk, clients_handler_sender.clone(), client_storage);
+
+    websocket::Listener::new(clients_addr).start(clients_handler_sender, forwarding_channel);
+    mixnet_handling::Listener::new(mix_addr).start(packet_processor);
+
+    info!("All up and running!");
 
     if let Err(e) = tokio::signal::ctrl_c().await {
         error!(
@@ -53,7 +77,7 @@ async fn main() {
     }
 
     println!(
-        "Received SIGINT - the provider will terminate now (threads are not YET nicely stopped)"
+        "Received SIGINT - the gateway will terminate now (threads are not YET nicely stopped)"
     );
 }
 

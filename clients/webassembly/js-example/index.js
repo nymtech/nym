5ef2a22cc77cd631f30042e33b4a7f1bc75bd1ac
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-05-21 16:57:38 +0100

    Adding some documentation to the webassembly client (#244)
    * Adding some documentation to the webassembly client
    
    * Noting that binary is unsupported for now

--- clients/webassembly/js-example/index.js
+++ clients/webassembly/js-example/index.js
@@ -36,30 +36,50 @@ async function main() {
     await nymClient.start();
 }
 
-// Create a Sphinx packet and send it to the mixnet through the gateway node. 
+/**
+ * Create a Sphinx packet and send it to the mixnet through the gateway node.
+ * 
+ * Message and recipient are taken from the values in the user interface.
+ *
+ * @param {Client} nymClient the nym client to use for message sending
+ */
 function sendMessageTo(nymClient) {
     var message = document.getElementById("message").value;
     var recipient = document.getElementById("recipient").value;
     nymClient.sendMessage(message, recipient);
     displaySend(message);
 }
-
+/**
+ * Display messages that have been sent up the websocket. Colours them blue.
+ *
+ * @param {string} message
+ */
 function displaySend(message) {
     let timestamp = new Date().toISOString().substr(11, 12);
     let out = "<p style='color: blue; word-break: break-all;'>" + timestamp + " <b>sent</b> >>> " + message + "</p >";
     document.getElementById("output").innerHTML = out + document.getElementById("output").innerHTML;
 }
 
+/**
+ * Display received text messages in the browser. Colour them green.
+ *
+ * @param {string} message
+ */
 function displayReceived(message) {
     let timestamp = new Date().toISOString().substr(11, 12);
     let out = "<p style='color: green; word-break: break-all;'>" + timestamp + " <b>received</b> >>> " + message + "</p >";
     document.getElementById("output").innerHTML = out + document.getElementById("output").innerHTML;
 }
 
+
+/**
+ * Display the nymClient's sender address in the user interface
+ *
+ * @param {Client} nymClient
+ */
 function displaySenderAddress(nymClient) {
     document.getElementById("sender").value = nymClient.formatAsRecipient();
 }
-
 
 // Let's get started!
 main();

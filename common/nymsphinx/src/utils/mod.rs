5ce74d1eb696ca4c5badaa5b7c20e679d7478406
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-06-10 16:40:05 +0100

    Removed redundant things from nymsphinx::utils

--- common/nymsphinx/src/utils/mod.rs
+++ common/nymsphinx/src/utils/mod.rs
@@ -12,5 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod encapsulation;
-pub mod poisson;
+use rand::Rng;
+use rand_distr::{Distribution, Exp};
+use std::time;
+
+// TODO: ask @AP why we are actually using Distribution::Exp(1/L) rather than just
+// Distribution::Poisson(L) directly?
+
+// TODO: should we put an extra trait bound on this to require `CryptoRng`? Could there be any attacks
+// because of weak rng used?
+pub fn sample_poisson_duration<R: Rng + ?Sized>(
+    rng: &mut R,
+    average_duration: time::Duration,
+) -> time::Duration {
+    // this is our internal code used by our traffic streams
+    // the error is only thrown if average delay is less than 0, which will never happen
+    // so call to unwrap is perfectly safe here
+    let exp = Exp::new(1.0 / average_duration.as_nanos() as f64).unwrap();
+    time::Duration::from_nanos(exp.sample(rng).round() as u64)
+}

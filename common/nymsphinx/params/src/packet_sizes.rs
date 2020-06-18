f4a6469902d07fa2afe2b7d4bca7db50a12b225d
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-06-18 10:27:15 +0100

    Reducing ACK packet size to exactly what we need

--- common/nymsphinx/params/src/packet_sizes.rs
+++ common/nymsphinx/params/src/packet_sizes.rs
@@ -18,7 +18,7 @@ use std::convert::TryFrom;
 
 // it's up to the smart people to figure those values out : )
 const REGULAR_PACKET_SIZE: usize = HEADER_SIZE + PAYLOAD_OVERHEAD_SIZE + 2 * 1024;
-const ACK_PACKET_SIZE: usize = HEADER_SIZE + PAYLOAD_OVERHEAD_SIZE + 24;
+const ACK_PACKET_SIZE: usize = HEADER_SIZE + PAYLOAD_OVERHEAD_SIZE + 21; // 16B IV + 5B ID
 const EXTENDED_PACKET_SIZE: usize = HEADER_SIZE + PAYLOAD_OVERHEAD_SIZE + 32 * 1024;
 
 pub struct InvalidPacketSize;

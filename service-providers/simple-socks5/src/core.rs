349e52bc4059b9e713b58b8d04786e3da2d40eae
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-07-16 17:13:32 +0100

    Receiving messages via websocket

--- service-providers/simple-socks5/src/core.rs
+++ service-providers/simple-socks5/src/core.rs
@@ -1,5 +1,8 @@
 use crate::websocket;
+use futures_util::StreamExt;
+use tokio::net::TcpStream;
 use tokio::runtime::Runtime;
+use tokio_tungstenite::WebSocketStream;
 use websocket::WebsocketConnectionError;
 
 pub struct Server {
@@ -12,17 +15,25 @@ impl Server {
         Server { runtime }
     }
 
+    /// Start all subsystems
+    pub fn start(&mut self) {
+        let websocket_stream = self.connect_websocket("ws://localhost:1977");
+        let (mut write, read) = websocket_stream.split();
+        self.runtime.block_on({
+            read.for_each(|msg| async {
+                let data = msg.unwrap().into_data();
+                println!("Received a message: {:?}", data);
+            })
+        });
+        println!("\nAll systems go. Press CTRL-C to stop the server.");
+    }
+
     /// Keep running until the user hits CTRL-C.
     pub fn run_forever(&mut self) {
         if let Err(e) = self.runtime.block_on(tokio::signal::ctrl_c()) {
             println!("Stopping with error: {:?}", e);
         }
         println!("\nStopping...");
-    }
-
-    pub fn start(&mut self) {
-        self.connect_websocket("ws://localhost:1977");
-        println!("\nAll systems go. Press CTRL-C to stop the server.");
     }
 
     /// TODO: use of `new` is suspicious. Should this live up in start and then get wired in as a trait? might be nice to test that way...
@@ -34,8 +45,7 @@ impl Server {
     /// code that's available on the internet).
     fn connect_websocket(&mut self, uri: &str) -> WebSocketStream<TcpStream> {
         self.runtime.block_on(async {
-            let ws = websocket::Connection::new(uri);
-            let ws_stream = match ws.connect().await {
+            let ws_stream = match websocket::Connection::new(uri).connect().await {
                 Ok(ws_stream) => {
                     println!("* connected to local websocket server at {}", uri);
                     ws_stream

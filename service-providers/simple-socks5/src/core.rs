04a551b4adc4e2a11dde55984796d9d49d7c79b8
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-07-16 15:14:38 +0100

    Nicer websocket start method

--- service-providers/simple-socks5/src/core.rs
+++ service-providers/simple-socks5/src/core.rs
@@ -25,16 +25,26 @@ impl Server {
         println!("\nAll systems go. Press CTRL-C to stop the server.");
     }
 
-    /// any use of `new` is suspicious. Should this live up in main and then get wired in as a trait? might be nice to test that way...
-    fn connect_websocket(&mut self, uri: &str) {
+    /// TODO: use of `new` is suspicious. Should this live up in start and then get wired in as a trait? might be nice to test that way...
+    /// TODO: later on, once we have all this shit connected up, try and *use*
+    /// the websocket::Connection instead of the stream directly, to wire things
+    /// together. I have a feeling this might make testing substantially easier
+    /// so that we can have small testable units of logic rather than a bunch
+    /// of un-testable hosepipes wired together (as in most Rust example
+    /// code that's available on the internet).
+    fn connect_websocket(&mut self, uri: &str) -> WebSocketStream<TcpStream> {
         self.runtime.block_on(async {
             let ws = websocket::Connection::new(uri);
-            match ws.connect().await {
-                Ok(_) => println!("* connected to local websocket server at {}", uri),
+            let ws_stream = match ws.connect().await {
+                Ok(ws_stream) => {
+                    println!("* connected to local websocket server at {}", uri);
+                    ws_stream
+                }
                 Err(WebsocketConnectionError::ConnectionNotEstablished) => {
                     panic!("Error: websocket connection attempt failed, is the Nym client running?")
                 }
             };
+            return ws_stream;
         })
     }
 }

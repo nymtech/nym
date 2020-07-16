dbe9aef58420019316db72df4a9ed8d04130870f
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-07-16 11:18:12 +0100

    Added some simple and ungraceful websocket connection error handling

--- service-providers/simple-socks5/src/core.rs
+++ service-providers/simple-socks5/src/core.rs
@@ -1,5 +1,6 @@
 use crate::websocket;
 use tokio::runtime::Runtime;
+use websocket::WebsocketConnectionError;
 
 pub struct Server {
     runtime: Runtime,
@@ -21,14 +22,19 @@ impl Server {
 
     pub fn start(&mut self) {
         self.connect_websocket("ws://localhost:1977");
-        println!("All systems go. Press CTRL-C to stop the server.");
+        println!("\nAll systems go. Press CTRL-C to stop the server.");
     }
 
     /// any use of `new` is suspicious. Should this live up in main and then get wired in as a trait? might be nice to test that way...
     fn connect_websocket(&mut self, uri: &str) {
         self.runtime.block_on(async {
             let ws = websocket::Connection::new(uri);
-            ws.connect().await.unwrap();
+            match ws.connect().await {
+                Ok(_) => println!("* connected to local websocket server at {}", uri),
+                Err(WebsocketConnectionError::ConnectionNotEstablished) => {
+                    panic!("Error: websocket connection attempt failed, is the Nym client running?")
+                }
+            };
         })
     }
 }

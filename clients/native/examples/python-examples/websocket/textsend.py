a21cc12613ccc0fe8e1c21b746cda51e744f9c3f
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-07-22 17:38:08 +0100

    Updated go and python examples to include replies in text and binary cases

--- clients/native/examples/python-examples/websocket/textsend.py
+++ clients/native/examples/python-examples/websocket/textsend.py
@@ -7,7 +7,7 @@ self_address_request = json.dumps({
 })
 
 
-async def send_text():
+async def send_text_without_reply():
     message = "Hello Nym!"
 
     uri = "ws://localhost:1977"
@@ -19,14 +19,55 @@ async def send_text():
         text_send = json.dumps({
             "type": "send",
             "message": message,
-            "recipient": self_address["address"]
+            "recipient": self_address["address"],
+            "withReplySurb": False,
         })
 
-        print("sending '{}' over the mix network...".format(message))
+        print("sending '{}' (*without* reply SURB) over the mix network...".format(message))
         await websocket.send(text_send)
 
         print("waiting to receive a message from the mix network...")
         received_message = await websocket.recv()
         print("received '{}' from the mix network".format(received_message))
 
-asyncio.get_event_loop().run_until_complete(send_text())
+
+async def send_text_with_reply():
+    message = "Hello Nym!"
+
+    uri = "ws://localhost:1977"
+    async with websockets.connect(uri) as websocket:
+        text_send = json.dumps({
+            "type": "send",
+            "message": message,
+            "recipient": self_address["address"],
+            "withReplySurb": True,
+        })
+
+        print("sending '{}' (*with* reply SURB) over the mix network...".format(message))
+        await websocket.send(text_send)
+
+        print("waiting to receive a message from the mix network...")
+        received_message = json.loads(await websocket.recv())
+        print("received '{}' from the mix network".format(received_message))
+
+        # use the received surb to send an anonymous reply!
+        reply_surb = received_message["replySurb"]
+
+        reply_message = "hello from reply SURB!"
+        reply = json.dumps({
+            "type": "reply",
+            "message": reply_message,
+            "replySurb": reply_surb
+        })
+
+        print("sending '{}' (using reply SURB!) over the mix network...".format(reply_message))
+        await websocket.send(reply)
+
+        print("waiting to receive a message from the mix network...")
+        received_message = await websocket.recv()
+        print("received '{}' from the mix network".format(received_message))
+
+
+
+# asyncio.get_event_loop().run_until_complete(send_text_without_reply())
+asyncio.get_event_loop().run_until_complete(send_text_with_reply())

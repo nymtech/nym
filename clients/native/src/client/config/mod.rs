bfbe81cd27662672245d12b71166c8502ba88b0d
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-08-27 15:21:29 +0100

    Made base client_core config

---
+++ clients/native/src/client/config/mod.rs
@@ -0,0 +1,157 @@
+// Copyright 2020 Nym Technologies SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::client::config::template::config_template;
+use client_core::config::Config as BaseConfig;
+use config::NymConfig;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+mod template;
+
+const DEFAULT_LISTENING_PORT: u16 = 1977;
+
+#[derive(Debug, Deserialize, PartialEq, Serialize, Clone, Copy)]
+#[serde(deny_unknown_fields)]
+pub enum SocketType {
+    WebSocket,
+    None,
+}
+
+impl SocketType {
+    pub fn from_string<S: Into<String>>(val: S) -> Self {
+        let mut upper = val.into();
+        upper.make_ascii_uppercase();
+        match upper.as_ref() {
+            "WEBSOCKET" | "WS" => SocketType::WebSocket,
+            _ => SocketType::None,
+        }
+    }
+}
+
+#[derive(Debug, Default, Deserialize, PartialEq, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Config {
+    #[serde(flatten)]
+    base: BaseConfig<Config>,
+
+    socket: Socket,
+}
+
+impl NymConfig for Config {
+    fn template() -> &'static str {
+        config_template()
+    }
+
+    fn config_file_name() -> String {
+        "config.toml".to_string()
+    }
+
+    fn default_root_directory() -> PathBuf {
+        dirs::home_dir()
+            .expect("Failed to evaluate $HOME value")
+            .join(".nym")
+            .join("clients")
+    }
+
+    fn root_directory(&self) -> PathBuf {
+        self.base.get_nym_root_directory()
+    }
+
+    fn config_directory(&self) -> PathBuf {
+        self.root_directory()
+            .join(self.base.get_id())
+            .join("config")
+    }
+
+    fn data_directory(&self) -> PathBuf {
+        self.root_directory().join(self.base.get_id()).join("data")
+    }
+}
+
+impl Config {
+    pub fn new<S: Into<String>>(id: S) -> Self {
+        Config {
+            base: BaseConfig::new(id),
+            socket: Default::default(),
+        }
+    }
+
+    pub fn with_socket(mut self, socket_type: SocketType) -> Self {
+        self.socket.socket_type = socket_type;
+        self
+    }
+
+    pub fn with_port(mut self, port: u16) -> Self {
+        self.socket.listening_port = port;
+        self
+    }
+
+    // getters
+    pub fn get_config_file_save_location(&self) -> PathBuf {
+        self.config_directory().join(Self::config_file_name())
+    }
+
+    pub fn get_base(&self) -> &BaseConfig<Self> {
+        &self.base
+    }
+    
+    pub fn get_base_mut(&mut self) -> &mut BaseConfig<Self> {
+        &mut self.base
+    }
+
+    pub fn get_socket_type(&self) -> SocketType {
+        self.socket.socket_type
+    }
+
+    pub fn get_listening_port(&self) -> u16 {
+        self.socket.listening_port
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Socket {
+    socket_type: SocketType,
+    listening_port: u16,
+}
+
+impl Default for Socket {
+    fn default() -> Self {
+        Socket {
+            socket_type: SocketType::WebSocket,
+            listening_port: DEFAULT_LISTENING_PORT,
+        }
+    }
+}
+
+#[cfg(test)]
+mod client_config {
+    use super::*;
+
+    #[test]
+    fn after_saving_default_config_the_loaded_one_is_identical() {
+        // need to figure out how to do something similar but without touching the disk
+        // or the file system at all...
+        let temp_location = tempfile::tempdir().unwrap().path().join("config.toml");
+        let default_config = Config::new("foomp".to_string());
+        default_config
+            .save_to_file(Some(temp_location.clone()))
+            .unwrap();
+
+        let loaded_config = Config::load_from_file(Some(temp_location), None).unwrap();
+
+        assert_eq!(default_config, loaded_config);
+    }
+}

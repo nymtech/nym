0b7800ee8b09a20d4ca954e98b457a64dde722ce
Dave <futurechimp@users.noreply.github.com>
2020-09-16 17:45:37 +0100

    Fixing startup message

--- network-monitor/src/main.rs
+++ network-monitor/src/main.rs
@@ -3,7 +3,7 @@ mod network;
 #[tokio::main]
 async fn main() {
     let uri = "ws://localhost:1977";
-    println!("Starting socks5 service provider:");
+    println!("Starting network monitor:");
     let network_monitor = network::Monitor::new(uri.into());
     network_monitor.run().await;
 }

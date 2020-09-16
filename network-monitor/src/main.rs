3c4d0fdc9de4094c54df44b915603b39be12073c
Dave <futurechimp@users.noreply.github.com>
2020-09-16 17:58:20 +0100

    Injecting good mixndodes

--- network-monitor/src/main.rs
+++ network-monitor/src/main.rs
@@ -4,7 +4,14 @@ mod network;
 async fn main() {
     let websocket_uri = "ws://localhost:1977";
     let directory_uri = "https://directory.nymtech.net";
+
+    // I haven't checked these, they're packed at random (I need to go grab our real ones from the server)
+    let good_mixnodes = vec![
+        "CQVy5fkf4M7EdmoLvH5MJEygqiPbfavUM3NH9eGDK1kt",
+        "GjpuFBVzk8KiNsydAaiZG3rZKsoDtv7djCRY1QatKkS5",
+        "EV2MTs7DBi95USRNM3hM8QBRiCoYNnXBzs67YHivv3Fh",
+    ];
     println!("Starting network monitor:");
-    let network_monitor = network::Monitor::new(directory_uri, websocket_uri);
+    let network_monitor = network::Monitor::new(directory_uri, good_mixnodes, websocket_uri);
     network_monitor.run().await;
 }
--- network-monitor/src/network/mod.rs
+++ network-monitor/src/network/mod.rs
@@ -7,13 +7,17 @@ mod websocket;
 
 pub struct Monitor {
     directory_uri: String,
+    good_mixnodes: Vec<String>,
     websocket_uri: String,
 }
 
 impl Monitor {
-    pub fn new(directory_uri: &str, websocket_uri: &str) -> Monitor {
+    pub fn new(directory_uri: &str, mixnodes: Vec<&str>, websocket_uri: &str) -> Monitor {
+        let good_mixnodes = mixnodes.iter().map(|s| s.to_string()).collect();
+
         Monitor {
             directory_uri: directory_uri.to_string(),
+            good_mixnodes,
             websocket_uri: websocket_uri.to_string(),
         }
     }
@@ -27,6 +31,8 @@ impl Monitor {
         let directory: Client = DirectoryClient::new(config);
         let topology = directory.get_topology().await;
         info!("Topology is: {:?}", topology);
+
+        info!("Good mixnodes is: {:?}", self.good_mixnodes);
     }
 }
 
@@ -36,7 +42,11 @@ mod constructing {
 
     #[test]
     fn works() {
-        let network_monitor = Monitor::new("https://directory.nymtech.net", "ws://localhost:1977");
+        let network_monitor = Monitor::new(
+            "https://directory.nymtech.net",
+            vec!["abcdefg"],
+            "ws://localhost:1977",
+        );
         assert_eq!(
             "https://directory.nymtech.net",
             network_monitor.directory_uri

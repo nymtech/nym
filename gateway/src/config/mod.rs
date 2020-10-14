91059c09ea61a21c5a55edec8b93119119521667
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-10-14 15:27:14 +0100

    Gateway using Duration for time intervals + human readable config fields for them

--- gateway/src/config/mod.rs
+++ gateway/src/config/mod.rs
@@ -19,7 +19,7 @@ use serde::{Deserialize, Serialize};
 use std::net::{IpAddr, SocketAddr};
 use std::path::PathBuf;
 use std::str::FromStr;
-use std::time;
+use std::time::Duration;
 
 pub mod persistence;
 mod template;
@@ -33,11 +33,11 @@ const DEFAULT_DIRECTORY_SERVER: &str = "https://directory.nymtech.net";
 
 // 'DEBUG'
 // where applicable, the below are defined in milliseconds
-const DEFAULT_PRESENCE_SENDING_DELAY: u64 = 10_000; // 10s
-const DEFAULT_PACKET_FORWARDING_INITIAL_BACKOFF: u64 = 10_000; // 10s
-const DEFAULT_PACKET_FORWARDING_MAXIMUM_BACKOFF: u64 = 300_000; // 5min
-const DEFAULT_INITIAL_CONNECTION_TIMEOUT: u64 = 1_500; // 1.5s
-const DEFAULT_CACHE_ENTRY_TTL: u64 = 30_000;
+const DEFAULT_PRESENCE_SENDING_DELAY: Duration = Duration::from_millis(10_000);
+const DEFAULT_PACKET_FORWARDING_INITIAL_BACKOFF: Duration = Duration::from_millis(10_000);
+const DEFAULT_PACKET_FORWARDING_MAXIMUM_BACKOFF: Duration = Duration::from_millis(300_000);
+const DEFAULT_INITIAL_CONNECTION_TIMEOUT: Duration = Duration::from_millis(1_500);
+const DEFAULT_CACHE_ENTRY_TTL: Duration = Duration::from_millis(30_000);
 
 const DEFAULT_STORED_MESSAGE_FILENAME_LENGTH: u16 = 16;
 const DEFAULT_MESSAGE_RETRIEVAL_LIMIT: u16 = 5;
@@ -362,8 +362,8 @@ impl Config {
         self.gateway.presence_directory_server.clone()
     }
 
-    pub fn get_presence_sending_delay(&self) -> time::Duration {
-        time::Duration::from_millis(self.debug.presence_sending_delay)
+    pub fn get_presence_sending_delay(&self) -> Duration {
+        self.debug.presence_sending_delay
     }
 
     pub fn get_mix_listening_address(&self) -> SocketAddr {
@@ -390,16 +390,16 @@ impl Config {
         self.clients_endpoint.ledger_path.clone()
     }
 
-    pub fn get_packet_forwarding_initial_backoff(&self) -> time::Duration {
-        time::Duration::from_millis(self.debug.packet_forwarding_initial_backoff)
+    pub fn get_packet_forwarding_initial_backoff(&self) -> Duration {
+        self.debug.packet_forwarding_initial_backoff
     }
 
-    pub fn get_packet_forwarding_maximum_backoff(&self) -> time::Duration {
-        time::Duration::from_millis(self.debug.packet_forwarding_maximum_backoff)
+    pub fn get_packet_forwarding_maximum_backoff(&self) -> Duration {
+        self.debug.packet_forwarding_maximum_backoff
     }
 
-    pub fn get_initial_connection_timeout(&self) -> time::Duration {
-        time::Duration::from_millis(self.debug.initial_connection_timeout)
+    pub fn get_initial_connection_timeout(&self) -> Duration {
+        self.debug.initial_connection_timeout
     }
 
     pub fn get_message_retrieval_limit(&self) -> u16 {
@@ -410,8 +410,8 @@ impl Config {
         self.debug.stored_messages_filename_length
     }
 
-    pub fn get_cache_entry_ttl(&self) -> time::Duration {
-        time::Duration::from_millis(self.debug.cache_entry_ttl)
+    pub fn get_cache_entry_ttl(&self) -> Duration {
+        self.debug.cache_entry_ttl
     }
 
     pub fn get_version(&self) -> &str {
@@ -581,20 +581,21 @@ impl Default for Logging {
 pub struct Debug {
     /// Initial value of an exponential backoff to reconnect to dropped TCP connection when
     /// forwarding sphinx packets.
-    /// The provided value is interpreted as milliseconds.
-    packet_forwarding_initial_backoff: u64,
+    #[serde(with = "humantime_serde")]
+    packet_forwarding_initial_backoff: Duration,
 
     /// Maximum value of an exponential backoff to reconnect to dropped TCP connection when
     /// forwarding sphinx packets.
-    /// The provided value is interpreted as milliseconds.
-    packet_forwarding_maximum_backoff: u64,
+    #[serde(with = "humantime_serde")]
+    packet_forwarding_maximum_backoff: Duration,
 
     /// Timeout for establishing initial connection when trying to forward a sphinx packet.
-    /// The provider value is interpreted as milliseconds.
-    initial_connection_timeout: u64,
+    #[serde(with = "humantime_serde")]
+    initial_connection_timeout: Duration,
 
     /// Delay between each subsequent presence data being sent.
-    presence_sending_delay: u64,
+    #[serde(with = "humantime_serde")]
+    presence_sending_delay: Duration,
 
     /// Length of filenames for new client messages.
     stored_messages_filename_length: u16,
@@ -605,8 +606,8 @@ pub struct Debug {
     message_retrieval_limit: u16,
 
     /// Duration for which a cached vpn processing result is going to get stored for.
-    /// The provided value is interpreted as milliseconds.
-    cache_entry_ttl: u64,
+    #[serde(with = "humantime_serde")]
+    cache_entry_ttl: Duration,
 }
 
 impl Default for Debug {

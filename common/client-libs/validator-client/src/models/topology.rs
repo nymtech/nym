7b14f7391216c4a22c65c6f08a2bcaecf5534cd4
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-10-27 10:28:08 +0000

    Change validator topology to convert into NymTopology without failure

--- common/client-libs/validator-client/src/models/topology.rs
+++ common/client-libs/validator-client/src/models/topology.rs
@@ -12,56 +12,61 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::models::gateway::{self, RegisteredGateway};
-use crate::models::mixnode::{self, RegisteredMix};
+use crate::models::gateway::RegisteredGateway;
+use crate::models::mixnode::RegisteredMix;
+use log::*;
 use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 use topology::{MixLayer, NymTopology};
-
-#[derive(Debug)]
-pub enum TopologyConversionError {
-    GatewayError(gateway::ConversionError),
-    MixError(mixnode::ConversionError),
-}
-
-impl From<gateway::ConversionError> for TopologyConversionError {
-    fn from(err: gateway::ConversionError) -> Self {
-        TopologyConversionError::GatewayError(err)
-    }
-}
-
-impl From<mixnode::ConversionError> for TopologyConversionError {
-    fn from(err: mixnode::ConversionError) -> Self {
-        TopologyConversionError::MixError(err)
-    }
-}
 
 // Topology shows us the current state of the overall Nym network
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Topology {
-    mix_nodes: Vec<RegisteredMix>,
-    gateways: Vec<RegisteredGateway>,
+    pub mix_nodes: Vec<RegisteredMix>,
+    pub gateways: Vec<RegisteredGateway>,
 }
 
-impl TryInto<NymTopology> for Topology {
-    type Error = TopologyConversionError;
-
-    fn try_into(self) -> Result<NymTopology, Self::Error> {
+// changed from `TryInto`. reason being is that we should not fail entire topology
+// conversion if there's one invalid node on the network screwing around
+impl Into<NymTopology> for Topology {
+    fn into(self) -> NymTopology {
         use std::collections::HashMap;
 
         let mut mixes = HashMap::new();
         for mix in self.mix_nodes.into_iter() {
             let layer = mix.mix_info.layer as MixLayer;
+            if layer == 0 || layer > 3 {
+                warn!(
+                    "{} says it's on invalid layer {}!",
+                    mix.mix_info.node_info.identity_key, layer
+                );
+                continue;
+            }
+            let mix_id = mix.mix_info.node_info.identity_key.clone();
+
             let layer_entry = mixes.entry(layer).or_insert(Vec::new());
-            layer_entry.push(mix.try_into()?)
+            match mix.try_into() {
+                Ok(mix) => layer_entry.push(mix),
+                Err(err) => {
+                    warn!("Mix {} is malformed - {:?}", mix_id, err);
+                    continue;
+                }
+            }
         }
 
         let mut gateways = Vec::with_capacity(self.gateways.len());
         for gate in self.gateways.into_iter() {
-            gateways.push(gate.try_into()?)
+            let gate_id = gate.gateway_info.node_info.identity_key.clone();
+            match gate.try_into() {
+                Ok(gate) => gateways.push(gate),
+                Err(err) => {
+                    warn!("Gateway {} is malformed - {:?}", gate_id, err);
+                    continue;
+                }
+            }
         }
 
-        Ok(NymTopology::new(mixes, gateways))
+        NymTopology::new(mixes, gateways)
     }
 }

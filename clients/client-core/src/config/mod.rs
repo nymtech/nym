293f386152aa2c45bb5a1aa3c4b9c07e2747822c
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-11-10 15:43:16 +0000

    Updated default sending rates

--- clients/client-core/src/config/mod.rs
+++ clients/client-core/src/config/mod.rs
@@ -32,9 +32,9 @@ pub const DEFAULT_VALIDATOR_REST_ENDPOINT: &str = "https://validator.nymtech.net
 const DEFAULT_ACK_WAIT_MULTIPLIER: f64 = 1.5;
 
 const DEFAULT_ACK_WAIT_ADDITION: Duration = Duration::from_millis(1_500);
-const DEFAULT_LOOP_COVER_STREAM_AVERAGE_DELAY: Duration = Duration::from_millis(1000);
-const DEFAULT_MESSAGE_STREAM_AVERAGE_DELAY: Duration = Duration::from_millis(100);
-const DEFAULT_AVERAGE_PACKET_DELAY: Duration = Duration::from_millis(100);
+const DEFAULT_LOOP_COVER_STREAM_AVERAGE_DELAY: Duration = Duration::from_millis(200);
+const DEFAULT_MESSAGE_STREAM_AVERAGE_DELAY: Duration = Duration::from_millis(20);
+const DEFAULT_AVERAGE_PACKET_DELAY: Duration = Duration::from_millis(50);
 const DEFAULT_TOPOLOGY_REFRESH_RATE: Duration = Duration::from_millis(30_000);
 const DEFAULT_TOPOLOGY_RESOLUTION_TIMEOUT: Duration = Duration::from_millis(5_000);
 const DEFAULT_GATEWAY_RESPONSE_TIMEOUT: Duration = Duration::from_millis(1_500);

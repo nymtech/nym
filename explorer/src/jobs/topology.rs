eb1097cc7e49beaa89122bf5378dc33f069f38dc
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-11-12 16:13:42 +0000

    Explorer public folder being relative to the binary

--- explorer/src/jobs/topology.rs
+++ explorer/src/jobs/topology.rs
@@ -7,6 +7,15 @@ pub async fn renew_periodically(validator_base_url: &str) -> Result<(), Error> {
     let url = format!("{}/{}", validator_base_url, RELATIVE_PATH);
 
     let topology_json = reqwest::get(&url).await?.text().await?;
-    file::save(topology_json, "public/downloads/topology.json");
+
+    let save_path = std::env::current_exe()
+        .expect("Failed to evaluate current exe path")
+        .parent()
+        .expect("the binary itself has no parent path?!")
+        .join("public")
+        .join("downloads")
+        .join("topology.json");
+
+    file::save(topology_json, save_path);
     Ok(())
 }

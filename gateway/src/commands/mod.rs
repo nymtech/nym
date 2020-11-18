728985ca3e3deb445020a11fe5721b62b92900f8
jstuczyn <jedrzej.stuczynski@gmail.com>
2020-11-18 10:03:09 +0000

    Unregister command for gateway

--- gateway/src/commands/mod.rs
+++ gateway/src/commands/mod.rs
@@ -18,6 +18,7 @@ use clap::ArgMatches;
 pub(crate) mod init;
 pub(crate) mod run;
 pub(crate) mod upgrade;
+pub(crate) mod unregister;
 
 pub(crate) fn override_config(mut config: Config, matches: &ArgMatches) -> Config {
     let mut was_mix_host_overridden = false;

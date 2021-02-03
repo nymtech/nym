dd285fa1c2e84dbbeb81bba4f6cc3c79e7f5034b
Dave <futurechimp@users.noreply.github.com>
2021-02-03 14:58:27 +0000

    Renaming contract package in usage

--- contracts/mixnet/examples/schema.rs
+++ contracts/mixnet/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use mixnet::msg::{Topology, HandleMsg, InitMsg, QueryMsg};
-use mixnet::state::State;
+use mixnet_contracts::msg::{HandleMsg, InitMsg, QueryMsg, Topology};
+use mixnet_contracts::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();

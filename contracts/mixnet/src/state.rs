fb9e00b1989d081df3e5d46f2cdfc06a4a4da859
Dave <futurechimp@users.noreply.github.com>
2021-02-11 11:44:03 +0000

    Moved models from types into state

--- contracts/mixnet/src/state.rs
+++ contracts/mixnet/src/state.rs
@@ -4,12 +4,14 @@ use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+// Contract-level stuff
+
 pub static CONFIG_KEY: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
     pub owner: HumanAddr,
-    pub mix_node_bonds: Vec<MixNodeBond>,
+    pub mix_node_bonds: Vec<MixNodeBond>, // TODO: whack this, we need to use a range instead
 }
 
 pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
@@ -19,3 +21,23 @@ pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
 pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
     singleton_read(storage, CONFIG_KEY)
 }
+
+// Mixnode-related stuff
+
+pub const PREFIX_MIXNODES: &[u8] = b"mixnodes";
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, JsonSchema)]
+pub struct MixNodeBond {
+    pub(crate) amount: Vec<Coin>,
+    pub(crate) owner: HumanAddr,
+    pub(crate) mix_node: MixNode,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize, JsonSchema)]
+pub struct MixNode {
+    pub(crate) host: String,
+    pub(crate) layer: u64,
+    pub(crate) location: String,
+    pub(crate) sphinx_key: String,
+    pub(crate) version: String,
+}
+

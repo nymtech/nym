24747a520c7c526284afbdf3c0ee22a0b3c8aac7
Dave <futurechimp@users.noreply.github.com>
2021-02-19 18:55:01 +0000

    Moving tests back into their own special home so they don't bloat our package

--- clients/validator/tests/caches/mixnodes.test.ts
+++ clients/validator/tests/caches/mixnodes.test.ts
@@ -1,9 +1,9 @@
 import { Test } from 'mocha';
 import { assert } from 'chai';
-import INetClient from '../net-client';
-import { Fixtures } from '../../tests/fixtures'
+import INetClient from '../../src/net-client';
+import { Fixtures } from '../fixtures'
 import { Mock } from 'moq.ts';
-import { MixnodesCache } from './mixnodes'
+import { MixnodesCache } from '../../src/caches/mixnodes'
 
 describe("Retrieving mixnodes: when the validator returns", () => {
     context("an empty list", () => {
@@ -33,6 +33,9 @@ describe("Retrieving mixnodes: when the validator returns", () => {
 
     context("a list of nodes that is longer than one page", () => {
         it("Should return the list", () => {
+            // What should we mock here? Maybe change perPage to like 2, and return some different sized lists? 
+            // We need to add in some kind of total pages variable in the response we get from the validator
+            // then test that we make multiple requests if that's not 1
             const perPage = 100;
             const mockClient = new Mock<INetClient>().setup(instance => instance.getMixnodes(1, perPage)).returns(Fixtures.nodeList2());
             const cache = new MixnodesCache(mockClient.object(), perPage);

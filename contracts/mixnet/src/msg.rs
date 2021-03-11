46a1d783e6e40f5c36db5cf1d879c50ddbdd3e33
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-03-11 10:22:25 +0000

    New message types

--- contracts/mixnet/src/msg.rs
+++ contracts/mixnet/src/msg.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::HumanAddr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::state::MixNode;
+use crate::state::{Gateway, MixNode};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {}
@@ -12,6 +12,8 @@ pub struct InitMsg {}
 pub enum HandleMsg {
     RegisterMixnode { mix_node: MixNode },
     UnRegisterMixnode {},
+    BondGateway { gateway: Gateway },
+    UnbondGateway {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -20,5 +22,9 @@ pub enum QueryMsg {
     GetMixNodes {
         limit: Option<u32>,
         start_after: Option<HumanAddr>,
+    },
+    GetGateways {
+        start_after: Option<HumanAddr>,
+        limit: Option<u32>,
     },
 }

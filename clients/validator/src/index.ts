a883709a7f1ac6efd47f628cd570eb0d8b106a52
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-03-25 16:45:22 +0000

    Validator client changes

--- clients/validator/src/index.ts
+++ clients/validator/src/index.ts
@@ -1,6 +1,5 @@
 import NetClient, { INetClient } from "./net-client";
 import { Gateway, GatewayBond, MixNode, MixNodeBond } from "./types";
-// import * as fs from "fs";
 import { Bip39, Random } from "@cosmjs/crypto";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import MixnodesCache from "./caches/mixnodes";
@@ -9,46 +8,45 @@ import { BroadcastTxResponse } from "@cosmjs/stargate"
 import { ExecuteResult, InstantiateOptions, InstantiateResult, UploadMeta, UploadResult } from "@cosmjs/cosmwasm";
 import { CoinMap, displayAmountToNative, MappedCoin, nativeCoinToDisplay, printableBalance, printableCoin } from "./currency";
 import GatewaysCache from "./caches/gateways";
+import QueryClient, {IQueryClient} from "./query-client";
 
 export { coins };
 export { Coin };
 export { displayAmountToNative, nativeCoinToDisplay, printableCoin, printableBalance, MappedCoin, CoinMap }
 
 export default class ValidatorClient {
-
     private readonly stakeDenom: string;
     private readonly gatewayBondingStake: number = 1000_000000
-
     url: string;
-    private netClient: INetClient;
+    private readonly client: INetClient | IQueryClient
     private mixNodesCache: MixnodesCache;
     private gatewayCache: GatewaysCache
-    private wallet: DirectSecp256k1HdWallet
-    readonly address: string;
     private readonly contractAddress: string;
 
-    private constructor(url: string, netClient: INetClient, wallet: DirectSecp256k1HdWallet, address: string, contractAddress: string, stakeDenom: string) {
+    private constructor(url: string, client: INetClient | IQueryClient, contractAddress: string, stakeDenom: string) {
         this.url = url;
-        this.netClient = netClient;
-        this.mixNodesCache = new MixnodesCache(netClient, 100);
-        this.gatewayCache = new GatewaysCache(netClient, 100);
-        this.address = address;
-        this.wallet = wallet;
+        this.client = client;
+        this.mixNodesCache = new MixnodesCache(client, 100);
+        this.gatewayCache = new GatewaysCache(client, 100);
         this.contractAddress = contractAddress;
         this.stakeDenom = stakeDenom;
     }
 
     static async connect(contractAddress: string, mnemonic: string, url: string, stakeDenom: string): Promise<ValidatorClient> {
         const wallet = await ValidatorClient.buildWallet(mnemonic);
-        const [{ address }] = await wallet.getAccounts();
         const netClient = await NetClient.connect(wallet, url, stakeDenom);
-        return new ValidatorClient(url, netClient, wallet, address, contractAddress, stakeDenom);
+        return new ValidatorClient(url, netClient, contractAddress, stakeDenom);
+    }
+
+    static async connectForQuery(contractAddress: string, url: string, stakeDenom: string): Promise<ValidatorClient> {
+        const queryClient = await QueryClient.connect(url)
+        return new ValidatorClient(url, queryClient, contractAddress, stakeDenom)
     }
 
     /**
      * TODO: re-enable this once we move back to client-side wallets running on people's machines
-     * instead of the web wallet. 
-     * 
+     * instead of the web wallet.
+     *
      * Loads a named mnemonic from the system's keystore.
      *
      * @param keyPath the name of the key in the keystore
@@ -87,7 +85,7 @@ export default class ValidatorClient {
     }
 
     getBalance(address: string): Promise<Coin | null> {
-        return this.netClient.getBalance(address);
+        return this.client.getBalance(address, this.stakeDenom);
     }
 
     /**
@@ -115,35 +113,52 @@ export default class ValidatorClient {
     *  Announce a mixnode, paying a fee.
     */
     async bond(mixNode: MixNode): Promise<ExecuteResult> {
-        const bond = [{ amount: "1000000000", denom: this.stakeDenom }];
-        const result = await this.netClient.executeContract(this.address, this.contractAddress, { register_mixnode: { mix_node: mixNode } }, "adding mixnode", bond);
-        console.log(`account ${this.address} added mixnode with ${mixNode.host}`);
-        return result;
+        if (this.client instanceof NetClient) {
+            const bond = [{ amount: "1000000000", denom: this.stakeDenom }];
+            const result = await this.client.executeContract(this.client.clientAddress, this.contractAddress, {register_mixnode: {mix_node: mixNode}}, "adding mixnode", bond);
+            console.log(`account ${this.client.clientAddress} added mixnode with ${mixNode.host}`);
+            return result;
+        } else {
+            throw new Error("Tried to bond with a query client")
+        }
+
     }
 
     /**
      * Unbond a mixnode, removing it from the network and reclaiming staked coins
      */
     async unbond(): Promise<ExecuteResult> {
-        const result = await this.netClient.executeContract(this.address, this.contractAddress, { un_register_mixnode: {} })
-        console.log(`account ${this.address} unbonded mixnode`);
-        return result;
+        if (this.client instanceof NetClient) {
+            const result = await this.client.executeContract(this.client.clientAddress, this.contractAddress, {un_register_mixnode: {}})
+            console.log(`account ${this.client.clientAddress} unbonded mixnode`);
+            return result;
+        } else {
+            throw new Error("Tried to unbond with a query client")
+        }
     }
 
     /**
      * Checks whether there is already a bonded mixnode associated with this client's address
      */
     async ownsMixNode(): Promise<boolean> {
-        const result = await this.netClient.ownsMixNode(this.contractAddress, this.address)
-        return result.has_node
+        if (this.client instanceof NetClient) {
+            const result = await this.client.ownsMixNode(this.contractAddress, this.client.clientAddress)
+            return result.has_node
+        } else {
+            throw new Error("tried to check mixnode ownership for an address-less client")
+        }
     }
 
     /**
      * Checks whether there is already a bonded gateway associated with this client's address
      */
     async ownsGateway(): Promise<boolean> {
-        const result = await this.netClient.ownsGateway(this.contractAddress, this.address)
-        return result.has_gateway
+        if (this.client instanceof NetClient) {
+            const result = await this.client.ownsGateway(this.contractAddress, this.client.clientAddress)
+            return result.has_gateway
+        } else {
+            throw new Error("tried to check gateway ownership for an address-less client")
+        }
     }
 
     /**
@@ -179,19 +194,27 @@ export default class ValidatorClient {
      *  Announce a gateway, paying a fee.
      */
     async bondGateway(gateway: Gateway): Promise<ExecuteResult> {
-        const bond = this.minimumGatewayBond()
-        const result = await this.netClient.executeContract(this.address, this.contractAddress, { bond_gateway: { gateway: gateway } }, "adding gateway", [bond]);
-        console.log(`account ${this.address} added gateway with ${gateway.mix_host}`);
-        return result;
+        if (this.client instanceof NetClient) {
+            const bond = this.minimumGatewayBond()
+            const result = await this.client.executeContract(this.client.clientAddress, this.contractAddress, {bond_gateway: {gateway: gateway}}, "adding gateway", [bond]);
+            console.log(`account ${this.client.clientAddress} added gateway with ${gateway.mix_host}`);
+            return result;
+        } else {
+            throw new Error("Tried to bond gateway with a query client")
+        }
     }
 
     /**
      * Unbond a gateway, removing it from the network and reclaiming staked coins
      */
     async unbondGateway(): Promise<ExecuteResult> {
-        const result = await this.netClient.executeContract(this.address, this.contractAddress, { unbond_gateway: {} })
-        console.log(`account ${this.address} unbonded gateway`);
-        return result;
+        if (this.client instanceof NetClient) {
+            const result = await this.client.executeContract(this.client.clientAddress, this.contractAddress, {unbond_gateway: {}})
+            console.log(`account ${this.client.clientAddress} unbonded gateway`);
+            return result;
+        } else {
+            throw new Error("Tried to unbond gateway with a query client")
+        }
     }
 
 
@@ -202,15 +225,68 @@ export default class ValidatorClient {
      * Send funds from one address to another.
      */
     async send(senderAddress: string, recipientAddress: string, coins: readonly Coin[], memo?: string): Promise<BroadcastTxResponse> {
-        return this.netClient.sendTokens(senderAddress, recipientAddress, coins, memo);
+        if (this.client instanceof NetClient) {
+            return this.client.sendTokens(senderAddress, recipientAddress, coins, memo);
+        } else {
+            throw new Error("Tried to use send with a query client");
+        }
     }
 
     async upload(senderAddress: string, wasmCode: Uint8Array, meta?: UploadMeta, memo?: string): Promise<UploadResult> {
-        return this.netClient.upload(senderAddress, wasmCode, meta, memo);
+        if (this.client instanceof NetClient) {
+            return this.client.upload(senderAddress, wasmCode, meta, memo);
+        } else {
+            throw new Error("Tried to upload with a query client");
+        }
     }
 
     public instantiate(senderAddress: string, codeId: number, initMsg: Record<string, unknown>, label: string, options?: InstantiateOptions): Promise<InstantiateResult> {
-        return this.netClient.instantiate(senderAddress, codeId, initMsg, label, options);
+        if (this.client instanceof NetClient) {
+            return this.client.instantiate(senderAddress, codeId, initMsg, label, options);
+        } else {
+            throw new Error("Tried to instantiate with a query client");
+        }
     }
 }
 
+
+/// One page of a possible multi-page set of mixnodes. The paging interface is quite
+/// inconvenient, as we don't have the two pieces of information we need to know
+/// in order to do paging nicely (namely `currentPage` and `totalPages` parameters).
+///
+/// Instead, we have only `start_next_page_after`, i.e. the key of the last record
+/// on this page. In order to get the *next* page, CosmWasm looks at that value,
+/// finds the next record, and builds the next page starting there. This happens
+/// **in series** rather than **in parallel** (!).
+///
+/// So we have some consistency problems:
+///
+/// * we can't make requests at a given block height, so the result set
+///    which we assemble over time may change while requests are being made.
+/// * at some point we will make a request for a `start_next_page_after` key
+///   which has just been deleted from the database.
+///
+/// TODO: more robust error handling on the "deleted key" case.
+export type PagedResponse = {
+    nodes: MixNodeBond[],
+    per_page: number, // TODO: camelCase
+    start_next_after: string, // TODO: camelCase
+}
+
+// a temporary way of achieving the same paging behaviour for the gateways
+// the same points made for `PagedResponse` stand here.
+export type PagedGatewayResponse = {
+    nodes: GatewayBond[],
+    per_page: number, // TODO: camelCase
+    start_next_after: string, // TODO: camelCase
+}
+
+export type MixOwnershipResponse = {
+    address: string,
+    has_node: boolean,
+}
+
+export type GatewayOwnershipResponse = {
+    address: string,
+    has_gateway: boolean,
+}

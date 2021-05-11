fff87c0adc2a1e65f9071c9b0949da5b243fafa4
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-05-11 15:43:47 +0100

    1.54 nightly clippy fixes

--- mixnode/src/config/mod.rs
+++ mixnode/src/config/mod.rs
@@ -195,8 +195,7 @@ impl Config {
 
         // first lets see if we received host:port or just host part of an address
         let host = host.into();
-        let split_host: Vec<_> = host.split(':').collect();
-        match split_host.len() {
+        match host.split(':').count() {
             1 => {
                 // we provided only 'host' part so we are going to reuse existing port
                 self.mixnode.announce_address =

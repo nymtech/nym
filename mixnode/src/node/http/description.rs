e357a7334f16ab7a443969f09c9492d40fedd51b
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2021-05-19 07:08:04 -0400

    Fixing comment on the /description endpoint

--- mixnode/src/node/http/description.rs
+++ mixnode/src/node/http/description.rs
@@ -2,8 +2,7 @@ use crate::node::node_description::NodeDescription;
 use rocket::State;
 use rocket_contrib::json::Json;
 
-/// Provides verifiable location (verloc) measurements for this mixnode - a list of the
-/// round-trip times, in milliseconds, for all other mixnodes that this node knows about.
+/// Returns a description of the node and why someone might want to delegate stake to it.
 #[get("/description")]
 pub(crate) fn description(description: State<NodeDescription>) -> Json<NodeDescription> {
     Json(description.inner().clone())

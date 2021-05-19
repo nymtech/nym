55b3362e77b56b29a9c7b2cab5379549d40b2df7
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-05-19 11:53:09 +0100

    Increased monitor run interval back to 15min

--- network-monitor/src/monitor/mod.rs
+++ network-monitor/src/monitor/mod.rs
@@ -19,7 +19,7 @@ pub(crate) mod sender;
 pub(crate) mod summary_producer;
 
 const PACKET_DELIVERY_TIMEOUT: Duration = Duration::from_secs(20);
-const MONITOR_RUN_INTERVAL: Duration = Duration::from_secs(2 * 60);
+const MONITOR_RUN_INTERVAL: Duration = Duration::from_secs(15 * 60);
 const GATEWAY_PING_INTERVAL: Duration = Duration::from_secs(60);
 
 pub(super) struct Monitor {

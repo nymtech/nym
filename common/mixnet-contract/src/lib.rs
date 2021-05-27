b0e11221638b2911bab5660bdb8824033d436dae
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-05-27 15:26:23 +0100

    Initial struct

--- common/mixnet-contract/src/lib.rs
+++ common/mixnet-contract/src/lib.rs
@@ -4,3 +4,10 @@ mod mixnode;
 pub use cosmwasm_std::{Coin, HumanAddr};
 pub use gateway::{Gateway, GatewayBond, GatewayOwnershipResponse, PagedGatewayResponse};
 pub use mixnode::{MixNode, MixNodeBond, MixOwnershipResponse, PagedResponse};
+
+pub struct LayerDistribution {
+    pub layer1: u32,
+    pub layer2: u32,
+    pub layer3: u32,
+    pub gateways: u32,
+}

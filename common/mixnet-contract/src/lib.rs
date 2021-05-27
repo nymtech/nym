27e704ab8073a29a81a57d9e37b5915b60743b11
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-05-27 11:42:11 +0100

    Reordered imports

--- common/mixnet-contract/src/lib.rs
+++ common/mixnet-contract/src/lib.rs
@@ -1,6 +1,6 @@
 mod gateway;
 mod mixnode;
 
-pub use cosmwasm_std::{Coin, Addr};
+pub use cosmwasm_std::{Addr, Coin};
 pub use gateway::{Gateway, GatewayBond, GatewayOwnershipResponse, PagedGatewayResponse};
 pub use mixnode::{MixNode, MixNodeBond, MixOwnershipResponse, PagedResponse};

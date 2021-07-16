43196b2d951cc3fa4f3d1515c46b75f2fa2c97cf
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2021-07-16 13:57:19 +0300

    Remove unnecessary bash magic

--- clients/validator/upload_contract/entrypoint.sh
+++ clients/validator/upload_contract/entrypoint.sh
@@ -4,12 +4,10 @@
 while ! [ -s /genesis_volume/genesis_mnemonic ]; do
 	sleep 1
 done
-MNEMONIC=$(cat /genesis_volume/genesis_mnemonic)
-
-sed -i 's/mnemonic = "";/mnemonic = "'"${MNEMONIC}"'";/' upload-wasm.ts
 
 # Wait until the validator opens its port
 while ! nc -z genesis_validator 26657; do
 	sleep 1
 done
-npx ts-node upload-wasm.ts | tail -n 1 | cut -d' ' -f 3 > /contract_volume/contract_address
+
+npx ts-node upload-wasm.ts

838b4e85cf616c53040991819bbf12f084ad0ea8
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2021-07-20 18:51:51 +0300

    Return the vec instead of the cache wrapper for topology

--- validator-api/src/cache/routes.rs
+++ validator-api/src/cache/routes.rs
@@ -1,17 +1,17 @@
 // Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::cache::{Cache, ValidatorCache};
+use crate::cache::ValidatorCache;
 use mixnet_contract::{GatewayBond, MixNodeBond};
 use rocket::serde::json::Json;
 use rocket::State;
 
 #[get("/mixnodes")]
-pub(crate) async fn get_mixnodes(cache: &State<ValidatorCache>) -> Json<Cache<Vec<MixNodeBond>>> {
-    Json(cache.mixnodes().await)
+pub(crate) async fn get_mixnodes(cache: &State<ValidatorCache>) -> Json<Vec<MixNodeBond>> {
+    Json(cache.mixnodes().await.value)
 }
 
 #[get("/gateways")]
-pub(crate) async fn get_gateways(cache: &State<ValidatorCache>) -> Json<Cache<Vec<GatewayBond>>> {
-    Json(cache.gateways().await)
+pub(crate) async fn get_gateways(cache: &State<ValidatorCache>) -> Json<Vec<GatewayBond>> {
+    Json(cache.gateways().await.value)
 }

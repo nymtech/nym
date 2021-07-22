793dbdef7e6a7fc9dc44168ceb35afa1cc3eb7c2
Fouad <fouad@pop-os.localdomain>
2021-07-22 12:00:46 +0100

    update types

--- wallet-web/components/bond/NodeBond.tsx
+++ wallet-web/components/bond/NodeBond.tsx
@@ -150,7 +150,7 @@ const BondNode = () => {
         <React.Fragment>
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
-                    <ExecFeeNotice name={"bonding"}/>
+                    <ExecFeeNotice name="bonding"/>
                     <Typography component="h1" variant="h4" align="center" className={classes.wrapper}>
                         Bond a {nodeType}
                     </Typography>

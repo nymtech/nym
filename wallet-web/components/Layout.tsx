87db1fd8c148a86e4326d9dd0add9c557a455b15
fmtabbara <fmtabbara@hotmail.co.uk>
2021-07-28 17:45:45 +0100

    update breakpoint for large screens

--- wallet-web/components/Layout.tsx
+++ wallet-web/components/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout = ({ children }: { children: React.ReactElement }) => {
       }}
     >
       <Grid container justify="center">
-        <Grid item xs={12} md={8} xl={4}>
+        <Grid item xs={12} md={8} xl={6}>
           {children}
         </Grid>
       </Grid>

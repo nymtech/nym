c87bf0ae3b057a75147072d6947e87f8f9f97cb2
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-08-03 15:33:45 +0100

    Cleaned up build.rs

--- validator-api/build.rs
+++ validator-api/build.rs
@@ -1,35 +1,19 @@
-use sqlx;
+use sqlx::{Connection, SqliteConnection};
 use std::env;
-use std::fs;
-use sqlx::SqliteConnection;
-use sqlx::Connection;
-
 
 #[tokio::main]
 async fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    //   cargo:rustc-env=DATABASE_URL=sqlite:///home/dave/workspace/projects/nym/nym/validator-api/target/debug/build/nym-validator-api-0baae7e145508d83/out/validator-api-example.sqlite
+    let database_path = format!("sqlite://{}/validator-api-example.sqlite", out_dir);
 
+    let mut conn = SqliteConnection::connect(&*format!("{}?mode=rwc", database_path))
+        .await
+        .expect("Failed to create SQLx database connection");
 
-    let database_path  = format!("sqlite://{}/validator-api-example.sqlite", out_dir);
-
-    // println!("database_path is: {}", &database_path);
-
-    println!("cargo:rustc-env=DATABASE_URL={}", &database_path);
-
-    fs::File::create(format!("{}/validator-api-example.sqlite", &out_dir)).unwrap();
-
-    // let opts = sqlx::sqlite::SqliteConnectOptions::new()
-    //     .filename(&database_path)
-    //     .create_if_missing(true);
-
-    // let connection_pool = sqlx::SqlitePool::connect_with(opts).await.unwrap();
-
-    let mut conn = SqliteConnection::connect(&database_path).await.unwrap();
-    
-    
     sqlx::migrate!("./migrations")
         .run(&mut conn)
         .await
-        .unwrap();
+        .expect("Failed to perform SQLx migrations");
+
+    println!("cargo:rustc-env=DATABASE_URL={}", &database_path);
 }

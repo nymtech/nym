4c5ccc82a1026445dbf1416bb4856ee0d281282e
Mark Sinclair <mmsinclair@gmail.com>
2021-08-03 18:13:10 +0100

    network-explorer-api: configure state with env var API_STATE_FILE or fall back to default value of `explorer-api-state.json`

--- explorer-api/src/state.rs
+++ explorer-api/src/state.rs
@@ -26,36 +26,47 @@ pub struct ExplorerApiStateOnDisk {
 #[derive(Clone)]
 pub(crate) struct ExplorerApiStateContext {
     pub(crate) inner: ExplorerApiState,
+    state_file: String,
 }
 
 impl ExplorerApiStateContext {
     pub(crate) fn new() -> Self {
         ExplorerApiStateContext {
             inner: ExplorerApiStateContext::read_from_file(),
+            state_file: std::env::var("API_STATE_FILE").unwrap_or(STATE_FILE.to_string()),
         }
     }
 
     pub(crate) fn read_from_file() -> ExplorerApiState {
-        let json_file_path = Path::new(STATE_FILE);
+        let json_file = get_state_file_path().to_string();
+        let json_file_path = Path::new(&json_file);
+        info!("Loading state from file {:?}...", json_file);
         match File::open(json_file_path) {
             Ok(file) => {
                 let state: ExplorerApiStateOnDisk =
                     serde_json::from_reader(file).expect("error while reading json");
-                info!("Loaded state from file {:?}", state);
+                info!("Loaded state from file {:?}: {:?}", json_file, state);
                 ExplorerApiState {
                     country_node_distribution: ConcurrentCountryNodesDistribution::attach(
                         state.country_node_distribution,
                     ),
                 }
             }
-            Err(_e) => ExplorerApiState {
-                country_node_distribution: ConcurrentCountryNodesDistribution::new(),
-            },
+            Err(_e) => {
+                warn!(
+                    "Failed to load state from file {:?}, starting with empty state!",
+                    json_file
+                );
+                ExplorerApiState {
+                    country_node_distribution: ConcurrentCountryNodesDistribution::new(),
+                }
+            }
         }
     }
 
     pub(crate) async fn write_to_file(&self) {
-        let json_file_path = Path::new(STATE_FILE);
+        let json_file = get_state_file_path().to_string();
+        let json_file_path = Path::new(&json_file);
         let file = File::create(json_file_path).expect("unable to create state json file");
         let state = ExplorerApiStateOnDisk {
             country_node_distribution: self.inner.country_node_distribution.get_all().await,
@@ -64,4 +75,10 @@ impl ExplorerApiStateContext {
         serde_json::to_writer(file, &state).expect("error writing state to disk");
         info!("Saved file to '{:?}'", json_file_path.canonicalize());
     }
+}
+
+fn get_state_file_path() -> String {
+    std::env::var("API_STATE_FILE")
+        .unwrap_or(STATE_FILE.to_string())
+        .to_string()
 }

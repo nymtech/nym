edc91e0e36b677c7940b166401349e9adbeda770
fmtabbara <fmtabbara@hotmail.co.uk>
2021-08-04 14:19:59 +0100

    use regex for validating  hostname

--- wallet-web/components/bond/utils.ts
+++ wallet-web/components/bond/utils.ts
@@ -31,34 +31,10 @@ export const validateKey = (key: string): boolean => {
 }
 
 export const isValidHostname = (value: string) => {
-  const validHostnameChars = /^[a-zA-Z0-9-.]{1,253}\.?$/g
-  if (!validHostnameChars.test(value)) {
-    return false
-  }
+  const hostnameRegex =
+    /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/g
 
-  if (value.endsWith('.')) {
-    value = value.slice(0, value.length - 1)
-  }
-
-  if (value.length > 253) {
-    return false
-  }
-
-  const labels = value.split('.')
-
-  const isValid = labels.every(function (label) {
-    const validLabelChars = /^([a-zA-Z0-9-]+)$/g
-
-    const validLabel =
-      validLabelChars.test(label) &&
-      label.length < 64 &&
-      !label.startsWith('-') &&
-      !label.endsWith('-')
-
-    return validLabel
-  })
-
-  return isValid
+  return hostnameRegex.test(value)
 }
 
 // check if its a valid semver

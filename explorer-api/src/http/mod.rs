2e900f66481930b16351c0b282b746c67dadb0d5
Mark Sinclair <mmsinclair@gmail.com>
2021-08-10 14:28:11 +0100

    explorer-api: use `rocket-cors` that is pinned in the `validator-api` and remove custom CORS handler

--- explorer-api/src/http/mod.rs
+++ explorer-api/src/http/mod.rs
@@ -1,20 +1,33 @@
 use log::info;
+use rocket::http::Method;
+use rocket::Request;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 use rocket_okapi::swagger_ui::make_swagger_ui;
 
 use crate::country_statistics::http::country_statistics_make_default_routes;
-use crate::http::cors::Cors;
 use crate::http::swagger::get_docs;
 use crate::mix_node::http::mix_node_make_default_routes;
 use crate::ping::http::ping_make_default_routes;
 use crate::state::ExplorerApiStateContext;
-use rocket::Request;
 
-mod cors;
 mod swagger;
 
 pub(crate) fn start(state: ExplorerApiStateContext) {
     tokio::spawn(async move {
         info!("Starting up...");
+
+        let allowed_origins = AllowedOrigins::all();
+
+        // You can also deserialize this
+        let cors = rocket_cors::CorsOptions {
+            allowed_origins,
+            allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+            allowed_headers: AllowedHeaders::some(&["*"]),
+            allow_credentials: true,
+            ..Default::default()
+        }
+        .to_cors()
+        .unwrap();
 
         let config = rocket::config::Config::release_default();
         rocket::build()
@@ -25,7 +38,7 @@ pub(crate) fn start(state: ExplorerApiStateContext) {
             .mount("/swagger", make_swagger_ui(&get_docs()))
             .register("/", catchers![not_found])
             .manage(state)
-            .attach(Cors)
+            .attach(cors)
             .launch()
             .await
     });

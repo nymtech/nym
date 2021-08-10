62250518eebf1aa9b2b10c137258d89609934618
Mark Sinclair <mmsinclair@gmail.com>
2021-08-10 11:55:20 +0100

    explorer-api: add CORS with wide open configuration

---
+++ explorer-api/src/http/cors.rs
@@ -0,0 +1,26 @@
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+// See https://github.com/SergioBenitez/Rocket/issues/25#issuecomment-838566038.
+// Tried to use `rocket_cors` however it requires lots of nightly builds,
+// so went for this instead to keep things simple
+
+pub(crate) struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "GET, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
--- explorer-api/src/http/mod.rs
+++ explorer-api/src/http/mod.rs
@@ -1,19 +1,21 @@
-mod swagger;
+use log::info;
+use rocket_okapi::swagger_ui::make_swagger_ui;
 
 use crate::country_statistics::http::country_statistics_make_default_routes;
+use crate::http::cors::CORS;
 use crate::http::swagger::get_docs;
 use crate::mix_node::http::mix_node_make_default_routes;
 use crate::ping::http::ping_make_default_routes;
 use crate::state::ExplorerApiStateContext;
-use log::info;
-use rocket_okapi::swagger_ui::make_swagger_ui;
+
+mod cors;
+mod swagger;
 
 pub(crate) fn start(state: ExplorerApiStateContext) {
     tokio::spawn(async move {
         info!("Starting up...");
 
         let config = rocket::config::Config::release_default();
-
         rocket::build()
             .configure(config)
             .mount("/countries", country_statistics_make_default_routes())
@@ -24,6 +26,7 @@ pub(crate) fn start(state: ExplorerApiStateContext) {
             .manage(state)
             // .manage(descriptor)
             // .manage(node_stats_pointer)
+            .attach(CORS)
             .launch()
             .await
     });

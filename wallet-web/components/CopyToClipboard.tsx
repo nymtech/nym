01697613fe02b991339d1474c7e7530508987479
fmtabbara <fmtabbara@hotmail.co.uk>
2021-08-11 15:34:51 +0100

    use fab button

--- wallet-web/components/CopyToClipboard.tsx
+++ wallet-web/components/CopyToClipboard.tsx
@@ -1,51 +1,84 @@
 import React, { useState } from 'react'
-import { Button, CircularProgress, useTheme } from '@material-ui/core'
-import { Check } from '@material-ui/icons'
+import { CircularProgress, Fab, makeStyles, Tooltip } from '@material-ui/core'
+import { Check, FileCopyOutlined } from '@material-ui/icons'
+import { green, grey } from '@material-ui/core/colors'
 
 const copy = (text: string): Promise<{ success: boolean; value: string }> => {
   return new Promise((resolve, reject) => {
     navigator.clipboard
       .writeText(text)
-      .then(() => resolve({ success: true, value: text }))
+      .then(() =>
+        setTimeout(() => resolve({ success: true, value: text }), 1000)
+      )
       .catch(() => reject({ success: false, value: 'Failed to copy' }))
   })
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  fabInitial: {
+    color: grey[600],
+    backgroundColor: grey[100],
+    '&:hover': {
+      backgroundColor: grey[200],
+    },
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: grey[400],
+    position: 'absolute',
+    top: -5,
+    left: -5,
+    zIndex: 1,
+  },
+}))
+
 export const CopyToClipboard = ({ text }) => {
   const [copied, setCopied] = useState(false)
   const [isCopying, setIsCopying] = useState(false)
+  const classes = useStyles()
 
-  const theme = useTheme()
+  const handleCopy = async () => {
+    setIsCopying(true)
+    setCopied(false)
+    const res = await copy(text)
+
+    if (res.success) {
+      setCopied(true)
+    } else {
+      console.log('Unable to copy to clipboard')
+    }
+    setIsCopying(false)
+  }
 
   return (
-    <Button
-      variant="outlined"
-      size="small"
-      onClick={async () => {
-        setIsCopying(true)
-        setCopied(false)
-
-        const res = await copy(text)
-
-        if (res.success) {
-          setCopied(true)
-        } else {
-          console.log('Unable to copy to clipboard')
-        }
-        setIsCopying(false)
-      }}
-      endIcon={
-        copied ? (
-          <Check style={{ color: theme.palette.success.dark }} />
-        ) : isCopying ? (
-          <CircularProgress
-            size={15}
-            style={{ color: theme.palette.success.dark }}
-          />
-        ) : null
-      }
-    >
-      {!copied ? 'Copy' : 'Copied'}
-    </Button>
+    <div className={classes.wrapper}>
+      <Tooltip title={!copied ? 'Copy' : 'Copied'}>
+        <Fab
+          size="small"
+          aria-label="save"
+          color="primary"
+          className={copied ? classes.buttonSuccess : classes.fabInitial}
+          onClick={handleCopy}
+        >
+          {copied ? <Check /> : <FileCopyOutlined />}
+        </Fab>
+      </Tooltip>
+      {isCopying && (
+        <CircularProgress size={50} className={classes.fabProgress} />
+      )}
+    </div>
   )
 }
--- wallet-web/pages/receive.tsx
+++ wallet-web/pages/receive.tsx
@@ -33,6 +33,7 @@ const Receive = () => {
                       style={{
                         display: 'flex',
                         justifyContent: 'space-between',
+                        flexWrap: 'wrap',
                       }}
                     >
                       <Typography

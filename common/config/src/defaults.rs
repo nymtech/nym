ae1e4da2c2f5cae60aea2e9476a0d9fedb0ff34b
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-08-11 16:59:43 +0100

    Experiment with new type of defaults

--- common/config/src/defaults.rs
+++ common/config/src/defaults.rs
@@ -1,12 +1,36 @@
 // Copyright 2020 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+pub struct ValidatorDetails<'a> {
+    // it is assumed those values are always valid since they're being provided in our defaults file
+    pub nymd_urls: &'a [&'a str],
+    pub api_urls: &'a [&'a str],
+}
+
+impl<'a> ValidatorDetails<'a> {
+    pub const fn new(nymd_urls: &'a [&'a str], api_urls: &'a [&'a str]) -> Self {
+        ValidatorDetails {
+            nymd_urls,
+            api_urls,
+        }
+    }
+}
+
+pub const DEFAULT_VALIDATORS: &[ValidatorDetails] = &[ValidatorDetails::new(
+    &[
+        "https://testnet-milhon-validator1.nymtech.net",
+        "https://testnet-milhon-validator2.nymtech.net",
+    ],
+    &["https://testnet-milhon-validator1.nymtech.net/api"],
+)];
+
 pub const DEFAULT_VALIDATOR_REST_ENDPOINTS: &[&str] = &[
     "http://testnet-milhon-validator1.nymtech.net:1317",
     "http://testnet-milhon-validator2.nymtech.net:1317",
 ];
 pub const DEFAULT_MIXNET_CONTRACT_ADDRESS: &str = "punk10pyejy66429refv3g35g2t7am0was7yalwrzen";
 pub const NETWORK_MONITOR_ADDRESS: &str = "punk1v9qauwdq5terag6uvfsdytcs2d0sdmfdy7hgk3";
+
 /// Defaults Cosmos Hub/ATOM path
 pub const COSMOS_DERIVATION_PATH: &str = "m/44'/118'/0'/0/0";
 pub const BECH32_PREFIX: &str = "punk";

94ebd69d3000b78c09d38bc8cee6c3c3cd635ad0
fmtabbara <fmtabbara@hotmail.co.uk>
2021-08-16 10:40:13 +0100

    remove unneeded config

--- wallet-web/next.config.js
+++
@@ -1,7 +0,0 @@
-// according to https://nextjs.org/docs/messages/webpack5 this should
-// improve speed of subsequent `next build` calls due to better caching
-module.exports = {
-    future: {
-        webpack5: true,
-    },
-}

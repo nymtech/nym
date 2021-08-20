7345bd81483a20e19dba464a11e6148e2e75c69c
fmtabbara <fmtabbara@hotmail.co.uk>
2021-08-20 11:51:27 +0100

    update navigation style

--- tauri-wallet/src/components/Nav.tsx
+++ tauri-wallet/src/components/Nav.tsx
@@ -1,12 +1,24 @@
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Theme,
+} from '@material-ui/core'
 import {
   AccountBalanceWalletRounded,
   ArrowBack,
   ArrowForward,
+  AttachMoney,
+  Cancel,
   ExitToApp,
+  HowToVote,
+  MoneyOff,
 } from '@material-ui/icons'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/styles'
+import clsx from 'clsx'
 
 const routesSchema = [
   {
@@ -25,19 +37,69 @@ const routesSchema = [
     Icon: <ArrowBack />,
   },
   {
+    label: 'Bond',
+    route: '/bond',
+    Icon: <AttachMoney />,
+  },
+  {
+    label: 'Unbond',
+    route: '/unbond',
+    Icon: <MoneyOff />,
+  },
+  {
+    label: 'Delegate',
+    route: '/delegate',
+    Icon: <HowToVote />,
+  },
+  {
+    label: 'Undelegate',
+    route: '/undelegate',
+    Icon: <Cancel />,
+  },
+  {
     label: 'Logout',
     route: '/signin',
     Icon: <ExitToApp />,
   },
 ]
 
-export const Nav = () => (
-  <List>
-    {routesSchema.map((r, i) => (
-      <ListItem button component={Link} to={r.route} key={i} color="red">
-        <ListItemIcon>{r.Icon}</ListItemIcon>
-        <ListItemText primary={r.label} />
-      </ListItem>
-    ))}
-  </List>
-)
+const useStyles = makeStyles((theme: Theme) => ({
+  navItem: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  selected: {
+    color: theme.palette.primary.main,
+  },
+}))
+
+export const Nav = () => {
+  const classes = useStyles()
+  const location = useLocation()
+
+  return (
+    <List>
+      {routesSchema.map((r, i) => (
+        <ListItem button component={Link} to={r.route} key={i}>
+          <ListItemIcon
+            className={clsx([
+              classes.navItem,
+              location.pathname === r.route ? classes.selected : undefined,
+            ])}
+          >
+            {r.Icon}
+          </ListItemIcon>
+          <ListItemText
+            primary={r.label}
+            primaryTypographyProps={{
+              className: clsx([
+                classes.navItem,
+                location.pathname === r.route ? classes.selected : undefined,
+              ]),
+            }}
+          />
+        </ListItem>
+      ))}
+    </List>
+  )
+}

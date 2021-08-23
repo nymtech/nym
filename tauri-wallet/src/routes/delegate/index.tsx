85f23eb3d1db51f8158cc6a4f29003a1eb7a6cfe
fmtabbara <fmtabbara@hotmail.co.uk>
2021-08-23 10:38:42 +0100

    create delegation page

---
+++ tauri-wallet/src/routes/delegate/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DelegateForm } from './DelegateForm'
+import { Layout, NymCard, Page } from '../../components'
+
+export const Delegate = () => {
+  return (
+    <Page>
+      <Layout>
+        <NymCard
+          title="Delegate"
+          subheader="Delegate to mixnode or gateway"
+          noPadding
+        >
+          <DelegateForm />
+        </NymCard>
+      </Layout>
+    </Page>
+  )
+}
--- tauri-wallet/src/routes/index.tsx
+++ tauri-wallet/src/routes/index.tsx
@@ -3,7 +3,8 @@ import { Switch, Route } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NotFound } from './404'
 import { Balance } from './balance'
-import { Bond } from './bond/bond'
+import { Bond } from './bond'
+import { Delegate } from './delegate'
 import { Receive } from './receive'
 import { Send } from './send'
 import { SignIn } from './sign-in'
@@ -17,17 +18,26 @@ export const Routes = () => (
       <Route path="/balance">
         <Balance />
       </Route>
-      <Route path="/bond">
-        <Bond />
-      </Route>
-      <Route path="/signin">
-        <SignIn />
+      <Route path="/send">
+        <Send />
       </Route>
       <Route path="/receive">
         <Receive />
       </Route>
-      <Route path="/send">
-        <Send />
+      <Route path="/bond">
+        <Bond />
+      </Route>
+      <Route path="/unbond">
+        <Bond />
+      </Route>
+      <Route path="/delegate">
+        <Delegate />
+      </Route>
+      <Route path="/undelegate">
+        <Delegate />
+      </Route>
+      <Route path="/signin">
+        <SignIn />
       </Route>
       <Route path="*">
         <NotFound />

b3858c54eb610026cb26941518ddbfd988a67a78
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-08-24 16:30:35 +0100

    Removed reward-specific node status routes

--- validator-api/src/node_status_api/routes.rs
+++ validator-api/src/node_status_api/routes.rs
@@ -1,7 +1,6 @@
 // Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::node_status_api::local_guard::LocalRequest;
 use crate::node_status_api::models::{
     ErrorResponse, GatewayStatusReport, GatewayUptimeHistory, MixnodeStatusReport,
     MixnodeUptimeHistory,
@@ -10,22 +9,6 @@ use crate::storage::NodeStatusStorage;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::State;
-
-#[get("/daily-chores")]
-pub(crate) async fn rewarding_chores(
-    _local_request: LocalRequest,
-    storage: &State<NodeStatusStorage>,
-) -> Result<&'static str, ErrorResponse> {
-    if storage
-        .daily_chores()
-        .await
-        .map_err(|err| ErrorResponse::new(err, Status::InternalServerError))?
-    {
-        Ok("Updated historical uptimes and purged old reports.")
-    } else {
-        Ok("The historical uptimes were already updated at some point today - nothing was done now.")
-    }
-}
 
 #[get("/mixnode/<pubkey>/report")]
 pub(crate) async fn mixnode_report(
@@ -73,26 +56,4 @@ pub(crate) async fn gateway_uptime_history(
         .await
         .map(Json)
         .map_err(|err| ErrorResponse::new(err, Status::NotFound))
-}
-
-#[get("/mixnodes/all/report")]
-pub(crate) async fn mixnodes_full_report(
-    storage: &State<NodeStatusStorage>,
-) -> Result<Json<Vec<MixnodeStatusReport>>, ErrorResponse> {
-    storage
-        .get_all_mixnode_reports()
-        .await
-        .map(Json)
-        .map_err(|err| ErrorResponse::new(err, Status::InternalServerError))
-}
-
-#[get("/gateways/all/report")]
-pub(crate) async fn gateways_full_report(
-    storage: &State<NodeStatusStorage>,
-) -> Result<Json<Vec<GatewayStatusReport>>, ErrorResponse> {
-    storage
-        .get_all_gateway_reports()
-        .await
-        .map(Json)
-        .map_err(|err| ErrorResponse::new(err, Status::InternalServerError))
 }

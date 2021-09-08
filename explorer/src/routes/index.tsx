c4c7ee82b5f2afe9d0256f16cd43736cada573a7
Adrian Thompson <adrian@nymtech.net>
2021-09-08 14:49:21 +0100

    very basic nav links added

--- explorer/src/routes/index.tsx
+++ explorer/src/routes/index.tsx
@@ -3,18 +3,26 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link,
   NavLink,
 } from 'react-router-dom';
 
 export const Routes: React.FC = () => (
   <Router>
+    <div style={{ display: 'flex', flexDirection: 'column', margin: 30 }}>
+      <NavLink to="/">Overview</NavLink>
+      <NavLink to="/mixnodes">Mix Nodes</NavLink>
+      <NavLink to="/mixnodes/map">Mix Nodes MAP</NavLink>
+      <NavLink to="/gateways">Gateways</NavLink>
+    </div>
     <Switch>
       <Route exact path="/">
         <h1>OVERVIEW</h1>
       </Route>
-      <Route path="/mixnodes">
-        <h1>MIX NODES</h1>
+      <Route exact path="/mixnodes">
+        <h1>MIX-NODES</h1>
+      </Route>
+      <Route path="/mixnodes/map">
+        <h1>THE MIX-NODES MAP</h1>
       </Route>
       <Route path="/gateways">
         <h1>GATEWAYS</h1>

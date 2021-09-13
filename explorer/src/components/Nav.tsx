ce7f51e4b29c2a86f207956a766ad11b77208b51
Adrian Thompson <adrian@nymtech.net>
2021-09-13 15:51:33 +0100

    now pulling down theme correctly not using makeStyles syntax

--- explorer/src/components/Nav.tsx
+++ explorer/src/components/Nav.tsx
@@ -132,9 +132,7 @@ export const Nav: React.FC = () => {
       <AppBar
         position="fixed"
         open={open}
-        // sx={{ bgcolor: 'red' }}
-        className={classes.nymAppBar}
-        // sx={{ bgcolor: theme.palette.primary.dark }}
+        sx={{ bgcolor: theme.palette.primary.dark }}
       >
         <Toolbar>
           <IconButton
@@ -155,13 +153,17 @@ export const Nav: React.FC = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
+      <Drawer
+        variant="permanent"
+        open={open}
+        sx={{ bgcolor: theme.palette.secondary.dark, height: '100vh' }}
+      >
+        <DrawerHeader sx={{ bgcolor: theme.palette.secondary.dark }}>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === 'rtl' ? (
               <ChevronRightIcon />
             ) : (
-              <ChevronLeftIcon />
+              <ChevronLeftIcon sx={{ color: theme.palette.primary.main }} />
             )}
           </IconButton>
         </DrawerHeader>
@@ -170,9 +172,16 @@ export const Nav: React.FC = () => {
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {index % 2 === 0 ? (
+                  <InboxIcon sx={{ color: theme.palette.primary.main }} />
+                ) : (
+                  <MailIcon sx={{ color: theme.palette.primary.main }} />
+                )}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText
+                primary={text}
+                sx={{ color: theme.palette.primary.main }}
+              />
             </ListItem>
           ))}
         </List>
@@ -181,9 +190,16 @@ export const Nav: React.FC = () => {
           {['All mail', 'Trash', 'Spam'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {index % 2 === 0 ? (
+                  <InboxIcon sx={{ color: theme.palette.primary.main }} />
+                ) : (
+                  <MailIcon sx={{ color: theme.palette.primary.main }} />
+                )}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText
+                primary={text}
+                sx={{ color: theme.palette.primary.main }}
+              />
             </ListItem>
           ))}
         </List>
@@ -194,30 +210,13 @@ export const Nav: React.FC = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
           dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
+          ultrices mi tempus imperdiet.
         </Typography>
         <Typography paragraph>
           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
           ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
           elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
+          sed nisi lacus sed viverra tellus.
         </Typography>
       </Box>
     </Box>

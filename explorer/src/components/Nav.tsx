63059aac291725493ed3a14ff3363eba49962419
Adrian Thompson <adrian@nymtech.net>
2021-09-13 10:51:14 +0100

    basic nav drawer working

--- explorer/src/components/Nav.tsx
+++ explorer/src/components/Nav.tsx
@@ -1,138 +1,193 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { List, ListItem, ListItemIcon, Theme } from '@mui/material';
-import {
-  Equalizer,
-  Close,
-  GroupWork,
-  PinDrop,
-  Menu as MenuIcon,
-} from '@mui/icons-material';
-import { makeStyles, ClassNameMap } from '@mui/styles';
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  navBar: {
-    backgroundColor: theme.palette.secondary.dark,
-    marginTop: 60,
-    height: '100vh',
-    width: 80,
-    transition: `${theme.transitions.duration.short}ms`,
-    display: 'flex',
-    flexDirection: 'column',
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(9)} + 1px)`,
   },
-  hamburgerIcon: {
-    color: theme.palette.secondary.light,
-  },
-  navListItem: {
-    borderTop: 0.4,
-    height: '72px',
-    padding: '24px !important',
-  },
-  activeListItem: {
-    backgroundColor: theme.palette.primary.dark,
-  },
-  navItem: {
-    color: theme.palette.primary.main,
-    fontWeight: theme.typography.fontWeightBold,
-    fontSize: theme.typography.fontSize,
-    fontFamily: theme.typography.fontFamily,
-    transition: `${theme.transitions.duration.short}ms`,
-    animation: '$myEffect 1s ease-in',
-  },
-  selected: {
-    color: theme.palette.primary.contrastText,
-  },
-  '@keyframes myEffect': {
-    '0%': {
-      opacity: 0,
-    },
-    '100%': {
-      opacity: 1,
-    },
-  },
-  '@keyframes myEffectExit': {
-    '0%': {
-      opacity: 1,
-    },
-    '100%': {
-      opacity: 0,
-    },
-  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
-const routesSchema = [
-  {
-    label: 'Overview',
-    route: '/overview',
-    Icon: <Equalizer style={{ color: '#F2F2F2' }} />,
-  },
-  {
-    label: 'Network Components',
-    route: '/network-components',
-    Icon: <GroupWork style={{ color: '#F2F2F2' }} />,
-  },
-  {
-    label: 'Node Map',
-    route: '/nodemap',
-    Icon: <PinDrop style={{ color: '#F2F2F2' }} />,
-  },
-];
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
+
 export const Nav: React.FC = () => {
-  const [sidebar, setSidebar] = React.useState<boolean>(false);
-  const classes: ClassNameMap = useStyles();
-  const location = useLocation();
-  const showSidebar = () => setSidebar(!sidebar);
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <>
-      <div
-        className={classes.navBar}
-        style={sidebar ? { width: 290 } : { width: 80 }}
-      >
-        <List>
-          <ListItem
-            button
-            className={classes.navListItem}
-            onClick={showSidebar}
-            style={{ height: 72 }}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open} sx={{ bgcolor: '#070B15' }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: '36px',
+              ...(open && { display: 'none' }),
+            }}
           >
-            <ListItemIcon>
-              {sidebar ? (
-                <Close className={classes.hamburgerIcon} />
-              ) : (
-                <MenuIcon className={classes.hamburgerIcon} />
-              )}
-            </ListItemIcon>
-          </ListItem>
-          {routesSchema.map(({ route, Icon, label }) => {
-            const isSelected: boolean = location.pathname === route;
-            return (
-              <React.Fragment key={route}>
-                <ListItem
-                  key={route}
-                  button
-                  component={Link}
-                  to={route}
-                  className={`${classes.navListItem} ${
-                    isSelected && classes.activeListItem
-                  }`}
-                >
-                  <ListItemIcon>{Icon}</ListItemIcon>
-                  {sidebar && (
-                    <p
-                      className={`${classes.navItem} ${
-                        isSelected && classes.selected
-                      }`}
-                    >
-                      {label}
-                    </p>
-                  )}
-                </ListItem>
-              </React.Fragment>
-            );
-          })}
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Network Explorer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
         </List>
-      </div>
-    </>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
+    </Box>
   );
 };

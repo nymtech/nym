fd597391dcc8b2087f514f83d64f49087347873f
Drazen Urch <drazen@urch.eu>
2021-09-14 15:15:26 +0200

    Migrate legacy delegation data (#771)
    * Skip ReadOnlyBucket deserialization errors
    
    * empty migration
    
    * clippy
    
    * cargo schema
    
    * Drop invalid delegation data
    
    * Dont drop old data
    
    * Add todo
    
    * Unify on type param
    
    * gateways are different
    
    * cargo fmt
    
    Co-authored-by: Drazen Urch <durch@users.noreply.guthub.com>

--- contracts/mixnet/src/contract.rs
+++ contracts/mixnet/src/contract.rs
@@ -10,7 +10,9 @@ use cosmwasm_std::{
     entry_point, to_binary, Addr, Decimal, Deps, DepsMut, Env, MessageInfo, QueryResponse,
     Response, Uint128,
 };
-use mixnet_contract::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg, StateParams};
+use mixnet_contract::{
+    ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg, RawDelegationData, StateParams,
+};
 
 pub const INITIAL_DEFAULT_EPOCH_LENGTH: u32 = 2;
 
@@ -205,31 +207,68 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> Result<QueryResponse, Cont
 }
 
 #[entry_point]
-pub fn migrate(deps: DepsMut, env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
-    use crate::storage::{gateways, gateways_read, mixnodes, mixnodes_read};
+pub fn migrate(mut deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+    use crate::storage::{
+        gateway_delegations, gateway_delegations_read, gateway_delegations_read_old, gateways_read,
+        mix_delegations, mix_delegations_read, mix_delegations_read_old, mixnodes_read,
+    };
+    use crate::transactions::delegations;
     use cosmwasm_std::{Order, StdResult};
-    use mixnet_contract::CURRENT_BLOCK_HEIGHT;
     use mixnet_contract::{GatewayBond, MixNodeBond};
-    use std::sync::atomic::Ordering;
 
-    CURRENT_BLOCK_HEIGHT.store(env.block.height, Ordering::Relaxed);
+    // Read existing delegations data, drop invalid values, and rewrite delegations data with valid data only
+    fn overwrite_mixnode_delegations_data(
+        identity: &str,
+        deps: &mut DepsMut,
+    ) -> Result<(), ContractError> {
+        let delegations_bucket = mix_delegations_read(deps.storage, identity);
+        let old_delegations_bucket = mix_delegations_read_old(deps.storage, identity);
+        let mut delegations_vec = delegations(delegations_bucket)?;
+        let old_delegations = delegations::<Uint128>(old_delegations_bucket)?;
+        for delegation in old_delegations {
+            delegations_vec.push((delegation.0, RawDelegationData::new(delegation.1, 1)))
+        }
 
-    let bonds = mixnodes_read(deps.storage)
+        for (key, delegation) in delegations_vec {
+            mix_delegations(deps.storage, identity).save(&key, &delegation)?;
+        }
+        Ok(())
+    }
+
+    fn overwrite_gateway_delegations_data(
+        identity: &str,
+        deps: &mut DepsMut,
+    ) -> Result<(), ContractError> {
+        let delegations_bucket = gateway_delegations_read(deps.storage, identity);
+        let old_delegations_bucket = gateway_delegations_read_old(deps.storage, identity);
+        let mut delegations_vec = delegations(delegations_bucket)?;
+        let old_delegations = delegations::<Uint128>(old_delegations_bucket)?;
+        for delegation in old_delegations {
+            delegations_vec.push((delegation.0, RawDelegationData::new(delegation.1, 1)))
+        }
+
+        for (key, delegation) in delegations_vec {
+            gateway_delegations(deps.storage, identity).save(&key, &delegation)?;
+        }
+        Ok(())
+    }
+
+    let mixnet_bonds = mixnodes_read(deps.storage)
         .range(None, None, Order::Ascending)
         .map(|res| res.map(|item| item.1))
         .collect::<StdResult<Vec<MixNodeBond>>>()?;
 
-    for bond in bonds {
-        mixnodes(deps.storage).save(bond.identity().as_bytes(), &bond)?;
+    for bond in mixnet_bonds {
+        overwrite_mixnode_delegations_data(bond.identity(), &mut deps)?;
     }
 
-    let bonds = gateways_read(deps.storage)
+    let gateway_bonds = gateways_read(deps.storage)
         .range(None, None, Order::Ascending)
         .map(|res| res.map(|item| item.1))
         .collect::<StdResult<Vec<GatewayBond>>>()?;
 
-    for bond in bonds {
-        gateways(deps.storage).save(bond.identity().as_bytes(), &bond)?;
+    for bond in gateway_bonds {
+        overwrite_gateway_delegations_data(bond.identity(), &mut deps)?;
     }
 
     Ok(Default::default())

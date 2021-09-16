c569d7e71631f81e255f3e73796e817f0cf05806
Adrian Thompson <adrian@nymtech.net>
2021-09-16 10:29:57 +0100

    map changed and updated to shades correctly

--- explorer/src/components/WorldMap.tsx
+++ explorer/src/components/WorldMap.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { geoEqualEarth, geoPath } from 'd3-geo';
-import { feature } from 'topojson-client';
-import { Feature, FeatureCollection, Geometry } from 'geojson';
+// import { csv, json } from 'd3-fetch';
+import { scaleLinear } from 'd3-scale';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import { Box, Grid, Typography } from '@mui/material';
+import { countriesData } from '../data/countriesData';
 
-const uuid = require('react-uuid');
-
-const cx = 400;
-const cy = 150;
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
 
 export type WorldMapProps = {
   // text: string;
@@ -15,81 +14,22 @@ export type WorldMapProps = {
   // url: string;
 };
 
+const colorScale: any = scaleLinear()
+  .domain([0, 1200])
+  // @ts-ignore
+  .range(['#ffedea', '#ff5233']);
+console.log('color scale works ', colorScale);
+
 export const WorldMap: React.FC<WorldMapProps> = () => {
-  const [geographies, setGeographies] = React.useState<
-    [] | Array<Feature<Geometry | null>>
-  >([]);
-
-  const [scale, setScale] = React.useState<number>(200);
-  const [windowResizing, setWindowResizing] = React.useState<boolean>(false);
-
-  const projection = geoEqualEarth()
-    .scale(scale)
-    .translate([cx, cy])
-    .rotate([0, 0]);
-
-  const fetchGeographicData = (): void => {
-    fetch(
-      'https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json',
-    ).then((response) => {
-      if (response.status !== 200) {
-        console.log('coordinates for the map have failed to load');
-        return;
-      }
-      response.json().then((worldData) => {
-        const mapFeatures: Array<Feature<Geometry | null>> = (
-          feature(
-            worldData,
-            worldData.objects.countries,
-          ) as unknown as FeatureCollection
-        ).features;
-        setGeographies(mapFeatures);
-      });
-    });
-  };
-
-  const renderMap = () => {
-    console.log('render the map...');
-  };
+  const [data, setData] = React.useState<Record<string, unknown>[]>([]);
 
   React.useEffect(() => {
-    fetchGeographicData();
-    renderMap();
+    setData(countriesData);
+    // json('/data/foo.json').then((res: []) => {
+    //   console.log('res is ', res);
+    //   setData(countriesData);
+    // });
   }, []);
-
-  // React.useEffect(() => {
-  //   let timeout: any;
-  //   const handleResize = () => {
-  //     clearTimeout(timeout);
-
-  //     setWindowResizing(true);
-
-  //     timeout = setTimeout(() => {
-  //       setWindowResizing(false);
-  //     }, 200);
-  //   };
-  //   window.addEventListener('resize', handleResize);
-
-  //   return () => window.removeEventListener('resize', handleResize);
-  // }, []);
-
-  // React.useEffect(() => {
-  //   if (!windowResizing) {
-  //     const n = window.innerWidth;
-  //     if (n < 830) {
-  //       console.log('tablet/mobile now');
-  //       setScale(150);
-  //     }
-  //     if (n > 829 && n < 1300) {
-  //       console.log('laptop now');
-  //       setScale(200);
-  //     }
-  //     if (n > 1300) {
-  //       console.log('Large Desktop now');
-  //       setScale(240);
-  //     }
-  //   }
-  // }, [windowResizing]);
 
   return (
     <Grid
@@ -115,27 +55,31 @@ export const WorldMap: React.FC<WorldMapProps> = () => {
           Distribution of nodes around the world
         </Typography>
 
-        <svg
-          width={scale * 4}
-          height={scale * 4}
-          viewBox={window.innerWidth < 600 ? '100 0 750 350' : '0 0 800 450'}
-          data-testid="svg"
-          style={{ border: '1px solid red' }}
+        <ComposableMap
+          style={{ backgroundColor: 'rgba(50, 60, 81, 1)', marginTop: 30 }}
+          projectionConfig={{
+            rotate: [-10, 0, 0],
+            scale: 187,
+          }}
         >
-          <g>
-            {(geographies as []).map((d, i) => (
-              <path
-                key={`path-${uuid()}`}
-                d={geoPath().projection(projection)(d) as string}
-                fill={`rgba(246, 195, 180,${
-                  (1 / (geographies ? geographies.length : 0)) * i
-                })`}
-                stroke="grey"
-                strokeWidth={0.5}
-              />
-            ))}
-          </g>
-        </svg>
+          {data.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      // @ts-ignore
+                      fill={d ? colorScale(d.nodes) : '#F5F4F6'}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          )}
+        </ComposableMap>
       </Box>
     </Grid>
   );

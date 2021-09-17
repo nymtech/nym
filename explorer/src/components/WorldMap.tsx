450f12bd7bf302ae16e5cb7cc7a1c0d8302ee25d
Adrian Thompson <adrian@nymtech.net>
2021-09-17 11:02:35 +0100

    added tooltip to worldmap

--- explorer/src/components/WorldMap.tsx
+++ explorer/src/components/WorldMap.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import { csv, json } from 'd3-fetch';
 import { scaleLinear } from 'd3-scale';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import ReactTooltip from 'react-tooltip';
 import { Box, Grid, Typography } from '@mui/material';
 import { countriesData } from '../data/countriesData';
 
@@ -18,17 +19,13 @@ const colorScale: any = scaleLinear()
   .domain([0, 1200])
   // @ts-ignore
   .range(['#ffedea', '#ff5233']);
-console.log('color scale works ', colorScale);
 
 export const WorldMap: React.FC<WorldMapProps> = () => {
+  const [tooltipContent, setTooltipContent] = React.useState<string>('');
   const [data, setData] = React.useState<Record<string, unknown>[]>([]);
 
   React.useEffect(() => {
     setData(countriesData);
-    // json('/data/foo.json').then((res: []) => {
-    //   console.log('res is ', res);
-    //   setData(countriesData);
-    // });
   }, []);
 
   return (
@@ -56,6 +53,7 @@ export const WorldMap: React.FC<WorldMapProps> = () => {
         </Typography>
 
         <ComposableMap
+          data-tip=""
           style={{ backgroundColor: 'rgba(50, 60, 81, 1)', marginTop: 30 }}
           projectionConfig={{
             rotate: [-10, 0, 0],
@@ -67,12 +65,30 @@ export const WorldMap: React.FC<WorldMapProps> = () => {
               {({ geographies }: any) =>
                 geographies.map((geo: any) => {
                   const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
+                  console.log('geo', geo);
+                  console.log('d', d);
                   return (
                     <Geography
                       key={geo.rsmKey}
                       geography={geo}
                       // @ts-ignore
                       fill={d ? colorScale(d.nodes) : '#F5F4F6'}
+                      onMouseEnter={() => {
+                        const { NAME_LONG } = geo.properties;
+                        setTooltipContent(
+                          // @ts-ignore
+                          `${NAME_LONG} | ${d?.nodes || 0}`,
+                        );
+                      }}
+                      onMouseLeave={() => {
+                        setTooltipContent('');
+                      }}
+                      style={{
+                        hover: {
+                          fill: 'black',
+                          outline: 'white',
+                        },
+                      }}
                     />
                   );
                 })
@@ -80,6 +96,7 @@ export const WorldMap: React.FC<WorldMapProps> = () => {
             </Geographies>
           )}
         </ComposableMap>
+        <ReactTooltip>{tooltipContent}</ReactTooltip>
       </Box>
     </Grid>
   );

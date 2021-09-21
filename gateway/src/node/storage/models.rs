403bcfbe13edd193a859be5cd7ecac28e039ba3a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-09-21 12:06:36 +0100

    Managed for persisted shared keys

---
+++ gateway/src/node/storage/models.rs
@@ -0,0 +1,18 @@
+// Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+pub(crate) struct PersistedSharedKeys {
+    pub(crate) client_address_bs58: String,
+    pub(crate) derived_aes128_ctr_blake3_hmac_keys_bs58: String,
+}
+
+pub(crate) struct StoredMessage {
+    pub(crate) id: i64,
+    pub(crate) client_address_bs58: String,
+    pub(crate) content: Vec<u8>,
+}
+
+// pub(crate) struct PersistedBandwidth {
+//     client_address_bs58: String,
+//     available: i64,
+// }

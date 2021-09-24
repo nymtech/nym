4989a373356460c311c195f7161a4528805835e3
Aid Thompson <adrian@nymtech.net>
2021-09-24 12:07:25 +0100

    broken - passing to Fouad

--- explorer/src/context/main.tsx
+++ explorer/src/context/main.tsx
@@ -1,46 +1,57 @@
 import { PaletteMode } from '@mui/material';
 import * as React from 'react';
-import { CountryDataResponse, GatewayResponse, MixNodeResponse, ValidatorsResponse } from 'src/typeDefs/explorer-api';
+import { CountryDataResponse, GatewayResponse, MixNodeResponse, ValidatorsResponse, BlockResponse } from 'src/typeDefs/explorer-api';
 import { Api } from '../api';
 
-type NodeApiResponse = {
-  data: MixNodeResponse | GatewayResponse | ValidatorsResponse | CountryDataResponse | number | null
-  error: string | null
+// type NodeApiResponse = {
+//   data: MixNodeResponse | GatewayResponse | ValidatorsResponse | CountryDataResponse | number | null
+//   error: string | null
+// }
+
+interface ApiState<RESPONSE> {
+  isLoading: boolean;
+  data?: RESPONSE;
+  error?: Error;
 }
 interface State {
   mode: PaletteMode
-  toggleMode: () => void
-  mixnodes: NodeApiResponse | null
-  gateways: NodeApiResponse | null
-  validators: NodeApiResponse | null
-  block: NodeApiResponse | null
-  countryData: NodeApiResponse | null
+  toggleMode?: () => void
+  mixnodes?: ApiState<MixNodeResponse>
+  gateways?: ApiState<GatewayResponse>
+  validators?: ApiState<ValidatorsResponse>
+  block?: ApiState<BlockResponse>
+  countryData?: ApiState<CountryDataResponse>
 };
 
-export const MainContext = React.createContext({} as State);
+export const MainContext = React.createContext<State>({ mode: "dark" });
 
 export const MainContextProvider: React.FC = ({ children }) => {
   // light/dark mode
   const [mode, setMode] = React.useState<PaletteMode>('dark');
 
   // various APIs for cards on Overview
-  const [mixnodes, setMixnodes] = React.useState<NodeApiResponse | null>(null);
-  const [gateways, setGateways] = React.useState<NodeApiResponse | null>(null);
-  const [validators, setValidators] = React.useState<NodeApiResponse | null>(null);
-  const [block, setBlock] = React.useState<NodeApiResponse | null>(null);
-  const [countryData, setCountryData] = React.useState<NodeApiResponse | null>(null);
+  const [mixnodes, setMixnodes] = React.useState<ApiState<MixNodeResponse>>();
+  const [gateways, setGateways] = React.useState<ApiState<GatewayResponse>>();
+  const [validators, setValidators] = React.useState<ApiState<ValidatorsResponse>>();
+  const [block, setBlock] = React.useState<ApiState<BlockResponse>>();
+  const [countryData, setCountryData] = React.useState<ApiState<CountryDataResponse>>();
 
   const toggleMode = () => setMode((m) => (m !== 'light' ? 'light' : 'dark'));
 
   const fetchMixnodes = async () => {
     try {
-      const res = await Api.fetchMixnodes();
-      setMixnodes({ data: res, error: null });
+      const data = await Api.fetchMixnodes();
+      setMixnodes({ data, isLoading: false })
     } catch (error) {
-      setMixnodes({ data: null, error: error instanceof Error ? error.message : '' });
+
+      let errorMessage = '';
+
+      if (error instanceof Error) {
+        errorMessage = error.message
+      }
+      setMixnodes({ error: errorMessage, isLoading: false });
     }
   };
-
   const fetchGateways = async () => {
     try {
       const res = await Api.fetchGateways();
@@ -49,7 +60,6 @@ export const MainContextProvider: React.FC = ({ children }) => {
       setGateways({ data: null, error: error instanceof Error ? error.message : '' });
     }
   };
-
   const fetchValidators = async () => {
     try {
       const res = await Api.fetchValidators();

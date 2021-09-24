61dfef2f9beaa1a034d3a96badf7b56a19abd6c5
Aid Thompson <adrian@nymtech.net>
2021-09-24 10:47:07 +0100

    refactor of class API fetch reqs

--- explorer/src/api/index.ts
+++ explorer/src/api/index.ts
@@ -11,52 +11,29 @@ import { MixNodeResponse, GatewayResponse, ValidatorsResponse, CountryDataRespon
 export class Api {
 
   static fetchMixnodes = async (): Promise<MixNodeResponse> => {
-    try {
-      const res = await fetch(MIXNODES_API);
-      const json = await res.json();
-      return json;
-    } catch (error: any) {
-      console.log('error ', error);
-      throw error;
-    }
+    const res = await fetch(MIXNODES_API);
+    return await res.json();
   };
 
   static fetchGateways = async (): Promise<GatewayResponse> => {
-    try {
-      const res = await fetch(GATEWAYS_API);
-      const json = await res.json();
-      return json;
-    } catch (error: any) {
-      console.log('error ', error);
-      return error;
-    }
+    const res = await fetch(GATEWAYS_API);
+    return await res.json();
   };
 
   static fetchValidators = async (): Promise<ValidatorsResponse> => {
-    try {
-      const res = await fetch(VALIDATORS_API);
-      const json = await res.json();
-      return json.result;
-    } catch (error: any) {
-      console.log('error ', error);
-      return error;
-    }
+    const res = await fetch(VALIDATORS_API);
+    const json = await res.json();
+    return json.result;
   };
-
+  
   static fetchBlock = async (): Promise<number> => {
-    try {
-      const res = await fetch(BLOCK_API);
-      const json = await res.json();
-      const { height } = json.result.block.header;
-      return height;
-    } catch (error: any) {
-      console.log('error ', error);
-      return error;
-    }
+    const res = await fetch(BLOCK_API);
+    const json = await res.json();
+    const { height } = json.result.block.header;
+    return height;
   };
 
   static fetchCountryData = async (): Promise<CountryDataResponse> => {
-    try {
       let arr: CountryDataResponse = [];
       const res = await fetch(COUNTRY_DATA_API);
       const json = await res.json();
@@ -64,12 +41,6 @@ export class Api {
         .forEach(ISO3 => { 
           arr.push({ ISO3, nodes: json[ISO3]}) 
         });
-
       return arr;
-    } catch (error: any) {
-      console.log('error ', error);
-      return error;
-    }
   };
-
 }

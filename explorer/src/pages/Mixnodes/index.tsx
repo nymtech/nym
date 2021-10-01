5c0e6e1eec8a8fb427ab6b9a5afc6705d17d03b8
fmtabbara <fmtabbara@hotmail.co.uk>
2021-10-01 14:31:10 +0100

    use loading prop in data grid component

--- explorer/src/pages/Mixnodes/index.tsx
+++ explorer/src/pages/Mixnodes/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { MixnodesDataGrid } from 'src/components/Mixnodes-DataGrid';
 
 export const PageMixnodes: React.FC = () => {
   return (
     <>
-      <Box component="main" sx={{ flexGrow: 1 }}>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <Typography sx={{ marginLeft: 3 }}>Mixnodes</Typography>
-          </Grid>
-          <Grid item xs={11}>
-            <MixnodesDataGrid />
-          </Grid>
-        </Grid>
-      </Box>
+      <Typography sx={{ marginBottom: 1 }} variant="h5">
+        Mixnodes
+      </Typography>
+      <MixnodesDataGrid />
     </>
   );
 };

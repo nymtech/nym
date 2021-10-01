b52a3dc2f335dd86134e868be633e79cd6b06730
fmtabbara <fmtabbara@hotmail.co.uk>
2021-10-01 14:18:07 +0100

    set up data grid component

--- explorer/src/pages/Mixnodes/index.tsx
+++ explorer/src/pages/Mixnodes/index.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import { MixnodesTable } from '../../components/MixnodesTable';
 import { MainContext } from 'src/context/main';
+import { MixnodesDataGrid } from 'src/components/Mixnodes-DataGrid';
 
 export const PageMixnodes: React.FC = () => {
-    const { mixnodes } = React.useContext(MainContext);
-    return (
-        <>
-            <Box component='main' sx={{ flexGrow: 1 }}>
-                <Grid container spacing={0}>
-                    <Grid item xs={12}>
-                        <Typography sx={{ marginLeft: 3 }}>
-                            Mixnodes
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={11}>
-                        {mixnodes !== undefined && (
-                            <MixnodesTable mixnodes={mixnodes} />
-                        )}
-                    </Grid>
-                </Grid>
-            </Box>
-        </>
-    )
-}
+  const { mixnodes } = React.useContext(MainContext);
+  return (
+    <>
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <Typography sx={{ marginLeft: 3 }}>Mixnodes</Typography>
+          </Grid>
+          <Grid item xs={11}>
+            {mixnodes !== undefined && <MixnodesDataGrid />}
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};

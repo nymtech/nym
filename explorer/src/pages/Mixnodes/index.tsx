b73e474a66f3d39b92ffef3016687a1a54a9b5ef
Aid Thompson <adrian@nymtech.net>
2021-10-08 14:56:33 +0100

    adding clickable location to handleSearch

--- explorer/src/pages/Mixnodes/index.tsx
+++ explorer/src/pages/Mixnodes/index.tsx
@@ -10,58 +10,25 @@ import { mixnodeToGridRow } from 'src/utils';
 import { TableToolbar } from 'src/components/TableToolbar';
 import { MixNodeResponse } from 'src/typeDefs/explorer-api';
 
-const columns = [
-  { field: 'owner', headerName: 'Owner', width: 380, },
-  {
-    field: 'identity_key',
-    headerName: 'Identity Key',
-    width: 420,
-    renderCell: (params: GridRenderCellParams) => {
-      return (
-        <Link to={`/network-components/mixnodes/${params.value}`} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
-          {params.value}
-        </Link>
-      )
-    }
-  },
-  {
-    field: 'bond',
-    headerName: 'Bond',
-    width: 130,
-  },
-  {
-    field: 'host',
-    headerName: 'IP:Port',
-    width: 130,
-  },
-  {
-    field: 'location',
-    headerName: 'Location',
-    width: 120,
-  },
-  {
-    field: 'layer',
-    headerName: 'Layer',
-    width: 100,
-    type: 'number',
-  },
-];
-
 export const PageMixnodes: React.FC = () => {
   const { mixnodes } = useContext(MainContext);
   const [filteredMixnodes, setFilteredMixnodes] = React.useState<MixNodeResponse>([])
   const [pageSize, setPageSize] = React.useState<string>("50");
+  const [searchTerm, setSearchTerm] = React.useState<string>('');
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const st = event.target.value.toLowerCase();
-    if (st === '' && mixnodes?.data) {
+  const handleSearch = (str: string) => {
+    setSearchTerm(str.toLowerCase())
+  }
+
+  React.useEffect(() => {
+    if (searchTerm === '' && mixnodes?.data) {
       setFilteredMixnodes(mixnodes?.data)
     } else {
       const filtered = mixnodes?.data?.filter((m) => {
         if (
-          m.location?.country_name.toLowerCase().includes(st) ||
-          m.mix_node.identity_key.toLocaleLowerCase().includes(st) ||
-          m.owner.toLowerCase().includes(st)
+          m.location?.country_name.toLowerCase().includes(searchTerm) ||
+          m.mix_node.identity_key.toLocaleLowerCase().includes(searchTerm) ||
+          m.owner.toLowerCase().includes(searchTerm)
         ) {
           return m;
         }
@@ -70,7 +37,83 @@ export const PageMixnodes: React.FC = () => {
         setFilteredMixnodes(filtered)
       }
     }
-  }
+  }, [searchTerm, mixnodes?.data])
+
+  const columns = [
+    {
+      field: 'owner',
+      headerName: 'Owner',
+      width: 380,
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <a href={`https://testnet-milhon-blocks.nymtech.net/account/${params.value}`} target='_blank' style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </a>
+        )
+      }
+    },
+    {
+      field: 'identity_key',
+      headerName: 'Identity Key',
+      width: 420,
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <Link to={`/network-components/mixnodes/${params.value}`} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </Link>
+        )
+      }
+    },
+    {
+      field: 'bond',
+      headerName: 'Bond',
+      width: 130,
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <Link to={`/network-components/mixnodes/${params.row.identity_key}`} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </Link>
+        )
+      }
+    },
+    {
+      field: 'host',
+      headerName: 'IP:Port',
+      width: 130,
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <Link to={`/network-components/mixnodes/${params.row.identity_key}`} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </Link>
+        )
+      }
+    },
+    {
+      field: 'location',
+      headerName: 'Location',
+      width: 120,
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <div onClick={() => handleSearch(params.value as string)} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </div>
+        )
+      }
+    },
+    {
+      field: 'layer',
+      headerName: 'Layer',
+      width: 100,
+      type: 'number',
+      renderCell: (params: GridRenderCellParams) => {
+        return (
+          <Link to={`/network-components/mixnodes/${params.row.identity_key}`} style={{ textDecoration: 'none', color: 'white', marginLeft: 16 }}>
+            {params.value}
+          </Link>
+        )
+      }
+    },
+  ];
 
   const handlePageSize = (event: SelectChangeEvent<string>) => {
     setPageSize(event.target.value);
@@ -91,6 +134,7 @@ export const PageMixnodes: React.FC = () => {
         onChangeSearch={handleSearch}
         onChangePageSize={handlePageSize}
         pageSize={pageSize}
+        searchTerm={searchTerm}
       />
       <UniversalDataGrid
         loading={mixnodes?.isLoading}

155b9d5bf8774bd89147847107036c1c2037495e
Aid Thompson <adrian@nymtech.net>
2021-10-13 22:26:54 +0100

    unfinished refactor BondBreakdown

--- explorer/src/components/BondBreakdown.tsx
+++ explorer/src/components/BondBreakdown.tsx
@@ -12,10 +12,14 @@ import { MainContext } from 'src/context/main';
 
 export function BondBreakdownTable() {
     const { mixnodeDetailInfo, delegations } = React.useContext(MainContext);
+    const [ allContentLoaded, setAllContentLoaded ] = React.useState<boolean>(false);
+    const [ showError, setShowError ] = React.useState<boolean>(false);
+
     const [bonds, setBonds] = React.useState({
         delegations: '0',
         pledges: '0',
         bondsTotal: '0',
+        hasLoaded: false,
     })
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down("sm"));
@@ -39,90 +43,112 @@ export function BondBreakdownTable() {
                 delegations: decimalisedDelegations,
                 pledges: decimalisedPledges,
                 bondsTotal: bondsTotal,
+                hasLoaded: true,
             });
         }
     }, [mixnodeDetailInfo]);
 
+    React.useEffect(() => {
+        const hasError = mixnodeDetailInfo?.error || delegations?.error ? true : false;
+        const hasAllMixnodeInfo = mixnodeDetailInfo?.data !== undefined && mixnodeDetailInfo?.data[0].mix_node ? true : false;
+        const hasAllDelegationsInfo = delegations?.data !== undefined && delegations?.data.length ? true : false;
+        const hasAllData = !hasError && hasAllMixnodeInfo && hasAllDelegationsInfo ? true : false;
+        setShowError(hasError);
+        setAllContentLoaded(hasAllData);
+    }, [mixnodeDetailInfo, delegations]);
+    
     const calcBondPercentage = (num: number) => {
-        if (mixnodeDetailInfo?.data && mixnodeDetailInfo?.data[0]) {
-            const rawDeligationAmount = Number(mixnodeDetailInfo?.data[0].total_delegation.amount);
-            const rawPledgeAmount = Number(mixnodeDetailInfo?.data[0].bond_amount.amount);
+        if (mixnodeDetailInfo?.data !== undefined && mixnodeDetailInfo?.data[0]) {
+            const rawDeligationAmount = Number(mixnodeDetailInfo.data[0].total_delegation.amount);
+            const rawPledgeAmount = Number(mixnodeDetailInfo.data[0].bond_amount.amount);
             const rawTotalBondsAmount = rawDeligationAmount + rawPledgeAmount;
             return (num * 100 / rawTotalBondsAmount).toFixed(1)
         }
     }
 
-    return (
-        <>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label='bond breakdown totals'>
-                    <TableBody>
-                        <TableRow sx={matches ? { minWidth: '70vw' } : null}>
-                            <TableCell
-                                sx={{
-                                    fontWeight: 'bold',
-                                    width: matches ? '90px' : 'auto',
-                                }}
-                                align='left'
-                            >
-                                Bond total
-                            </TableCell>
-                            <TableCell align='left'>
-                                {bonds.bondsTotal}
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell
-                                sx={{
-                                    width: matches ? '90px' : 'auto',
-                                }}
-                                align='left'
-                            >
-                                Pledge total
-                            </TableCell>
-                            <TableCell align='left'>
-                                {bonds.pledges}
-                            </TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell
-                                sx={{
-                                    width: matches ? '90px' : 'auto',
-                                }}
-                                align='left'
-                            >
-                                Delegation total
-                            </TableCell>
-                            <TableCell align='left'>
-                                {bonds.delegations}
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
+    if (mixnodeDetailInfo?.isLoading) {
+        return <p>Loading...</p>
+    }
 
-                { delegations?.data !== undefined && delegations?.data[0] && (
-                    <Table sx={{ minWidth: 650 }} aria-label='delegation totals'>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell sx={{ fontWeight: 'bold' }} align='left'>Delegators</TableCell>
-                                <TableCell sx={{ fontWeight: 'bold' }} align='left'>Stake</TableCell>
-                                <TableCell sx={{ fontWeight: 'bold' }} align='left'>% of Bond</TableCell>
-                            </TableRow>
-                        </TableHead>
+    if (showError) {
+        return <p>We are unable to retrieve a Mixnode with that ID. Please try later or Contact Us.</p>
+    }
+
+    if (!showError && allContentLoaded) {
+        return (
+            <>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label='bond breakdown totals'>
                         <TableBody>
-                            {delegations.data.map(({ owner, amount: { amount, denom } }) => {
-                                return (
-                                    <TableRow key={owner}>
-                                        <TableCell sx={matches ? { width: 190 } : null} align='left'>{owner}</TableCell>
-                                        <TableCell align='left'>{printableCoin({ amount: amount.toString(), denom })}</TableCell>
-                                        <TableCell align='left'>{calcBondPercentage(amount)}%</TableCell>
-                                    </TableRow>
-                                )
-                            })}
+                            <TableRow sx={matches ? { minWidth: '70vw' } : null}>
+                                <TableCell
+                                    sx={{
+                                        fontWeight: 'bold',
+                                        width: matches ? '90px' : 'auto',
+                                    }}
+                                    align='left'
+                                >
+                                    Bond total
+                                </TableCell>
+                                <TableCell align='left'>
+                                    {bonds.bondsTotal}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell
+                                    sx={{
+                                        width: matches ? '90px' : 'auto',
+                                    }}
+                                    align='left'
+                                >
+                                    Pledge total
+                                </TableCell>
+                                <TableCell align='left'>
+                                    {bonds.pledges}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell
+                                    sx={{
+                                        width: matches ? '90px' : 'auto',
+                                    }}
+                                    align='left'
+                                >
+                                    Delegation total
+                                </TableCell>
+                                <TableCell align='left'>
+                                    {bonds.delegations}
+                                </TableCell>
+                            </TableRow>
                         </TableBody>
                     </Table>
-                )}
-            </TableContainer>
-        </>
-    );
+    
+                    { delegations?.data !== undefined && delegations?.data[0] && (
+                        <Table sx={{ minWidth: 650 }} aria-label='delegation totals'>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{ fontWeight: 'bold' }} align='left'>Delegators</TableCell>
+                                    <TableCell sx={{ fontWeight: 'bold' }} align='left'>Stake</TableCell>
+                                    <TableCell sx={{ fontWeight: 'bold' }} align='left'>% of Bond</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {delegations.data.map(({ owner, amount: { amount, denom } }) => {
+                                    return (
+                                        <TableRow key={owner}>
+                                            <TableCell sx={matches ? { width: 190 } : null} align='left'>{owner}</TableCell>
+                                            <TableCell align='left'>{printableCoin({ amount: amount.toString(), denom })}</TableCell>
+                                            <TableCell align='left'>{calcBondPercentage(amount)}%</TableCell>
+                                        </TableRow>
+                                    )
+                                })}
+                            </TableBody>
+                        </Table>
+                    )}
+                </TableContainer>
+            </>
+        );
+    } else {
+        return null;
+    }
 }

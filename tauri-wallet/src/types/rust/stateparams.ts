cb79c477dfc83248faecd62d1b31c5942afdc9df
fmtabbara <fmtabbara@users.noreply.github.com>
2021-10-14 14:59:11 +0000

    [ci skip] Generate TS types

--- tauri-wallet/src/types/rust/stateparams.ts
+++ tauri-wallet/src/types/rust/stateparams.ts
@@ -1,10 +1,11 @@
 export interface TauriStateParams {
-  epoch_length: number
-  minimum_mixnode_bond: string
-  minimum_gateway_bond: string
-  mixnode_bond_reward_rate: string
-  gateway_bond_reward_rate: string
-  mixnode_delegation_reward_rate: string
-  gateway_delegation_reward_rate: string
-  mixnode_active_set_size: number
+  epoch_length: number;
+  minimum_mixnode_bond: string;
+  minimum_gateway_bond: string;
+  mixnode_bond_reward_rate: string;
+  gateway_bond_reward_rate: string;
+  mixnode_delegation_reward_rate: string;
+  gateway_delegation_reward_rate: string;
+  mixnode_active_set_size: number;
+  gateway_active_set_size: number;
 }

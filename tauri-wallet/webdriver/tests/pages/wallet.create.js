5a270f64892c0269ed118936c3b142c0427a2eb2
Tommy <tommyvez@protonmail.com>
2021-10-21 11:02:28 +0100

    pushing new wallet create - WIP

---
+++ tauri-wallet/webdriver/tests/pages/wallet.create.js
@@ -0,0 +1,19 @@
+class WalletCreate {
+
+    get createAccount() {return $('#root > div > div:nth-child(2) > div > div > div:nth-child(2) > button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-disableElevation.MuiButton-fullWidth > span.MuiButton-label')}
+
+    get accountCreatedSuccessfully() { return $('#root > div > div:nth-child(2) > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-justify-content-xs-center > div:nth-child(1) > p')}
+
+    get walletMnemonicCreated() {return $('#root > div > div:nth-child(2) > div > p')}
+
+    get walletMnemonicValue(){ return $('#root > div > div:nth-child(2) > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-justify-content-xs-center > div.MuiPaper-root.MuiCard-root.MuiPaper-outlined.MuiPaper-rounded > div > div:nth-child(2) > p')}
+
+    get punkAddress(){ return $('#root > div > div:nth-child(2) > div > div > div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-justify-content-xs-center > div.MuiPaper-root.MuiCard-root.MuiPaper-outlined.MuiPaper-rounded > div > div:nth-child(5) > p')}
+    
+    get backToSignIn() {return $('#root > div > div:nth-child(2) > div > div > div:nth-child(2) > button > span.MuiButton-label')}
+
+    get signInButton() {return $('[data-testid="signButton"]')}
+
+
+}
+module.exports = new WalletCreate()
--- tauri-wallet/webdriver/tests/specs/newuser/test.wallet.create.js
+++ tauri-wallet/webdriver/tests/specs/newuser/test.wallet.create.js
@@ -1,12 +1,26 @@
 const walletLogin = require('../../pages/wallet.login');
+const walletSignUp = require('../../pages/wallet.create')
 const textConstants = require('../../../common/constants/text-constants');
+
+const WALLET_SUCCESS = "Please store your mnemonic in a safe place. You'll need it to access your wallet"
 
 describe("non existing wallet holder", () => {
     it("create new account", async () => {
+
         const signInText = await walletLogin.signInLabel.getText();
         expect(signInText).toEqual(textConstants.homePageSignIn);
 
         await walletLogin.createNewAccount.click();
+
+        await walletSignUp.createAccount.click();
+
+        await walletSignUp.accountCreatedSuccessfully.waitForEnabled({ timeout: 6000 });
+
+        const getWalletText = await walletSignUp.punkAddress.getText()
+        expect(getWalletText.length).toEqual(43)
+
+        const getMnemonic = await walletSignUp.walletMnemonicCreated.getText()
+        expect(getMnemonic).toEqual(WALLET_SUCCESS)
 
     })
 });

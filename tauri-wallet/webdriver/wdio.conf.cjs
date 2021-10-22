8a63df3e82c3157e9f7c27b3ef86dfa611112df1
Tommy <tommyvez@protonmail.com>
2021-10-22 17:27:32 +0100

    remove duplicate loglevel output

--- tauri-wallet/webdriver/wdio.conf.cjs
+++ tauri-wallet/webdriver/wdio.conf.cjs
@@ -39,7 +39,6 @@ exports.config = {
   // ===================
   // Define all options that are relevant for the WebdriverIO instance here
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'silent',
   bail: 0,
   framework: 'mocha',
   reporters: ['spec'],

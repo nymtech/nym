0ea3183288903c7b05a73093831baf2abdc8001f
Tommy <tommyvez@protonmail.com>
2021-10-22 14:08:54 +0100

    set logging to silent

--- tauri-wallet/webdriver/wdio.conf.cjs
+++ tauri-wallet/webdriver/wdio.conf.cjs
@@ -39,7 +39,7 @@ exports.config = {
   // ===================
   // Define all options that are relevant for the WebdriverIO instance here
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'debug',
+  logLevel: 'silent',
   bail: 0,
   framework: 'mocha',
   reporters: ['spec'],

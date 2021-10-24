f03f2be844fd146137747be85947052d1ce7e79d
Aid Thompson <adrian@nymtech.net>
2021-10-24 07:36:01 +0100

    re-added external links to Socials

--- explorer/src/components/Socials.tsx
+++ explorer/src/components/Socials.tsx
@@ -3,18 +3,19 @@ import { Box, IconButton } from '@mui/material';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import { GITHUB_LINK, TELEGRAM_LINK, TWITTER_LINK } from 'src/api/constants';
 
 export const Socials: React.FC<{ disableDarkMode?: boolean }> = ({
   disableDarkMode,
 }) => (
   <Box>
-    <IconButton>
+    <IconButton component="a" href={TELEGRAM_LINK} target="_blank">
       <TelegramIcon sx={{ color: disableDarkMode ? 'white' : null }} />
     </IconButton>
-    <IconButton>
+    <IconButton component="a" href={GITHUB_LINK} target="_blank">
       <GitHubIcon sx={{ color: disableDarkMode ? 'white' : null }} />
     </IconButton>
-    <IconButton>
+    <IconButton component="a" href={TWITTER_LINK} target="_blank">
       <TwitterIcon sx={{ color: disableDarkMode ? 'white' : null }} />
     </IconButton>
   </Box>

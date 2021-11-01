7cee479a9812aa866c00181dfa8b480c22d73cf6
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-11-01 16:48:53 +0000

    Introduced rewarding_interval_nonce to contract state

--- contracts/mixnet/src/state.rs
+++ contracts/mixnet/src/state.rs
@@ -12,6 +12,10 @@ pub struct State {
     pub network_monitor_address: Addr,
     pub params: StateParams,
 
+    // keep track of the changes to the current rewarding interval
+    pub rewarding_interval_nonce: u64,
+    pub rewarding_in_progress: bool,
+
     // helper values to avoid having to recalculate them on every single payment operation
     pub mixnode_epoch_bond_reward: Decimal, // reward per epoch expressed as a decimal like 0.05
     pub mixnode_epoch_delegation_reward: Decimal, // reward per epoch expressed as a decimal like 0.05

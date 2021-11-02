222ed869e60440bddaefe1eef9f55e6786a34b86
Aid Thompson <adrian@nymtech.net>
2021-11-02 16:30:29 +0000

    added hamburger and changed appbar to fixed

--- explorer/src/index.tsx
+++ explorer/src/index.tsx
@@ -89,6 +89,7 @@ const AppWrapper = () => {
         styleOverrides: {
           paper: {
             background: palette.secondary.dark,
+            marginTop: 64,
           },
         },
       },

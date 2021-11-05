8666911beec0909d220ca0eb95dc24e8b9612485
Mark Sinclair <mmsinclair@gmail.com>
2021-11-05 11:38:04 +0000

    Remove unused file

--- explorer/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-// import { PageGateways } from './Gateways';
-// import { PageOverview } from './Overview';
-// import { PageMixnodes } from './Mixnodes';
-// import { PageMixnodesMap } from './MixnodesMap';
-
-// export { PageGateways, PageMixnodes, PageMixnodesMap, PageOverview };

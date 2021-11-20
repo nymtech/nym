60c359d47c9c5977d7dd382f0baa03bb18d84790
dave <dave@nym-mbp.lan>
2021-11-20 10:58:19 +0000

    ibid

---
+++ contracts/mixnet/src/query_support.rs
@@ -0,0 +1,24 @@
+// Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+pub(crate) const BOND_PAGE_MAX_LIMIT: u32 = 100;
+pub(crate) const BOND_PAGE_DEFAULT_LIMIT: u32 = 50;
+
+// currently the maximum limit before running into memory issue is somewhere between 1150 and 1200
+pub(crate) const DELEGATION_PAGE_MAX_LIMIT: u32 = 750;
+pub(crate) const DELEGATION_PAGE_DEFAULT_LIMIT: u32 = 500;
+
+/// Adds a 0 byte to terminate the `start_after` value given. This allows CosmWasm
+/// to get the succeeding key as the start of the next page.
+// S works for both `String` and `Addr` and that's what we wanted
+pub fn calculate_start_value<S: AsRef<str>>(start_after: Option<S>) -> Option<Vec<u8>> {
+    start_after.as_ref().map(|identity| {
+        identity
+            .as_ref()
+            .as_bytes()
+            .iter()
+            .cloned()
+            .chain(std::iter::once(0))
+            .collect()
+    })
+}

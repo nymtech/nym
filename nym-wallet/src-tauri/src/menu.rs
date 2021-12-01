3c05cf29e6250611b685eb0df112e6ec1556a65c
Tommy Verrall <tommyvez@protonmail.com>
2021-12-01 18:22:07 +0000

    fix duplication

--- nym-wallet/src-tauri/src/menu.rs
+++ nym-wallet/src-tauri/src/menu.rs
@@ -1,6 +1,6 @@
 use tauri::Menu;
 #[cfg(target_os = "macos")]
-use tauri::{Menu, MenuItem, Submenu};
+use tauri::{MenuItem, Submenu};
 
 pub trait AddDefaultSubmenus {
   fn add_default_app_submenu_if_macos(self) -> Self;

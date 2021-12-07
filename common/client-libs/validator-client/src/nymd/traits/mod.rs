f42f76901a9996684f596b6e77473577b1d07b98
Drazen Urch <drazen@urch.eu>
2021-12-07 19:56:10 +0100

    Add VestingExecute and VestingQuery client traits (#944)
    * Add VestingExecute and VestingQuery client traits
    
    * cover ed25519 verification
    
    * cargo fmt
    
    * `Unix` newline-style
    
    * Remove newline force
    
    Co-authored-by: Drazen Urch <durch@users.noreply.guthub.com>

---
+++ common/client-libs/validator-client/src/nymd/traits/mod.rs
@@ -0,0 +1,8 @@
+// Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+mod vesting_query_client;
+mod vesting_signing_client;
+
+pub use vesting_query_client::VestingQueryClient;
+pub use vesting_signing_client::VestingSigningClient;

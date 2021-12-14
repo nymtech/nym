7f63377d22bcf5561d26821fa0ad14fd69cadebc
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2021-12-14 12:33:12 +0200

    Update contract addresses

--- common/network-defaults/src/sandbox.rs
+++ common/network-defaults/src/sandbox.rs
@@ -4,10 +4,10 @@
 pub const BECH32_PREFIX: &str = "nymt";
 pub const DENOM: &str = "unymt";
 
-pub const DEFAULT_MIXNET_CONTRACT_ADDRESS: &str = "tnym14hj2tavq8fpesdwxxcu44rty3hh90vhu5ksfyn";
-pub const DEFAULT_VESTING_CONTRACT_ADDRESS: &str = "tnym17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9epkgzu";
-pub const COSMOS_CONTRACT_ADDRESS: &str = "tnym1nc5tatafv6eyq7llkr2gv50ff9e22mnf3r3jqq";
-pub const REWARDING_VALIDATOR_ADDRESS: &str = "tnym1y2hv6t02f99jws8u6esyhen8jy4x4vq703eqkh";
+pub const DEFAULT_MIXNET_CONTRACT_ADDRESS: &str = "nymt14hj2tavq8fpesdwxxcu44rty3hh90vhuysqrsr";
+pub const DEFAULT_VESTING_CONTRACT_ADDRESS: &str = "nymt1nc5tatafv6eyq7llkr2gv50ff9e22mnfp9pc5s";
+pub const COSMOS_CONTRACT_ADDRESS: &str = "nymt17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9f8xzkv";
+pub const REWARDING_VALIDATOR_ADDRESS: &str = "nymt17zujduc46wvkwvp6f062mm5xhr7jc3fewvqu9e";
 pub const ETH_CONTRACT_ADDRESS: [u8; 20] =
     hex_literal::hex!("9fEE3e28c17dbB87310A51F13C4fbf4331A6f102");
 

c1e809fd99d5be4c8f5bd77b38cf25d92e163b7c
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-12-16 15:35:30 +0000

    Feature/ts client update (#956)
    * Made client compile again + set auto fees
    
    * Simplified client construction by allowing only a single URL
    
    * wip
    
    * Simplified signing assertion
    
    * Initial implementation of queries
    
    * Implemented all basic nymd queries
    
    * Validator API queries
    
    * Signing related queries
    
    * Using default arguments
    
    * Removed redundant else branches
    
    * `eslint` and `prettier` formatting on Typescript validator client
    
    * Removed cyclic import on Coin type
    
    * Missing direct dependencies
    
    * Ingoring cyclic imports
    
    * Removed unused argument
    
    Co-authored-by: Mark Sinclair <mmsinclair@gmail.com>

--- clients/validator/src/types.ts
+++ clients/validator/src/types.ts
@@ -1,125 +1,162 @@
-import { Coin } from "@cosmjs/stargate";
+import { Coin } from '@cosmjs/stargate';
 
+// TODO: ideally we'd have re-exported those using that fancy crate that builds ts types from rust
 
-/// One page of a possible multi-page set of mixnodes. The paging interface is quite
-/// inconvenient, as we don't have the two pieces of information we need to know
-/// in order to do paging nicely (namely `currentPage` and `totalPages` parameters).
-///
-/// Instead, we have only `start_next_page_after`, i.e. the key of the last record
-/// on this page. In order to get the *next* page, CosmWasm looks at that value,
-/// finds the next record, and builds the next page starting there. This happens
-/// **in series** rather than **in parallel** (!).
-///
-/// So we have some consistency problems:
-///
-/// * we can't make requests at a given block height, so the result set
-///    which we assemble over time may change while requests are being made.
-/// * at some point we will make a request for a `start_next_page_after` key
-///   which has just been deleted from the database.
-///
-/// TODO: more robust error handling on the "deleted key" case.
+export type MixnetContractVersion = {
+  build_timestamp: string;
+  build_version: string;
+  commit_sha: string;
+  commit_timestamp: string;
+  commit_branch: string;
+  rustc_version: string;
+};
+
 export type PagedMixnodeResponse = {
-    nodes: MixNodeBond[],
-    per_page: number, // TODO: camelCase
-    start_next_after: string, // TODO: camelCase
-}
+  nodes: MixNodeBond[];
+  per_page: number;
+  start_next_after?: string;
+};
 
-// a temporary way of achieving the same paging behaviour for the gateways
-// the same points made for `PagedResponse` stand here.
 export type PagedGatewayResponse = {
-    nodes: GatewayBond[],
-    per_page: number, // TODO: camelCase
-    start_next_after: string, // TODO: camelCase
-}
+  nodes: GatewayBond[];
+  per_page: number;
+  start_next_after?: string;
+};
 
 export type MixOwnershipResponse = {
-    address: string,
-    has_node: boolean,
-}
+  address: string;
+  mixnode?: MixNodeBond;
+};
 
 export type GatewayOwnershipResponse = {
-    address: string,
-    has_gateway: boolean,
-}
+  address: string;
+  gateway?: GatewayBond;
+};
 
-export type ContractSettingsParams = {
-    epoch_length: number,
-    // ideally I'd want to define those as `number` rather than `string`, but
-    // rust-side they are defined as Uint128 and Decimal that don't have
-    // native javascript representations and therefore are interpreted as strings after deserialization
-    minimum_mixnode_bond: string,
-    minimum_gateway_bond: string,
-    mixnode_bond_reward_rate: string,
-    gateway_bond_reward_rate: string,
-    mixnode_delegation_reward_rate: string,
-    gateway_delegation_reward_rate: string,
-    mixnode_active_set_size: number,
-    gateway_active_set_size: number,
-}
+export type ContractStateParams = {
+  // ideally I'd want to define those as `number` rather than `string`, but
+  // rust-side they are defined as Uint128 and that don't have
+  // native javascript representations and therefore are interpreted as strings after deserialization
+  minimum_mixnode_pledge: string;
+  minimum_gateway_pledge: string;
+  mixnode_rewarded_set_size: number;
+  mixnode_active_set_size: number;
+};
+
+export type RewardingIntervalResponse = {
+  current_rewarding_interval_starting_block: number;
+  current_rewarding_interval_nonce: number;
+  rewarding_in_progress: boolean;
+};
+
+export type LayerDistribution = {
+  gateways: number;
+  layer1: number;
+  layer2: number;
+  layer3: number;
+};
 
 export type Delegation = {
-    owner: string,
-    amount: Coin,
-}
+  owner: string;
+  node_identity: string;
+  amount: Coin;
+  block_height: number;
+  proxy?: string;
+};
 
 export type PagedMixDelegationsResponse = {
-    node_owner: string,
-    delegations: Delegation[],
-    start_next_after: string
-}
+  delegations: Delegation[];
+  start_next_after?: string;
+};
 
-export type PagedGatewayDelegationsResponse = {
-    node_owner: string,
-    delegations: Delegation[],
-    start_next_after: string
-}
+export type PagedDelegatorDelegationsResponse = {
+  delegations: Delegation[];
+  start_next_after?: string;
+};
 
+export type PagedAllDelegationsResponse = {
+  delegations: Delegation[];
+  start_next_after?: [string, string];
+};
+
+export type RewardingResult = {
+  operator_reward: string;
+  total_delegator_reward: string;
+};
+
+export type NodeRewardParams = {
+  period_reward_pool: string;
+  k: string;
+  reward_blockstamp: number;
+  circulating_supply: string;
+  uptime: string;
+  sybil_resistance_percent: number;
+};
+
+export type DelegatorRewardParams = {
+  node_reward_params: NodeRewardParams;
+  sigma: number;
+  profit_margin: number;
+  node_profit: number;
+};
+
+export type PendingDelegatorRewarding = {
+  running_results: RewardingResult;
+  next_start: string;
+  rewarding_params: DelegatorRewardParams;
+};
+
+export type RewardingStatus = { Complete: RewardingResult } | { PendingNextDelegatorPage: PendingDelegatorRewarding };
+
+export type MixnodeRewardingStatusResponse = {
+  status?: RewardingStatus;
+};
 
 export enum Layer {
-    Gateway,
-    One,
-    Two,
-    Three,
+  Gateway,
+  One,
+  Two,
+  Three,
 }
 
-export type MixNodeBond = { // TODO: change name to MixNodeBond
-    owner: string,
-    mix_node: MixNode,    // TODO: camelCase this later once everything else works
-    layer: Layer,
-    bond_amount: Coin,
-    total_delegation: Coin,
-}
+export type MixNodeBond = {
+  owner: string;
+  mix_node: MixNode;
+  layer: Layer;
+  bond_amount: Coin;
+  total_delegation: Coin;
+};
 
 export type MixNode = {
-    host: string,
-    mix_port: number,
-    verloc_port: number,
-    http_api_port: number,
-    sphinx_key: string, // TODO: camelCase this later once everything else works
-    identity_key: string,
-    version: string,
-}
+  host: string;
+  mix_port: number;
+  verloc_port: number;
+  http_api_port: number;
+  sphinx_key: string;
+  identity_key: string;
+  version: string;
+};
 
 export type GatewayBond = {
-    owner: string
-    gateway: Gateway,
+  owner: string;
+  gateway: Gateway;
 
-    bond_amount: Coin,
-    total_delegation: Coin,
-}
+  bond_amount: Coin;
+  total_delegation: Coin;
+};
 
 export type Gateway = {
-    host: string,
-    mix_port: number,
-    clients_port: number,
-    location: string,
-    sphinx_key: string,
-    identity_key: string,
-    version: string
-}
+  host: string;
+  mix_port: number;
+  clients_port: number;
+  location: string;
+  sphinx_key: string;
+  identity_key: string;
+  version: string;
+};
 
 export type SendRequest = {
-    senderAddress: string,
-    recipientAddress: string,
-    transferAmount: readonly Coin[]
-}
+  senderAddress: string;
+  recipientAddress: string;
+  transferAmount: readonly Coin[];
+};

de0bc746e473795bd2a4e590e170cf5ae4747e54
fmtabbara <fmtabbara@hotmail.co.uk>
2021-12-17 11:23:48 +0000

    webpack prod fix

--- nym-wallet/webpack.prod.js
+++ nym-wallet/webpack.prod.js
@@ -1,9 +1,11 @@
 var path = require('path')
+const common = require('./webpack.common')
+const { default: merge } = require('webpack-merge')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'production',
   node: {
     __dirname: false,
   },
   entry: path.resolve(__dirname, './src/index'),
-}
+})

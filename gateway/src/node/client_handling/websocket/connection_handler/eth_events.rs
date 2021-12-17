2b05cdd8eaf0a5ca3cde8eecccf57f34a7c44b70
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2021-12-17 16:17:02 +0200

    Rename contract constant in network defaults

--- gateway/src/node/client_handling/websocket/connection_handler/eth_events.rs
+++ gateway/src/node/client_handling/websocket/connection_handler/eth_events.rs
@@ -19,7 +19,9 @@ use bandwidth_claim_contract::payment::LinkPaymentData;
 use credentials::token::bandwidth::TokenCredential;
 use crypto::asymmetric::identity::{PublicKey, Signature};
 use gateway_client::bandwidth::eth_contract;
-use network_defaults::{COSMOS_CONTRACT_ADDRESS, DENOM, ETH_EVENT_NAME, ETH_MIN_BLOCK_DEPTH};
+use network_defaults::{
+    BANDWIDTH_CLAIM_CONTRACT_ADDRESS, DENOM, ETH_EVENT_NAME, ETH_MIN_BLOCK_DEPTH,
+};
 use validator_client::nymd::{
     AccountId, CosmosCoin, Decimal, Denom, NymdClient, SigningNymdClient,
 };
@@ -42,7 +44,7 @@ impl ERC20Bridge {
             Mnemonic::from_str(&cosmos_mnemonic).expect("Invalid Cosmos mnemonic provided");
         let nymd_client = NymdClient::connect_with_mnemonic(
             nymd_url.as_ref(),
-            AccountId::from_str(COSMOS_CONTRACT_ADDRESS).ok(),
+            AccountId::from_str(BANDWIDTH_CLAIM_CONTRACT_ADDRESS).ok(),
             None,
             mnemonic,
             None,

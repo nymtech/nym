bd892e00bd994d26240d7be23ac585f4951c6835
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2021-12-21 11:16:30 +0200

    Switch to sandbox as default build

--- common/network-defaults/build.rs
+++ common/network-defaults/build.rs
@@ -3,8 +3,8 @@
 
 fn main() {
     match option_env!("NETWORK") {
-        None | Some("milhon") => println!("cargo:rustc-cfg=network=\"milhon\"",),
-        Some("sandbox") => println!("cargo:rustc-cfg=network=\"sandbox\"",),
+        Some("milhon") => println!("cargo:rustc-cfg=network=\"milhon\"",),
+        None | Some("sandbox") => println!("cargo:rustc-cfg=network=\"sandbox\"",),
         Some("qa") => println!("cargo:rustc-cfg=network=\"qa\""),
         _ => panic!("No such network"),
     }

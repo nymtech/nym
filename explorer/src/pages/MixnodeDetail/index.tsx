c64421015223ce6da032035952daa990e62d9774
Mark Sinclair <mmsinclair@gmail.com>
2022-01-07 18:06:25 +0000

    Tidy up mixnode detail page adding new state provider and a guard component to handle loading, error and not found states

--- explorer/src/pages/MixnodeDetail/index.tsx
+++ explorer/src/pages/MixnodeDetail/index.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
-import { Box, Grid, Typography } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  CircularProgress,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { ColumnsType, DetailTable } from 'src/components/DetailTable';
-import { scrollToRef } from 'src/utils';
 import { useParams } from 'react-router-dom';
-import { BondBreakdownTable } from 'src/components/BondBreakdown';
+import { BondBreakdownTable } from 'src/components/MixNodes/BondBreakdown';
 import { ComponentError } from 'src/components/ComponentError';
 import { ContentCard } from 'src/components/ContentCard';
 import { TwoColSmallTable } from 'src/components/TwoColSmallTable';
 import { UptimeChart } from 'src/components/UptimeChart';
 import { WorldMap } from 'src/components/WorldMap';
-import { useMainContext } from 'src/context/main';
-import { MixnodeRowType, mixnodeToGridRow } from '../../components/MixNodes';
 import { MixNodeDetailSection } from '../../components/MixNodes/DetailSection';
+import {
+  MixnodeContextProvider,
+  useMixnodeContext,
+} from '../../context/mixnode';
+import { Title } from '../../components/Title';
 
 const columns: ColumnsType[] = [
   {
@@ -59,176 +68,194 @@ const columns: ColumnsType[] = [
   },
 ];
 
+/**
+ * Wrapper component that adds the mixnode content based on the `id` in the address URL
+ */
 export const PageMixnodeDetail: React.FC = () => {
-  const ref = React.useRef();
-  const [mixNodeRow, setMixNodeRow] = React.useState<
-    MixnodeRowType | undefined
-  >();
-  const {
-    fetchMixnodeById,
-    mixnodeDetailInfo,
-    fetchStatsById,
-    fetchDelegationsById,
-    fetchUptimeStoryById,
-    fetchStatusById,
-    fetchMixnodeDescriptionById,
-    stats,
-    status,
-    uptimeStory,
-    mixnodeDescription,
-  } = useMainContext();
-  const { id }: any = useParams();
+  const { id } = useParams<{ id: string | undefined }>();
 
-  React.useEffect(() => {
-    const hasNoDetail = id && !mixnodeDetailInfo;
-    const hasIncorrectDetail =
-      id &&
-      mixnodeDetailInfo?.data &&
-      mixnodeDetailInfo?.data[0].mix_node.identity_key !== id;
-    if (hasNoDetail || hasIncorrectDetail) {
-      fetchMixnodeById(id);
-      fetchDelegationsById(id);
-      fetchStatsById(id);
-      fetchStatusById(id);
-      fetchUptimeStoryById(id);
-      fetchMixnodeDescriptionById(id);
-    } else if (mixnodeDetailInfo?.data !== undefined) {
-      setMixNodeRow(mixnodeToGridRow(mixnodeDetailInfo?.data)[0]);
-    }
-  }, [id, mixnodeDetailInfo]);
-
-  React.useEffect(() => {
-    scrollToRef(ref);
-  }, [ref]);
+  if (!id) {
+    return (
+      <Alert severity="error">Oh no! No mixnode identity key specified</Alert>
+    );
+  }
 
   return (
-    <>
-      <Box component="main" ref={ref}>
-        <Grid container spacing={2} mt={1} mb={6}>
-          <Grid item xs={12}>
-            {mixNodeRow && mixnodeDescription?.data && (
-              <MixNodeDetailSection
-                mixNodeRow={mixNodeRow}
-                mixnodeDescription={mixnodeDescription.data}
-              />
-            )}
-          </Grid>
-        </Grid>
+    <MixnodeContextProvider mixNodeIdentityKey={id}>
+      <PageMixnodeDetailGuard />
+    </MixnodeContextProvider>
+  );
+};
 
-        <Grid container>
-          <Grid item xs={12}>
-            <DetailTable
-              columnsData={columns}
-              tableName="Mixnode detail table"
-              rows={mixNodeRow ? [mixNodeRow] : []}
+/**
+ * Guard component to handle loading and not found states
+ */
+const PageMixnodeDetailGuard: React.FC = () => {
+  const { mixNode } = useMixnodeContext();
+  const { id } = useParams<{ id: string | undefined }>();
+
+  if (mixNode?.isLoading) {
+    return <CircularProgress />;
+  }
+
+  if (mixNode?.error) {
+    console.error(mixNode?.error);
+    return (
+      <Alert severity="error">
+        Oh no! Could not load mixnode <code>{id || ''}</code>
+      </Alert>
+    );
+  }
+
+  // loaded, but not found
+  if (mixNode && !mixNode.isLoading && !mixNode.data) {
+    return (
+      <Alert severity="warning">
+        <AlertTitle>Mixnode not found</AlertTitle>
+        Sorry, we could not find a mixnode with id <code>{id || ''}</code>
+      </Alert>
+    );
+  }
+
+  return <PageMixnodeDetailWithState />;
+};
+
+/**
+ * Shows mix node details
+ */
+const PageMixnodeDetailWithState: React.FC = () => {
+  const { mixNode, mixNodeRow, description, stats, status, uptimeStory } =
+    useMixnodeContext();
+
+  return (
+    <Box component="main">
+      <Title text="Mixnode Detail" />
+
+      <Grid container spacing={2} mt={1} mb={6}>
+        <Grid item xs={12}>
+          {mixNodeRow && description?.data && (
+            <MixNodeDetailSection
+              mixNodeRow={mixNodeRow}
+              mixnodeDescription={description.data}
             />
-          </Grid>
+          )}
         </Grid>
+      </Grid>
 
-        <Grid container spacing={2} mt={0}>
-          <Grid item xs={12}>
-            <ContentCard title="Bond Breakdown">
-              <BondBreakdownTable />
-            </ContentCard>
-          </Grid>
+      <Grid container>
+        <Grid item xs={12}>
+          <DetailTable
+            columnsData={columns}
+            tableName="Mixnode detail table"
+            rows={mixNodeRow ? [mixNodeRow] : []}
+          />
         </Grid>
+      </Grid>
 
-        <Grid container spacing={2} mt={0}>
-          <Grid item xs={12} md={4}>
-            <ContentCard title="Mixnode Stats">
-              {stats && (
-                <>
-                  {stats.error && (
-                    <ComponentError text="There was a problem retrieving this nodes stats." />
-                  )}
-                  <TwoColSmallTable
-                    loading={stats.isLoading}
-                    error={stats?.error?.message}
-                    title="Since startup"
-                    keys={['Received', 'Sent', 'Explicitly dropped']}
-                    values={[
-                      stats?.data?.packets_received_since_startup || 0,
-                      stats?.data?.packets_sent_since_startup || 0,
-                      stats?.data?.packets_explicitly_dropped_since_startup ||
-                        0,
-                    ]}
-                  />
-                  <TwoColSmallTable
-                    loading={stats.isLoading}
-                    error={stats?.error?.message}
-                    title="Since last update"
-                    keys={['Received', 'Sent', 'Explicitly dropped']}
-                    values={[
-                      stats?.data?.packets_received_since_last_update || 0,
-                      stats?.data?.packets_sent_since_last_update || 0,
-                      stats?.data
-                        ?.packets_explicitly_dropped_since_last_update || 0,
-                    ]}
-                    marginBottom
-                  />
-                </>
-              )}
-              {!stats && <Typography>No stats information</Typography>}
-            </ContentCard>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            {uptimeStory && (
-              <ContentCard title="Uptime story">
-                {uptimeStory.error && (
-                  <ComponentError text="There was a problem retrieving uptime history." />
-                )}
-                <UptimeChart
-                  loading={uptimeStory.isLoading}
-                  xLabel="date"
-                  yLabel="uptime"
-                  uptimeStory={uptimeStory}
-                />
-              </ContentCard>
-            )}
-          </Grid>
+      <Grid container spacing={2} mt={0}>
+        <Grid item xs={12}>
+          <ContentCard title="Bond Breakdown">
+            <BondBreakdownTable />
+          </ContentCard>
         </Grid>
+      </Grid>
 
-        <Grid container spacing={2} mt={0}>
-          <Grid item xs={12} md={4}>
-            {status && (
-              <ContentCard title="Mixnode Status">
-                {status.error && (
-                  <ComponentError text="There was a problem retrieving port information" />
+      <Grid container spacing={2} mt={0}>
+        <Grid item xs={12} md={4}>
+          <ContentCard title="Mixnode Stats">
+            {stats && (
+              <>
+                {stats.error && (
+                  <ComponentError text="There was a problem retrieving this nodes stats." />
                 )}
                 <TwoColSmallTable
-                  loading={status.isLoading}
-                  error={status?.error?.message}
-                  keys={['Mix port', 'Verloc port', 'HTTP port']}
-                  values={[1789, 1790, 8000].map((each) => each)}
-                  icons={
-                    (status?.data?.ports &&
-                      Object.values(status.data.ports)) || [false, false, false]
-                  }
+                  loading={stats.isLoading}
+                  error={stats?.error?.message}
+                  title="Since startup"
+                  keys={['Received', 'Sent', 'Explicitly dropped']}
+                  values={[
+                    stats?.data?.packets_received_since_startup || 0,
+                    stats?.data?.packets_sent_since_startup || 0,
+                    stats?.data?.packets_explicitly_dropped_since_startup || 0,
+                  ]}
                 />
-              </ContentCard>
+                <TwoColSmallTable
+                  loading={stats.isLoading}
+                  error={stats?.error?.message}
+                  title="Since last update"
+                  keys={['Received', 'Sent', 'Explicitly dropped']}
+                  values={[
+                    stats?.data?.packets_received_since_last_update || 0,
+                    stats?.data?.packets_sent_since_last_update || 0,
+                    stats?.data?.packets_explicitly_dropped_since_last_update ||
+                      0,
+                  ]}
+                  marginBottom
+                />
+              </>
             )}
-          </Grid>
-          <Grid item xs={12} md={8}>
-            {mixnodeDetailInfo && (
-              <ContentCard title="Location">
-                {mixnodeDetailInfo?.error && (
-                  <ComponentError text="There was a problem retrieving this mixnode location" />
-                )}
-                {mixnodeDetailInfo.data &&
-                  mixnodeDetailInfo?.data[0]?.location && (
-                    <WorldMap
-                      loading={mixnodeDetailInfo.isLoading}
-                      userLocation={[
-                        mixnodeDetailInfo?.data[0]?.location?.lng,
-                        mixnodeDetailInfo?.data[0]?.location?.lat,
-                      ]}
-                    />
-                  )}
-              </ContentCard>
-            )}
-          </Grid>
+            {!stats && <Typography>No stats information</Typography>}
+          </ContentCard>
         </Grid>
-      </Box>
-    </>
+        <Grid item xs={12} md={8}>
+          {uptimeStory && (
+            <ContentCard title="Uptime story">
+              {uptimeStory.error && (
+                <ComponentError text="There was a problem retrieving uptime history." />
+              )}
+              <UptimeChart
+                loading={uptimeStory.isLoading}
+                xLabel="date"
+                yLabel="uptime"
+                uptimeStory={uptimeStory}
+              />
+            </ContentCard>
+          )}
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} mt={0}>
+        <Grid item xs={12} md={4}>
+          {status && (
+            <ContentCard title="Mixnode Status">
+              {status.error && (
+                <ComponentError text="There was a problem retrieving port information" />
+              )}
+              <TwoColSmallTable
+                loading={status.isLoading}
+                error={status?.error?.message}
+                keys={['Mix port', 'Verloc port', 'HTTP port']}
+                values={[1789, 1790, 8000].map((each) => each)}
+                icons={
+                  (status?.data?.ports && Object.values(status.data.ports)) || [
+                    false,
+                    false,
+                    false,
+                  ]
+                }
+              />
+            </ContentCard>
+          )}
+        </Grid>
+        <Grid item xs={12} md={8}>
+          {mixNode && (
+            <ContentCard title="Location">
+              {mixNode?.error && (
+                <ComponentError text="There was a problem retrieving this mixnode location" />
+              )}
+              {mixNode.data && mixNode?.data?.location && (
+                <WorldMap
+                  loading={mixNode.isLoading}
+                  userLocation={[
+                    mixNode?.data?.location?.lng,
+                    mixNode?.data?.location?.lat,
+                  ]}
+                />
+              )}
+            </ContentCard>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
   );
 };

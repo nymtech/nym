6e3773a095fa70b43cb5f725d804b9008062cbd8
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2022-01-11 16:05:15 +0100

    Feature/remove unused profit margin (#1011)
    * Remove unused field, to avoid confusion
    
    * Add migration code
    
    * Update tests
    
    * Make clippy happy

--- contracts/mixnet/src/support/tests/fixtures.rs
+++ contracts/mixnet/src/support/tests/fixtures.rs
@@ -59,7 +59,6 @@ pub(crate) fn stored_mixnode_bond_fixture(owner: &str) -> mixnodes_storage::Stor
             ..super::fixtures::mix_node_fixture()
         },
         None,
-        None,
     )
 }
 

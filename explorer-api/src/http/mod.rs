99a984781941ee8a52574953d9d77a6faf1860b5
Mark Sinclair <mmsinclair@gmail.com>
2022-01-12 14:16:39 +0000

    Make clippy happy

--- explorer-api/src/http/mod.rs
+++ explorer-api/src/http/mod.rs
@@ -89,7 +89,7 @@ fn get_servers() -> Vec<rocket_okapi::okapi::openapi3::Server> {
         return vec![];
     }
     return vec![rocket_okapi::okapi::openapi3::Server {
-        url: std::env::var("OPEN_API_BASE").unwrap_or("/api/v1/".to_owned()),
+        url: std::env::var("OPEN_API_BASE").unwrap_or_else(|_| "/api/v1/".to_owned()),
         description: Some("API".to_owned()),
         ..Default::default()
     }];

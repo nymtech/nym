6d3eae9f4e87a26628ad105ddbb7cd8121249482
Mark Sinclair <mmsinclair@gmail.com>
2022-01-17 13:47:14 +0000

    Fix navbar colour

--- explorer/src/theme/theme.ts
+++ explorer/src/theme/theme.ts
@@ -100,7 +100,7 @@ const networkExplorerPalette = (
     },
     topNav: {
       ...variant.topNav,
-      appBar: '#070B15',
+      appBar: '#080715',
       socialIcons: '#F2F2F2',
     },
     footer: {

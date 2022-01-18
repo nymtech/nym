3a39fff0069ab616e1a2369ef12e84a5ab59cdac
fmtabbara <fmtabbara@hotmail.co.uk>
2022-01-18 10:16:36 +0000

    PR updates

--- nym-wallet/src/pages/unbond/index.tsx
+++ nym-wallet/src/pages/unbond/index.tsx
@@ -22,36 +22,38 @@ export const Unbond = () => {
   return (
     <Layout>
       <NymCard title="Unbond" subheader="Unbond a mixnode or gateway" noPadding Icon={UnbondIcon}>
-        {ownership?.hasOwnership && (
-          <Alert
-            severity="info"
-            data-testid="bond-noded"
-            action={
-              <Button
-                data-testid="un-bond"
-                disabled={isLoading}
-                onClick={async () => {
-                  setIsLoading(true)
-                  await unbond(ownership.nodeType)
-                  await userBalance.fetchBalance()
-                  await getBondDetails()
-                  await checkOwnership()
-                  setIsLoading(false)
-                }}
-                color="inherit"
-              >
-                Unbond
-              </Button>
-            }
-            sx={{ m: 2 }}
-          >
-            {`Looks like you already have a ${ownership.nodeType} bonded.`}
-          </Alert>
-        )}
-        <Box sx={{ p: 3 }}>
-          <Fee feeType="UnbondMixnode" />
-        </Box>
-        {!ownership.hasOwnership && (
+        {ownership?.hasOwnership ? (
+          <>
+            <Alert
+              severity="info"
+              data-testid="bond-noded"
+              action={
+                <Button
+                  data-testid="un-bond"
+                  disabled={isLoading}
+                  onClick={async () => {
+                    setIsLoading(true)
+                    await unbond(ownership.nodeType)
+                    await userBalance.fetchBalance()
+                    await getBondDetails()
+                    await checkOwnership()
+                    setIsLoading(false)
+                  }}
+                  color="inherit"
+                >
+                  Unbond
+                </Button>
+              }
+              sx={{ m: 2 }}
+            >
+              {`Looks like you already have a ${ownership.nodeType} bonded.`}
+            </Alert>
+
+            <Box sx={{ p: 3 }}>
+              <Fee feeType="UnbondMixnode" />
+            </Box>
+          </>
+        ) : (
           <Alert severity="info" sx={{ m: 3 }} data-testid="no-bond">
             You don't currently have a bonded node
           </Alert>

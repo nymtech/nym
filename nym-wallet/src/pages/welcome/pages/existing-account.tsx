15a4cf3ccfffff8367298aad476a6f22c08a6c9c
fmtabbara <fmtabbara@hotmail.co.uk>
2022-01-25 10:27:09 +0000

    finish exisiting account login

--- nym-wallet/src/pages/welcome/pages/existing-account.tsx
+++ nym-wallet/src/pages/welcome/pages/existing-account.tsx
@@ -1,6 +1,44 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useContext, useState } from 'react'
+import { Alert, Button, Stack, TextField } from '@mui/material'
+import { Subtitle } from '../components'
+import { ClientContext } from '../../../context/main'
+import { signInWithMnemonic } from '../../../requests'
 
-export const ExistingAccount: React.FC<{ page: "existing account"}> = () => {
-  return <Typography>Existing account</Typography>
+export const ExistingAccount: React.FC<{ page: 'existing account' }> = () => {
+  const [mnemonic, setMnemonic] = useState<string>()
+  const [inputError, setInputError] = useState<string>()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { logIn } = useContext(ClientContext)
+
+  const handleSignIn = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+
+    setIsLoading(true)
+    setInputError(undefined)
+
+    try {
+      const res = await signInWithMnemonic(mnemonic || '')
+      setIsLoading(false)
+      logIn(res)
+    } catch (e: any) {
+      setIsLoading(false)
+      setInputError(e)
+    }
+  }
+
+  return (
+    <Stack spacing={3} sx={{ width: 400 }} alignItems="center">
+      <Subtitle subtitle="Enter your mnemonic from existing wallet" />
+      <TextField value={mnemonic} onChange={(e) => setMnemonic(e.target.value)} multiline rows={5} fullWidth />
+      {inputError && (
+        <Alert severity="error" variant="outlined" data-testid="error" sx={{ color: 'error.light', width: '100%' }}>
+          {inputError}
+        </Alert>
+      )}
+      <Button variant="contained" size="large" fullWidth onClick={handleSignIn}>
+        Next
+      </Button>
+    </Stack>
+  )
 }

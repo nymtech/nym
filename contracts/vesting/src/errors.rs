4f1ed4b4e3f4866d2945157b015eb13422e8e59e
durch <durch@users.noreply.github.com>
2022-01-28 14:13:26 +0100

    Tag contract errors, and print out lines for easier QA

--- contracts/vesting/src/errors.rs
+++ contracts/vesting/src/errors.rs
@@ -4,42 +4,42 @@ use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
-    #[error("{0}")]
+    #[error("VESTING ({}): {0}", line!())]
     Std(#[from] StdError),
-    #[error("Account does not exist - {0}")]
+    #[error("VESTING ({}): Account does not exist - {0}", line!())]
     NoAccountForAddress(String),
-    #[error("Only admin can perform this action, {0} is not admin")]
+    #[error("VESTING ({}): Only admin can perform this action, {0} is not admin", line!())]
     NotAdmin(String),
-    #[error("Balance not found for existing account ({0}), this is a bug")]
+    #[error("VESTING ({}): Balance not found for existing account ({0}), this is a bug", line!())]
     NoBalanceForAddress(String),
-    #[error("Insufficient balance for address {0} -> {1}")]
+    #[error("VESTING ({}): Insufficient balance for address {0} -> {1}", line!())]
     InsufficientBalance(String, u128),
-    #[error("Insufficient spendable balance for address {0} -> {1}")]
+    #[error("VESTING ({}): Insufficient spendable balance for address {0} -> {1}", line!())]
     InsufficientSpendable(String, u128),
     #[error(
-        "Only delegation owner can perform delegation actions, {0} is not the delegation owner"
-    )]
+        "VESTING ({}):Only delegation owner can perform delegation actions, {0} is not the delegation owner"
+    , line!())]
     NotDelegate(String),
-    #[error("Total vesting amount is inprobably low -> {0}, this is likely an error")]
+    #[error("VESTING ({}): Total vesting amount is inprobably low -> {0}, this is likely an error", line!())]
     ImprobableVestingAmount(u128),
-    #[error("Address {0} has already bonded a node")]
+    #[error("VESTING ({}): Address {0} has already bonded a node", line!())]
     AlreadyBonded(String),
-    #[error("Received empty funds vector")]
+    #[error("VESTING ({}): Received empty funds vector", line!())]
     EmptyFunds,
-    #[error("Received wrong denom: {0}, expected {1}")]
+    #[error("VESTING ({}): Received wrong denom: {0}, expected {1}", line!())]
     WrongDenom(String, String),
-    #[error("Received multiple denoms, expected 1")]
+    #[error("VESTING ({}): Received multiple denoms, expected 1", line!())]
     MultipleDenoms,
-    #[error("No delegations found for account {0}, mix_identity {1}")]
+    #[error("VESTING ({}): No delegations found for account {0}, mix_identity {1}", line!())]
     NoSuchDelegation(Addr, IdentityKey),
-    #[error("Only mixnet contract can perform this operation, got {0}")]
+    #[error("VESTING ({}): Only mixnet contract can perform this operation, got {0}", line!())]
     NotMixnetContract(Addr),
-    #[error("Calculation underflowed")]
+    #[error("VESTING ({}): Calculation underflowed", line!())]
     Underflow,
-    #[error("No bond found for account {0}")]
+    #[error("VESTING ({}): No bond found for account {0}", line!())]
     NoBondFound(String),
-    #[error("Action can only be executed by account owner -> {0}")]
+    #[error("VESTING ({}): Action can only be executed by account owner -> {0}", line!())]
     NotOwner(String),
-    #[error("Invalid address: {0}")]
+    #[error("VESTING ({}): Invalid address: {0}", line!())]
     InvalidAddress(String),
 }

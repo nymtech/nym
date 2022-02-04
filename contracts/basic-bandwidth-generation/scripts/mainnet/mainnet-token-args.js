1622286ee31f67fb89628608a3b93aac8404001d
mfahampshire <mfahampshire@gmail.com>
2022-02-04 17:16:25 +0100

    removed old address

--- contracts/basic-bandwidth-generation/scripts/mainnet/mainnet-token-args.js
+++ contracts/basic-bandwidth-generation/scripts/mainnet/mainnet-token-args.js
@@ -1,7 +1,7 @@
 // arguments for verification of gravity contract via hardhat-etherscan plugin
 // npx hardhat verify --constructor-args ./scripts/mainnet/mainnet-token-args.js  --network mainnet 0xCf6DeE9947fdDc958985E5283e63d41CBC83Ff61
 module.exports = [
-    "0xa4108aA1Ec4967F8b52220a4f7e94A8201F2D906",
+    "",
     "NYM",
     "NYM",
     6

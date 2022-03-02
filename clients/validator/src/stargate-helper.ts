b966f962c81e37f96d07299cb5b8800c0400814b
Tommy Verrall <tommyvez@protonmail.com>
2022-03-02 17:07:15 +0000

    allow main-net prefix and denom to work

--- clients/validator/src/stargate-helper.ts
+++ clients/validator/src/stargate-helper.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import { GasPrice } from '@cosmjs/stargate';
 
+const mainnetPrefix = 'n';
+const mainnetDenom = 'nym';
+
 export function nymGasPrice(prefix: string): GasPrice {
-  return GasPrice.fromString(`0.025u${prefix}`); // TODO: ideally this ugly conversion shouldn't be hardcoded here.
+  if (typeof prefix === 'string') {
+    if (prefix === mainnetPrefix) {
+      prefix = mainnetDenom;
+    }
+    return GasPrice.fromString(`0.025u${prefix}`); // TODO: ideally this ugly conversion shouldn't be hardcoded here.
+  }
+  else {
+    throw new Error(`${prefix} is not of type string`);
+  }
 }
 
 export const downloadWasm = async (url: string): Promise<Uint8Array> => {

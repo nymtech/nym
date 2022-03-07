48c1fcaf9365f6f491fc2aa60336432f1812dce1
Mark Sinclair <mmsinclair@gmail.com>
2022-03-07 18:54:12 +0000

    Add network selector component and storybook support for Nym themes

--- ts-packages/react-components/.storybook/preview.js
+++ ts-packages/react-components/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { NymThemeProvider } from '@nymproject/mui-theme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,27 @@ export const parameters = {
     },
   },
 }
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
+
+const withThemeProvider=(Story,context)=>{
+  return (
+    <NymThemeProvider mode={context.globals.theme}>
+      <Story {...context} />
+    </NymThemeProvider>
+  )
+}
+export const decorators = [withThemeProvider];

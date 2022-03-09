dcdc367a39d819c3c24a44719a89f2478bb8e9bf
Mark Sinclair <mmsinclair@gmail.com>
2022-03-09 18:58:14 +0000

    Add error boundary

--- explorer/src/index.tsx
+++ explorer/src/index.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import { App } from './App';
 import { MainContextProvider } from './context/main';
 import './styles.css';
 import { NetworkExplorerThemeProvider } from './theme';
+import { ErrorBoundaryContent } from './errors/ErrorBoundaryContent';
 
 ReactDOM.render(
-  <MainContextProvider>
-    <NetworkExplorerThemeProvider>
-      <Router>
-        <App />
-      </Router>
-    </NetworkExplorerThemeProvider>
-  </MainContextProvider>,
+  <ErrorBoundary FallbackComponent={ErrorBoundaryContent}>
+    <MainContextProvider>
+      <NetworkExplorerThemeProvider>
+        <Router>
+          <App />
+        </Router>
+      </NetworkExplorerThemeProvider>
+    </MainContextProvider>
+  </ErrorBoundary>,
   document.getElementById('app'),
 );

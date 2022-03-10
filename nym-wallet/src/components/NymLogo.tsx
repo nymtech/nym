6b951095a5b8656f803c906131e31f8cc20ebf1e
Mark Sinclair <mmsinclair@gmail.com>
2022-03-10 14:39:34 +0000

    Use shared logo component

--- nym-wallet/src/components/NymLogo.tsx
+++ nym-wallet/src/components/NymLogo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../images/logo-background.svg';
+import { NymLogo as NymLogoReact } from '@nymproject/react';
 
 const imgSize = {
   small: 40,
@@ -7,4 +7,6 @@ const imgSize = {
   large: 120,
 };
 
-export const NymLogo = ({ size = 'medium' }: { size?: 'small' | 'medium' | 'large' }) => <Logo width={imgSize[size]} />;
+export const NymLogo = ({ size = 'medium' }: { size?: 'small' | 'medium' | 'large' }) => (
+  <NymLogoReact width={imgSize[size]} />
+);

bcd5531f4076a1e241af188d6151ca03b4bd16c4
fmtabbara <fmtabbara@users.noreply.github.com>
2022-03-10 15:36:02 +0000

    [ci skip] Generate TS types

--- nym-wallet/src/types/rust/balance.ts
+++ nym-wallet/src/types/rust/balance.ts
@@ -1,6 +1,3 @@
-import type { Coin } from './coin';
+import type { Coin } from "./coin";
 
-export interface Balance {
-  coin: Coin;
-  printable_balance: string;
-}
+export interface Balance { coin: Coin, printable_balance: string, }
--- nym-wallet/src/types/rust/pledgedata.ts
+++ nym-wallet/src/types/rust/pledgedata.ts
@@ -1,6 +1,3 @@
-import type { Coin } from './coin';
+import type { Coin } from "./coin";
 
-export interface PledgeData {
-  amount: Coin;
-  block_time: bigint;
-}
+export interface PledgeData { amount: Coin, block_time: bigint, }

fe02bc46311f9fa867af4b4aab76d2b45c7c0916
Jon Häggblad <jon.haggblad@gmail.com>
2022-03-15 15:04:34 +0100

    wallet: add logging

--- nym-wallet/src-tauri/src/main.rs
+++ nym-wallet/src-tauri/src/main.rs
@@ -29,6 +29,8 @@ use crate::operations::vesting;
 use crate::state::State;
 
 fn main() {
+  setup_logging();
+
   tauri::Builder::default()
     .manage(Arc::new(RwLock::new(State::default())))
     .invoke_handler(tauri::generate_handler![
@@ -86,4 +88,27 @@ fn main() {
     .menu(Menu::new().add_default_app_submenu_if_macos())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+fn setup_logging() {
+  let mut log_builder = pretty_env_logger::formatted_timed_builder();
+  if let Ok(s) = ::std::env::var("RUST_LOG") {
+    log_builder.parse_filters(&s);
+  } else {
+    // default to 'Info'
+    log_builder.filter(None, log::LevelFilter::Info);
+  }
+
+  log_builder
+    .filter_module("hyper", log::LevelFilter::Warn)
+    .filter_module("tokio_reactor", log::LevelFilter::Warn)
+    .filter_module("reqwest", log::LevelFilter::Warn)
+    .filter_module("mio", log::LevelFilter::Warn)
+    .filter_module("want", log::LevelFilter::Warn)
+    .filter_module("sled", log::LevelFilter::Warn)
+    .filter_module("tungstenite", log::LevelFilter::Warn)
+    .filter_module("tokio_tungstenite", log::LevelFilter::Warn)
+    .filter_module("rustls", log::LevelFilter::Warn)
+    .filter_module("tokio_util", log::LevelFilter::Warn)
+    .init();
 }

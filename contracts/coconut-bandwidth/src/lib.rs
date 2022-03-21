26d8add24631a5aa28090ac4508a0170539a1324
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2022-03-21 16:32:26 +0200

    Activate instantiate test and remove unused code

--- contracts/coconut-bandwidth/src/lib.rs
+++ contracts/coconut-bandwidth/src/lib.rs
@@ -7,12 +7,10 @@ mod error;
 mod support;
 mod transactions;
 
-use cosmwasm_std::{
-    entry_point, to_binary, Deps, DepsMut, Env, MessageInfo, QueryResponse, Response,
-};
+use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response};
 
 use crate::error::ContractError;
-use coconut_bandwidth_contract::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use coconut_bandwidth_contract::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg};
 
 /// Instantiate the contract.
 ///
@@ -43,61 +41,35 @@ pub fn execute(
 }
 
 #[entry_point]
-pub fn query(_deps: Deps<'_>, _env: Env, _msg: QueryMsg) -> Result<QueryResponse, ContractError> {
-    // let query_res = match msg {
-    //     QueryMsg::GetPayments { start_after, limit } => {
-    //         to_binary(&queries::query_payments_paged(deps, start_after, limit)?)
-    //     }
-    // };
-    //
-    // Ok(query_res?)
-    Ok(to_binary("")?)
-}
-
-#[entry_point]
 pub fn migrate(_deps: DepsMut<'_>, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
     Ok(Default::default())
 }
 
-// #[cfg(test)]
-// pub mod tests {
-//     use super::*;
-//     use bandwidth_claim_contract::payment::PagedPaymentResponse;
-//     use config::defaults::DENOM;
-//     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-//     use cosmwasm_std::{coins, from_binary};
-//
-//     #[test]
-//     fn initialize_contract() {
-//         let mut deps = mock_dependencies();
-//         let env = mock_env();
-//         let msg = InstantiateMsg {};
-//         let info = mock_info("creator", &[]);
-//
-//         let res = instantiate(deps.as_mut(), env.clone(), info, msg).unwrap();
-//         assert_eq!(0, res.messages.len());
-//
-//         // payments should be empty after initialization
-//         let res = query(
-//             deps.as_ref(),
-//             env.clone(),
-//             QueryMsg::GetPayments {
-//                 start_after: None,
-//                 limit: Option::from(2),
-//             },
-//         )
-//         .unwrap();
-//         let page: PagedPaymentResponse = from_binary(&res).unwrap();
-//         assert_eq!(0, page.payments.len()); // there are no payments in the list when it's just been initialized
-//
-//         // Contract balance should match what we initialized it as
-//         assert_eq!(
-//             coins(0, DENOM),
-//             vec![deps
-//                 .as_ref()
-//                 .querier
-//                 .query_balance(env.contract.address, DENOM)
-//                 .unwrap()]
-//         );
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use config::defaults::DENOM;
+    use cosmwasm_std::coins;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+
+    #[test]
+    fn initialize_contract() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let msg = InstantiateMsg {};
+        let info = mock_info("creator", &[]);
+
+        let res = instantiate(deps.as_mut(), env.clone(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+
+        // Contract balance should be 0
+        assert_eq!(
+            coins(0, DENOM),
+            vec![deps
+                .as_ref()
+                .querier
+                .query_balance(env.contract.address, DENOM)
+                .unwrap()]
+        );
+    }
+}

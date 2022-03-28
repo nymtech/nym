7443acf3833eba61d086b01e73276708d73f2503
durch <durch@users.noreply.github.com>
2022-03-28 13:05:34 +0200

    Different values for mixes and gateways

--- validator-api/src/config/template.rs
+++ validator-api/src/config/template.rs
@@ -21,7 +21,8 @@ mixnet_contract_address = '{{ base.mixnet_contract_address }}'
 [network_monitor]
 
 # Mixnodes and gateways with relialability lower the this get blacklisted by network monitor, get no traffic and cannot be selected into a rewarded set.
-min_reliability = {{ network_monitor.min_reliability }}
+min_mixnode_reliability = {{ network_monitor.min_mixnode_reliability }} # deafults to 50
+min_gateway_reliability = {{ network_monitor.min_gateway_reliability }} # defaults to 20
 
 # Specifies whether network monitoring service is enabled in this process.
 enabled = {{ network_monitor.enabled }}

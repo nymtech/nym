3e549d76e41e5ce87b78ae011d31a7662526d301
Mark Sinclair <mmsinclair@gmail.com>
2022-03-29 16:47:39 +0100

    Add `fill` prop to Nym wordmark component

--- ts-packages/react-components/src/components/logo/NymWordmark.tsx
+++ ts-packages/react-components/src/components/logo/NymWordmark.tsx
@@ -3,7 +3,7 @@ import Wordmark from '@assets/logo/logo-wordmark.svg';
 import { useTheme } from '@mui/material';
 import { LogoProps } from './LogoProps';
 
-export const NymWordmark: React.FC<LogoProps> = ({ height, width }) => {
+export const NymWordmark: React.FC<LogoProps & { fill?: string }> = ({ height, width, fill }) => {
   const theme = useTheme();
-  return <Wordmark height={height} width={width} fill={theme.palette.text.primary} />;
+  return <Wordmark height={height} width={width} fill={fill || theme.palette.text.primary} />;
 };

abf7e1f6ada9dabbefd337f3c90fe4bf2648cfdc
Mark Sinclair <mmsinclair@gmail.com>
2022-03-29 17:17:25 +0100

    Bundle fonts into wallet

--- nym-wallet/src/theme/index.tsx
+++ nym-wallet/src/theme/index.tsx
@@ -3,19 +3,14 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { getDesignTokens } from './theme';
 import { ClientContext } from '../context/main';
+import { NymWalletThemeWithMode } from './NymWalletTheme';
 
 /**
  * Provides the theme for the Network Explorer by reacting to the light/dark mode choice stored in the app context.
  */
 export const NymWalletTheme: React.FC = ({ children }) => {
   const { mode } = useContext(ClientContext);
-  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-  );
+  return <NymWalletThemeWithMode mode={mode}>{children}</NymWalletThemeWithMode>;
 };
 
 export const WelcomeTheme: React.FC = ({ children }) => {

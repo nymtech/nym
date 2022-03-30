7a306cf8479067d42f561ebc0220c8b76405d926
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-03-30 15:10:53 +0100

    Benchmark for g2 precomputation

--- common/crypto/dkg/benches/benchmarks.rs
+++ common/crypto/dkg/benches/benchmarks.rs
@@ -1,3 +1,4 @@
+use bls12_381::{G2Affine, G2Prepared};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use dkg::bte::encryption::BabyStepGiantStepLookup;
 
@@ -7,7 +8,18 @@ pub fn precompute_default_bsgs_table(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, precompute_default_bsgs_table);
+pub fn precomputing_g2_generator_for_miller_loop(c: &mut Criterion) {
+    let g2 = G2Affine::generator();
+    c.bench_function("bsgs default table", |b| {
+        b.iter(|| black_box(G2Prepared::from(g2)))
+    });
+}
+
+criterion_group!(
+    benches,
+    precompute_default_bsgs_table,
+    precomputing_g2_generator_for_miller_loop
+);
 criterion_main!(benches);
 
 // TODO: benchmark using affine vs projective representation throughout the crate

72825a2ad388f913f0b3a666bf45b551ca4524a9
gala1234 <calero.vg@gmail.com>
2022-03-30 13:40:02 +0200

    start validators fetching

--- nym-wallet/src/requests/index.ts
+++ nym-wallet/src/requests/index.ts
@@ -5,3 +5,4 @@ export * from './contract';
 export * from './vesting';
 export * from './network';
 export * from './queries';
+// export * from './validators';
---
+++ nym-wallet/src/requests/validators.ts
@@ -0,0 +1,11 @@
+//TODO once merge branch feature/wallet-expose-validator-urls
+
+// import {
+//     ValidatorUrls
+// } from '../types';
+
+// export const getValidators = async (): Promise<ValidatorUrls> => {
+//     const ValidatorUrls: ValidatorUrls = await invoke('locked_coins');
+//     const major = await minorToMajor(coin.amount);
+//     return major;
+//   };

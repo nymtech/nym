2bebb4b0c2b84a2783071034c09fcf12769d9c51
fmtabbara <fmtabbara@hotmail.co.uk>
2022-04-13 18:07:31 +0100

    remove unused component

--- nym-wallet/src/components/Accounts/AccountColor.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { CircleTwoTone } from '@mui/icons-material';
-import stc from 'string-to-color';
-
-export const AccountColor = ({ address }: { address: string }) => <CircleTwoTone sx={{ color: stc(address) }} />;

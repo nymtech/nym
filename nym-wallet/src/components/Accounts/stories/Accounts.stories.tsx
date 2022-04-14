3250d6982e22d9160452d94877060b50f5c87760
fmtabbara <fmtabbara@hotmail.co.uk>
2022-04-14 14:31:55 +0100

    refactor to separate logic from view

--- nym-wallet/src/components/Accounts/stories/Accounts.stories.tsx
+++ nym-wallet/src/components/Accounts/stories/Accounts.stories.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Accounts } from 'src/components/Accounts';
+
 import { v4 as uuid4 } from 'uuid';
+import { AccountsContainer } from '../AccountContainer';
 
 export default {
   title: 'Wallet / Multi Account',
-  component: Accounts,
-} as ComponentMeta<typeof Accounts>;
+  component: AccountsContainer,
+} as ComponentMeta<typeof AccountsContainer>;
 
-const Template: ComponentStory<typeof Accounts> = (args) => (
+const Template: ComponentStory<typeof AccountsContainer> = (args) => (
   <Box display="flex" alignContent="center">
-    <Accounts {...args} />
+    <AccountsContainer {...args} />
   </Box>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  storedAccounts: [{ name: 'Account 1', address: uuid4() }],
-};
-
-export const MultipleAccounts = Template.bind({});
-MultipleAccounts.args = {
   storedAccounts: [
     { name: 'Account 1', address: uuid4() },
     { name: 'Account 2', address: uuid4() },

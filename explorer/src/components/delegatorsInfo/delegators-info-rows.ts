733dc377d6b6fe9a509e86e966f5eb65ca1d2407
Mark Sinclair <mmsinclair@gmail.com>
2022-04-25 18:08:40 +0100

    Fix up formatting and types

--- explorer/src/components/delegatorsInfo/delegators-info-rows.ts
+++ explorer/src/components/delegatorsInfo/delegators-info-rows.ts
@@ -1,37 +1,37 @@
 import { currencyToString } from '../../utils/currency';
 import { useMixnodeContext } from '../../context/mixnode';
+import { DelegatorsInfoRowWithIndex } from './types';
 
-export const delegatorsInfoRows: any = () => {
+export const delegatorsInfoRows = (): DelegatorsInfoRowWithIndex => {
+  const { economicDynamicsStats, mixNode } = useMixnodeContext();
 
-    const { economicDynamicsStats, mixNode } = useMixnodeContext();
+  const estimatedNodeRewards = economicDynamicsStats?.data?.estimated_total_node_reward || 0;
+  const estimatedOperatorRewards = economicDynamicsStats?.data?.estimated_operator_reward || 0;
+  const activeSetProbability = economicDynamicsStats?.data?.active_set_inclusion_probability || 0;
+  const stakeSaturation = economicDynamicsStats?.data?.stake_saturation || 0;
+  const profitMargin = mixNode?.data?.mix_node.profit_margin_percent || 0;
 
-    const estimatedNodeRewards = economicDynamicsStats?.data?.estimated_total_node_reward || 0;
-    const estimatedOperatorRewards = economicDynamicsStats?.data?.estimated_operator_reward || 0;
-    const activeSetProbability = economicDynamicsStats?.data?.active_set_inclusion_probability || 0;
-    const stakeSaturation = economicDynamicsStats?.data?.stake_saturation || 0;
-    const profitMargin = mixNode?.data?.mix_node.profit_margin_percent || 0;
-
-
-    return ({
+  return {
     id: 1,
     estimated_total_reward: {
-        value: currencyToString(estimatedNodeRewards.toString()),
+      value: currencyToString(estimatedNodeRewards.toString()),
     },
     estimated_operator_reward: {
-        value: currencyToString(estimatedOperatorRewards.toString()),
+      value: currencyToString(estimatedOperatorRewards.toString()),
     },
     active_set_probability: {
-        value: `${(activeSetProbability * 100).toFixed(2)} %`,
-        visualProgressValue: activeSetProbability,
+      value: `${(activeSetProbability * 100).toFixed(2)} %`,
+      visualProgressValue: activeSetProbability,
     },
     stake_saturation: {
-        value: `${(stakeSaturation * 100).toFixed(2)} %`,
-        visualProgressValue: stakeSaturation,
+      value: `${(stakeSaturation * 100).toFixed(2)} %`,
+      visualProgressValue: stakeSaturation,
     },
     profit_margin: {
-        value: `${profitMargin} %`,
+      value: `${profitMargin} %`,
     },
     avg_uptime: {
-        value: economicDynamicsStats?.data?.current_interval_uptime,
-    }
-})}
+      value: economicDynamicsStats?.data?.current_interval_uptime,
+    },
+  };
+};
--- explorer/src/components/delegatorsInfo/delegators-info-table.tsx
+++ explorer/src/components/delegatorsInfo/delegators-info-table.tsx
@@ -1,109 +1,123 @@
 import * as React from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Typography } from '@mui/material';
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
-import { useTheme, Theme, styled } from '@mui/material/styles';
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import { styled, Theme, useTheme } from '@mui/material/styles';
+import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip';
 import LinearProgress from '@mui/material/LinearProgress';
 import { cellStyles } from '../Universal-DataGrid';
 import { InfoSVG } from '../../icons/InfoSVG';
-import { ColumnsType, RowsType, UniversalTableProps } from './types';
+import { DelegatorsInfoRowWithIndex, RowsType } from './types';
+import { ColumnsType, UniversalTableProps } from '../DetailTable';
 
 const tooltipBackGroundColor = '#A0AED1';
 
 const formatCellValues = (val: RowsType, field: string, theme: Theme) => {
-    if (val.visualProgressValue) {
-        return (
-            <Box sx={{ display: 'flex', alignItems: 'center' }} id='field' >
-                <Typography sx={{ mr: 1, fontWeight: '600', fontSize: '12px' }}>
-                    {val.value}
-                </Typography>
-                <LinearProgress variant="determinate" value={val.visualProgressValue} color='inherit' sx={{ width: '100px', borderRadius: '5px', backgroundColor: theme.palette.nym.networkExplorer.nav.text }} />
-            </Box>
-        )
-    }
+  if (val.visualProgressValue) {
     return (
-            <Typography sx={{ mr: 1, fontWeight: '600', fontSize: '12px' }} id={field} >
-                {val.value}
-            </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center' }} id="field">
+        <Typography sx={{ mr: 1, fontWeight: '600', fontSize: '12px' }}>{val.value}</Typography>
+        <LinearProgress
+          variant="determinate"
+          value={val.visualProgressValue}
+          color="inherit"
+          sx={{ width: '100px', borderRadius: '5px', backgroundColor: theme.palette.nym.networkExplorer.nav.text }}
+        />
+      </Box>
     );
-}
+  }
+  return (
+    <Typography sx={{ mr: 1, fontWeight: '600', fontSize: '12px' }} id={field}>
+      {val.value}
+    </Typography>
+  );
+};
 
 export const DelegatorsInfoTable: React.FC<{
-    tableName: string;
-    columnsData: ColumnsType[];
-    rows: [];
+  tableName: string;
+  columnsData: ColumnsType[];
+  rows: DelegatorsInfoRowWithIndex[];
 }> = ({ tableName, columnsData, rows }: UniversalTableProps) => {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
-        <Tooltip {...props} classes={{ popper: className }} />
-    ))({
-        [`& .${tooltipClasses.tooltip}`]: {
-            maxWidth: 230,
-            background: tooltipBackGroundColor,
-            color: theme.palette.nym.networkExplorer.nav.hover,
-        },
-    });
+  const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))({
+    [`& .${tooltipClasses.tooltip}`]: {
+      maxWidth: 230,
+      background: tooltipBackGroundColor,
+      color: theme.palette.nym.networkExplorer.nav.hover,
+    },
+  });
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label={tableName}>
-                <TableHead>
-                    <TableRow>
-                        {columnsData?.map(({ field, title, flex, tooltipInfo }) => (
-                            <TableCell key={field} sx={{ fontSize: 14, fontWeight: 600, flex }}>
-                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                    {tooltipInfo && (
-                                        <Box sx={{ mr: .5, display: 'flex', alignItems: 'center' }}>
-                                            <CustomTooltip
-                                                title={tooltipInfo}
-                                                id={field}
-                                                placement='top-start'
-                                                sx={{
-                                                    '& .MuiTooltip-arrow': {
-                                                        color: '#A0AED1',
-                                                    },
-                                                }}
-                                                arrow
-                                            >
-                                                <IconButton disableFocusRipple disableRipple>
-                                                    <InfoSVG />
-                                                </IconButton>
-                                            </CustomTooltip>
-                                        </Box>
-                                    )}
-                                    {title}
-                                </Box>
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((eachRow) => (
-                        <TableRow key={eachRow.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            {columnsData?.map((_, index) => (
-                                <TableCell
-                                    key={_.title}
-                                    component="th"
-                                    scope="row"
-                                    variant="body"
-                                    sx={{
-                                        ...cellStyles,
-                                        color: theme.palette.nym.wallet.fee,
-                                        padding: 2,
-                                        width: 200,
-                                        fontSize: 12,
-                                        fontWeight: 600,
-                                    }}
-                                    data-testid={`${_.title.replace(/ /g, '-')}-value`}
-                                >
-                                    {formatCellValues(eachRow[columnsData[index].field], columnsData[index].field, theme)}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label={tableName}>
+        <TableHead>
+          <TableRow>
+            {columnsData?.map(({ field, title, flex, tooltipInfo }) => (
+              <TableCell key={field} sx={{ fontSize: 14, fontWeight: 600, flex }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  {tooltipInfo && (
+                    <Box sx={{ mr: 0.5, display: 'flex', alignItems: 'center' }}>
+                      <CustomTooltip
+                        title={tooltipInfo}
+                        id={field}
+                        placement="top-start"
+                        sx={{
+                          '& .MuiTooltip-arrow': {
+                            color: '#A0AED1',
+                          },
+                        }}
+                        arrow
+                      >
+                        <IconButton disableFocusRipple disableRipple>
+                          <InfoSVG />
+                        </IconButton>
+                      </CustomTooltip>
+                    </Box>
+                  )}
+                  {title}
+                </Box>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((eachRow) => (
+            <TableRow key={eachRow.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              {columnsData?.map((_, index) => (
+                <TableCell
+                  key={_.title}
+                  component="th"
+                  scope="row"
+                  variant="body"
+                  sx={{
+                    ...cellStyles,
+                    color: theme.palette.nym.wallet.fee,
+                    padding: 2,
+                    width: 200,
+                    fontSize: 12,
+                    fontWeight: 600,
+                  }}
+                  data-testid={`${_.title.replace(/ /g, '-')}-value`}
+                >
+                  {formatCellValues(eachRow[columnsData[index].field], columnsData[index].field, theme)}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };

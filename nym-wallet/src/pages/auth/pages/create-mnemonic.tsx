b82f91dc07289074d7d9d0f77415b449fa9577c8
pierre <dommerc.pierre@gmail.com>
2022-06-09 13:37:39 +0200

    feat(wallet): style adjustment create mnemonic

--- nym-wallet/src/pages/auth/pages/create-mnemonic.tsx
+++ nym-wallet/src/pages/auth/pages/create-mnemonic.tsx
@@ -1,11 +1,9 @@
 import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Container, Stack } from '@mui/material';
 import { AuthContext } from 'src/context/auth';
-import { Check, ContentCopySharp } from '@mui/icons-material';
 import { useClipboard } from 'use-clipboard-copy';
-import { WordTiles } from '../components';
-import { Warning } from '../../../components';
+import { Mnemonic } from '../../../components';
 
 export const CreateMnemonic = () => {
   const { mnemonic, mnemonicWords, generateMnemonic, resetState } = useContext(AuthContext);
@@ -19,54 +17,31 @@ export const CreateMnemonic = () => {
 
   const { copy, copied } = useClipboard({ copiedTimeout: 5000 });
   return (
-    <Stack alignItems="center" spacing={3}>
-      <Typography sx={{ color: 'common.white', fontWeight: 600 }} variant="h4" textAlign="center">
-        Copy and save or write down your mnemonic
-      </Typography>
+    <Container maxWidth="xs">
+      <Stack alignItems="center" spacing={3} maxWidth="xs">
+        <Mnemonic mnemonic={mnemonic} handleCopy={copy} copied={copied} />
 
-      <Warning>
-        <Typography>
-          Below is your 24 word mnemonic, make sure to store it in a safe place for accessing your wallet in the future.
-        </Typography>
-      </Warning>
-
-      <WordTiles mnemonicWords={mnemonicWords} showIndex />
-
-      <Button
-        color="inherit"
-        disableElevation
-        size="large"
-        onClick={() => {
-          copy(mnemonic);
-        }}
-        sx={{
-          width: 250,
-        }}
-        endIcon={!copied ? <ContentCopySharp /> : <Check color="success" />}
-      >
-        Copy mnemonic
-      </Button>
-
-      <Button
-        variant="contained"
-        color="primary"
-        disableElevation
-        size="large"
-        onClick={() => history.push('/verify-mnemonic')}
-        sx={{ width: 250 }}
-        disabled={!copied}
-      >
-        I saved my mnemonic
-      </Button>
-      <Button
-        onClick={() => {
-          resetState();
-          history.goBack();
-        }}
-        color="inherit"
-      >
-        Back
-      </Button>
-    </Stack>
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          size="large"
+          onClick={() => history.push('/verify-mnemonic')}
+          sx={{ width: 250 }}
+          disabled={!copied}
+        >
+          I saved my mnemonic
+        </Button>
+        <Button
+          onClick={() => {
+            resetState();
+            history.goBack();
+          }}
+          color="inherit"
+        >
+          Back
+        </Button>
+      </Stack>
+    </Container>
   );
 };

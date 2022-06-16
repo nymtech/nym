37f501aae998134b71b6d815a5c6013c2e6a1651
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-06-16 14:25:00 +0100

    Fixed getting SMT logical processor count

--- mixnode/src/node/http/hardware.rs
+++ mixnode/src/node/http/hardware.rs
@@ -1,8 +1,8 @@
-use cupid::ExtendedTopologyEnumeration;
+use cupid::{ExtendedTopologyEnumeration, TopologyType};
 use rocket::serde::{json::Json, Serialize};
 use sysinfo::{System, SystemExt};
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 #[serde(crate = "rocket::serde")]
 pub(crate) struct Hardware {
     ram: String,
@@ -10,13 +10,13 @@ pub(crate) struct Hardware {
     crypto_hardware: Option<CryptoHardware>,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 #[serde(crate = "rocket::serde")]
 pub(crate) struct CryptoHardware {
     aesni: bool,
     avx2: bool,
     brand_string: String,
-    logical_processor_count: Option<u32>,
+    smt_logical_processor_count: Vec<u32>,
     osxsave: bool,
     sgx: bool,
     xsave: bool,
@@ -58,24 +58,27 @@ fn hardware_from_sysinfo(crypto_hardware: Option<CryptoHardware>) -> Option<Hard
 /// Note: this information is generally only available on x86 platforms for Linux.
 fn hardware_info_from_cupid() -> Option<CryptoHardware> {
     cupid::master().map(|info| -> CryptoHardware {
-        let mut logical_processor_count = None;
-        if let Some(cpu_count) = info.extended_topology_enumeration() {
-            logical_processor_count = Some(
-                info.extended_topology_enumeration()
-                    .unwrap()
-                    .map(|topology| topology.logical_processor_count())
-                    .collect::<Vec<u32>>()
-                    .first()
-                    .unwrap()
-                    .to_owned(),
-            )
-        };
+        let smt_logical_processor_count =
+            if let Some(extended_topology) = info.extended_topology_enumeration() {
+                extended_topology
+                    .clone()
+                    .filter_map(|entry| {
+                        if entry.level_type() == TopologyType::SMT {
+                            Some(entry.logical_processor_count())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect()
+            } else {
+                Vec::new()
+            };
 
         CryptoHardware {
             aesni: info.aesni(),
             avx2: info.avx2(),
             brand_string: info.brand_string().map(String::from).unwrap_or_default(),
-            logical_processor_count,
+            smt_logical_processor_count,
             osxsave: info.osxsave(),
             sgx: info.sgx(),
             xsave: info.xsave(),

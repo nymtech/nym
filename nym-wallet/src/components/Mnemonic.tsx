b2df4ca4fd14009c47160e8d9c56e310c0856268
Pierre Dommerc <dommerc.pierre@gmail.com>
2022-06-20 17:22:23 +0200

    feat(wallet): style changes to match new design (#1333)
    * feat(wallet): style changes to match new design
    
    * fix(wallet): style
    
    * feat(wallet): style changes to match new design
    
    * fix(wallet): style
    
    * feat(wallet): style adjustment create mnemonic
    
    * feat(wallet): style adjustment create mnemonic

--- nym-wallet/src/components/Mnemonic.tsx
+++ nym-wallet/src/components/Mnemonic.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Alert, Button, Stack, TextField, Typography } from '@mui/material';
+import { Button, Stack, TextField, Typography } from '@mui/material';
 import { Check, ContentCopySharp } from '@mui/icons-material';
+import { Warning } from './Warning';
 
 export const Mnemonic = ({
   mnemonic,
@@ -12,11 +13,11 @@ export const Mnemonic = ({
   handleCopy: (text?: string) => void;
 }) => (
   <Stack spacing={2} alignItems="center">
-    <Alert severity="warning" icon={false} sx={{ display: 'block' }}>
+    <Warning>
       <Typography sx={{ textAlign: 'center' }}>
         Below is your 24 word mnemonic, make sure to store it in a safe place for accessing your wallet in the future
       </Typography>
-    </Alert>
+    </Warning>
     <TextField multiline rows={3} value={mnemonic} fullWidth />
 
     <Button

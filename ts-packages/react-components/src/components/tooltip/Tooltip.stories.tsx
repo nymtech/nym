f4979679a27de66a21eecf95395715c5a197588e
gala1234 <calero.vg@gmail.com>
2022-06-21 17:10:16 +0200

    fixing text color in the story

--- ts-packages/react-components/src/components/tooltip/Tooltip.stories.tsx
+++ ts-packages/react-components/src/components/tooltip/Tooltip.stories.tsx
@@ -12,10 +12,10 @@ export const Default = () => <Tooltip title="tooltip" id="field-name" placement=
 export const NEStyle = () => {
   return (
     <Tooltip
-      title="Figma NE Styles"
+      title="Level of stake saturation for this node. Nodes receive more rewards the higher their saturation level, up to 100%. Beyond 100% no additional rewards are granted. The current stake saturation level is: 1 million NYM, computed as S/K where S is  total amount of tokens available to stakeholders and K is the number of nodes in the reward set."
       id="field-name"
       placement="top-start"
-      textColor="#A0AED1"
+      textColor="#111826"
       bgColor="#A0AED1"
       maxWidth={230}
       arrow

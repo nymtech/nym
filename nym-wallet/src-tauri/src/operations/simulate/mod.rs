ee902618c230845e7afb045caca956b85c10dae3
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-06-21 10:46:45 +0100

    Incorporating GasAdjustment into wallet fee simulation

--- nym-wallet/src-tauri/src/operations/simulate/mod.rs
+++ nym-wallet/src-tauri/src/operations/simulate/mod.rs
@@ -1,15 +1,29 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use cosmrs::tx;
 use nym_types::currency::MajorCurrencyAmount;
 use nym_types::fees::FeeDetails;
-use validator_client::nymd::cosmwasm_client::types::GasInfo;
-use validator_client::nymd::{tx, CosmosCoin, Fee, GasPrice};
+use validator_client::nymd::cosmwasm_client::types::{GasInfo, SimulateResponse};
+use validator_client::nymd::{
+    CosmosCoin, Fee, GasAdjustable, GasAdjustment, GasPrice, SigningNymdClient,
+};
+use validator_client::Client;
 
 pub mod admin;
 pub mod cosmos;
 pub mod mixnet;
 pub mod vesting;
+
+pub(crate) fn detailed_fee(
+    client: &Client<SigningNymdClient>,
+    simulate_response: SimulateResponse,
+) -> FeeDetails {
+    let gas_price = client.nymd.gas_price().clone();
+    let gas_adjustment = client.nymd.gas_adjustment();
+
+    SimulateResult::new(simulate_response.gas_info, gas_price, gas_adjustment).detailed_fee()
+}
 
 // technically we could have also exposed a result: Option<AbciResult> field from the SimulateResponse,
 // but in the context of the wallet it's really irrelevant and useless for the time being
@@ -19,13 +33,19 @@ pub(crate) struct SimulateResult {
     // for example if you attempt to send a 'BondMixnode' with invalid signature
     pub gas_info: Option<GasInfo>,
     pub gas_price: GasPrice,
+    pub gas_adjustment: GasAdjustment,
 }
 
 impl SimulateResult {
-    pub fn new(gas_info: Option<GasInfo>, gas_price: GasPrice) -> Self {
+    pub fn new(
+        gas_info: Option<GasInfo>,
+        gas_price: GasPrice,
+        gas_adjustment: GasAdjustment,
+    ) -> Self {
         SimulateResult {
             gas_info,
             gas_price,
+            gas_adjustment,
         }
     }
 
@@ -39,7 +59,7 @@ impl SimulateResult {
 
     fn to_fee_amount(&self) -> Option<CosmosCoin> {
         self.gas_info
-            .map(|gas_info| &self.gas_price * gas_info.gas_used)
+            .map(|gas_info| &self.gas_price * gas_info.gas_used.adjust_gas(self.gas_adjustment))
     }
 
     fn to_fee(&self) -> Fee {

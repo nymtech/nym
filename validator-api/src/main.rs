7e0fe34fa4b99f68bf41a0bb87822380a6fb75db
Jon Häggblad <jon.haggblad@gmail.com>
2022-06-22 21:58:44 +0200

    validator-api: some comments

--- validator-api/src/main.rs
+++ validator-api/src/main.rs
@@ -543,26 +543,26 @@ async fn run_validator_api(matches: ArgMatches<'static>) -> Result<()> {
     // if network monitor is disabled, we're not going to be sending any rewarding hence
     // we're not starting signing client
     if config.get_network_monitor_enabled() {
+        // Main storage
+        let storage = rocket.state::<ValidatorApiStorage>().unwrap().clone();
+
         // setup our daily uptime updater. Note that if network monitor is disabled, then we have
         // no data for the updates and hence we don't need to start it up
-        let storage = rocket.state::<ValidatorApiStorage>().unwrap().clone();
         let uptime_updater = HistoricalUptimeUpdater::new(storage.clone());
         tokio::spawn(async move { uptime_updater.run().await });
 
+        // spawn the cache refresher
         let validator_cache_refresher = ValidatorCacheRefresher::new(
             signing_nymd_client.clone(),
             config.get_caching_interval(),
             validator_cache.clone(),
             Some(storage.clone()),
         );
-
-        // spawn our cacher
         tokio::spawn(async move { validator_cache_refresher.run().await });
 
+        // spawn rewarded set updater
         let mut rewarded_set_updater =
             RewardedSetUpdater::new(signing_nymd_client, validator_cache.clone(), storage).await?;
-
-        // spawn rewarded set updater
         tokio::spawn(async move { rewarded_set_updater.run().await.unwrap() });
     } else {
         let nymd_client = Client::new_query(&config);

ac9e81b09543b06a8fc84a1540c71c1f5bd5e255
Jon Häggblad <jon.haggblad@gmail.com>
2022-06-22 22:45:55 +0200

    wallet: remove broken platform constants
    Due to a bug (typo) these constants were actually never used, and at
    this point we can't fix it without breaking backwards compatibility.

--- nym-wallet/src-tauri/src/platform_constants.rs
+++ nym-wallet/src-tauri/src/platform_constants.rs
@@ -4,27 +4,7 @@
 // Specify filenames and other platform specific constants to respect platform conventions, or at
 // least, something popular on each respective platform.
 
-cfg_if::cfg_if! {
-    if #[cfg(target_os = "linux")] {
-        pub const CONFIG_DIR_NAME: &str = "nym-wallet";
-        pub const CONFIG_FILENAME: &str = "config.toml";
-        pub const STORAGE_DIR_NAME: &str = "nym-wallet";
-        pub const WALLET_INFO_FILENAME: &str = "saved-wallet.json";
-    } else if #[cfg(taret_os = "macos")] {
-        pub const CONFIG_DIR_NAME: &str = "nym-wallet";
-        pub const CONFIG_FILENAME: &str = "config.toml";
-        pub const STORAGE_DIR_NAME: &str = "nym-wallet";
-        pub const WALLET_INFO_FILENAME: &str = "saved-wallet.json";
-    } else if #[cfg(taret_os = "windows")] {
-        pub const CONFIG_DIR_NAME: &str = "NymWallet";
-        pub const CONFIG_FILENAME: &str = "Config.toml";
-        pub const STORAGE_DIR_NAME: &str = "NymWallet";
-        pub const WALLET_INFO_FILENAME: &str = "saved_wallet.json";
-    } else {
-        // This case is likely to be a unix-y system
-        pub const CONFIG_DIR_NAME: &str = "nym-wallet";
-        pub const CONFIG_FILENAME: &str = "config.toml";
-        pub const STORAGE_DIR_NAME: &str = "nym-wallet";
-        pub const WALLET_INFO_FILENAME: &str = "saved-wallet.json";
-    }
-}
+pub const CONFIG_DIR_NAME: &str = "nym-wallet";
+pub const CONFIG_FILENAME: &str = "config.toml";
+pub const STORAGE_DIR_NAME: &str = "nym-wallet";
+pub const WALLET_INFO_FILENAME: &str = "saved-wallet.json";

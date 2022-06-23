f4f0268f70a02902a6f24c8512cdbe1f67564cab
fmtabbara <fmtabbara@hotmail.co.uk>
2022-06-23 15:14:43 +0100

    move appbar component into folder

--- nym-wallet/src/components/AppBar/AppBar.tsx
+++ nym-wallet/src/components/AppBar/AppBar.tsx
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react';
-import { AppBar as MuiAppBar, Grid, IconButton, Toolbar } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { Logout } from '@mui/icons-material';
 import TerminalIcon from '@mui/icons-material/Terminal';
-import { AppContext } from '../context/main';
-import { NetworkSelector } from './NetworkSelector';
-import { Node as NodeIcon } from '../svg-icons/node';
-import { MultiAccounts } from './Accounts';
-import { config } from '../config';
+import { AppBar as MuiAppBar, Grid, IconButton, Toolbar } from '@mui/material';
+import { Send } from 'src/components/Send';
+import { config } from '../../config';
+import { AppContext } from '../../context/main';
+import { MultiAccounts } from '../Accounts';
+import { NetworkSelector } from '../NetworkSelector';
+import { AppMenu } from './AppMenu';
 
 export const AppBar = () => {
-  const { logOut, handleShowTerminal, appEnv, handleShowSettings, showSettings } = useContext(AppContext);
-  const navigate = useNavigate();
+  const { handleShowTerminal, appEnv } = useContext(AppContext);
+
   return (
     <MuiAppBar position="sticky" sx={{ boxShadow: 'none', bgcolor: 'transparent' }}>
       <Toolbar disableGutters>
@@ -24,7 +23,7 @@ export const AppBar = () => {
               <NetworkSelector />
             </Grid>
           </Grid>
-          <Grid item container justifyContent="flex-end" md={12} lg={5} spacing={2}>
+          <Grid item container justifyContent="flex-end" alignItems="center" md={12} lg={5} spacing={2}>
             {(appEnv?.SHOW_TERMINAL || config.IS_DEV_MODE) && (
               <Grid item>
                 <IconButton size="small" onClick={handleShowTerminal} sx={{ color: 'nym.background.dark' }}>
@@ -33,6 +32,12 @@ export const AppBar = () => {
               </Grid>
             )}
             <Grid item>
+              <Send />
+            </Grid>
+            <Grid item>
+              <AppMenu />
+            </Grid>
+            {/* <Grid item>
               <IconButton
                 onClick={handleShowSettings}
                 sx={{ color: showSettings ? 'primary.main' : 'nym.background.dark' }}
@@ -42,17 +47,10 @@ export const AppBar = () => {
               </IconButton>
             </Grid>
             <Grid item>
-              <IconButton
-                size="small"
-                onClick={async () => {
-                  await logOut();
-                  navigate('/');
-                }}
-                sx={{ color: 'nym.background.dark' }}
-              >
+              <IconButton size="small" onClick={logOut} sx={{ color: 'nym.background.dark' }}>
                 <Logout fontSize="small" />
               </IconButton>
-            </Grid>
+            </Grid> */}
           </Grid>
         </Grid>
       </Toolbar>

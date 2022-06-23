9538aebeea66c162bc8b520ed42f00127c7005aa
fmtabbara <fmtabbara@hotmail.co.uk>
2022-06-23 15:15:22 +0100

    add additional prop for making modal list item text bold

--- nym-wallet/src/components/Delegation/ModalListItem.tsx
+++ nym-wallet/src/components/Delegation/ModalListItem.tsx
@@ -6,12 +6,17 @@ export const ModalListItem: React.FC<{
   label: string;
   divider?: boolean;
   hidden?: boolean;
+  strong?: boolean;
   value: React.ReactNode;
-}> = ({ label, value, hidden, divider }) => (
+}> = ({ label, value, hidden, divider, strong }) => (
   <Box sx={{ display: hidden ? 'none' : 'block' }}>
     <Stack direction="row" justifyContent="space-between">
-      <Typography fontSize="smaller">{label}:</Typography>
-      <Typography fontSize="smaller">{value}</Typography>
+      <Typography fontSize="smaller" fontWeight={strong ? 600 : undefined}>
+        {label}:
+      </Typography>
+      <Typography fontSize="smaller" fontWeight={strong ? 600 : undefined}>
+        {value}
+      </Typography>
     </Stack>
     {divider && <ModalDivider />}
   </Box>

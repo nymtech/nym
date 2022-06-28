d6f4e450f25b7f623335043a5756108e74885e44
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-06-28 15:14:17 +0100

    commented out all code

--- contracts/mixnet/src/lib.rs
+++ contracts/mixnet/src/lib.rs
@@ -1,14 +1,13 @@
 // Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-mod constants;
-pub mod contract;
-mod delegations;
-mod error;
-mod gateways;
-mod interval;
-mod mixnet_contract_settings;
-mod mixnodes;
-mod queued_migrations;
-mod rewards;
-mod support;
+// mod constants;
+// pub mod contract;
+// mod delegations;
+// mod error;
+// mod gateways;
+// mod interval;
+// mod mixnet_contract_settings;
+// mod mixnodes;
+// mod rewards;
+// mod support;

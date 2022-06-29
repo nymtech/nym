60f6b435e9187ebab2fda9871b266ec8edf7f52c
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-06-29 17:48:17 +0100

    wip

--- common/cosmwasm-smart-contracts/mixnet-contract/src/error.rs
+++ common/cosmwasm-smart-contracts/mixnet-contract/src/error.rs
@@ -1,4 +1,5 @@
-use cosmwasm_std::Decimal;
+use crate::IdentityKey;
+use cosmwasm_std::{Addr, Coin, Decimal};
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
@@ -17,6 +18,33 @@ pub enum MixnetContractError {
         minuend: Decimal,
         subtrahend: Decimal,
     },
+
+    #[error("Not enough funds sent for node pledge. (received {received}, minimum {minimum})")]
+    InsufficientPledge { received: Coin, minimum: Coin },
+
+    #[error("Mixnode ({identity}) does not exist")]
+    MixNodeBondNotFound { identity: IdentityKey },
+
+    #[error("{owner} does not seem to own any mixnodes")]
+    NoAssociatedMixNodeBond { owner: Addr },
+
+    #[error("{owner} does not seem to own any gateways")]
+    NoAssociatedGatewayBond { owner: Addr },
+
+    #[error("This address has already bonded a mixnode")]
+    AlreadyOwnsMixnode,
+
+    #[error("This address has already bonded a gateway")]
+    AlreadyOwnsGateway,
+
+    #[error("Unauthorized")]
+    Unauthorized,
+
+    #[error("Wrong coin denomination. Received: {received}, expected: {expected}")]
+    WrongDenom { received: String, expected: String },
+
+    #[error("Received multiple coin types during staking")]
+    MultipleDenoms,
     //
     // #[error("Overflow Error")]
     // OverflowError(#[from] cosmwasm_std::OverflowError),
@@ -34,6 +62,6 @@ pub enum MixnetContractError {
     //     #[from]
     //     source: cosmwasm_std::StdError,
     // },
-    // #[error("Division by zero at {}", line!())]
+    // #[error("Division by zero at {}")]
     // DivisionByZero,
 }

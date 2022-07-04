91b9e2a1a76419811772212fe94f998c77939123
fmtabbara <fmtabbara@hotmail.co.uk>
2022-07-04 11:58:23 +0100

    update to bonding confirmation modal

--- nym-wallet/src/pages/bond/components/ConfirmationModal.tsx
+++ nym-wallet/src/pages/bond/components/ConfirmationModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, CircularProgress } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { FeeDetails, MajorCurrencyAmount } from '@nymproject/types';
 import { SimpleModal } from 'src/components/Modals/SimpleModal';
 import { ModalFee } from 'src/components/Modals/ModalFee';
@@ -20,7 +20,7 @@ export const ConfirmationModal = ({
 }) => (
   <SimpleModal
     header="Bond confirmation"
-    open={true}
+    open
     onOk={onConfirm}
     okLabel="Confirm"
     hideCloseIcon
@@ -31,11 +31,9 @@ export const ConfirmationModal = ({
     }
   >
     <Box sx={{ mt: 3 }}>
-      <>
-        <ModalListItem label="Mixnode identity" value={identity} />
-        <ModalListItem label="Amount" value={`${amount.amount} ${amount.denom}`} />
-        <ModalFee fee={fee} isLoading={false} />
-      </>
+      <ModalListItem label="Mixnode identity" value={identity} />
+      <ModalListItem label="Amount" value={`${amount.amount} ${amount.denom}`} />
+      <ModalFee fee={fee} isLoading={false} />
     </Box>
   </SimpleModal>
 );

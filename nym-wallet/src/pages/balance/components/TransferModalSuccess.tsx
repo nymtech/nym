dd285ea7bbbf1643415e2923322019f48a908a0f
fmtabbara <fmtabbara@hotmail.co.uk>
2022-07-05 15:59:57 +0100

    use confirmation modal

--- nym-wallet/src/pages/balance/components/TransferModalSuccess.tsx
+++ nym-wallet/src/pages/balance/components/TransferModalSuccess.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, Stack, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { Link } from '@nymproject/react/link/Link';
+import { ConfirmationModal } from 'src/components';
 import { TTransactionDetails } from '../types';
 
 export const SuccessModal = ({ tx, onClose }: { tx?: TTransactionDetails; onClose: () => void }) => (
-  <Dialog open onClose={onClose}>
-    <DialogContent sx={{ width: 500 }}>
-      <Stack alignItems="center" spacing={2}>
-        <Typography>Transfer to balance successful</Typography>
-        {tx && (
-          <>
-            <Typography variant="h5">{tx.amount}</Typography>
-            <Link href={tx.url} target="_blank" sx={{ ml: 1 }} text="View on blockchain" />
-          </>
-        )}
-      </Stack>
-    </DialogContent>
-    <DialogActions>
-      <Button fullWidth size="large" variant="contained" onClick={onClose}>
-        Done
-      </Button>
-    </DialogActions>
-  </Dialog>
+  <ConfirmationModal
+    open
+    title={<Typography>Transfer to balance successful</Typography>}
+    onClose={onClose}
+    onConfirm={onClose}
+    maxWidth="xs"
+    fullWidth
+    confirmButton="Done"
+  >
+    <Stack alignItems="center" spacing={2}>
+      {tx && (
+        <>
+          <Typography variant="h5">{tx.amount}</Typography>
+          <Link href={tx.url} target="_blank" sx={{ ml: 1 }} text="View on blockchain" />
+        </>
+      )}
+    </Stack>
+  </ConfirmationModal>
 );

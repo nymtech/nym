37adebfcd2263679ec8c3fb1faa546c9c55a73ee
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-07-07 15:24:23 +0100

    Full delegation flow

--- common/cosmwasm-smart-contracts/mixnet-contract/src/pending_events.rs
+++ common/cosmwasm-smart-contracts/mixnet-contract/src/pending_events.rs
@@ -1,8 +1,30 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::NodeId;
+use cosmwasm_std::{Addr, Coin};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum PendingEpochEvent {
-    //
+    // can't just pass the `Delegation` struct here as it's impossible to determine
+    // `cumulative_reward_ratio` ahead of time
+    Delegate {
+        owner: Addr,
+        mix_id: NodeId,
+        amount: Coin,
+        proxy: Option<Addr>,
+    },
+    Undelegate {
+        owner: Addr,
+        mix_id: NodeId,
+        proxy: Option<Addr>,
+    },
 }
 
-pub enum PendingIntervalEvent {}
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum PendingIntervalEvent {
+    ChangeMixCostParams {
+        //
+    },
+}

c4b2007841a6b50a73c0817f1adfa2eef8662fbb
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-07-14 11:32:16 +0100

    started restoring unit tests

--- contracts/mixnet/src/interval/helpers.rs
+++ contracts/mixnet/src/interval/helpers.rs
@@ -9,18 +9,75 @@ use mixnet_contract_common::Interval;
 
 pub(crate) fn change_epochs_in_interval(
     store: &mut dyn Storage,
-    current_interval: Option<Interval>,
+    mut current_interval: Interval,
     epochs_in_interval: u32,
 ) -> Result<(), MixnetContractError> {
-    let mut interval = match current_interval {
-        Some(interval) => interval,
-        None => storage::current_interval(store)?,
-    };
-
     let mut rewarding_params = rewards_storage::REWARDING_PARAMS.load(store)?;
     rewarding_params.apply_epochs_in_interval_change(epochs_in_interval);
     rewards_storage::REWARDING_PARAMS.save(store, &rewarding_params)?;
 
-    interval.force_change_epochs_in_interval(epochs_in_interval);
-    Ok(storage::save_interval(store, &interval)?)
+    current_interval.force_change_epochs_in_interval(epochs_in_interval);
+    Ok(storage::save_interval(store, &current_interval)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::support::tests::test_helpers;
+    use cosmwasm_std::Decimal;
+
+    #[test]
+    fn changing_epochs_in_interval() {
+        let two = Decimal::from_atomics(2u32, 0).unwrap();
+        let mut deps = test_helpers::init_contract();
+
+        let initial_interval = storage::current_interval(&deps.storage).unwrap();
+        let initial_params = rewards_storage::REWARDING_PARAMS
+            .load(&deps.storage)
+            .unwrap();
+
+        // if we half the number of epochs, the reward budget should get doubled
+        change_epochs_in_interval(
+            &mut deps.storage,
+            initial_interval,
+            initial_interval.epochs_in_interval() / 2,
+        )
+        .unwrap();
+        let updated_interval = storage::current_interval(&deps.storage).unwrap();
+        let updated_params = rewards_storage::REWARDING_PARAMS
+            .load(&deps.storage)
+            .unwrap();
+
+        assert_eq!(
+            updated_interval.epochs_in_interval(),
+            initial_interval.epochs_in_interval() / 2
+        );
+        assert_eq!(
+            updated_params.interval.epoch_reward_budget,
+            initial_params.interval.epoch_reward_budget * two
+        );
+
+        // and similarly when we double number of epochs, the reward budget should get halved
+        change_epochs_in_interval(
+            &mut deps.storage,
+            initial_interval,
+            initial_interval.epochs_in_interval() * 2,
+        )
+        .unwrap();
+
+        // if we half the number of epochs, the reward budget should get doubled
+        let updated_interval = storage::current_interval(&deps.storage).unwrap();
+        let updated_params = rewards_storage::REWARDING_PARAMS
+            .load(&deps.storage)
+            .unwrap();
+
+        assert_eq!(
+            updated_interval.epochs_in_interval(),
+            initial_interval.epochs_in_interval() * 2
+        );
+        assert_eq!(
+            updated_params.interval.epoch_reward_budget,
+            initial_params.interval.epoch_reward_budget / two
+        );
+    }
 }

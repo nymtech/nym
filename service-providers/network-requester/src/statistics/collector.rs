248893824147b0381ad73b73ecd20f991f865df9
Bogdan-Ștefan Neacșu <bogdan@nymtech.net>
2022-07-18 17:16:34 +0300

    Network requester takes only mainnet client address

--- service-providers/network-requester/src/statistics/collector.rs
+++ service-providers/network-requester/src/statistics/collector.rs
@@ -12,7 +12,6 @@ use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::RwLock;
 
-use network_defaults::DEFAULT_NETWORK;
 use nymsphinx::addressing::clients::Recipient;
 use ordered_buffer::OrderedMessageSender;
 use socks5_requests::{ConnectionId, Message as Socks5Message, RemoteAddress, Request};
@@ -59,6 +58,7 @@ pub struct StatsProviderConfigEntry {
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
+#[allow(dead_code)]
 pub struct OptionalStatsProviderConfig {
     mainnet: Option<StatsProviderConfigEntry>,
     sandbox: Option<StatsProviderConfigEntry>,
@@ -67,15 +67,7 @@ pub struct OptionalStatsProviderConfig {
 
 impl OptionalStatsProviderConfig {
     pub fn stats_client_address(&self) -> Option<String> {
-        let entry_config = match DEFAULT_NETWORK {
-            network_defaults::all::Network::MAINNET => self.mainnet.clone(),
-            network_defaults::all::Network::SANDBOX => self.sandbox.clone(),
-            network_defaults::all::Network::QA => self.qa.clone(),
-            network_defaults::all::Network::CUSTOM { .. } => {
-                panic!("custom network is not supported")
-            }
-        };
-        entry_config.map(|e| e.stats_client_address)
+        self.mainnet.clone().map(|e| e.stats_client_address)
     }
 }
 

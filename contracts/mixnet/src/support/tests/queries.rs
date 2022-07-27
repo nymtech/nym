bf1cd13880f67e514f0e7076637aa3132a71665a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-07-27 17:08:18 +0100

    Restored gateway tx tests

--- contracts/mixnet/src/support/tests/queries.rs
+++ contracts/mixnet/src/support/tests/queries.rs
@@ -29,6 +29,8 @@ pub fn get_mix_nodes(
     page.nodes
 }
 
+// I honestly don't know why we're using this way of querying in tests, but I couldn't be bothered to change it
+// since I haven't done anything to gateways
 pub fn get_gateways(deps: &mut OwnedDeps<MockStorage, MockApi, MockQuerier>) -> Vec<GatewayBond> {
     let result = query(
         deps.as_ref(),
@@ -42,7 +44,19 @@ pub fn get_gateways(deps: &mut OwnedDeps<MockStorage, MockApi, MockQuerier>) -> 
 
     let page: PagedGatewayResponse = from_binary(&result).unwrap();
     if page.start_next_after.is_some() {
-        panic!("we didn't manage to get all gateways in a single query")
+        let next_page = query(
+            deps.as_ref(),
+            mock_env(),
+            QueryMsg::GetGateways {
+                start_after: page.start_next_after,
+                limit: None,
+            },
+        )
+        .unwrap();
+        let next_page: PagedGatewayResponse = from_binary(&next_page).unwrap();
+        if !next_page.nodes.is_empty() {
+            panic!("we didn't manage to get all gateways in a single query")
+        }
     }
     page.nodes
 }

0944f65fd396725a1823756a19b3ed539bbe83c3
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-02 12:08:36 +0100

    Unit tests for helper functions

--- contracts/mixnet/src/rewards/models.rs
+++ contracts/mixnet/src/rewards/models.rs
@@ -4,14 +4,14 @@
 use cosmwasm_std::Decimal;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize, Default, Debug, Clone, Copy, Eq, PartialEq)]
 pub(crate) struct RewardPoolChange {
     /// Indicates amount that shall get moved from the reward pool to the staking supply
     /// upon the current interval finishing.
     pub removed: Decimal,
 
     // this will be used once coconut credentials are in use;
-    /// Indicates amount that shall get added to the both reward pool and not touch the staking supply
+    /// Indicates amount that shall get added to the reward pool and not touch the staking supply
     /// upon the current interval finishing.
     #[allow(unused)]
     pub added: Decimal,

eabbd5790bfbda75f9e1cd4b44883a8c2f289e7a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-02 12:08:36 +0100

    Unit tests for helper functions

--- contracts/mixnet/src/mixnodes/helpers.rs
+++ contracts/mixnet/src/mixnodes/helpers.rs
@@ -117,7 +117,7 @@ pub(crate) fn cleanup_post_unbond_mixnode_storage(
 
     // if there are no pending delegations to return, we can also
     // purge all information regarding rewarding parameters
-    if current_details.rewarding_details.delegates == Decimal::zero() {
+    if current_details.rewarding_details.unique_delegations == 0 {
         rewards_storage::MIXNODE_REWARDING.remove(storage, node_id);
     } else {
         // otherwise just set operator's tokens to zero as to indicate they have unbonded
@@ -141,4 +141,331 @@ pub(crate) fn cleanup_post_unbond_mixnode_storage(
         },
     )?;
     storage::decrement_layer_count(storage, current_details.bond_information.layer)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::support::tests::fixtures::{
+        mix_node_cost_params_fixture, mix_node_fixture, TEST_COIN_DENOM,
+    };
+    use crate::support::tests::test_helpers::TestSetup;
+    use cosmwasm_std::coin;
+
+    const OWNER_EXISTS: &str = "mix-owner-existing";
+    const OWNER_UNBONDING: &str = "mix-owner-unbonding";
+    const OWNER_UNBONDED: &str = "mix-owner-unbonded";
+    const OWNER_UNBONDED_LEFTOVER: &str = "mix-owner-unbonded-leftover";
+
+    // create a mixnode that is bonded, unbonded, in the process of unbonding and unbonded with leftover mix rewarding details
+    fn setup_mix_combinations(test: &mut TestSetup) -> Vec<NodeId> {
+        let mix_id_exists = test.add_dummy_mixnode(OWNER_EXISTS, None);
+        let mix_id_unbonding = test.add_dummy_mixnode(OWNER_UNBONDING, None);
+        let mix_id_unbonded = test.add_dummy_mixnode(OWNER_UNBONDED, None);
+        let mix_id_unbonded_leftover = test.add_dummy_mixnode(OWNER_UNBONDED_LEFTOVER, None);
+
+        // manually adjust delegation info as to indicate the rewarding information shouldnt get removed
+        let mut rewarding_details = test.mix_rewarding(mix_id_unbonded_leftover);
+        rewarding_details.delegates = Decimal::raw(12345);
+        rewarding_details.unique_delegations = 1;
+        rewards_storage::MIXNODE_REWARDING
+            .save(
+                test.deps_mut().storage,
+                mix_id_unbonded_leftover,
+                &rewarding_details,
+            )
+            .unwrap();
+
+        test.immediately_unbond_mixnode(mix_id_unbonded);
+        test.immediately_unbond_mixnode(mix_id_unbonded_leftover);
+        test.start_unbonding_mixnode(mix_id_unbonding);
+
+        vec![
+            mix_id_exists,
+            mix_id_unbonding,
+            mix_id_unbonded,
+            mix_id_unbonded_leftover,
+        ]
+    }
+
+    #[test]
+    fn getting_mixnode_bond_by_owner() {
+        let mut test = TestSetup::new();
+
+        let owner_exists = Addr::unchecked(OWNER_EXISTS);
+        let owner_unbonding = Addr::unchecked(OWNER_UNBONDING);
+        let owner_unbonded = Addr::unchecked(OWNER_UNBONDED);
+        let owner_unbonded_leftover = Addr::unchecked(OWNER_UNBONDED_LEFTOVER);
+
+        let ids = setup_mix_combinations(&mut test);
+        let mix_id_exists = ids[0];
+        let mix_id_unbonding = ids[1];
+
+        // if this is a normally bonded mixnode, all should be fine
+        let res = must_get_mixnode_bond_by_owner(test.deps().storage, &owner_exists).unwrap();
+        assert_eq!(res.id, mix_id_exists);
+
+        // if node is in the process of unbonding, we still should be capable of retrieving its details
+        let res = must_get_mixnode_bond_by_owner(test.deps().storage, &owner_unbonding).unwrap();
+        assert_eq!(res.id, mix_id_unbonding);
+
+        // but if node has unbonded, the information is purged and query fails
+        let res = must_get_mixnode_bond_by_owner(test.deps().storage, &owner_unbonded);
+        assert_eq!(
+            res,
+            Err(MixnetContractError::NoAssociatedMixNodeBond {
+                owner: owner_unbonded
+            })
+        );
+
+        let res = must_get_mixnode_bond_by_owner(test.deps().storage, &owner_unbonded_leftover);
+        assert_eq!(
+            res,
+            Err(MixnetContractError::NoAssociatedMixNodeBond {
+                owner: owner_unbonded_leftover
+            })
+        );
+    }
+
+    #[test]
+    fn getting_mixnode_details_by_id() {
+        let mut test = TestSetup::new();
+
+        let ids = setup_mix_combinations(&mut test);
+        let mix_id_exists = ids[0];
+        let mix_id_unbonding = ids[1];
+        let mix_id_unbonded = ids[2];
+        let mix_id_unbonded_leftover = ids[3];
+
+        let res = get_mixnode_details_by_id(test.deps().storage, mix_id_exists)
+            .unwrap()
+            .unwrap();
+        assert_eq!(res.bond_information.id, mix_id_exists);
+
+        let res = get_mixnode_details_by_id(test.deps().storage, mix_id_unbonding)
+            .unwrap()
+            .unwrap();
+        assert_eq!(res.bond_information.id, mix_id_unbonding);
+
+        let res = get_mixnode_details_by_id(test.deps().storage, mix_id_unbonded).unwrap();
+        assert!(res.is_none());
+
+        let res = get_mixnode_details_by_id(test.deps().storage, mix_id_unbonded_leftover).unwrap();
+        assert!(res.is_none())
+    }
+
+    #[test]
+    fn getting_mixnode_details_by_owner() {
+        let mut test = TestSetup::new();
+
+        let owner_exists = Addr::unchecked(OWNER_EXISTS);
+        let owner_unbonding = Addr::unchecked(OWNER_UNBONDING);
+        let owner_unbonded = Addr::unchecked(OWNER_UNBONDED);
+        let owner_unbonded_leftover = Addr::unchecked(OWNER_UNBONDED_LEFTOVER);
+
+        let ids = setup_mix_combinations(&mut test);
+        let mix_id_exists = ids[0];
+        let mix_id_unbonding = ids[1];
+
+        // if this is a normally bonded mixnode, all should be fine
+        let res = get_mixnode_details_by_owner(test.deps().storage, owner_exists)
+            .unwrap()
+            .unwrap();
+        assert_eq!(res.bond_information.id, mix_id_exists);
+
+        // if node is in the process of unbonding, we still should be capable of retrieving its details
+        let res = get_mixnode_details_by_owner(test.deps().storage, owner_unbonding)
+            .unwrap()
+            .unwrap();
+        assert_eq!(res.bond_information.id, mix_id_unbonding);
+
+        // but if node has unbonded, the information is purged and query fails
+        let res = get_mixnode_details_by_owner(test.deps().storage, owner_unbonded).unwrap();
+        assert!(res.is_none());
+
+        let res =
+            get_mixnode_details_by_owner(test.deps().storage, owner_unbonded_leftover).unwrap();
+        assert!(res.is_none());
+    }
+
+    #[test]
+    fn saving_new_mixnode() {
+        let mut test = TestSetup::new();
+
+        // get some mixnodes in
+        test.add_dummy_mixnode("owner1", None);
+        test.add_dummy_mixnode("owner2", None);
+        test.add_dummy_mixnode("owner3", None);
+        test.add_dummy_mixnode("owner4", None);
+        test.add_dummy_mixnode("owner5", None);
+
+        let env = test.env();
+        let id_key = "identity-key";
+        let sphinx_key = "sphinx-key";
+        let mut mixnode = mix_node_fixture();
+        mixnode.identity_key = id_key.into();
+        mixnode.sphinx_key = sphinx_key.into();
+        let cost_params = mix_node_cost_params_fixture();
+        let owner = Addr::unchecked("mix-owner");
+        let pledge = coin(100_000_000, TEST_COIN_DENOM);
+
+        let (id, layer) = save_new_mixnode(
+            test.deps_mut().storage,
+            env.clone(),
+            mixnode,
+            cost_params.clone(),
+            owner.clone(),
+            None,
+            pledge.clone(),
+        )
+        .unwrap();
+        assert_eq!(id, 6);
+        assert_eq!(layer, Layer::Three);
+
+        assert_eq!(
+            storage::MIXNODE_ID_COUNTER
+                .load(test.deps().storage)
+                .unwrap(),
+            6
+        );
+        assert_eq!(storage::LAYERS.load(test.deps().storage).unwrap().layer3, 2);
+        let mix_details = get_mixnode_details_by_id(test.deps().storage, id)
+            .unwrap()
+            .unwrap();
+        assert_eq!(mix_details.mix_id(), id);
+        assert_eq!(mix_details.original_pledge(), &pledge);
+        assert_eq!(
+            mix_details.bond_information.bonding_height,
+            env.block.height
+        );
+
+        // try to add node with duplicate identity...
+        let mut mixnode = mix_node_fixture();
+        mixnode.identity_key = id_key.into();
+        let res = save_new_mixnode(
+            test.deps_mut().storage,
+            env.clone(),
+            mixnode,
+            cost_params.clone(),
+            Addr::unchecked("different-owner"),
+            None,
+            pledge.clone(),
+        );
+        assert!(res.is_err());
+
+        // and duplicate owner...
+        let mixnode = mix_node_fixture();
+        let res = save_new_mixnode(
+            test.deps_mut().storage,
+            env.clone(),
+            mixnode,
+            cost_params.clone(),
+            owner,
+            None,
+            pledge.clone(),
+        );
+        assert!(res.is_err());
+
+        // and duplicate sphinx key...
+        let mut mixnode = mix_node_fixture();
+        mixnode.sphinx_key = sphinx_key.into();
+        let res = save_new_mixnode(
+            test.deps_mut().storage,
+            env,
+            mixnode,
+            cost_params,
+            Addr::unchecked("different-owner"),
+            None,
+            pledge,
+        );
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn cleaning_post_unbond_storage() {
+        let mut test = TestSetup::new();
+
+        let mix_id = test.add_dummy_mixnode("mix-owner", None);
+        let mix_id_leftover = test.add_dummy_mixnode("mix-owner-leftover", None);
+
+        // manually adjust delegation info as to indicate the rewarding information shouldnt get removed
+        let mut rewarding_details = test.mix_rewarding(mix_id_leftover);
+        rewarding_details.delegates = Decimal::raw(12345);
+        rewarding_details.unique_delegations = 1;
+        rewards_storage::MIXNODE_REWARDING
+            .save(test.deps_mut().storage, mix_id_leftover, &rewarding_details)
+            .unwrap();
+
+        let bond1 = storage::mixnode_bonds()
+            .load(test.deps().storage, mix_id)
+            .unwrap();
+        let bond2 = storage::mixnode_bonds()
+            .load(test.deps().storage, mix_id_leftover)
+            .unwrap();
+
+        let env = test.env();
+        let details1 = get_mixnode_details_by_id(test.deps().storage, mix_id)
+            .unwrap()
+            .unwrap();
+        cleanup_post_unbond_mixnode_storage(test.deps_mut().storage, &env, &details1).unwrap();
+
+        // bond information is gone
+        let bond = storage::mixnode_bonds()
+            .may_load(test.deps().storage, mix_id)
+            .unwrap();
+        assert!(bond.is_none());
+
+        // rewarding information is gone
+        let mix_rewarding = rewards_storage::MIXNODE_REWARDING
+            .may_load(test.deps().storage, mix_id)
+            .unwrap();
+        assert!(mix_rewarding.is_none());
+
+        // unbonded details are inserted
+        let unbonded_details = storage::UNBONDED_MIXNODES
+            .load(test.deps().storage, mix_id)
+            .unwrap();
+        let expected = UnbondedMixnode {
+            identity: bond1.mix_node.identity_key,
+            owner: bond1.owner,
+            unbonding_height: env.block.height,
+        };
+        assert_eq!(unbonded_details, expected);
+
+        // layers are decremented
+        assert_eq!(storage::LAYERS.load(test.deps().storage).unwrap().layer1, 0);
+
+        let details2 = get_mixnode_details_by_id(test.deps().storage, mix_id_leftover)
+            .unwrap()
+            .unwrap();
+        cleanup_post_unbond_mixnode_storage(test.deps_mut().storage, &env, &details2).unwrap();
+
+        // bond information is gone
+        let bond = storage::mixnode_bonds()
+            .may_load(test.deps().storage, mix_id_leftover)
+            .unwrap();
+        assert!(bond.is_none());
+
+        // rewarding information is NOT gone, but operator field is reset
+        let mix_rewarding = rewards_storage::MIXNODE_REWARDING
+            .may_load(test.deps().storage, mix_id_leftover)
+            .unwrap()
+            .unwrap();
+        assert!(mix_rewarding.operator.is_zero());
+        assert_eq!(mix_rewarding.unique_delegations, 1);
+
+        // unbonded details are inserted
+        let unbonded_details = storage::UNBONDED_MIXNODES
+            .load(test.deps().storage, mix_id_leftover)
+            .unwrap();
+        let expected = UnbondedMixnode {
+            identity: bond2.mix_node.identity_key,
+            owner: bond2.owner,
+            unbonding_height: env.block.height,
+        };
+        assert_eq!(unbonded_details, expected);
+
+        // layers are decremented
+        assert_eq!(storage::LAYERS.load(test.deps().storage).unwrap().layer2, 0);
+    }
 }
--- contracts/mixnet/src/rewards/helpers.rs
+++ contracts/mixnet/src/rewards/helpers.rs
@@ -71,3 +71,208 @@ pub(crate) fn withdraw_delegator_reward(
     storage::MIXNODE_REWARDING.save(store, mix_id, &mix_rewarding)?;
     Ok(reward)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mixnodes::helpers::get_mixnode_details_by_id;
+    use crate::rewards::models::RewardPoolChange;
+    use crate::support::tests::test_helpers::{assert_decimals, performance, TestSetup};
+    use cosmwasm_std::Uint128;
+    use mixnet_contract_common::rewarding::helpers::truncate_reward_amount;
+
+    #[test]
+    fn applying_reward_pool_changes() {
+        let mut test = TestSetup::new();
+
+        let epochs_in_interval = test.current_interval().epochs_in_interval();
+        let epochs_in_interval_dec = Decimal::from_atomics(epochs_in_interval, 0).unwrap();
+        let start_rewarding_params = test.rewarding_params();
+
+        // nothing changes if pending changes are empty
+        apply_reward_pool_changes(test.deps_mut().storage).unwrap();
+        assert_eq!(start_rewarding_params, test.rewarding_params());
+
+        // normal case of having distributed some rewards
+        let distributed_rewards = Decimal::from_atomics(100_000_000u32, 0).unwrap();
+        storage::PENDING_REWARD_POOL_CHANGE
+            .save(
+                test.deps_mut().storage,
+                &RewardPoolChange {
+                    removed: distributed_rewards,
+                    added: Default::default(),
+                },
+            )
+            .unwrap();
+        apply_reward_pool_changes(test.deps_mut().storage).unwrap();
+
+        let updated_rewarding_params = test.rewarding_params();
+
+        // updates reward pool
+        assert_eq!(
+            updated_rewarding_params.interval.reward_pool,
+            start_rewarding_params.interval.reward_pool - distributed_rewards
+        );
+
+        // updates staking supply
+        assert_eq!(
+            updated_rewarding_params.interval.staking_supply,
+            start_rewarding_params.interval.staking_supply + distributed_rewards
+        );
+
+        // updates epoch rewarding budget
+        assert_eq!(
+            updated_rewarding_params.interval.epoch_reward_budget,
+            updated_rewarding_params.interval.reward_pool / epochs_in_interval_dec
+                * updated_rewarding_params.interval.interval_pool_emission
+        );
+
+        // updates stake saturation point
+        assert_eq!(
+            updated_rewarding_params.interval.stake_saturation_point,
+            updated_rewarding_params.interval.staking_supply
+                / Decimal::from_atomics(updated_rewarding_params.rewarded_set_size, 0).unwrap()
+        );
+
+        // resets changes back to 0
+        assert_eq!(
+            storage::PENDING_REWARD_POOL_CHANGE
+                .load(test.deps().storage)
+                .unwrap(),
+            Default::default()
+        );
+
+        // future case of having to also increase the reward pool
+        let added_credentials = Decimal::from_atomics(50_000_000u32, 0).unwrap();
+        storage::PENDING_REWARD_POOL_CHANGE
+            .save(
+                test.deps_mut().storage,
+                &RewardPoolChange {
+                    removed: distributed_rewards,
+                    added: added_credentials,
+                },
+            )
+            .unwrap();
+        apply_reward_pool_changes(test.deps_mut().storage).unwrap();
+
+        let updated_rewarding_params2 = test.rewarding_params();
+
+        // updates reward pool
+        assert_eq!(
+            updated_rewarding_params2.interval.reward_pool,
+            updated_rewarding_params.interval.reward_pool - distributed_rewards + added_credentials
+        );
+
+        // updates staking supply
+        assert_eq!(
+            updated_rewarding_params2.interval.staking_supply,
+            updated_rewarding_params.interval.staking_supply + distributed_rewards
+        );
+
+        // updates epoch rewarding budget
+        assert_eq!(
+            updated_rewarding_params2.interval.epoch_reward_budget,
+            updated_rewarding_params2.interval.reward_pool / epochs_in_interval_dec
+                * updated_rewarding_params2.interval.interval_pool_emission
+        );
+
+        // updates stake saturation point
+        assert_eq!(
+            updated_rewarding_params2.interval.stake_saturation_point,
+            updated_rewarding_params2.interval.staking_supply
+                / Decimal::from_atomics(updated_rewarding_params2.rewarded_set_size, 0).unwrap()
+        );
+
+        // resets changes back to 0
+        assert_eq!(
+            storage::PENDING_REWARD_POOL_CHANGE
+                .load(test.deps().storage)
+                .unwrap(),
+            Default::default()
+        );
+    }
+
+    #[test]
+    fn withdrawing_operator_reward() {
+        let mut test = TestSetup::new();
+
+        let pledge = Uint128::new(250_000_000);
+        let pledge_dec = Decimal::from_atomics(250_000_000u32, 0).unwrap();
+        let mix_id = test.add_dummy_mixnode("mix-owner", Some(pledge));
+
+        // no rewards
+        let mix_details = get_mixnode_details_by_id(test.deps().storage, mix_id)
+            .unwrap()
+            .unwrap();
+        let res = withdraw_operator_reward(test.deps_mut().storage, mix_details).unwrap();
+        assert_eq!(res.amount, Uint128::zero());
+
+        test.skip_to_next_epoch_end();
+        test.update_rewarded_set(vec![mix_id]);
+        let dist1 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        test.skip_to_next_epoch_end();
+        let dist2 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        test.skip_to_next_epoch_end();
+        let dist3 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        let mix_details = get_mixnode_details_by_id(test.deps().storage, mix_id)
+            .unwrap()
+            .unwrap();
+        let res = withdraw_operator_reward(test.deps_mut().storage, mix_details).unwrap();
+        assert_eq!(
+            res.amount,
+            truncate_reward_amount(dist1.operator + dist2.operator + dist3.operator)
+        );
+        let updated = test.mix_rewarding(mix_id);
+        assert_eq!(updated.operator, pledge_dec)
+    }
+
+    #[test]
+    fn withdrawing_delegator_reward() {
+        let mut test = TestSetup::new();
+
+        let delegation_amount = Uint128::new(2500_000_000);
+        let delegation_dec = Decimal::from_atomics(2500_000_000u32, 0).unwrap();
+        let mix_id = test.add_dummy_mixnode("mix-owner", None);
+        let delegator = "delegator";
+        test.add_immediate_delegation(delegator, delegation_amount, mix_id);
+
+        // no rewards
+        let delegation = test.delegation(mix_id, delegator, &None);
+        let mix_rewarding = test.mix_rewarding(mix_id);
+        let res =
+            withdraw_delegator_reward(test.deps_mut().storage, delegation, mix_rewarding).unwrap();
+        assert_eq!(res.amount, Uint128::zero());
+
+        test.skip_to_next_epoch_end();
+        test.update_rewarded_set(vec![mix_id]);
+        let dist1 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        test.skip_to_next_epoch_end();
+        let dist2 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        test.skip_to_next_epoch_end();
+        let dist3 = test.reward_with_distribution(mix_id, performance(100.0));
+
+        let delegation_pre = test.delegation(mix_id, delegator, &None);
+        let mix_rewarding = test.mix_rewarding(mix_id);
+        let res = withdraw_delegator_reward(
+            test.deps_mut().storage,
+            delegation_pre.clone(),
+            mix_rewarding,
+        )
+        .unwrap();
+        let reward = dist1.delegates + dist2.delegates + dist3.delegates;
+        assert_eq!(res.amount, truncate_reward_amount(reward));
+        let updated = test.mix_rewarding(mix_id);
+        assert_decimals(updated.delegates, delegation_dec);
+
+        let delegation_post = test.delegation(mix_id, delegator, &None);
+        assert_ne!(
+            delegation_pre.cumulative_reward_ratio,
+            delegation_post.cumulative_reward_ratio
+        )
+    }
+}

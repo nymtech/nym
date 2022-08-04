d883e57e15004152228e0e59f38767315e358795
fmtabbara <fmtabbara@hotmail.co.uk>
2022-08-04 15:34:31 +0100

    fix nymcard title size

--- nym-wallet/src/components/Title.tsx
+++ nym-wallet/src/components/Title.tsx
@@ -4,7 +4,7 @@ import { Box, Typography } from '@mui/material';
 export const Title: React.FC<{ title: string | React.ReactNode; Icon?: React.ReactNode }> = ({ title, Icon }) => (
   <Box width="100%" display="flex" alignItems="center">
     {Icon}
-    <Typography width="100%" variant="h6" sx={{ fontWeight: 600 }}>
+    <Typography width="100%" variant="h5" sx={{ fontWeight: 600 }}>
       {title}
     </Typography>
   </Box>

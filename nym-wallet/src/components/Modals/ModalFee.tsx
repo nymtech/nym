18e4c5adda489590720213c5fb6f1fe847d29d27
fmtabbara <fmtabbara@hotmail.co.uk>
2022-08-05 16:58:19 +0100

    rebase develop

--- nym-wallet/src/components/Modals/ModalFee.tsx
+++ nym-wallet/src/components/Modals/ModalFee.tsx
@@ -13,14 +13,9 @@ const getValue = ({ fee, isLoading, error }: TFeeProps) => {
   return '-';
 };
 
-<<<<<<< HEAD
-export const ModalFee = ({ fee, isLoading, error }: TFeeProps) => (
-  <ModalListItem label="Estimated fee for this operation:" value={getValue({ fee, isLoading, error })} />
-=======
 export const ModalFee = ({ fee, isLoading, error, divider }: TFeeProps) => (
   <>
-    <ModalListItem label="Fee for this operation" value={getValue({ fee, isLoading, error })} />
+    <ModalListItem label="Fee for this operation:" value={getValue({ fee, isLoading, error })} />
     {divider && <ModalDivider />}
   </>
->>>>>>> 1685352c8 (check balance and fetch fee on bond mixnode request)
 );

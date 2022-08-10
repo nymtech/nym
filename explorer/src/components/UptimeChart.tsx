a74a99d81d1024e7ad9c0193dae753c58b92c847
Gala <calero.vg@gmail.com>
2022-08-10 16:38:56 +0200

    cleaning

--- explorer/src/components/UptimeChart.tsx
+++ explorer/src/components/UptimeChart.tsx
@@ -23,7 +23,7 @@ export const UptimeChart: React.FC<ChartProps> = ({ title, xLabel, yLabel, uptim
   const color = theme.palette.text.primary;
   React.useEffect(() => {
     if (uptimeStory.data?.history) {
-      const allFormattedChartData: FormattedChartData = [['Date', 'Routing Score']];
+      const allFormattedChartData: FormattedChartData = [['Date', 'Uptime']];
       uptimeStory.data.history.forEach((eachDate) => {
         const formattedDateUptimeRecord: FormattedDateRecord = [
           format(new Date(eachDate.date), 'MMM dd'),
@@ -34,7 +34,7 @@ export const UptimeChart: React.FC<ChartProps> = ({ title, xLabel, yLabel, uptim
       setFormattedChartData(allFormattedChartData);
     } else {
       const emptyData: any = [
-        ['Date', 'Routing Score'],
+        ['Date', 'Uptime'],
         ['Jul 27', 10],
       ];
       setFormattedChartData(emptyData);
@@ -55,7 +55,7 @@ export const UptimeChart: React.FC<ChartProps> = ({ title, xLabel, yLabel, uptim
             uptimeStory.data
               ? formattedChartData
               : [
-                  ['Date', 'Routing Score'],
+                  ['Date', 'Uptime'],
                   [format(new Date(Date.now()), 'MMM dd'), 0],
                 ]
           }

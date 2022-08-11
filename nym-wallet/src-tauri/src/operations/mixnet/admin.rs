446a5b9e03f9c948934391f1afb28220d6f9a0e2
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-11 14:20:13 +0100

    'update_contract_settings'

--- nym-wallet/src-tauri/src/operations/mixnet/admin.rs
+++ nym-wallet/src-tauri/src/operations/mixnet/admin.rs
@@ -1,10 +1,12 @@
+// Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::error::BackendError;
-use crate::nymd_client;
 use crate::state::WalletState;
 use mixnet_contract_common::ContractStateParams;
+use nym_types::transaction::TransactionExecuteResult;
 use nym_wallet_types::admin::TauriContractStateParams;
-use std::convert::TryInto;
-use validator_client::nymd::traits::MixnetQueryClient;
+use validator_client::nymd::traits::{MixnetQueryClient, MixnetSigningClient};
 use validator_client::nymd::Fee;
 
 #[tauri::command]
@@ -28,17 +30,25 @@ pub async fn update_contract_settings(
     params: TauriContractStateParams,
     fee: Option<Fee>,
     state: tauri::State<'_, WalletState>,
-) -> Result<TauriContractStateParams, BackendError> {
-    todo!()
-    // let mixnet_contract_settings_params: ContractStateParams = params.try_into()?;
-    // log::info!(
-    //     ">>> Updating contract settings: {:?}",
-    //     mixnet_contract_settings_params
-    // );
-    // nymd_client!(state)
-    //     .update_contract_settings(mixnet_contract_settings_params.clone(), fee)
-    //     .await?;
-    // let res = mixnet_contract_settings_params.into();
-    // log::trace!("<<< {:?}", res);
-    // Ok(res)
+) -> Result<TransactionExecuteResult, BackendError> {
+    let guard = state.read().await;
+    let reg = guard.registered_coins()?;
+    let client = &guard.current_client()?.nymd;
+    let fee_amount = guard.convert_tx_fee(fee.as_ref());
+
+    let mixnet_contract_settings_params: ContractStateParams =
+        params.try_convert_to_mixnet_contract_params(reg)?;
+    log::info!(
+        ">>> Updating contract settings: {:?}",
+        mixnet_contract_settings_params
+    );
+    let res = client
+        .update_contract_state_params(mixnet_contract_settings_params, fee)
+        .await?;
+
+    log::info!("<<< tx hash = {}", res.transaction_hash);
+    log::trace!("<<< {:?}", res);
+    Ok(TransactionExecuteResult::from_execute_result(
+        res, fee_amount,
+    )?)
 }

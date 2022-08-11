cddb67f8f1049684d441087f47f7324988ed8e1a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-11 16:19:53 +0100

    Command for obtaining current interval details

--- nym-wallet/src-tauri/src/operations/mixnet/interval.rs
+++ nym-wallet/src-tauri/src/operations/mixnet/interval.rs
@@ -2,14 +2,14 @@ use crate::error::BackendError;
 use crate::nymd_client;
 use crate::state::WalletState;
 use nym_wallet_types::interval::Interval;
+use validator_client::nymd::traits::MixnetQueryClient;
 
 #[tauri::command]
 pub async fn get_current_interval(
     state: tauri::State<'_, WalletState>,
 ) -> Result<Interval, BackendError> {
-    todo!()
-    // log::info!(">>> Get current interval");
-    // let interval = nymd_client!(state).get_current_interval().await?;
-    // log::info!("<<< current interval = {}", interval);
-    // Ok(interval.into())
+    log::info!(">>> Get current interval");
+    let res = nymd_client!(state).get_current_interval_details().await?;
+    log::info!("<<< current interval = {:?}", res);
+    Ok(res.interval.into())
 }

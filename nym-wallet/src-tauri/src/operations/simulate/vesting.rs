4bbdae48138d83a584181cde10986b6803b8e81e
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-12 15:05:12 +0100

    Vesting simulations

--- nym-wallet/src-tauri/src/operations/simulate/vesting.rs
+++ nym-wallet/src-tauri/src/operations/simulate/vesting.rs
@@ -4,10 +4,37 @@
 use crate::error::BackendError;
 use crate::operations::simulate::FeeDetails;
 use crate::WalletState;
-use mixnet_contract_common::IdentityKey;
-use mixnet_contract_common::{Gateway, MixNode};
+use mixnet_contract_common::{Gateway, MixNode, NodeId};
+use mixnet_contract_common::{IdentityKey, MixNodeConfigUpdate};
 use nym_types::currency::DecCoin;
+use nym_types::mixnode::MixNodeCostParams;
 use vesting_contract_common::ExecuteMsg;
+
+async fn simulate_vesting_operation(
+    msg: ExecuteMsg,
+    raw_funds: Option<DecCoin>,
+    state: &WalletState,
+) -> Result<FeeDetails, BackendError> {
+    let guard = state.read().await;
+    let funds = if let Some(funds) = raw_funds
+        .map(|c| guard.attempt_convert_to_base_coin(c))
+        .transpose()?
+    {
+        vec![funds]
+    } else {
+        Vec::new()
+    };
+
+    let client = guard.current_client()?;
+    let vesting_contract = client.nymd.vesting_contract_address();
+
+    let msg = client
+        .nymd
+        .wrap_contract_execute_message(vesting_contract, &msg, funds)?;
+
+    let result = client.nymd.simulate(vec![msg]).await?;
+    guard.create_detailed_fee(result)
+}
 
 #[tauri::command]
 pub async fn simulate_vesting_bond_gateway(
@@ -16,165 +43,114 @@ pub async fn simulate_vesting_bond_gateway(
     owner_signature: String,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let pledge = guard.attempt_convert_to_base_coin(pledge)?;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::BondGateway {
-    //         gateway,
-    //         owner_signature,
-    //         amount: pledge.into(),
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let amount = guard.attempt_convert_to_base_coin(pledge)?.into();
+
+    simulate_vesting_operation(
+        ExecuteMsg::BondGateway {
+            gateway,
+            owner_signature,
+            amount,
+        },
+        None,
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_unbond_gateway(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::UnbondGateway {},
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_vesting_operation(ExecuteMsg::UnbondGateway {}, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_bond_mixnode(
     mixnode: MixNode,
+    cost_params: MixNodeCostParams,
     owner_signature: String,
     pledge: DecCoin,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let pledge = guard.attempt_convert_to_base_coin(pledge)?;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::BondMixnode {
-    //         mix_node: mixnode,
-    //         owner_signature,
-    //         amount: pledge.into(),
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let reg = guard.registered_coins()?;
+    let cost_params = cost_params.try_convert_to_mixnet_contract_cost_params(reg)?;
+    let amount = guard.attempt_convert_to_base_coin(pledge)?.into();
+
+    simulate_vesting_operation(
+        ExecuteMsg::BondMixnode {
+            mix_node: mixnode,
+            cost_params,
+            owner_signature,
+            amount,
+        },
+        None,
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_unbond_mixnode(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::UnbondMixnode {},
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_vesting_operation(ExecuteMsg::UnbondMixnode {}, None, &state).await
 }
 
 #[tauri::command]
-pub async fn simulate_vesting_update_mixnode(
-    profit_margin_percent: u8,
+pub async fn simulate_vesting_update_mixnode_cost_params(
+    new_costs: MixNodeCostParams,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::UpdateMixnodeConfig {
-    //         profit_margin_percent,
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let reg = guard.registered_coins()?;
+    let new_costs = new_costs.try_convert_to_mixnet_contract_cost_params(reg)?;
+
+    simulate_vesting_operation(
+        ExecuteMsg::UpdateMixnodeCostParams { new_costs },
+        None,
+        &state,
+    )
+    .await
+}
+
+#[tauri::command]
+pub async fn simulate_vesting_update_mixnode_config(
+    update: MixNodeConfigUpdate,
+    state: tauri::State<'_, WalletState>,
+) -> Result<FeeDetails, BackendError> {
+    simulate_vesting_operation(
+        ExecuteMsg::UpdateMixnodeConfig { new_config: update },
+        None,
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_delegate_to_mixnode(
-    identity: &str,
+    mix_id: NodeId,
     amount: DecCoin,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let amount = guard.attempt_convert_to_base_coin(amount)?.into();
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::DelegateToMixnode {
-    //         mix_identity: identity.to_string(),
-    //         amount,
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let amount = guard.attempt_convert_to_base_coin(amount)?.into();
+
+    simulate_vesting_operation(
+        ExecuteMsg::DelegateToMixnode { mix_id, amount },
+        None,
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_undelegate_from_mixnode(
-    identity: &str,
+    mix_id: NodeId,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::UndelegateFromMixnode {
-    //         mix_identity: identity.to_string(),
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_vesting_operation(ExecuteMsg::UndelegateFromMixnode { mix_id }, None, &state).await
 }
 
 #[tauri::command]
@@ -182,81 +158,22 @@ pub async fn simulate_withdraw_vested_coins(
     amount: DecCoin,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let amount = guard.attempt_convert_to_base_coin(amount)?.into();
-    //
-    // let client = guard.current_client()?;
-    // let vesting_contract = client.nymd.vesting_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     vesting_contract,
-    //     &ExecuteMsg::WithdrawVestedCoins { amount },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let amount = guard.attempt_convert_to_base_coin(amount)?.into();
+    simulate_vesting_operation(ExecuteMsg::WithdrawVestedCoins { amount }, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_claim_operator_reward(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_vesting_claim_operator_reward(None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
-}
-
-#[tauri::command]
-pub async fn simulate_vesting_compound_operator_reward(
-    state: tauri::State<'_, WalletState>,
-) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_vesting_compound_operator_reward(None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
+    simulate_vesting_operation(ExecuteMsg::ClaimOperatorReward {}, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_vesting_claim_delegator_reward(
-    mix_identity: IdentityKey,
+    mix_id: NodeId,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_vesting_claim_delegator_reward(mix_identity, None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
-}
-
-#[tauri::command]
-pub async fn simulate_vesting_compound_delegator_reward(
-    mix_identity: IdentityKey,
-    state: tauri::State<'_, WalletState>,
-) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_vesting_compound_delegator_reward(mix_identity, None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
+    simulate_vesting_operation(ExecuteMsg::ClaimDelegatorReward { mix_id }, None, &state).await
 }

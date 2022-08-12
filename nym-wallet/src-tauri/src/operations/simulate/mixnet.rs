0e73a3d88cc8a602328a5034a78ecc0fa22480bb
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-12 12:41:19 +0100

    mixnet-related simulation commands

--- nym-wallet/src-tauri/src/operations/simulate/mixnet.rs
+++ nym-wallet/src-tauri/src/operations/simulate/mixnet.rs
@@ -4,9 +4,36 @@
 use crate::error::BackendError;
 use crate::operations::simulate::FeeDetails;
 use crate::WalletState;
-use mixnet_contract_common::IdentityKey;
-use mixnet_contract_common::{ExecuteMsg, Gateway, MixNode};
+use mixnet_contract_common::{ExecuteMsg, Gateway, MixNode, NodeId};
+use mixnet_contract_common::{IdentityKey, MixNodeConfigUpdate};
 use nym_types::currency::DecCoin;
+use nym_types::mixnode::MixNodeCostParams;
+
+async fn simulate_mixnet_operation(
+    msg: ExecuteMsg,
+    raw_funds: Option<DecCoin>,
+    state: &WalletState,
+) -> Result<FeeDetails, BackendError> {
+    let guard = state.read().await;
+    let funds = if let Some(funds) = raw_funds
+        .map(|c| guard.attempt_convert_to_base_coin(c))
+        .transpose()?
+    {
+        vec![funds]
+    } else {
+        Vec::new()
+    };
+
+    let client = guard.current_client()?;
+    let mixnet_contract = client.nymd.mixnet_contract_address();
+
+    let msg = client
+        .nymd
+        .wrap_contract_execute_message(mixnet_contract, &msg, funds)?;
+
+    let result = client.nymd.simulate(vec![msg]).await?;
+    guard.create_detailed_fee(result)
+}
 
 #[tauri::command]
 pub async fn simulate_bond_gateway(
@@ -15,213 +42,118 @@ pub async fn simulate_bond_gateway(
     owner_signature: String,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let pledge = guard.attempt_convert_to_base_coin(pledge)?;
-    //
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // // TODO: I'm still not 100% convinced whether this should be exposed here or handled somewhere else in the client code
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::BondGateway {
-    //         gateway,
-    //         owner_signature,
-    //     },
-    //     vec![pledge],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(
+        ExecuteMsg::BondGateway {
+            gateway,
+            owner_signature,
+        },
+        Some(pledge),
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_unbond_gateway(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::UnbondGateway {},
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(ExecuteMsg::UnbondGateway {}, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_bond_mixnode(
     mixnode: MixNode,
+    cost_params: MixNodeCostParams,
     owner_signature: String,
     pledge: DecCoin,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let pledge = guard.attempt_convert_to_base_coin(pledge)?;
-    //
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::BondMixnode {
-    //         mix_node: mixnode,
-    //         owner_signature,
-    //     },
-    //     vec![pledge],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let reg = guard.registered_coins()?;
+    let cost_params = cost_params.try_convert_to_mixnet_contract_cost_params(reg)?;
+
+    simulate_mixnet_operation(
+        ExecuteMsg::BondMixnode {
+            mix_node: mixnode,
+            cost_params,
+            owner_signature,
+        },
+        Some(pledge),
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_unbond_mixnode(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::UnbondMixnode {},
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(ExecuteMsg::UnbondMixnode {}, None, &state).await
 }
 
 #[tauri::command]
-pub async fn simulate_update_mixnode(
-    profit_margin_percent: u8,
+pub async fn simulate_update_mixnode_cost_params(
+    new_costs: MixNodeCostParams,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::UpdateMixnodeConfig {
-    //         profit_margin_percent,
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    let guard = state.read().await;
+    let reg = guard.registered_coins()?;
+    let new_costs = new_costs.try_convert_to_mixnet_contract_cost_params(reg)?;
+
+    simulate_mixnet_operation(
+        ExecuteMsg::UpdateMixnodeCostParams { new_costs },
+        None,
+        &state,
+    )
+    .await
+}
+
+#[tauri::command]
+pub async fn simulate_update_mixnode_config(
+    update: MixNodeConfigUpdate,
+    state: tauri::State<'_, WalletState>,
+) -> Result<FeeDetails, BackendError> {
+    simulate_mixnet_operation(
+        ExecuteMsg::UpdateMixnodeConfig { new_config: update },
+        None,
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_delegate_to_mixnode(
-    identity: &str,
+    mix_id: NodeId,
     amount: DecCoin,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let delegation = guard.attempt_convert_to_base_coin(amount)?;
-    //
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::DelegateToMixnode {
-    //         mix_identity: identity.to_string(),
-    //     },
-    //     vec![delegation],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(
+        ExecuteMsg::DelegateToMixnode { mix_id },
+        Some(amount),
+        &state,
+    )
+    .await
 }
 
 #[tauri::command]
 pub async fn simulate_undelegate_from_mixnode(
-    identity: &str,
+    mix_id: NodeId,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    // let mixnet_contract = client.nymd.mixnet_contract_address();
-    //
-    // let msg = client.nymd.wrap_contract_execute_message(
-    //     mixnet_contract,
-    //     &ExecuteMsg::UndelegateFromMixnode {
-    //         mix_identity: identity.to_string(),
-    //     },
-    //     vec![],
-    // )?;
-    //
-    // let result = client.nymd.simulate(vec![msg]).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(ExecuteMsg::UndelegateFromMixnode { mix_id }, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_claim_operator_reward(
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client.nymd.simulate_claim_operator_reward(None).await?;
-    // guard.create_detailed_fee(result)
-}
-
-#[tauri::command]
-pub async fn simulate_compound_operator_reward(
-    state: tauri::State<'_, WalletState>,
-) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client.nymd.simulate_compound_operator_reward(None).await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(ExecuteMsg::WithdrawOperatorReward {}, None, &state).await
 }
 
 #[tauri::command]
 pub async fn simulate_claim_delegator_reward(
-    mix_identity: IdentityKey,
+    mix_id: NodeId,
     state: tauri::State<'_, WalletState>,
 ) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_claim_delegator_reward(mix_identity, None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
-}
-
-#[tauri::command]
-pub async fn simulate_compound_delegator_reward(
-    mix_identity: IdentityKey,
-    state: tauri::State<'_, WalletState>,
-) -> Result<FeeDetails, BackendError> {
-    todo!()
-    // let guard = state.read().await;
-    // let client = guard.current_client()?;
-    //
-    // let result = client
-    //     .nymd
-    //     .simulate_compound_delegator_reward(mix_identity, None)
-    //     .await?;
-    // guard.create_detailed_fee(result)
+    simulate_mixnet_operation(ExecuteMsg::WithdrawDelegatorReward { mix_id }, None, &state).await
 }

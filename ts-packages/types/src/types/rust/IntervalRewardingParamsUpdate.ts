e21b3eb74c54c6493027bc3d7b8c7fcc4a63ca0b
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-08-15 10:07:20 +0100

    Exporting reward-related types

---
+++ ts-packages/types/src/types/rust/IntervalRewardingParamsUpdate.ts
@@ -0,0 +1,2 @@
+
+export interface IntervalRewardingParamsUpdate { reward_pool: string | null, staking_supply: string | null, sybil_resistance_percent: string | null, active_set_work_factor: string | null, interval_pool_emission: string | null, rewarded_set_size: number | null, }

1bb137f87f4560bd6cd5c6e5b6ab944546a8beb4
Pierre Dommerc <dommerc.pierre@gmail.com>
2022-08-18 18:53:03 +0200

    Nym-connect - service provider persistant storage (#1540)
    * feat(nym-connect): local storage service provider
    
    * feat(nym-connect): local storage service provider
    
    * feat(nym-connect): local storage service provider
    
    * Add some extra height to the window to stop the scrollbar apearing
    
    * Show the service description when selecting a Service Provider
    
    * Bump version
    
    * Update changelog
    
    * fix(nym-connect): hotfix
    
    * fix(nym-connect): hotfix
    
    * fix(nym-connect): wrong disabled state for connection button
    
    Co-authored-by: Mark Sinclair <mmsinclair@gmail.com>

--- nym-connect/src/layouts/DefaultLayout.tsx
+++ nym-connect/src/layouts/DefaultLayout.tsx
@@ -6,6 +6,7 @@ import { ConnectionStatusKind } from '../types';
 import { NeedHelp } from '../components/NeedHelp';
 import { ServiceProviderSelector } from '../components/ServiceProviderSelector';
 import { ServiceProvider, Services } from '../types/directory';
+import { useClientContext } from '../context/main';
 
 export const DefaultLayout: React.FC<{
   status: ConnectionStatusKind;
@@ -20,6 +21,8 @@ export const DefaultLayout: React.FC<{
     setServiceProvider(newServiceProvider);
     onServiceProviderChange?.(newServiceProvider);
   };
+  const { serviceProvider: currentSp } = useClientContext();
+
   return (
     <AppWindowFrame>
       <Typography fontWeight="400" fontSize="12px" textAlign="center" sx={{ opacity: 0.6 }}>
@@ -31,10 +34,10 @@ export const DefaultLayout: React.FC<{
         <br />
         Nym mixnet for privacy.
       </Typography>
-      <ServiceProviderSelector services={services} onChange={handleServiceProviderChange} />
+      <ServiceProviderSelector services={services} onChange={handleServiceProviderChange} currentSp={currentSp} />
       <ConnectionButton
         status={status}
-        disabled={serviceProvider === undefined}
+        disabled={serviceProvider === undefined && currentSp === undefined}
         busy={busy}
         isError={isError}
         onClick={onConnectClick}

7efe7e90c117aef1c207ddf51c34cad15b5cf690
pierre <dommerc.pierre@gmail.com>
2022-08-23 11:49:51 +0200

    fix(nym-wallet): tauri request parameters

--- nym-wallet/src/requests/utils.ts
+++ nym-wallet/src/requests/utils.ts
@@ -7,5 +7,5 @@ export const getEnv = async () => invokeWrapper<AppEnv>('get_env');
 export const tryConvertIdentityToMixId = async (mixIdentity: string) =>
   invokeWrapper<number | null>('try_convert_pubkey_to_mix_id', { mixIdentity });
 
-export const getDefaultMixnodeCostParams = async (profit_margin_percent: string) =>
-  invokeWrapper<MixNodeCostParams>('default_mixnode_cost_params', { profit_margin_percent });
+export const getDefaultMixnodeCostParams = async (profitMarginPercent: string) =>
+  invokeWrapper<MixNodeCostParams>('default_mixnode_cost_params', { profitMarginPercent });

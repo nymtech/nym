7b53003edb48d2f423c990482f9b41b03b1134ad
Gala <calero.vg@gmail.com>
2022-08-31 18:49:47 +0200

    wip verification modal

--- nym-wallet/src/components/Bonding/modals/UnbondModal.tsx
+++ nym-wallet/src/components/Bonding/modals/UnbondModal.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { Typography } from '@mui/material';
+import { useState } from 'react';
+import { Typography, Alert, TextField } from '@mui/material';
 import { useEffect } from 'react';
 import { TBondedGateway, TBondedMixnode } from 'src/context';
 import { useGetFee } from 'src/hooks/useGetFee';
@@ -21,8 +21,12 @@ interface Props {
   onError: (e: string) => void;
 }
 
+type TUnbondModalStep = 1 | 2;
+
 export const UnbondModal = ({ node, onConfirm, onClose, onError }: Props) => {
   const { fee, isFeeLoading, getFee, feeError } = useGetFee();
+  const [step, setStep] = useState<TUnbondModalStep>(1);
+  const [verificationText, setVerificationText] = useState<string>('');
 
   useEffect(() => {
     if (feeError) {
@@ -47,28 +51,63 @@ export const UnbondModal = ({ node, onConfirm, onClose, onError }: Props) => {
       getFee(simulateVestingUnbondGateway, {});
     }
   }, [node]);
-
+  console.log('step', step === 1);
   return (
-    <SimpleModal
-      open
-      header="Unbond"
-      subHeader="Unbond and remove your node from the mixnet"
-      okLabel="Unbond"
-      onOk={onConfirm}
-      onClose={onClose}
-    >
-      <ModalListItem label="Amount to unbond" value={`${node.bond.amount} ${node.bond.denom.toUpperCase()}`} divider />
-      {isMixnode(node) && (
-        <ModalListItem
-          label="Operator rewards"
-          value={
-            node.operatorRewards ? `${node.operatorRewards.amount} ${node.operatorRewards.denom.toUpperCase()}` : '-'
-          }
-          divider
-        />
+    <div>
+      {step === 1 && (
+        <SimpleModal
+          open
+          header="Unbond"
+          subHeader="If you unbond your node you will loose all your delegators!"
+          okLabel="Unbond"
+          okDisabled={Boolean(!verificationText.match('UNBOUND'))}
+          onOk={onConfirm}
+          onClose={onClose}
+        >
+          <Alert severity="error">
+            This action is irreversible and it will not be possible to restore the current state again
+          </Alert>
+          <Typography>
+            To unbond, type <span style={{ color: 'orange' }}>UNBOND</span> in the field below and click UNBOND button
+          </Typography>
+          <TextField
+            type="input"
+            value={verificationText}
+            onChange={(e) => setVerificationText(e.target.value)}
+            autoFocus
+            fullWidth
+          />
+        </SimpleModal>
       )}
-      <ModalFee isLoading={isFeeLoading} fee={fee} divider />
-      <Typography fontSize="small">Tokens will be transferred to the account you are logged in with now</Typography>
-    </SimpleModal>
+      {step === 2 && (
+        <SimpleModal
+          open
+          header="Unbond"
+          subHeader="Unbond and remove your node from the mixnet"
+          okLabel="Unbond"
+          onOk={onConfirm}
+          onClose={onClose}
+        >
+          <ModalListItem
+            label="Amount to unbond"
+            value={`${node.bond.amount} ${node.bond.denom.toUpperCase()}`}
+            divider
+          />
+          {isMixnode(node) && (
+            <ModalListItem
+              label="Operator rewards"
+              value={
+                node.operatorRewards
+                  ? `${node.operatorRewards.amount} ${node.operatorRewards.denom.toUpperCase()}`
+                  : '-'
+              }
+              divider
+            />
+          )}
+          <ModalFee isLoading={isFeeLoading} fee={fee} divider />
+          <Typography fontSize="small">Tokens will be transferred to the account you are logged in with now</Typography>
+        </SimpleModal>
+      )}
+    </div>
   );
 };

666d8665aef32c7f17373d8f4146cc2d66cc55c1
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-09-01 15:23:44 +0100

    Updated typescript side of things

--- ts-packages/types/src/types/rust/DelegationRecord.ts
+++
@@ -1,8 +0,0 @@
-import type { DecCoin } from './DecCoin';
-
-export interface DelegationRecord {
-  amount: DecCoin;
-  block_height: bigint;
-  delegated_on_iso_datetime: string;
-  uses_vesting_contract_tokens: boolean;
-}

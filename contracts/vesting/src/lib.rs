2363f3ad0a1d9e1f70942c2f8f470848f8828765
Drazen Urch <drazen@urch.eu>
2022-09-07 01:41:51 +0200

    Initial docs pass (#1582)

--- contracts/vesting/src/lib.rs
+++ contracts/vesting/src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(rustdoc::private_intra_doc_links)]
+//! Nym vesting contract, providing vesting accounts with ability to stake unvested tokens
+
 pub mod contract;
 mod errors;
 mod queued_migrations;

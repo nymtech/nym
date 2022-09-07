a99782825c445c5988d590d19279c7419d200683
pierre <dommerc.pierre@gmail.com>
2022-09-07 15:48:25 +0200

    feat(wallet): add a request to verify a signature

--- nym-wallet/src-tauri/src/operations/signatures/sign.rs
+++ nym-wallet/src-tauri/src/operations/signatures/sign.rs
@@ -1,5 +1,19 @@
+use std::str::FromStr;
+
 use crate::error::BackendError;
 use crate::state::WalletState;
+use cosmrs::crypto::secp256k1::{Signature, VerifyingKey};
+use cosmrs::crypto::PublicKey;
+use k256::ecdsa::signature::Verifier;
+use serde::Serialize;
+use serde_json::json;
+
+#[derive(Debug, Serialize)]
+pub struct SignatureOutputJson {
+    pub account_id: String,
+    pub public_key: PublicKey,
+    pub signature: String,
+}
 
 #[tauri::command]
 pub async fn sign(
@@ -12,14 +26,15 @@ pub async fn sign(
     let derived_accounts = wallet.try_derive_accounts()?;
     match derived_accounts.first() {
         Some(account) => {
-            log::info!(
-                "<<< Signing message with account {} and public key {:#?}",
-                account.address(),
-                account.public_key()
-            );
+            log::info!("<<< Signing message");
             let signature = wallet.sign_raw_with_account(account, message.as_bytes())?;
             let signature_as_hex_string = signature.to_string();
-            log::info!(">>> Signature: {}", &signature_as_hex_string);
+            let output = SignatureOutputJson {
+                account_id: account.address().to_string(),
+                public_key: account.public_key(),
+                signature: signature_as_hex_string.to_string(),
+            };
+            log::info!(">>> Signing data {}", json!(output),);
             Ok(signature_as_hex_string)
         }
         None => {
@@ -29,4 +44,30 @@ pub async fn sign(
             ))
         }
     }
+}
+
+#[tauri::command]
+pub async fn verify(
+    public_key_as_json: String,
+    signature_as_hex: String,
+    message: String,
+    _state: tauri::State<'_, WalletState>,
+) -> Result<(), BackendError> {
+    let public_key = PublicKey::from_json(&public_key_as_json)?;
+    if public_key.type_url() != PublicKey::SECP256K1_TYPE_URL {
+        return Err(BackendError::SignatureError(
+            "Sorry, we only support secp256k1 public keys at the moment".to_string(),
+        ));
+    }
+
+    log::info!("<<< Verifying signature [{}]", signature_as_hex);
+    let verifying_key = VerifyingKey::from_sec1_bytes(&public_key.to_bytes())?;
+    let signature = Signature::from_str(&signature_as_hex)?;
+    let message_as_bytes = message.into_bytes();
+    Ok(verifying_key
+        .verify(&message_as_bytes, &signature)
+        .map_err(|e| {
+            log::error!(">>> Verification failed, wrong signature");
+            e
+        })?)
 }

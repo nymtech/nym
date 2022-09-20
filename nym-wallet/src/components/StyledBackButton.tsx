4648967e93e8453b29113abe90c890b6cc24f0bb
Gala <calero.vg@gmail.com>
2022-09-20 12:43:51 +0200

    fix build and refactor from CR

--- nym-wallet/src/components/StyledBackButton.tsx
+++ nym-wallet/src/components/StyledBackButton.tsx
@@ -3,7 +3,7 @@ import { Button, SxProps } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 export const StyledBackButton = ({ onBack, sx }: { onBack: () => void; sx?: SxProps }) => (
-  <Button disableFocusRipple size="large" variant="outlined" onClick={onBack} sx={{ ...sx }}>
+  <Button disableFocusRipple size="large" variant="outlined" onClick={onBack} sx={sx}>
     <ArrowBackIosNewIcon fontSize="small" />
   </Button>
 );

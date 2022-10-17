f2c72432c0dd5dff5af253e172ece30bb1f7b34c
pierre <dommerc.pierre@gmail.com>
2022-10-17 16:04:21 +0200

    feat(wallet): new account howto modals

--- nym-wallet/src/components/Accounts/Accounts.tsx
+++ nym-wallet/src/components/Accounts/Accounts.tsx
@@ -1,11 +1,13 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AccountsContext, AppContext } from 'src/context';
+import { isPasswordCreated } from 'src/requests';
 import { EditAccountModal } from './modals/EditAccountModal';
 import { AddAccountModal } from './modals/AddAccountModal';
 import { AccountsModal } from './modals/AccountsModal';
 import { MnemonicModal } from './modals/MnemonicModal';
 import { AccountOverview } from './AccountOverview';
 import { MultiAccountHowTo } from './MultiAccountHowTo';
+import { MultiAccountWithPwdHowTo } from './MultiAccountWithPwdHowTo';
 
 export const Accounts = () => {
   const { accounts, selectedAccount, setDialogToDisplay } = useContext(AccountsContext);
@@ -23,14 +25,29 @@ export const Accounts = () => {
 
 export const SingleAccount = () => {
   const [showHowToDialog, setShowHowToDialog] = useState(false);
+  const [passwordExist, setPasswordExist] = useState(false);
   const { clientDetails } = useContext(AppContext);
+
+  useEffect(() => {
+    const checkPassword = async () => {
+      if (await isPasswordCreated()) {
+        setPasswordExist(true);
+      }
+    };
+    checkPassword();
+  }, [clientDetails]);
+
   return (
     <>
       <AccountOverview
         account={{ id: 'Account 1', address: clientDetails?.client_address || '' }}
         onClick={() => setShowHowToDialog(true)}
       />
-      <MultiAccountHowTo show={showHowToDialog} handleClose={() => setShowHowToDialog(false)} />
+      {passwordExist ? (
+        <MultiAccountWithPwdHowTo show={showHowToDialog} handleClose={() => setShowHowToDialog(false)} />
+      ) : (
+        <MultiAccountHowTo show={showHowToDialog} handleClose={() => setShowHowToDialog(false)} />
+      )}
     </>
   );
 };

80c21b3ed9ca8313862591535f2e528150a365af
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-11-01 11:46:47 +0000

    (chore) setting up a common/logging crate (#1730)

--- service-providers/network-requester/src/main.rs
+++ service-providers/network-requester/src/main.rs
@@ -5,6 +5,7 @@ use clap::CommandFactory;
 use clap::Subcommand;
 use clap::{Args, Parser};
 use completions::{fig_generate, ArgShell};
+use logging::setup_logging;
 
 use network_defaults::DEFAULT_WEBSOCKET_LISTENING_PORT;
 use nymsphinx::addressing::clients::Recipient;
@@ -102,22 +103,4 @@ async fn main() {
     let args = Cli::parse();
 
     execute(args).await;
-}
-
-fn setup_logging() {
-    let mut log_builder = pretty_env_logger::formatted_timed_builder();
-    if let Ok(s) = ::std::env::var("RUST_LOG") {
-        log_builder.parse_filters(&s);
-    } else {
-        // default to 'Info'
-        log_builder.filter(None, log::LevelFilter::Info);
-    }
-
-    log_builder
-        .filter_module("hyper", log::LevelFilter::Warn)
-        .filter_module("tokio_reactor", log::LevelFilter::Warn)
-        .filter_module("reqwest", log::LevelFilter::Warn)
-        .filter_module("mio", log::LevelFilter::Warn)
-        .filter_module("want", log::LevelFilter::Warn)
-        .init();
 }

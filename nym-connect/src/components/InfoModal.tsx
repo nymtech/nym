350be4c678664c7fc52a20ec6ca1ce2334860322
fmtabbara <fmtabbara@hotmail.co.uk>
2022-11-09 16:55:47 +0000

    create generic modal component

---
+++ nym-connect/src/components/InfoModal.tsx
@@ -0,0 +1,69 @@
+import { Close, ErrorOutline } from '@mui/icons-material';
+import { Box, IconButton, Modal, Theme, Typography } from '@mui/material';
+import React from 'react';
+
+const styles = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 200,
+  bgcolor: '#292E34',
+  border: (theme: Theme) => `1px solid ${theme.palette.grey[700]}`,
+  p: 1.5,
+  borderRadius: 0.5,
+  height: 'fit-content',
+};
+
+const ModalTitle = ({ title }: { title: string }) => {
+  return (
+    <Box textAlign="center" mt={-2}>
+      <ErrorOutline sx={{ color: 'warning.main' }} />
+      <Typography variant="body2" textAlign="center" sx={{ color: 'warning.main' }}>
+        {title}
+      </Typography>
+    </Box>
+  );
+};
+
+const ModalBody = ({ description, children }: { description: string; children?: React.ReactElement }) => (
+  <Box textAlign="center" mt={1}>
+    <Typography fontSize="small">{description}</Typography>
+    {children}
+  </Box>
+);
+
+export const InfoModal = ({
+  title,
+  description,
+  show,
+  children,
+  Action,
+  onClose,
+}: {
+  title: string;
+  description: string;
+  show: boolean;
+  children?: React.ReactElement;
+  Action?: React.ReactNode;
+  onClose?: () => void;
+}) => (
+  <Modal open={show} onClose={onClose}>
+    <Box sx={styles}>
+      {onClose && (
+        <Box display="flex" justifyContent="flex-end">
+          <IconButton size="small" onClick={onClose}>
+            <Close sx={{ fontSize: 14 }} />
+          </IconButton>
+        </Box>
+      )}
+      <ModalTitle title={title} />
+      <ModalBody description={description}>{children}</ModalBody>
+      {Action && (
+        <Box mt={1} textAlign="center">
+          {Action}
+        </Box>
+      )}
+    </Box>
+  </Modal>
+);

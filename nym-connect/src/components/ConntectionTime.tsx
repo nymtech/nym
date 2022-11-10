c39ec9524455ff9183ab1a7734c3523d92c86923
fmtabbara <fmtabbara@hotmail.co.uk>
2022-11-10 09:30:10 +0000

    create separate connection time component

---
+++ nym-connect/src/components/ConntectionTime.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { Stack, Typography } from '@mui/material';
+import { DateTime } from 'luxon';
+
+export const ConnectionTime = ({ connectedSince }: { connectedSince?: DateTime }) => {
+  const [duration, setDuration] = React.useState<string>();
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (connectedSince) {
+        setDuration(DateTime.now().diff(connectedSince).toFormat('hh:mm:ss'));
+      }
+    }, 500);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [connectedSince]);
+  return (
+    <Stack alignItems="center" sx={{ mt: 3 }}>
+      <Typography variant="caption" sx={{ color: 'grey.500' }}>
+        Connection time
+      </Typography>
+      <Typography letterSpacing="0.1em">{duration || '00:00:00'}</Typography>
+    </Stack>
+  );
+};

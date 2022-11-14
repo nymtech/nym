dbd0e6b4fae81fb8f81fe2c76ec8ec19677bfa72
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-11-14 14:24:03 +0000

    Ability to update mixnode config in maintenance mode

--- contracts/mixnet/src/contract.rs
+++ contracts/mixnet/src/contract.rs
@@ -188,6 +188,9 @@ pub fn execute(
             Ok(Response::new())
         }
 
+        ExecuteMsg::UpdateMixnodeConfig { new_config } => {
+            crate::mixnodes::transactions::try_update_mixnode_config(deps, info, new_config)
+        }
         _ => Err(MixnetContractError::MigrationInProgress),
     }
 }

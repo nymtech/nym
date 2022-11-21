a33e848d6d69022bd2c2cd5358951b35e7107b55
pierre <dommerc.pierre@gmail.com>
2022-11-21 16:57:27 +0100

    Revert "feat(explorer-api): add route to fetch nym terms&cdts"
    This reverts commit 876f752697d89061b1904e1ddd1d5bcb7045dc5c.

---
+++ explorer-api/src/mix_nodes/utils.rs
@@ -0,0 +1,18 @@
+// Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::mix_nodes::location::GeoLocation;
+use isocountry::CountryCode;
+
+pub(crate) fn map_2_letter_to_3_letter_country_code(geo: &GeoLocation) -> String {
+    match CountryCode::for_alpha2(&geo.country_code) {
+        Ok(three_letter_country_code) => three_letter_country_code.alpha3().to_string(),
+        Err(_e) => {
+            warn!(
+                "❌ Oh no! map_2_letter_to_3_letter_country_code failed for '{:#?}'",
+                geo
+            );
+            "???".to_string()
+        }
+    }
+}

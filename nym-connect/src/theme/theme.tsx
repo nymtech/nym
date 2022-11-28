d004db803726f845c6ad1f535eed22caeda4a26c
Fouad <fmtabbara@hotmail.co.uk>
2022-11-28 11:22:11 +0000

    Feature/nym connect UI updates (#1784)
    * create custom titlebar
    
    * create help page
    
    * create generic modal component
    
    * create separate connection time component
    
    * link to shipyard docs
    
    * move timer to separate component and update connection status component usage
    
    * use separate component for copying ip and port details
    
    * only show infomodal once after connection
    
    * set service provider on tauri side
    
    * Emit events when stopped
    
    * listen and unlisten for tauri events
    
    * connect: add trace log to get_services
    
    * Add back CI notifications
    
    * Update README
    
    Co-authored-by: Jon Häggblad <jon.haggblad@gmail.com>
    Co-authored-by: Mark Sinclair <14054343+mmsinclair@users.noreply.github.com>

--- nym-connect/src/theme/theme.tsx
+++ nym-connect/src/theme/theme.tsx
@@ -23,8 +23,9 @@ const nymPalette: NymPalette = {
   highlight: '#FB6E4E',
   success: '#21D073',
   info: '#60D7EF',
+  warning: '#FFE600',
   fee: '#967FF0',
-  background: { light: '#F4F6F8', dark: '#121726' },
+  background: { light: '#F4F6F8', dark: '#1D2125' },
   text: {
     light: '#F2F2F2',
     dark: '#121726',
@@ -92,6 +93,9 @@ const variantToMUIPalette = (variant: NymPaletteVariant): PaletteOptions => ({
   },
   info: {
     main: nymPalette.info,
+  },
+  warning: {
+    main: nymPalette.warning,
   },
   background: {
     default: variant.background.main,

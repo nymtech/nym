8bd0355248b6db46dabee574244d55dcefc69832
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2022-11-28 13:14:30 +0000

    prototyping wip

--- clients/client-core/src/client/replies/reply_storage/backend/mod.rs
+++ clients/client-core/src/client/replies/reply_storage/backend/mod.rs
@@ -1,18 +1,43 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::client::replies::reply_storage::{ReceivedReplySurbsMap, SentReplyKeys, UsedSenderTags};
+use async_trait::async_trait;
 use nymsphinx::anonymous_replies::SurbEncryptionKey;
 
 #[cfg(target_arch = "wasm32")]
 mod browser_backend;
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32")), feature = "fs-surb-storage")]
 mod fs_backend;
 
-// implemented via trait as implementations are going to vary wildly between wasm and non-wasm targets
-// TODO: might need to be transformed into an async-trait. not sure yet
-trait ReplyStorageBackend {
+// #[cfg(all(test, feature = "std"))]
+// third case: node with actual filesystem
+
+pub struct Empty {}
+
+impl ReplyStorageBackend for Empty {
+    type StorageError = ();
+
+    // fn insert_encryption_key(&mut self, _key: SurbEncryptionKey) -> Result<(), Self::StorageError> {
+    //     Ok(())
+    // }
+}
+
+#[async_trait]
+pub trait ReplyStorageBackend {
     type StorageError;
 
-    fn insert_encryption_key(&mut self, key: SurbEncryptionKey) -> Result<(), Self::StorageError>;
+    // reply keys and surbs would need additional field set when data is loaded
+    // so if there's some failure, we'd trash it all
+
+    async fn flush_reply_keys_storage(&self) -> Result<(), Self::StorageError>;
+    async fn flush_received_reply_surbs_storage(&self) -> Result<(), Self::StorageError>;
+    async fn flush_used_tags_storage(&self) -> Result<(), Self::StorageError>;
+
+    async fn load_reply_keys_storage(&self) -> Result<SentReplyKeys, Self::StorageError>;
+    async fn load_received_reply_surbs_storage(
+        &self,
+    ) -> Result<ReceivedReplySurbsMap, Self::StorageError>;
+    async fn load_used_tags_storage(&self) -> Result<UsedSenderTags, Self::StorageError>;
 }
--- clients/client-core/src/client/replies/reply_storage/mod.rs
+++ clients/client-core/src/client/replies/reply_storage/mod.rs
@@ -5,30 +5,31 @@ pub use crate::client::replies::reply_storage::combined::CombinedReplyStorage;
 pub use crate::client::replies::reply_storage::key_storage::SentReplyKeys;
 pub use crate::client::replies::reply_storage::surb_storage::ReceivedReplySurbsMap;
 pub use crate::client::replies::reply_storage::tag_storage::UsedSenderTags;
+pub use backend::ReplyStorageBackend;
 
+mod backend;
 mod combined;
 mod key_storage;
 mod surb_storage;
 mod tag_storage;
 
-// TODO: TO BE DEALT WITH IN ANOTHER PR
-// TODO: TO BE DEALT WITH IN ANOTHER PR
-// TODO: TO BE DEALT WITH IN ANOTHER PR
-// mod backend;
-//
-// // only really exists to get information about shutdown and save data to the backing storage
-// pub struct ReplyStorage<T> {
-//     combined_storage: CombinedReplyStorage,
-//     backend: T,
-// }
-//
-// impl<T> Drop for ReplyStorage<T> {
-//     fn drop(&mut self) {
-//         println!("REPLY STORAGE IS GETTING DROPPED - WE SHOULD FLUSH ALL OUR DATA TO THE BACKING STORAGE!!")
-//         // todo!("flush everything to backend storage")
-//     }
-// }
-//
-// impl<T> ReplyStorage<T> {
-//
-// }
+// only really exists to get information about shutdown and save data to the backing storage
+pub struct ReplyStorage<T = backend::Empty>
+where
+    T: ReplyStorageBackend,
+{
+    combined_storage: CombinedReplyStorage,
+    backend: T,
+}
+
+impl<T> Drop for ReplyStorage<T>
+where
+    T: ReplyStorageBackend,
+{
+    fn drop(&mut self) {
+        println!("REPLY STORAGE IS GETTING DROPPED - WE SHOULD FLUSH ALL OUR DATA TO THE BACKING STORAGE!!")
+        // todo!("flush everything to backend storage")
+    }
+}
+
+impl<T> ReplyStorage<T> where T: ReplyStorageBackend {}

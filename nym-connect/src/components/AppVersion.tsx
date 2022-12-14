4ece8b7e8f29049b4aecf028a831b85d54f81a1d
Fouad <fmtabbara@hotmail.co.uk>
2022-12-14 13:04:54 +0000

    Feature/nym connect experimental software text (#2692)
    * use experimental text

--- nym-connect/src/components/AppVersion.tsx
+++ nym-connect/src/components/AppVersion.tsx
@@ -6,7 +6,11 @@ export const AppVersion = () => {
   const { appVersion } = useClientContext();
 
   return (
-    <Typography fontSize="small" textAlign="center" sx={{ color: 'grey.600' }}>
+    <Typography
+      fontSize="small"
+      textAlign="center"
+      sx={{ color: 'grey.600', position: 'absolute', bottom: 10, width: '100%' }}
+    >
       Version {appVersion}
     </Typography>
   );

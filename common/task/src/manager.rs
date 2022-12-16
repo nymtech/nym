052a9a71c2411cf114aa6fe27719eee0b465c89c
Jon Häggblad <jon.haggblad@gmail.com>
2022-12-16 17:03:39 +0100

    client: tweak shutdown order (#2724)
    * clients: slightly tweak shutdown
    
    * tweak log

--- common/task/src/manager.rs
+++ common/task/src/manager.rs
@@ -158,7 +158,7 @@ impl TaskManager {
     }
 
     pub async fn wait_for_shutdown(&mut self) {
-        log::info!("Waiting for shutdown");
+        log::debug!("Waiting for shutdown");
         if let Some(notify_rx) = self.notify_rx.take() {
             drop(notify_rx);
         }
@@ -269,7 +269,7 @@ impl TaskClient {
     pub async fn recv_with_delay(&mut self) {
         self.recv()
             .then(|msg| async move {
-                sleep(Duration::from_secs(1)).await;
+                sleep(Duration::from_secs(2)).await;
                 msg
             })
             .await

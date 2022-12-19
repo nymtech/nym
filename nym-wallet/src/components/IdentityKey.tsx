60c8185beadc4ebf920ef3ab8b8bdc2c3f584962
Gala <calero.vg@gmail.com>
2022-12-19 15:27:35 +0100

    cleaning

--- nym-wallet/src/components/IdentityKey.tsx
+++ nym-wallet/src/components/IdentityKey.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Stack, Typography, Tooltip } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { CopyToClipboard } from '@nymproject/react/clipboard/CopyToClipboard';
 import { splice } from 'src/utils';
 
 export const IdentityKey = ({ identityKey }: { identityKey: string }) => (
-  <Stack direction="row" width="fit-content">
+  <Stack direction="row">
     <Typography variant="body2" component="span" fontWeight={400} sx={{ mr: 1, color: 'text.primary' }}>
       {splice(6, identityKey)}
     </Typography>

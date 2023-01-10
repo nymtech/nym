f11af00f4d3fd121bf4acc1df08e9459d5a5f4e8
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-01-10 11:16:48 +0000

    inlining openapi route spec

--- nym-api/src/support/http/mod.rs
+++ nym-api/src/support/http/mod.rs
@@ -32,13 +32,11 @@ pub(crate) async fn setup_rocket(
     let openapi_settings = rocket_okapi::settings::OpenApiSettings::default();
     let mut rocket = rocket::build();
 
-    let custom_route_spec = (vec![], openapi::custom_openapi_spec());
-
     mount_endpoints_and_merged_docs! {
         rocket,
         "/v1".to_owned(),
         openapi_settings,
-        "/" => custom_route_spec,
+        "/" => (vec![], openapi::custom_openapi_spec()),
         "" => circulating_supply_api::circulating_supply_routes(&openapi_settings),
         "" => nym_contract_cache::nym_contract_cache_routes(&openapi_settings),
         "/status" => node_status_api::node_status_routes(&openapi_settings, config.get_network_monitor_enabled()),

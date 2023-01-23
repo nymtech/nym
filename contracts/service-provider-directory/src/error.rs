63a1123ef39672009267b6d743be44a1fa390af9
mx <maxhampshire@pm.me>
2023-01-23 10:46:33 +0100

    added acl to delete()

--- contracts/service-provider-directory/src/error.rs
+++ contracts/service-provider-directory/src/error.rs
@@ -1,8 +1,10 @@
-use cosmwasm_std::{StdError};
+use cosmwasm_std::{Addr, StdError};
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     StdError(#[from] StdError),
+    #[error("{sender} is not owner of service")]
+    Unauthorized { sender: Addr },
 }

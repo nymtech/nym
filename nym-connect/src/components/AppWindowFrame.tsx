95f483bd6117939a06607053681db395bc14eb42
fmtabbara <fmtabbara@hotmail.co.uk>
2023-01-25 18:15:48 +0000

    render title based on route

--- nym-connect/src/components/AppWindowFrame.tsx
+++ nym-connect/src/components/AppWindowFrame.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { CustomTitleBar } from './CustomTitleBar';
-import { AppVersion } from './AppVersion';
+import { useLocation, useParams } from 'react-router-dom';
 
-export const AppWindowFrame: FCWithChildren = ({ children }) => (
-  <Box
-    sx={{
-      display: 'grid',
-      borderRadius: '12px',
-      gridTemplateRows: '40px 1fr 30px',
-      height: '100vh',
-      overflowY: 'hidden',
-    }}
-  >
-    <CustomTitleBar />
-    <Box style={{ padding: '16px' }}>{children}</Box>
-    <AppVersion />
-  </Box>
-);
+export const AppWindowFrame: FCWithChildren = ({ children }) => {
+  const location = useLocation();
+
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        borderRadius: '12px',
+        gridTemplateRows: '40px 1fr',
+        height: '100vh',
+      }}
+    >
+      <CustomTitleBar path={location.pathname} />
+      <Box style={{ padding: '16px' }}>{children}</Box>
+    </Box>
+  );
+};

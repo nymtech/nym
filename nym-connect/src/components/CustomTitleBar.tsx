d86a3ce10157d50ba62437e2fd069c28cdea8454
fmtabbara <fmtabbara@hotmail.co.uk>
2023-01-25 18:21:59 +0000

    render title from route

--- nym-connect/src/components/CustomTitleBar.tsx
+++ nym-connect/src/components/CustomTitleBar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { ArrowBack, Close, HelpOutline, Minimize } from '@mui/icons-material';
-import { Box, IconButton } from '@mui/material';
+import { ArrowBack, Close, Menu, Minimize } from '@mui/icons-material';
+import { Box, IconButton, Typography } from '@mui/material';
 import { NymWordmark } from '@nymproject/react/logo/NymWordmark';
 import { appWindow } from '@tauri-apps/api/window';
-import { useClientContext } from 'src/context/main';
+import { useNavigate } from 'react-router-dom';
 
 const customTitleBarStyles = {
   titlebar: {
@@ -24,22 +24,36 @@ const CustomButton = ({ Icon, onClick }: { Icon: React.JSXElementConstructor<any
   </IconButton>
 );
 
-export const CustomTitleBar = () => {
-  const { showHelp, handleShowHelp } = useClientContext();
+const MenuIcon = () => {
+  const navigate = useNavigate();
+  return <CustomButton Icon={Menu} onClick={() => navigate('/menu')} />;
+};
+
+const ArrowBackIcon = () => {
+  const navigate = useNavigate();
+  return <CustomButton Icon={ArrowBack} onClick={() => navigate(-1)} />;
+};
+
+const getTitleIcon = (path: string) => {
+  if (path !== '/') {
+    const title = path.split('/').slice(-1);
+    return (
+      <Typography textTransform="capitalize" fontWeight={700}>
+        {title}
+      </Typography>
+    );
+  }
+  return <NymWordmark width={36} />;
+};
+
+export const CustomTitleBar = ({ path = '/' }: { path?: string }) => {
+  console.log(path);
+
   return (
     <Box data-tauri-drag-region style={customTitleBarStyles.titlebar}>
       {/* set width to keep logo centered */}
-      <Box sx={{ width: '40px' }}>
-        <CustomButton
-          Icon={!showHelp ? HelpOutline : ArrowBack}
-          onClick={() => {
-            handleShowHelp();
-          }}
-        />
-      </Box>
-
-      <NymWordmark width={36} />
-
+      <Box sx={{ width: '40px' }}>{path === '/' ? <MenuIcon /> : <ArrowBackIcon />}</Box>
+      {getTitleIcon(path)}
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
         <CustomButton Icon={Minimize} onClick={() => appWindow.minimize()} />
         <CustomButton Icon={Close} onClick={() => appWindow.close()} />

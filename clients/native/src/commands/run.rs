3099f2ead3a9d2060ab02c098c5cfba49072b53a
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2023-02-08 13:00:58 +0200

    Hide coconut runtime flags (#2990)

--- clients/native/src/commands/run.rs
+++ clients/native/src/commands/run.rs
@@ -23,7 +23,7 @@ pub(crate) struct Run {
     id: String,
 
     /// Comma separated list of rest endpoints of the nyxd validators
-    #[clap(long, alias = "nymd_validators", value_delimiter = ',')]
+    #[clap(long, alias = "nymd_validators", value_delimiter = ',', hide = true)]
     nyxd_urls: Option<Vec<url::Url>>,
 
     /// Comma separated list of rest endpoints of the API validators
@@ -59,7 +59,7 @@ pub(crate) struct Run {
 
     /// Set this client to work in a enabled credentials mode that would attempt to use gateway
     /// with bandwidth credential requirement.
-    #[clap(long)]
+    #[clap(long, hide = true)]
     enabled_credentials_mode: Option<bool>,
 }
 

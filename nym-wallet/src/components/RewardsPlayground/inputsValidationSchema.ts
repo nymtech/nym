aaa4b2ffc344f7a939e2d13e6281b2716022adb9
fmtabbara <fmtabbara@hotmail.co.uk>
2023-02-13 22:58:50 +0000

    remove upper limit restriction

--- nym-wallet/src/components/RewardsPlayground/inputsValidationSchema.ts
+++ nym-wallet/src/components/RewardsPlayground/inputsValidationSchema.ts
@@ -35,7 +35,7 @@ export const inputValidationSchema = Yup.object().shape({
     .test('Is valid operator cost value', (value, ctx) => {
       const stringValueToNumber = Math.round(Number(value));
 
-      if (isGreaterThan(stringValueToNumber, -1) && isLessThan(stringValueToNumber, 101)) return true;
+      if (isGreaterThan(stringValueToNumber, -1)) return true;
       return ctx.createError({ message: 'Operator cost must be a valid number' });
     }),
 });

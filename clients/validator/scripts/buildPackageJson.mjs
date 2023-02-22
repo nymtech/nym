2bc85c4320793282097d3a63086111349eaa8bc3
fmtabbara <fmtabbara@hotmail.co.uk>
2023-02-22 17:05:57 +0000

    add bundle script

---
+++ clients/validator/scripts/buildPackageJson.mjs
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+// parse the package.json from the SDK, so we can keep fields like the name and version
+const json = JSON.parse(fs.readFileSync('./package.json').toString());
+
+// defaults (NB: these are in the output file locations)
+const main = 'index.js';
+const types = 'index.d.ts';
+
+// make a package.json for the CommonJS bundle
+const packageJsonCommonJS = {
+  name: `${json.name}-commonjs`,
+  version: json.version,
+  license: json.license,
+  author: json.author,
+  type: 'commonjs',
+  main,
+  types,
+};
+
+// make a package.json for the bundle
+const packageJson = {
+  name: json.name,
+  version: json.version,
+  license: json.license,
+  author: json.author,
+  main,
+  types,
+};
+
+fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));

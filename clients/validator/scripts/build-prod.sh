16e63f8f5c0a67b2c893a6ddd53bce7bb69ab6b1
fmtabbara <fmtabbara@hotmail.co.uk>
2023-02-22 17:05:57 +0000

    add bundle script

---
+++ clients/validator/scripts/build-prod.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+rm -rf ./dist || true
+rm -rf ../../dist || true
+
+
+# Bundle application
+
+yarn build
+
+# Bundle types
+
+yarn build:types
+
+# Build package.json for bundle
+
+node ./scripts/buildPackageJson.mjs
+
+
+# move the output outside of the yarn/npm workspaces
+
+mv ./dist ../../
+
+echo "Output can be found in:"
+realpath ../../dist

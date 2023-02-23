a86cac5b856e4d8f14e8eb5053655759c2158776
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-02-23 10:05:38 +0000

    updated gateways 'sign' command

--- gateway/src/commands/sign.rs
+++ gateway/src/commands/sign.rs
@@ -8,31 +8,37 @@ use crate::{
     commands::ensure_config_version_compatibility,
     config::persistence::pathfinder::GatewayPathfinder,
 };
-use anyhow::{anyhow, Result};
+use anyhow::{bail, Result};
 use clap::{ArgGroup, Args};
 use nym_crypto::asymmetric::identity;
 use std::error::Error;
 use validator_client::nyxd;
 
 #[derive(Args, Clone)]
-#[clap(group(ArgGroup::new("sign").required(true).args(&["wallet_address", "text"])))]
-pub struct Sign {
+#[clap(group(ArgGroup::new("sign").required(true).args(&["wallet_address", "text", "contract_msg"])))]
+pub(crate) struct Sign {
     /// The id of the mixnode you want to sign with
     #[clap(long)]
     id: String,
 
     /// Signs your blockchain address with your identity key
-    #[clap(long)]
+    // the alias here is included for backwards compatibility (1.1.4 and before)
+    #[clap(long, alias = "address")]
     wallet_address: Option<nyxd::AccountId>,
 
     /// Signs an arbitrary piece of text with your identity key
     #[clap(long)]
     text: Option<String>,
+
+    /// Signs a transaction-specific payload, that is going to be sent to the smart contract, with your identity key
+    #[clap(long)]
+    contract_msg: Option<String>,
 }
 
 enum SignedTarget {
     Text(String),
     Address(nyxd::AccountId),
+    ContractMsg(String),
 }
 
 impl TryFrom<Sign> for SignedTarget {
@@ -43,11 +49,13 @@ impl TryFrom<Sign> for SignedTarget {
             Ok(SignedTarget::Text(text))
         } else if let Some(address) = args.wallet_address {
             Ok(SignedTarget::Address(address))
+        } else if let Some(msg) = args.contract_msg {
+            Ok(SignedTarget::ContractMsg(msg))
         } else {
             // This is unreachable, and hopefully clap will support it explicitly by outputting an
             // enum from the ArgGroup in the future.
             // See: https://github.com/clap-rs/clap/issues/2621
-            Err(anyhow!("Error: missing signed target flag"))
+            bail!("Error: missing signed target flag")
         }
     }
 }
@@ -88,6 +96,18 @@ fn print_signed_text(private_key: &identity::PrivateKey, text: &str) {
     );
 }
 
+fn print_signed_contract_msg(private_key: &identity::PrivateKey, raw_msg: &str) {
+    // we don't really care about what particular information is embedded inside of it,
+    // we just want to know if user correctly copied the string, i.e. whether it's a valid json
+    if serde_json::from_str::<serde_json::Value>(raw_msg).is_err() {
+        println!("it seems you have incorrectly copied the message to sign. Make sure you didn't accidentally skip any characters")
+    } else {
+        let msg = raw_msg.trim();
+        let signature = private_key.sign(msg.trim().as_bytes()).to_base58_string();
+        println!("The base58-encoded signature on\n{msg}\nis:\n{signature}");
+    }
+}
+
 pub fn execute(args: Sign) -> Result<(), Box<dyn Error + Send + Sync>> {
     let config = build_config(args.id.clone(), OverrideConfig::default())?;
     ensure_config_version_compatibility(&config)?;
@@ -99,6 +119,9 @@ pub fn execute(args: Sign) -> Result<(), Box<dyn Error + Send + Sync>> {
     match signed_target {
         SignedTarget::Text(text) => print_signed_text(identity_keypair.private_key(), &text),
         SignedTarget::Address(addr) => print_signed_address(identity_keypair.private_key(), addr)?,
+        SignedTarget::ContractMsg(raw_msg) => {
+            print_signed_contract_msg(identity_keypair.private_key(), &raw_msg)
+        }
     }
 
     Ok(())

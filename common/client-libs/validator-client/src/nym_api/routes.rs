81e36246af281a159e84c8de725bcf61fee9d817
fmtabbara <fmtabbara@hotmail.co.uk>
2023-03-07 10:29:41 +0000

    add new unfiltered endpoint
    add new unfiltered endpoint

--- common/client-libs/validator-client/src/nym_api/routes.rs
+++ common/client-libs/validator-client/src/nym_api/routes.rs
@@ -8,6 +8,7 @@ pub const MIXNODES: &str = "mixnodes";
 pub const GATEWAYS: &str = "gateways";
 
 pub const DETAILED: &str = "detailed";
+pub const DETAILED_UNFILTERED: &str = "detailed-unfiltered";
 pub const ACTIVE: &str = "active";
 pub const REWARDED: &str = "rewarded";
 pub const COCONUT_ROUTES: &str = "coconut";

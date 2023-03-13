adb5ed7c30f016bab9edbb92e779fb9996b4b888
benedettadavico <benedetta.davico@gmail.com>
2023-03-13 12:14:14 +0100

    typo fix

--- common/commands/src/validator/mixnet/operators/gateway/mod.rs
+++ common/commands/src/validator/mixnet/operators/gateway/mod.rs
@@ -24,8 +24,8 @@ pub enum MixnetOperatorsGatewayCommands {
     Unbond(unbond_gateway::Args),
     /// Bond to a gateway with locked tokens
     VestingBond(vesting_bond_gateway::Args),
-    /// Unbound from a gateway (when originally using locked tokens)
-    VestingUnbound(vesting_unbond_gateway::Args),
+    /// Unbond from a gateway (when originally using locked tokens)
+    VestingUnbond(vesting_unbond_gateway::Args),
     /// Create base58-encoded payload required for producing valid bonding signature.
     CreateGatewayBondingSignPayload(gateway_bonding_sign_payload::Args),
 }

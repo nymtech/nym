81e31c055072dc5b2d7067b6a460d318a7a5ee6f
pierre <dommerc.pierre@gmail.com>
2023-03-24 12:17:34 +0100

    build(nc-mobile): remove webpack-favicons plugin

--- nym-connect/mobile/webpack.prod.js
+++ nym-connect/mobile/webpack.prod.js
@@ -6,10 +6,19 @@ const entry = {
   app: path.resolve(__dirname, 'src/index.tsx'),
 };
 
-module.exports = merge(common, {
+const config = merge(common, {
   mode: 'production',
   node: {
     __dirname: false,
   },
   entry,
 });
+
+// Remove WebpackFavicons plugin as it makes FDroid build more
+// difficult to configure since webpack-favicons depends on sharp,
+// which depends on system library libvips
+// As we are building for mobile, this is useless anyway
+// TODO do not base deletion on index
+config.plugins.splice(2, 1);
+
+module.exports = config;

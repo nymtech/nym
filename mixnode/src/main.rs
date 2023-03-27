f838ddffe29091fed3f5a28623924d116b380d7d
durch <durch@users.noreply.github.com>
2023-03-27 14:21:44 +0200

    Remove mixnode modifications

--- mixnode/src/main.rs
+++ mixnode/src/main.rs
@@ -60,11 +60,6 @@ impl Cli {
     }
 }
 
-#[cfg(feature = "cpu-cycles")]
-pub fn cpu_cycles() {
-    info!("{}", cpu_cycles::cpucycles())
-}
-
 #[tokio::main]
 async fn main() {
     setup_logging();

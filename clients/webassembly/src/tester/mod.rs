a34a0fb07d0d128f18b7f66d8195e3f2e5fc15a0
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-04-14 15:45:48 +0100

    splitting up 'test_node'

--- clients/webassembly/src/tester/mod.rs
+++ clients/webassembly/src/tester/mod.rs
@@ -19,6 +19,7 @@ use nym_gateway_client::GatewayClient;
 use nym_sphinx::addressing::clients::Recipient;
 use nym_sphinx::addressing::nodes::NodeIdentity;
 use nym_sphinx::params::PacketSize;
+use nym_sphinx::preparer::PreparedFragment;
 use nym_task::TaskManager;
 use nym_topology::NymTopology;
 use rand::rngs::OsRng;
@@ -29,11 +30,12 @@ use std::time::Duration;
 use tokio::sync::Mutex as AsyncMutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
-use wasm_utils::{console_log, console_warn, PromisableResult};
+use wasm_utils::{check_promise_result, console_log, console_warn, PromisableResult};
 
 pub(crate) mod helpers;
 
 pub type NodeTestMessage = TestMessage<WasmTestMessageExt>;
+type LockedGatewayClient = Arc<AsyncMutex<GatewayClient<FakeClient<DirectSigningNyxdClient>>>>;
 
 pub(crate) const DEFAULT_TEST_TIMEOUT: Duration = Duration::from_secs(10);
 pub(crate) const DEFAULT_TEST_PACKETS: u32 = 20;
@@ -47,7 +49,7 @@ pub struct NymNodeTester {
 
     // blame all those mutexes on being unable to have an async method with internal mutability...
     tester: Arc<SyncMutex<NodeTester<OsRng>>>,
-    gateway_client: Arc<AsyncMutex<GatewayClient<FakeClient<DirectSigningNyxdClient>>>>,
+    gateway_client: LockedGatewayClient,
 
     // we have to put it behind the lock due to wasm limitations and borrowing...
     // the mutex acquisition should be instant as there aren't going to be any threads attempting
@@ -185,6 +187,83 @@ impl NymNodeTesterBuilder {
     }
 }
 
+async fn test_mixnode(
+    test_packets: Vec<PreparedFragment>,
+    gateway_client: LockedGatewayClient,
+    processed_receiver: ReceivedReceiverWrapper,
+    timeout: Duration,
+) -> Result<NodeTestResult, WasmClientError> {
+    let num_test_packets = test_packets.len() as u32;
+
+    let expected_ack_ids = test_packets
+        .iter()
+        .map(|p| p.fragment_identifier)
+        .collect::<HashSet<_>>();
+
+    let mix_packets = test_packets.into_iter().map(|p| p.mix_packet).collect();
+
+    // start by clearing any messages that might have been received between tests
+    processed_receiver.clear_received_channel().await;
+
+    // locking the gateway client so that we could get mutable access to data without having to declare
+    // self mutable
+    let mut gateway_permit = gateway_client.lock().await;
+    gateway_permit.batch_send_mix_packets(mix_packets).await?;
+
+    let mut received_valid_messages = 0;
+    let mut received_valid_acks = 0;
+    let mut duplicate_packets = 0;
+    let mut duplicate_acks = 0;
+
+    let mut timeout_fut = wasm_timer::Delay::new(timeout);
+    let mut receiver_permit = processed_receiver.lock().await;
+
+    loop {
+        tokio::select! {
+            _ = &mut timeout_fut => {
+                break
+            }
+            received_packet = receiver_permit.next() => {
+                let Some(received_packet) = received_packet else {
+                    todo!()
+                };
+                match received_packet {
+                    Received::Message(msg) => {
+                        console_log!("received msg! raw: {msg}");
+                        let inner = msg.into_inner_data();
+                        let foo = String::from_utf8_lossy(&inner);
+                        console_log!("inner: {foo}");
+                        // TODO: parsing, validating etc
+                        received_valid_messages += 1;
+                    },
+                    Received::Ack(frag_id) => {
+                        console_log!("received ack! raw: {frag_id}");
+
+                        if expected_ack_ids.contains(&frag_id) {
+                            received_valid_acks += 1;
+                        } else {
+                            console_warn!("received an ack that was not part of the test! (id: {frag_id})")
+                        }
+                    }
+                }
+
+                if received_valid_acks == received_valid_messages && received_valid_messages == num_test_packets {
+                    console_log!("already received all the packets! finishing the test...");
+                    break
+                }
+            }
+        }
+    }
+
+    Ok(NodeTestResult {
+        sent_packets: num_test_packets,
+        received_packets: received_valid_messages,
+        received_acks: received_valid_acks,
+        duplicate_packets,
+        duplicate_acks,
+    })
+}
+
 #[wasm_bindgen]
 impl NymNodeTester {
     #[wasm_bindgen(constructor)]
@@ -212,6 +291,19 @@ impl NymNodeTester {
         })
     }
 
+    fn prepare_test_packets(
+        &self,
+        mixnode_identity: String,
+        test_nonce: u32,
+        num_test_packets: u32,
+    ) -> Result<Vec<PreparedFragment>, WasmClientError> {
+        let test_ext = WasmTestMessageExt::new(test_nonce);
+        let mut tester_permit = self.tester.lock().expect("mutex got poisoned");
+        tester_permit
+            .existing_identity_mixnode_test_packets(mixnode_identity, test_ext, num_test_packets)
+            .map_err(Into::into)
+    }
+
     pub fn test_node(
         &self,
         mixnode_identity: String,
@@ -221,97 +313,28 @@ impl NymNodeTester {
         let timeout = timeout_millis
             .map(Duration::from_millis)
             .unwrap_or(DEFAULT_TEST_TIMEOUT);
-
         let num_test_packets = num_test_packets.unwrap_or(DEFAULT_TEST_PACKETS);
 
         // I simultaneously feel both disgusted and amazed by this workaround
         let test_nonce = self.current_test_nonce.fetch_add(1, Ordering::Relaxed);
-
-        let test_ext = WasmTestMessageExt::new(test_nonce);
-
-        let mut tester_permit = self.tester.lock().expect("mutex got poisoned");
-        let test_packets = match tester_permit.existing_identity_mixnode_test_packets(
+        let test_packets = check_promise_result!(self.prepare_test_packets(
             mixnode_identity,
-            test_ext,
-            num_test_packets,
-        ) {
-            Ok(packets) => packets,
-            Err(err) => return WasmClientError::from(err).into_rejected_promise(),
-        };
-
-        let expected_ack_ids = test_packets
-            .iter()
-            .map(|p| p.fragment_identifier)
-            .collect::<HashSet<_>>();
-
-        let mix_packets = test_packets.into_iter().map(|p| p.mix_packet).collect();
+            test_nonce,
+            num_test_packets
+        ));
 
         let processed_receiver_clone = self.processed_receiver.clone();
         let gateway_client_clone = Arc::clone(&self.gateway_client);
 
         future_to_promise(async move {
-            // start by clearing any messages that might have been received between tests
-            processed_receiver_clone.clear_received_channel().await;
-
-            // locking the gateway client so that we could get mutable access to data without having to declare
-            // self mutable
-            let mut gateway_permit = gateway_client_clone.lock().await;
-            if let Err(err) = gateway_permit.batch_send_mix_packets(mix_packets).await {
-                return Err(WasmClientError::from(err).into());
-            }
-
-            let mut received_valid_messages = 0;
-            let mut received_valid_acks = 0;
-            let mut duplicate_packets = 0;
-            let mut duplicate_acks = 0;
-
-            let mut timeout_fut = wasm_timer::Delay::new(timeout);
-            let mut receiver_permit = processed_receiver_clone.lock().await;
-
-            loop {
-                tokio::select! {
-                    _ = &mut timeout_fut => {
-                        break
-                    }
-                    received_packet = receiver_permit.next() => {
-                        let Some(received_packet) = received_packet else {
-                            todo!()
-                        };
-                        match received_packet {
-                            Received::Message(msg) => {
-                                console_log!("received msg! raw: {msg}");
-                                let inner = msg.into_inner_data();
-                                let foo = String::from_utf8_lossy(&inner);
-                                console_log!("inner: {foo}");
-                                // TODO: parsing, validating etc
-                                received_valid_messages += 1;
-                            },
-                            Received::Ack(frag_id) => {
-                                console_log!("received ack! raw: {frag_id}");
-
-                                if expected_ack_ids.contains(&frag_id) {
-                                    received_valid_acks += 1;
-                                } else {
-                                    console_warn!("received an ack that was not part of the test! (id: {frag_id})")
-                                }
-                            }
-                        }
-
-                        if received_valid_acks == received_valid_messages && received_valid_messages == num_test_packets {
-                            console_log!("already received all the packets! finishing the test...");
-                            break
-                        }
-                    }
-                }
-            }
-
-            Ok(JsValue::from(NodeTestResult {
-                sent_packets: num_test_packets,
-                received_packets: received_valid_messages,
-                received_acks: received_valid_acks,
-                duplicate_packets,
-                duplicate_acks,
-            }))
+            test_mixnode(
+                test_packets,
+                gateway_client_clone,
+                processed_receiver_clone,
+                timeout,
+            )
+            .await
+            .into_promise_result()
         })
     }
 }

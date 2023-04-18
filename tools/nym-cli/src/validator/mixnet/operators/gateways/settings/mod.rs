9549bed8bb6ea27159f7d0d305e1397e9ff2fd82
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-04-18 16:28:13 +0100

    exposed missing gateway commands in nym-cli

---
+++ tools/nym-cli/src/validator/mixnet/operators/gateways/settings/mod.rs
@@ -0,0 +1,21 @@
+// Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+use nym_cli_commands::context::{create_signing_client, ClientArgs};
+use nym_network_defaults::NymNetworkDetails;
+
+pub(crate) async fn execute(
+    global_args: ClientArgs,
+    settings: nym_cli_commands::validator::mixnet::operators::gateway::settings::MixnetOperatorsGatewaySettings,
+    network_details: &NymNetworkDetails,
+) -> anyhow::Result<()> {
+    match settings.command {
+        nym_cli_commands::validator::mixnet::operators::gateway::settings::MixnetOperatorsGatewaySettingsCommands::UpdateConfig(args) => {
+            nym_cli_commands::validator::mixnet::operators::gateway::settings::update_config::update_config(args, create_signing_client(global_args, network_details)?).await
+        }
+        nym_cli_commands::validator::mixnet::operators::gateway::settings::MixnetOperatorsGatewaySettingsCommands::VestingUpdateConfig(args) => {
+            nym_cli_commands::validator::mixnet::operators::gateway::settings::vesting_update_config::vesting_update_config(args, create_signing_client(global_args, network_details)?).await
+        }
+    }
+    Ok(())
+}

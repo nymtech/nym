819fe98cde9dfcc59fbea48fdb9d8de7f2aec641
fmtabbara <fmtabbara@hotmail.co.uk>
2023-05-02 09:55:50 +0100

    add page layout for app pages

--- nym-browser-extension/src/layouts/PageLayout.tsx
+++ nym-browser-extension/src/layouts/PageLayout.tsx
@@ -1,16 +1,23 @@
-import { Box } from '@mui/system';
 import React, { useState } from 'react';
-import AppBar from 'src/components/ui/AppBar';
-import { MenuDrawer } from 'src/components/ui';
+import { Box } from '@mui/material';
+import { AppBar, MenuDrawer } from 'src/components/ui';
+
+const layoutStyle = {
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gridTemplateRows: '50px 1fr',
+  gridColumnGap: '0px',
+  gridRowGap: '0px',
+};
 
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>
+    <Box sx={layoutStyle}>
       <AppBar onMenuOpen={() => setMenuOpen(true)} />
       <MenuDrawer open={menuOpen} onClose={() => setMenuOpen(false)} />
       <Box sx={{ p: 2 }}>{children}</Box>
-    </>
+    </Box>
   );
 };

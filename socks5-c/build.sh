d1e256e6ff3d05d8057341d5066791397b65c60b
Jon Häggblad <jon.haggblad@gmail.com>
2023-05-14 22:52:22 +0200

    Update build.sh

--- socks5-c/build.sh
+++ socks5-c/build.sh
@@ -4,7 +4,7 @@ export API=33
 export TOOLCHAIN="$NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64"
 export TARGET_CC="$TOOLCHAIN/bin/x86_64-linux-android$API-clang"
 export TARGET_AR="$TOOLCHAIN/bin/llvm-ar"
-export PATH="$TOOLCHAIN/bin/:$PATH"
-export RANLIB="$TOOLCHAIN/bin/llvm-ranlib"
+export TARGET_RANLIB="$TOOLCHAIN/bin/llvm-ranlib"
+export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$TOOLCHAIN/bin/x86_64-linux-android$API-clang"
 
-cargo build --lib --target x86_64-linux-android
+cargo build --lib --target x86_64-linux-android --release

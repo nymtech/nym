5e7dbb8cd8c38b1a74554e124f2fa8560ee05dea
pierre <dommerc.pierre@gmail.com>
2023-05-15 16:15:11 +0200

    android: start socks5 process in a separated thread

--- android/nyms5/app/src/main/java/net/nymtech/nyms5/MainActivity.kt
+++ android/nyms5/app/src/main/java/net/nymtech/nyms5/MainActivity.kt
@@ -1,9 +1,9 @@
 package net.nymtech.nyms5
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,14 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import net.nymtech.nyms5.ui.theme.Nyms5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val result = Socks5().runtest()
-        Log.i(result, "TEST ****")
+        val viewModel: MainViewModel by viewModels()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    // Update UI elements
+                }
+            }
+        }
 
         setContent {
             Nyms5Theme {
---
+++ android/nyms5/app/src/main/java/net/nymtech/nyms5/MainViewModel.kt
@@ -0,0 +1,26 @@
+package net.nymtech.nyms5
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = Socks5().runtest()
+            result?.let { Log.d("App", "result: $it") }
+        }
+        Log.d("App", "libnyms5 CALLED")
+    }
+
+    // Expose screen UI state
+    private val _uiState = MutableStateFlow(false)
+    val uiState: StateFlow<Boolean> = _uiState.asStateFlow()
+
+}

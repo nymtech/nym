817e624adccb2fa778a4a40593afb393c2e4feaa
Jon Häggblad <jon.haggblad@gmail.com>
2023-05-16 10:05:44 +0200

    Remove the old non-working logger

--- socks5-c/src/lib.rs
+++ socks5-c/src/lib.rs
@@ -45,28 +45,6 @@ pub mod android {
 
         setup_env(None);
 
-        // let mut log_builder = pretty_env_logger::formatted_timed_builder();
-        // if let Ok(s) = ::std::env::var("RUST_LOG") {
-        //     log_builder.parse_filters(&s);
-        // } else {
-        //     // default to 'Info'
-        //     log_builder.filter(None, log::LevelFilter::Info);
-        // }
-
-        // log_builder
-        //     .filter_module("hyper", log::LevelFilter::Warn)
-        //     .filter_module("tokio_reactor", log::LevelFilter::Warn)
-        //     .filter_module("reqwest", log::LevelFilter::Warn)
-        //     .filter_module("mio", log::LevelFilter::Warn)
-        //     .filter_module("want", log::LevelFilter::Warn)
-        //     .filter_module("tungstenite", log::LevelFilter::Warn)
-        //     .filter_module("tokio_tungstenite", log::LevelFilter::Warn)
-        //     .filter_module("handlebars", log::LevelFilter::Warn)
-        //     .filter_module("sled", log::LevelFilter::Warn)
-        //     .init();
-
-        // log::info!("HEREE****");
-
         // TODO: does that leak memory and do we have to have a separate free method?
         // I'd assume not because the allocation came from the caller
         // let c_str = unsafe { CStr::from_ptr(service_provider) };

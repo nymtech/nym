eeb5b854e5894e48b73f6bb350de276f5d4ab0dd
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-05-30 11:26:20 +0100

    introduced a call to check if database was already initialised (#3465)
    * introduced a call to check if database was already initialised
    
    * use extension storage method to check for db existance
    
    ---------
    
    Co-authored-by: fmtabbara <fmtabbara@hotmail.co.uk>

--- nym-browser-extension/src/routes/index.tsx
+++ nym-browser-extension/src/routes/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 import { Home } from 'src/pages/';
+import { ExtensionStorage } from '@nymproject/extension-storage';
 import { RegisterRoutes } from './register';
 import { UserRoutes } from './user';
 import { LoginRoutes } from './login';
@@ -8,7 +9,18 @@ import { LoginRoutes } from './login';
 const Router = process.env.NODE_ENV === 'development' ? BrowserRouter : MemoryRouter;
 
 export const AppRoutes = () => {
-  const userHasAccount = localStorage.getItem('nym-browser-extension');
+  const [userHasAccount, setUserHasAccount] = useState(null);
+
+  useEffect(() => {
+    const checkUserHasAccount = async () => {
+      const hasAccount = await ExtensionStorage.exists();
+      setUserHasAccount(hasAccount);
+    };
+
+    checkUserHasAccount();
+  }, []);
+
+  if (userHasAccount === null) return null;
 
   return (
     <Router>

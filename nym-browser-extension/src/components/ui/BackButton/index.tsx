240901cd51b41773ab74ae81c662603ca9cbe402
fmtabbara <fmtabbara@hotmail.co.uk>
2023-05-31 15:28:01 +0100

    allow BackButton component to have a defined location

--- nym-browser-extension/src/components/ui/BackButton/index.tsx
+++ nym-browser-extension/src/components/ui/BackButton/index.tsx
@@ -3,10 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { ArrowBackIosRounded } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 
-export const BackButton = () => {
+export const BackButton = ({ onBack }: { onBack?: () => void }) => {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    onBack ? onBack() : navigate(-1);
+  };
   return (
-    <IconButton size="small" onClick={() => navigate(-1)}>
+    <IconButton size="small" onClick={handleClick}>
       <ArrowBackIosRounded fontSize="small" />
     </IconButton>
   );

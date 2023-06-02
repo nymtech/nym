65a74aed4769e2cafcaae6de75bf12482f7bccfe
Jon Häggblad <jon.haggblad@gmail.com>
2023-06-02 09:48:27 +0200

    rustfmt

--- common/cosmwasm-smart-contracts/service-provider-directory/src/lib.rs
+++ common/cosmwasm-smart-contracts/service-provider-directory/src/lib.rs
@@ -1,9 +1,9 @@
+pub mod error;
 pub mod events;
 pub mod msg;
 pub mod response;
 pub mod signing_types;
 pub mod types;
-pub mod error;
 
 // Re-export all types at the top-level
 pub use types::*;

91ad3e7b9e476b479d6f0a9efb3f9ca60b38027b
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-06-05 10:07:15 +0100

    removed explicit packet_type argument when starting base client
    it's known implicitly from the previously passed config struct

--- clients/native/src/client/mod.rs
+++ clients/native/src/client/mod.rs
@@ -139,7 +139,7 @@ impl SocketClient {
 
         let base_builder = self.create_base_client_builder().await?;
         let packet_type = self.config.base.debug.traffic.packet_type;
-        let mut started_client = base_builder.start_base(packet_type).await?;
+        let mut started_client = base_builder.start_base().await?;
         let self_address = started_client.address;
         let client_input = started_client.client_input.register_producer();
         let client_output = started_client.client_output.register_consumer();
@@ -168,7 +168,7 @@ impl SocketClient {
 
         let base_builder = self.create_base_client_builder().await?;
         let packet_type = self.config.base.debug.traffic.packet_type;
-        let mut started_client = base_builder.start_base(packet_type).await?;
+        let mut started_client = base_builder.start_base().await?;
         let address = started_client.address;
         let client_input = started_client.client_input.register_producer();
         let client_output = started_client.client_output.register_consumer();

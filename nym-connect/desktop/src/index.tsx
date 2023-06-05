cde5992c27d573b8be7e93188eedda2f53f5d6f7
pierre <dommerc.pierre@gmail.com>
2023-06-05 13:15:55 +0200

    add sentry init to wallet, nc desktop

--- nym-connect/desktop/src/index.tsx
+++ nym-connect/desktop/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { getVersion } from '@tauri-apps/api/app';
 import { GlobalStyles } from '@mui/material';
 import { ClientContextProvider } from './context/main';
 import { ErrorFallback } from './components/Error';
@@ -11,6 +13,35 @@ import { TestAndEarnContextProvider } from './components/Growth/context/TestAndE
 import { AppRoutes } from './routes';
 
 const elem = document.getElementById('root');
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://625e2658da4945a7a253f3ee04413a31@o967446.ingest.sentry.io/4505306292289536',
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ['localhost'],
+      }),
+      new Sentry.Replay(),
+    ],
+
+    // TODO adjust this in the future, 100% is not recommended for production
+    tracesSampleRate: 1.0,
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+
+  Sentry.setTag('app_env', 'production');
+
+  (async () => {
+    getVersion().then((version) => {
+      Sentry.setTag('app_version', version);
+    });
+  })();
+}
 
 if (elem) {
   const root = createRoot(elem);

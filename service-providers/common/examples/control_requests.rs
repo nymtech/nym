96927fc5bfec4a101a5e096194903166da7019aa
Jon Häggblad <jon.haggblad@gmail.com>
2023-06-08 10:16:48 +0200

    Restore control_requests example

--- service-providers/common/examples/control_requests.rs
+++ service-providers/common/examples/control_requests.rs
@@ -7,17 +7,14 @@ use nym_sdk::mixnet::{IncludedSurbs, MixnetClient, Recipient, ReconstructedMessa
 use nym_service_providers_common::interface::{
     ControlRequest, ControlResponse, ProviderInterfaceVersion, Request, Response, ResponseContent,
 };
-use nym_socks5_requests::{Socks5Request, Socks5Response};
 
 fn parse_control_response(received: Vec<ReconstructedMessage>) -> ControlResponse {
     assert_eq!(received.len(), 1);
-    let response: Response<Socks5Request> = Response::try_from_bytes(&received[0].message).unwrap();
+    let response: Response = Response::try_from_bytes(&received[0].message).unwrap();
     match response.content {
         ResponseContent::Control(control) => control,
-        ResponseContent::ProviderData(data) => {
-            println!("received provider data: {:?}", data);
-            dbg!(&data);
-            panic!();
+        ResponseContent::ProviderData(_) => {
+            panic!("received provider data even though we sent control request!")
         }
     }
 }
@@ -33,20 +30,16 @@ async fn wait_for_control_response(client: &mut MixnetClient) -> ControlResponse
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    nym_bin_common::logging::setup_logging();
-
     // technically we don't need to start the entire client with all the subroutines,
     // but I needed an easy way of sending to and receiving from the mixnet
     // and that was the most straightforward way of achieving it
     let mut client = MixnetClient::connect_new().await.unwrap();
-    //let provider: Recipient = "AN8eLxYWFitCkMn92zim3PrPszxJZDYyFFKP7qnnAAew.8UAxL3LwQBis6WpM3GGXaqKGaVdnLCpGJWumHT6KNdTH@77TSuVU8d1oXKbPzjec2xh4i3Wj5WwUyy9Lr36sm8gZm".parse().unwrap();
-    let provider: Recipient = "85xZUGsMi8koFGXnoKQpn3wkcajSghu99uW3P4aQqP3E.D5PdTNtPZnek28BsctU1pYGFzMSTx9z7g9EL9ryjDB5Z@9xJM74FwwHhEKKJHihD21QSZnHM2QBRMoFx9Wst6qNBS".parse().unwrap();
+    let provider: Recipient = "AN8eLxYWFitCkMn92zim3PrPszxJZDYyFFKP7qnnAAew.8UAxL3LwQBis6WpM3GGXaqKGaVdnLCpGJWumHT6KNdTH@77TSuVU8d1oXKbPzjec2xh4i3Wj5WwUyy9Lr36sm8gZm".parse().unwrap();
 
     // generic service provider request, so we don't even need to care it's to the socks5 provider
     let request_health = ControlRequest::Health;
     let request_binary_info = ControlRequest::BinaryInfo;
     let request_versions = ControlRequest::SupportedRequestVersions;
-    //let request_open_proxy = ControlRequest::OpenProxy;
 
     let full_request_health: Request =
         Request::new_control(ProviderInterfaceVersion::new_current(), request_health);
@@ -88,31 +81,6 @@ async fn main() -> anyhow::Result<()> {
         .await;
     let response = wait_for_control_response(&mut client).await;
     println!("response to 'SupportedRequestVersions' request: {response:#?}");
-
-    //let full_request_versions: Request =
-    //Request::new_control(ProviderInterfaceVersion::new_current(), request_open_proxy);
-
-    use nym_socks5_requests::Socks5ProtocolVersion;
-
-    let open_proxy_request = Request::new_provider_data(
-        ProviderInterfaceVersion::new_current(),
-        //Socks5Request::new_open_proxy(Socks5ProtocolVersion::new_current()),
-        Socks5Request::new_query(
-            Socks5ProtocolVersion::new_current(),
-            //nym_socks5_requests::QueryRequest::OpenProxy,
-            nym_socks5_requests::QueryRequest::Description,
-        ),
-    );
-    println!("Sending 'Open Proxy' request...");
-    client
-        .send_bytes(
-            provider,
-            open_proxy_request.into_bytes(),
-            //IncludedSurbs::none(),
-            IncludedSurbs::new(10), //crashes??
-        )
-        .await;
-    let response = wait_for_control_response(&mut client).await;
 
     Ok(())
 }

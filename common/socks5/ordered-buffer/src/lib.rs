7228331db67cd727116f7dabeea0a6b86f9c3972
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-06-08 15:46:31 +0100

    removed OrderedMessage

--- common/socks5/ordered-buffer/src/lib.rs
+++ common/socks5/ordered-buffer/src/lib.rs
@@ -1,12 +1,8 @@
 mod buffer;
 #[deprecated]
-mod message;
-#[deprecated]
 mod sender;
 
 pub use buffer::{OrderedMessageBuffer, ReadContiguousData};
-pub use message::MessageError;
-pub use message::OrderedMessage;
 pub use sender::OrderedMessageSender;
 use std::sync::atomic::AtomicU64;
 use std::sync::Arc;

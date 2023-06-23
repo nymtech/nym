0115b02be3a75556c12f832572c7e7e755bf6fed
Fouad <fmtabbara@hotmail.co.uk>
2023-06-23 14:00:28 +0100

    Wallet - Node Tester (#3551)
    * error handling + edge cases + types
    
    * use bonded node id
    
    * add UI and move feature to node-settings dir
    
    * use error modal
    
    * add type for postMessage arg
    
    * add timeout for node test
    
    * update storybook ci
    
    * fix CI for typescript linting
    
    * fix print node test results
    
    * replace react-to-print lib with vanilla solution
    
    * async print
    
    * update wallet changelog

--- nym-wallet/src/pages/bonding/node-settings/node-settings.constant.tsx
+++ nym-wallet/src/pages/bonding/node-settings/node-settings.constant.tsx
@@ -1,7 +1,9 @@
 export const makeNavItems = (isMixnode: boolean) => {
-  const navItems = ['General', 'Unbond'];
+  const navItems: NavItems[] = ['General', 'Unbond'];
 
-  if (isMixnode) navItems.splice(1, 0, 'Playground');
+  if (isMixnode) navItems.splice(1, 0, 'Test my node', 'Playground');
 
   return navItems;
 };
+
+export type NavItems = 'General' | 'Unbond' | 'Test my node' | 'Playground';

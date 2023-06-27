e615678f125a0986051ca0055775fc612005f48f
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-06-27 14:26:57 +0100

    using hardcoded rootCAs

--- clients/webassembly/go-mix-conn/cmd/wasm/ssl_helper.go
+++ clients/webassembly/go-mix-conn/cmd/wasm/ssl_helper.go
@@ -7,14 +7,9 @@ import (
 	"crypto/tls"
 )
 
-func tlsConfig() tls.Config {
+func tlsConfig(serverName string) tls.Config {
 	return tls.Config{
-		//VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-		//	Error("TODO: implement VerifyPeerCertificate")
-		//	return nil
-		//},
-		// Set InsecureSkipVerify to skip the default validation we are
-		// replacing. This will not disable VerifyConnection.
-		InsecureSkipVerify: true,
+		ServerName: serverName,
+		RootCAs:    rootCerts(),
 	}
 }

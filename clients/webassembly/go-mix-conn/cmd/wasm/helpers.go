078c5775f5a7e5c33acec2e7abf098d4ffdd53bb
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-06-28 09:58:14 +0100

    getting origin from self.location.origin

--- clients/webassembly/go-mix-conn/cmd/wasm/helpers.go
+++ clients/webassembly/go-mix-conn/cmd/wasm/helpers.go
@@ -15,7 +15,7 @@ type jsFn func(this js.Value, args []js.Value) (any, error)
 var (
 	jsErr     = js.Global().Get("Error")
 	jsPromise = js.Global().Get("Promise")
-	origin    = js.Global().Get("origin").String()
+	origin    = js.Global().Get("location").Get("origin").String()
 )
 
 // AsyncFunc converts a Go-JS function into a Promise

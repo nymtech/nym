75cf440d7b65644aab9c81983cb02c1efb99a63d
pierre <dommerc.pierre@gmail.com>
2023-06-30 10:59:02 +0200

    add matrix and monero to supported apps

--- nym-connect/desktop/src/pages/menu/Apps.tsx
+++ nym-connect/desktop/src/pages/menu/Apps.tsx
@@ -3,33 +3,30 @@ import { Divider, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 
 const appsSchema = {
-  messagingApps: ['Telegram', 'Keybase'],
-  wallets: ['Blockstream', 'Electrum'],
+  messagingApps: ['Telegram', 'Matrix', 'Keybase'],
+  wallets: ['Monero', 'Blockstream', 'Electrum'],
 };
 
 export const CompatibleApps = () => (
   <Box>
-    <Typography fontSize="small" color="grey.600" sx={{ mb: 2 }}>
+    <Typography fontWeight="bold" variant="body2" sx={{ mb: 2 }}>
       Supported apps
     </Typography>
     <Typography color="nym.highlight" sx={{ mb: 2 }}>
       Messaging apps
     </Typography>
 
-    <Divider sx={{ mb: 2 }} />
-
-    <Box sx={{ mb: 4 }}>
+    <Box sx={{ mb: 2 }}>
       {appsSchema.messagingApps.map((app) => (
         <Typography variant="body2" color="grey.400" sx={{ mb: 2 }} key={app}>
           {app}
         </Typography>
       ))}
     </Box>
+    <Divider sx={{ mb: 2 }} />
     <Typography color="nym.highlight" sx={{ mb: 2 }}>
       Wallets
     </Typography>
-
-    <Divider sx={{ mb: 2 }} />
 
     <Box sx={{ mb: 4 }}>
       {appsSchema.wallets.map((wallet) => (

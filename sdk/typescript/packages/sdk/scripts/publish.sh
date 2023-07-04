16d6444169ff033e5894fc644ce4f36cfae2497a
Mark Sinclair <mmsinclair@users.noreply.github.com>
2023-07-04 15:30:49 +0100

    Add GH Actions workflow to publish the SDK

---
+++ sdk/typescript/packages/sdk/scripts/publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+rm -rf dist || true
+rm -rf ../../../../dist || true
+
+yarn
+yarn build
+cd ../../../../dist/sdk
+
+cd cjs
+echo "Publishing CommonJS package to NPM.."
+npm publish --access=public
+cd ..
+
+cd esm
+echo "Publishing ESM package to NPM.."
+npm publish --access=public
+cd ..

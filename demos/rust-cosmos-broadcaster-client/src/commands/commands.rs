980ad454964b4a7070ba1ec34b64a7ee5001017d
mfahampshire <maxhampshire@pm.me>
2023-07-06 16:28:01 +0200

    setting up sdk links

--- demos/rust-cosmos-broadcaster-client/src/commands/commands.rs
+++ demos/rust-cosmos-broadcaster-client/src/commands/commands.rs
@@ -1,17 +1,33 @@
+use nym_cli_commands::validator::mixnet::Mixnet;
+use nym_crypto::generic_array::sequence;
 use nym_sphinx_addressing::clients::Recipient;
 use nym_validator_client::nyxd::CosmWasmClient;
+use nym_validator_client::nyxd::cosmwasm_client::types::SequenceResponse;
 use nym_validator_client::signing::direct_wallet::DirectSecp256k1HdWallet;
 use nym_validator_client::signing::tx_signer::TxSigner;
 use nym_validator_client::signing::SignerData;
 use cosmrs::bank::MsgSend;
-use cosmrs::rpc::{HttpClient};
+use cosmrs::rpc::{HttpClient, Id};
 use cosmrs::tx::Msg;
 use cosmrs::{tx, AccountId, Coin, Denom};
 use bip39; 
 use bs58; 
-use nym_sdk::mixnet;
+use nym_sdk::mixnet::{self, MixnetClient};
+use serde::{Deserialize, Serialize};
+// use serde_json::Result; 
 
-pub async fn offline_sign(mnemonic: bip39::Mnemonic, to: AccountId) -> String {
+#[derive(Deserialize, Serialize)]
+struct SequenceRequestData<'a> {
+    validator: &'a str, 
+    signer_address: AccountId
+}
+
+struct SequenceResponseData {
+    sequence_response: SequenceResponse, 
+    chain_id: Id
+}
+
+pub async fn offline_sign(mnemonic: bip39::Mnemonic, to: AccountId, client: &mut MixnetClient , sp_address: Recipient) -> String {
 
     // TODO take coin amount from function args, + load network vars from config file. 
     let prefix = "n";
@@ -24,18 +40,32 @@ pub async fn offline_sign(mnemonic: bip39::Mnemonic, to: AccountId) -> String {
     // local 'client' ONLY signing messages
     let tx_signer = TxSigner::new(signer);
 
+/////////////// to go on server side
     // possibly remote client that doesn't do ANY signing
     // (only broadcasts + queries for sequence numbers)
-    let broadcaster = HttpClient::new(validator).unwrap();
+    // let broadcaster = HttpClient::new(validator).unwrap();
+    // // get sequence information and chain ID from the service 
+    // // add sdk, send req for sequence 
+    // // get signer information
+// let sequence_response = broadcaster.get_sequence(&signer_address).await.unwrap();
+    // let chain_id = broadcaster.get_chain_id().await.unwrap();
+/////////////// 
  
-/////////////// to go on server side: need client from main passed as arg to send w sdk!!  
-    // get sequence information and chain ID from the service 
-    // add sdk, send req for sequence 
-    // get signer information
-    let sequence_response = broadcaster.get_sequence(&signer_address).await.unwrap();
-    let chain_id = broadcaster.get_chain_id().await.unwrap();
-///////////////
- 
+    let message = SequenceRequestData{
+        validator, 
+        signer_address
+    }; 
+    // send req to client 
+    client.send_str(sp_address, &serde_json::to_string(&message).unwrap()).await;
+    let res = client.wait_for_messages().await; 
+    for i in res.unwrap().iter() {
+        println!("{:#?}", i); 
+    }
+    // parse json of res to get signer_data and chain_id, store in SeqResData struct 
+
+    todo!()
+
+/* 
     // use the response to create SignerData instance 
     let signer_data = SignerData::new_from_sequence_response(sequence_response, chain_id);
 
@@ -69,16 +99,10 @@ pub async fn offline_sign(mnemonic: bip39::Mnemonic, to: AccountId) -> String {
     let tx_bytes = tx_raw.to_bytes().unwrap();
     let base58_tx_bytes = bs58::encode(tx_bytes).into_string();
     base58_tx_bytes
-
+*/
 }
 
-pub async fn send_tx(base58_tx: String, sp_address: Recipient) -> Option<Vec<mixnet::ReconstructedMessage>> {
-
-    // TODO pass created client as arg frm main instead 
-    let mut client = mixnet::MixnetClient::connect_new().await.unwrap();
-    let our_address = client.nym_address();
-    println!("Our client nym address is: {our_address}");
-
+pub async fn send_tx(base58_tx: String, sp_address: Recipient, client: &MixnetClient) -> Option<Vec<mixnet::ReconstructedMessage>> {
     // send message w sdk to broadcaster who will do: 
     /* 
         // decode the base58 tx to vec<u8>
@@ -90,13 +114,9 @@ pub async fn send_tx(base58_tx: String, sp_address: Recipient) -> Option<Vec<mix
 
         // send res back via SURBs 
      */
-    client.send_str(sp_address, &base58_tx).await; // send as base58 encoded and it can be decoded by the SP 
-    println!("\nWaiting for reply\n");
-    let res = client.wait_for_messages().await; 
-
-    // disconnect client 
-    // return the res to return to main thread 
-    client.disconnect().await;
-    res 
-
+    // client.send_str(sp_address, &base58_tx).await; // send as base58 encoded and it can be decoded by the SP 
+    // println!("\nWaiting for reply\n");
+    // let res = client.wait_for_messages().await; 
+    // res 
+    todo!()
 }

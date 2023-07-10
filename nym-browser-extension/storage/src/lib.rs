a09e66c43afb4fef0342829ff7be45782127a6ed
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-07-10 13:05:39 +0100

    fixed browser extension storage build + added it to CI

--- nym-browser-extension/storage/src/lib.rs
+++ nym-browser-extension/storage/src/lib.rs
@@ -1,14 +1,22 @@
 // Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use wasm_bindgen::prelude::*;
-
+#[cfg(target_arch = "wasm32")]
 pub mod error;
+
+#[cfg(target_arch = "wasm32")]
 pub mod storage;
 
+#[cfg(target_arch = "wasm32")]
 pub use error::ExtensionStorageError;
+
+#[cfg(target_arch = "wasm32")]
 pub use storage::ExtensionStorage;
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the

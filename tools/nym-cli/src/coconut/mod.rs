16313991edfd6bfc486860f55abb56b20e9c3a79
farbanas <arbanasfran@gmail.com>
2023-07-19 19:22:39 +0200

    remove one layer of coconut in nym-cli

--- tools/nym-cli/src/coconut/mod.rs
+++ tools/nym-cli/src/coconut/mod.rs
@@ -1,1 +1,19 @@
-pub(crate) mod coconut;
+use nym_cli_commands::context::{create_signing_client_with_nym_api, ClientArgs};
+use nym_network_defaults::NymNetworkDetails;
+
+pub(crate) async fn execute(
+    global_args: ClientArgs,
+    coconut: nym_cli_commands::coconut::Coconut,
+    network_details: &NymNetworkDetails,
+) -> anyhow::Result<()> {
+    match coconut.command {
+        nym_cli_commands::coconut::CoconutCommands::IssueCredentials(args) => {
+            nym_cli_commands::coconut::issue_credentials::execute(
+                args,
+                create_signing_client_with_nym_api(global_args, network_details)?,
+            )
+            .await
+        }
+    }
+    Ok(())
+}

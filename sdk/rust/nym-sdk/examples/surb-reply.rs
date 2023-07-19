f6e19e8233dacf19821e40f2924df267eb1264f0
mfahampshire <maxhampshire@pm.me>
2023-07-19 11:58:37 +0200

    removed unused import

--- sdk/rust/nym-sdk/examples/surb-reply.rs
+++ sdk/rust/nym-sdk/examples/surb-reply.rs
@@ -1,5 +1,5 @@
 use nym_sdk::mixnet::{
-    AnonymousSenderTag, MixnetClient, MixnetClientBuilder, ReconstructedMessage, StoragePaths,
+    AnonymousSenderTag, MixnetClientBuilder, ReconstructedMessage, StoragePaths,
 };
 use std::path::PathBuf; 
 

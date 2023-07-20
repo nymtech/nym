415fe4605c133133625f71ac1a2184aba3f448b3
Pierre Dommerc <dommerc.pierre@gmail.com>
2023-07-20 13:17:49 +0200

    feat(nc-desktop): add sentry to backend (#3652)

--- nym-connect/desktop/src-tauri/src/logging.rs
+++ nym-connect/desktop/src-tauri/src/logging.rs
@@ -1,4 +1,6 @@
 use fern::colors::{Color, ColoredLevelConfig};
+use log::Level;
+use sentry::Level as SentryLevel;
 use serde::Serialize;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use std::str::FromStr;
@@ -22,7 +24,10 @@ fn formatted_time() -> String {
     _now.format(&format).unwrap()
 }
 
-pub fn setup_logging(app_handle: tauri::AppHandle) -> Result<(), log::SetLoggerError> {
+pub fn setup_logging(
+    app_handle: tauri::AppHandle,
+    monitoring: bool,
+) -> Result<(), log::SetLoggerError> {
     let colors = ColoredLevelConfig::new()
         .trace(Color::Magenta)
         .debug(Color::Blue)
@@ -61,6 +66,21 @@ pub fn setup_logging(app_handle: tauri::AppHandle) -> Result<(), log::SetLoggerE
                 level: record.level().into(),
             };
             app_handle.emit_all("log://log", msg).unwrap();
+        }))
+        .chain(fern::Output::call(move |record| {
+            if !monitoring {
+                return;
+            }
+            let level = match record.level() {
+                Level::Error => SentryLevel::Error,
+                Level::Warn => SentryLevel::Warning,
+                Level::Info => SentryLevel::Info,
+                _ => SentryLevel::Debug,
+            };
+            // only send error and warn logs to sentry
+            if let Level::Error | Level::Warn = record.level() {
+                sentry::capture_message(&record.args().to_string(), level);
+            };
         }));
 
     base_config
--- nym-connect/desktop/src-tauri/src/main.rs
+++ nym-connect/desktop/src-tauri/src/main.rs
@@ -25,6 +25,10 @@ mod state;
 mod tasks;
 mod window;
 
+// TODO DSN shouldn't be hardcoded
+const SENTRY_DSN: &str =
+    "https://68a2c55113ed47aaa30b9899039b0799@o967446.ingest.sentry.io/4505483113594880";
+
 fn main() {
     setup_env(None);
     println!("Starting up...");
@@ -32,7 +36,40 @@ fn main() {
     // As per breaking change description here
     // https://github.com/tauri-apps/tauri/blob/feac1d193c6d618e49916ad0707201f43d5cdd36/tooling/bundler/CHANGELOG.md
     if let Err(error) = fix_path_env::fix() {
-        log::warn!("Failed to fix PATH: {error}");
+        println!("Failed to fix PATH: {error}");
+    }
+
+    let user_data = UserData::read().unwrap_or_else(|e| {
+        println!("{}", e);
+        println!("Fallback to default");
+        UserData::default()
+    });
+
+    let monitoring = user_data.monitoring.unwrap_or(false);
+
+    let _guard = sentry::init((
+        SENTRY_DSN,
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            sample_rate: 1.0, // TODO lower this in prod
+            traces_sample_rate: 1.0,
+            ..Default::default() // TODO add data scrubbing
+                                 // see https://docs.sentry.io/platforms/rust/data-management/sensitive-data/
+        },
+    ));
+
+    sentry::configure_scope(|scope| {
+        scope.set_user(Some(sentry::User {
+            id: Some("nym".into()),
+            ..Default::default()
+        }));
+    });
+
+    if monitoring {
+        println!("Sentry reporting and monitoring is enabled");
+    } else {
+        println!("Monitoring is disabled, dropping sentry guard");
+        drop(_guard)
     }
 
     let user_data = UserData::read().unwrap_or_else(|e| {
@@ -86,7 +123,7 @@ fn main() {
                 );
             }
         })
-        .setup(|app| Ok(crate::logging::setup_logging(app.app_handle())?))
+        .setup(move |app| Ok(crate::logging::setup_logging(app.app_handle(), monitoring)?))
         .system_tray(create_tray_menu())
         .on_system_tray_event(tray_menu_event_handler)
         .run(context)

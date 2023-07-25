f4e917ec0b00799e2462426a9728f284e82444a0
farbanas <arbanasfran@gmail.com>
2023-07-25 09:26:00 +0200

    fixing clippy errors

--- common/issue-credential/src/recovery_storage.rs
+++ common/issue-credential/src/recovery_storage.rs
@@ -20,21 +20,19 @@ impl RecoveryStorage {
         let entries = read_dir(&self.recovery_dir)?;
 
         let mut paths = vec![];
-        for entry in entries {
-            if let Some(ok_entry) = entry.ok() {
-                let path = ok_entry.path();
-                if path.is_file() {
-                    paths.push(path)
-                }
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_file() {
+                paths.push(path)
             }
         }
 
         let mut vouchers = vec![];
         for path in paths {
-            if let Some(mut file) = File::open(path).ok() {
+            if let Ok(mut file) = File::open(path) {
                 let mut buff = Vec::new();
                 if file.read_to_end(&mut buff).is_ok() {
-                    if let Some(voucher) = BandwidthVoucher::try_from_bytes(&buff).ok() {
+                    if let Ok(voucher) = BandwidthVoucher::try_from_bytes(&buff) {
                         vouchers.push(voucher)
                     }
                 }

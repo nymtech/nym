981f567131acf04889e5949716cf0acda9758f56
Simon Wicky <simon@nymtech.net>
2023-07-26 14:44:56 +0200

    first try at noisestream

--- common/nymnoise/src/lib.rs
+++ common/nymnoise/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use bytes::{BufMut, BytesMut};
 use log::*;
 use nym_topology::NymTopology;
 use pin_project::pin_project;
@@ -21,6 +22,8 @@ use tokio::{
 };
 
 const NOISE_HS_PATTERN: &str = "Noise_XKpsk3_25519_AESGCM_SHA256";
+const MAXMSGLEN: usize = 65535;
+const HEADER_SIZE: usize = 2;
 
 /// Wrapper around a TcpStream
 #[pin_project]
@@ -28,6 +31,7 @@ pub struct NoiseStream {
     #[pin]
     inner_stream: TcpStream,
     noise: TransportState,
+    storage: BytesMut,
 }
 
 impl NoiseStream {
@@ -35,6 +39,7 @@ impl NoiseStream {
         NoiseStream {
             inner_stream,
             noise,
+            storage: BytesMut::with_capacity(MAXMSGLEN + HEADER_SIZE),
         }
     }
 }
@@ -43,20 +48,42 @@ impl AsyncRead for NoiseStream {
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
-        buf: &mut tokio::io::ReadBuf<'_>,
+        buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let mut projected_self = self.project();
-        return Pin::new(&mut projected_self.inner_stream).poll_read(cx, buf);
-        let mut inner_vec = vec![0u8; 65535];
+        let projected_self = self.project();
+        let mut inner_vec = vec![0u8; MAXMSGLEN + HEADER_SIZE];
         let mut noise_buf = ReadBuf::new(&mut inner_vec);
-        match Pin::new(&mut projected_self.inner_stream).poll_read(cx, &mut noise_buf) {
+
+        match projected_self.inner_stream.poll_read(cx, &mut noise_buf) {
             Poll::Pending => return Poll::Pending,
             Poll::Ready(Ok(())) => {
-                let mut payload = vec![0u8; 65535];
+                let bytes_read = [
+                    &projected_self.storage[..projected_self.storage.len()],
+                    noise_buf.filled(),
+                ]
+                .concat();
+                projected_self.storage.clear();
+                //We can't read the lenght
+                if bytes_read.len() < HEADER_SIZE {
+                    projected_self.storage.put_slice(&bytes_read);
+                    cx.waker().wake_by_ref();
+                    return Poll::Pending;
+                }
+
+                let msg_len = ((bytes_read[0] as usize) << 8) + (bytes_read[1] as usize);
+                //we can't read the whole message
+                if bytes_read.len() < HEADER_SIZE + msg_len {
+                    projected_self.storage.put_slice(&bytes_read);
+                    cx.waker().wake_by_ref();
+                    return Poll::Pending;
+                }
+
+                //we have a full Noise message available
+                let mut payload = vec![0u8; MAXMSGLEN];
                 println!("Read : {:?}", &noise_buf);
                 let len = projected_self
                     .noise
-                    .read_message(&noise_buf.filled(), &mut payload)
+                    .read_message(&bytes_read, &mut payload)
                     .unwrap();
                 buf.put_slice(&payload[..len]);
                 return Poll::Ready(Ok(()));
@@ -71,16 +98,19 @@ impl AsyncWrite for NoiseStream {
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, std::io::Error>> {
-        let mut projected_self = self.project();
-        return Pin::new(&mut projected_self.inner_stream).poll_write(cx, &buf);
-        //let mut stream = self.project().inner_stream;
-        let mut noise_buf = vec![0u8; 65535];
+        let projected_self = self.project();
+        let mut noise_buf = vec![0u8; MAXMSGLEN];
         let len = projected_self
             .noise
             .write_message(buf, &mut noise_buf)
             .unwrap();
         println!("Will send : {:?}", &noise_buf[..len]);
-        let res = Pin::new(&mut projected_self.inner_stream).poll_write(cx, &noise_buf[..len]);
+        let to_send = [
+            &[(buf.len() >> 8) as u8, (buf.len() & 0xff) as u8],
+            &noise_buf[..len],
+        ]
+        .concat();
+        let res = projected_self.inner_stream.poll_write(cx, &to_send);
         println!("Sent : {:?}", res);
         res
     }
@@ -89,16 +119,14 @@ impl AsyncWrite for NoiseStream {
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Result<(), std::io::Error>> {
-        let mut stream = self.project().inner_stream;
-        Pin::new(&mut stream).poll_flush(cx)
+        self.project().inner_stream.poll_flush(cx)
     }
 
     fn poll_shutdown(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Result<(), std::io::Error>> {
-        let mut stream = self.project().inner_stream;
-        Pin::new(&mut stream).poll_shutdown(cx)
+        self.project().inner_stream.poll_shutdown(cx)
     }
 }
 
@@ -139,7 +167,7 @@ pub async fn upgrade_noise_initiator(
         .build_initiator()?;
 
     //Actual Handshake
-    let mut buf = vec![0u8; 65535];
+    let mut buf = vec![0u8; MAXMSGLEN];
     // -> e, es
     let len = handshake.write_message(&[], &mut buf).unwrap();
     send(&mut conn, &buf[..len]).await;
@@ -194,7 +222,7 @@ pub async fn upgrade_noise_responder(
         .build_responder()?;
 
     //Actual Handshake
-    let mut buf = vec![0u8; 65535];
+    let mut buf = vec![0u8; MAXMSGLEN];
     // <- e, es
     handshake
         .read_message(&recv(&mut conn).await.unwrap(), &mut buf)
@@ -216,7 +244,7 @@ pub async fn upgrade_noise_responder(
 
 /// Hyper-basic stream transport receiver. 16-bit BE size followed by payload.
 async fn recv(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
-    let mut msg_len_buf = [0u8; 2];
+    let mut msg_len_buf = [0u8; HEADER_SIZE];
     stream.read_exact(&mut msg_len_buf).await?;
     let msg_len = ((msg_len_buf[0] as usize) << 8) + (msg_len_buf[1] as usize);
     let mut msg = vec![0u8; msg_len];

ec2e9082e01fd71d7d189f01da6a89bd203165c4
Jon Häggblad <jon.haggblad@gmail.com>
2023-07-27 16:19:52 +0200

    Extract out some functions in geo_aware_provider

--- common/client-core/src/client/topology_control/geo_aware_provider.rs
+++ common/client-core/src/client/topology_control/geo_aware_provider.rs
@@ -8,118 +8,24 @@ use nym_topology::{
     provider_trait::{async_trait, TopologyProvider},
     NymTopology,
 };
-use nym_validator_client::client::MixId;
+use nym_validator_client::client::{MixId, MixNodeDetails};
 use url::Url;
 
-pub struct GeoAwareTopologyProvider {
-    validator_client: nym_validator_client::client::NymApiClient,
-    filter_on: String,
+const EXPLORER_API_MIXNODES_URL: &str = "https://explorer.nymtech.net/api/v1/mix-nodes";
+
+async fn fetch_mixnodes_from_explorer_api() -> Option<Vec<PrettyDetailedMixNodeBond>> {
+    reqwest::get(EXPLORER_API_MIXNODES_URL)
+        .await
+        .ok()?
+        .json::<Vec<PrettyDetailedMixNodeBond>>()
+        .await
+        .ok()
 }
 
-impl GeoAwareTopologyProvider {
-    pub fn new(nym_api_url: Url, filter_on: String) -> GeoAwareTopologyProvider {
-        GeoAwareTopologyProvider {
-            validator_client: nym_validator_client::client::NymApiClient::new(nym_api_url),
-            filter_on,
-        }
-    }
-
-    async fn get_topology(&self) -> Option<NymTopology> {
-        let mixnodes = match self.validator_client.get_cached_active_mixnodes().await {
-            Err(err) => {
-                error!("failed to get network mixnodes - {err}");
-                return None;
-            }
-            Ok(mixes) => mixes,
-        };
-
-        let gateways = match self.validator_client.get_cached_gateways().await {
-            Err(err) => {
-                error!("failed to get network gateways - {err}");
-                return None;
-            }
-            Ok(gateways) => gateways,
-        };
-
-        // Also fetch mixnodes cached by explorer-api, with the purpose of getting their
-        // geolocation.
-        debug!("Fetching mixnodes from explorer-api...");
-        let mixnodes_from_explorer_api =
-            reqwest::get("https://explorer.nymtech.net/api/v1/mix-nodes")
-                .await
-                .unwrap()
-                .json::<Vec<PrettyDetailedMixNodeBond>>()
-                .await
-                .unwrap();
-
-        // Partition mixnodes_from_explorer_api according to the value of two_letter_iso_country_code
-        let mixnodes_from_explorer_api_by_continent = mixnodes_from_explorer_api.into_iter().fold(
-            HashMap::<String, Vec<MixId>>::new(),
-            |mut acc, m| {
-                if let Some(ref location) = m.location {
-                    let country_code = location.two_letter_iso_country_code.clone();
-                    if let Some(continent_code) = country_code_to_continent_code(&country_code) {
-                        let mixnodes = acc.entry(continent_code).or_insert_with(Vec::new);
-                        mixnodes.push(m.mix_id);
-                    }
-                }
-                acc
-            },
-        );
-
-        // Create a string with the number of mixnodes per continent.
-        let mixnodes_from_explorer_api_by_continent_string =
-            mixnodes_from_explorer_api_by_continent
-                .iter()
-                .map(|(k, v)| format!("{}: {}", k, v.len()))
-                .collect::<Vec<_>>()
-                .join(", ");
-        debug!(
-            "Mixnode distribution - {}",
-            mixnodes_from_explorer_api_by_continent_string
-        );
-
-        // Filter mixnodes so that only the items that also exist in the mixnodes_from_explorer_api_by_continent for the key given by filter_on.
-        let mixnodes = mixnodes
-            .into_iter()
-            .filter(|m| {
-                if let Some(ids) = mixnodes_from_explorer_api_by_continent.get(&self.filter_on) {
-                    ids.contains(&m.mix_id())
-                } else {
-                    // If the key is not setup, or no mixnodes exist for the key, then return false.
-                    false
-                }
-            })
-            .collect::<Vec<_>>();
-
-        // Check layer distribution
-        let mut layer_counts = mixnodes.iter().map(|m| m.layer()).fold(
-            HashMap::<Layer, usize>::new(),
-            |mut acc, layer| {
-                let count = acc.entry(layer).or_insert(0);
-                *count += 1;
-                acc
-            },
-        );
-
-        // and check the integrity
-        for layer in &[Layer::One, Layer::Two, Layer::Three] {
-            layer_counts.entry(*layer).or_insert(0);
-            let count = layer_counts[layer];
-            if count < 2 {
-                error!("There are only {} mixnodes in layer {:?}", count, layer);
-                return None;
-            }
-        }
-
-        Some(nym_topology_from_detailed(mixnodes, gateways))
-    }
-}
-
-// We map contry codes to continent codes, but we do it manually to reserve the right to tweak this
-// distribution for our purposes.
-// Also, at the time of writing I didn't find a simple crate that did this mapping...
-fn country_code_to_continent_code(country_code: &str) -> Option<String> {
+// We map contry codes into group, which initially are continent codes to a first approximation,
+// but we do it manually to reserve the right to tweak this distribution for our purposes.
+// TODO: replace String with enum
+fn country_code_to_group_code(country_code: &str) -> Option<String> {
     match country_code {
         // Europe
         "AT" => Some("EU".to_string()),
@@ -194,6 +100,119 @@ fn country_code_to_continent_code(country_code: &str) -> Option<String> {
             println!("Unknown country code: {}", country_code);
             None
         }
+    }
+}
+
+fn group_mixnodes_by_country_code(
+    mixnodes: Vec<PrettyDetailedMixNodeBond>,
+) -> HashMap<String, Vec<MixId>> {
+    mixnodes
+        .into_iter()
+        .fold(HashMap::<String, Vec<MixId>>::new(), |mut acc, m| {
+            if let Some(ref location) = m.location {
+                let country_code = location.two_letter_iso_country_code.clone();
+                if let Some(group_code) = country_code_to_group_code(&country_code) {
+                    let mixnodes = acc.entry(group_code).or_insert_with(Vec::new);
+                    mixnodes.push(m.mix_id);
+                }
+            }
+            acc
+        })
+}
+
+fn log_mixnode_distribution(mixnodes: &HashMap<String, Vec<MixId>>) {
+    let mixnode_distribution = mixnodes
+        .iter()
+        .map(|(k, v)| format!("{}: {}", k, v.len()))
+        .collect::<Vec<_>>()
+        .join(", ");
+    debug!("Mixnode distribution - {}", mixnode_distribution);
+}
+
+fn count_mixnodes_per_layer(mixnodes: Vec<MixNodeDetails>) -> HashMap<Layer, usize> {
+    mixnodes
+        .iter()
+        .map(|m| m.layer())
+        .fold(HashMap::<Layer, usize>::new(), |mut acc, layer| {
+            let count = acc.entry(layer).or_insert(0);
+            *count += 1;
+            acc
+        })
+}
+
+fn check_layer_integrity(mixnodes: Vec<MixNodeDetails>) -> Result<(), ()> {
+    let mut layer_counts = count_mixnodes_per_layer(mixnodes);
+    for layer in &[Layer::One, Layer::Two, Layer::Three] {
+        layer_counts.entry(*layer).or_insert(0);
+        let count = layer_counts[layer];
+        if count < 2 {
+            error!("There are only {} mixnodes in layer {:?}", count, layer);
+            return Err(());
+        }
+    }
+    Ok(())
+}
+
+pub struct GeoAwareTopologyProvider {
+    validator_client: nym_validator_client::client::NymApiClient,
+    filter_on: String,
+}
+
+impl GeoAwareTopologyProvider {
+    pub fn new(nym_api_url: Url, filter_on: String) -> GeoAwareTopologyProvider {
+        GeoAwareTopologyProvider {
+            validator_client: nym_validator_client::client::NymApiClient::new(nym_api_url),
+            filter_on,
+        }
+    }
+
+    async fn get_topology(&self) -> Option<NymTopology> {
+        let mixnodes = match self.validator_client.get_cached_active_mixnodes().await {
+            Err(err) => {
+                error!("failed to get network mixnodes - {err}");
+                return None;
+            }
+            Ok(mixes) => mixes,
+        };
+
+        let gateways = match self.validator_client.get_cached_gateways().await {
+            Err(err) => {
+                error!("failed to get network gateways - {err}");
+                return None;
+            }
+            Ok(gateways) => gateways,
+        };
+
+        // Also fetch mixnodes cached by explorer-api, with the purpose of getting their
+        // geolocation.
+        debug!("Fetching mixnodes from explorer-api...");
+        let Some(mixnodes_from_explorer_api) = fetch_mixnodes_from_explorer_api().await else {
+            error!("failed to get mixnodes from explorer-api");
+            return None;
+        };
+
+        // Partition mixnodes_from_explorer_api according to the value of
+        // two_letter_iso_country_code.
+        // NOTE: we construct the full distribution here, but only use the one we're interested in.
+        // The reason we this instead of a straight filter is that this opens up the possibility to
+        // complement a small grouping with mixnodes from adjecent countries.
+        let mixnode_distribution = group_mixnodes_by_country_code(mixnodes_from_explorer_api);
+        log_mixnode_distribution(&mixnode_distribution);
+
+        let Some(filtered_mixnode_ids) = mixnode_distribution.get(&self.filter_on) else {
+            error!("no mixnodes found for: {}", self.filter_on);
+            return None;
+        };
+
+        let mixnodes = mixnodes
+            .into_iter()
+            .filter(|m| filtered_mixnode_ids.contains(&m.mix_id()))
+            .collect::<Vec<_>>();
+
+        // TODO: return real error type
+        check_layer_integrity(mixnodes.clone()).ok()?;
+
+        Some(nym_topology_from_detailed(mixnodes, gateways))
     }
 }
 

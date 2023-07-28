37ea1206ad8fd09eb636e760415018a8aa2c4fb6
Jon Häggblad <jon.haggblad@gmail.com>
2023-07-28 13:33:34 +0200

    rustfmt

--- nym-connect/desktop/src-tauri/src/tasks.rs
+++ nym-connect/desktop/src-tauri/src/tasks.rs
@@ -50,7 +50,10 @@ fn override_config_from_env(config: &mut Config, privacy_level: &PrivacyLevel) {
         // TODO: selectable in the UI
         let default_country_group = CountryGroup::Europe;
         log::warn!("Using geo-aware mixnode selection: {default_country_group}");
-        config.core.base.set_topology_structure(TopologyStructure::GeoAware(default_country_group));
+        config
+            .core
+            .base
+            .set_topology_structure(TopologyStructure::GeoAware(default_country_group));
     }
 }
 

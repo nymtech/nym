9bdc3b260fa6b27763d6e92004b3aaa1fe3f9c33
mfahampshire <maxhampshire@pm.me>
2023-09-04 13:39:48 +0200

    changed version bumper script: removed platform_release_version references

--- documentation/bump_versions.sh
+++ documentation/bump_versions.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-# this takes two args: platform release version and wallet release version.
+# takes one manadatory arg and one optional arg: wallet release and minimum rust versions
 # it then uses sed to bump them in the three book.toml files.
 #
-# e.g if the upcoming platform release was v1.1.29 and the release version 1.2.9 you'd run this as:
-# `./bump_versions.sh "1.1.29" "1.2.9"`
+# e.g if the upcoming wallet release version was 1.2.9 you'd run this as:
+# `./bump_versions.sh "1.2.9"`
 #
-# you can also set the minumum rust version by passing an optional 3rd argument:
-# `./bump_versions.sh "1.1.29" "1.2.9" "1.67"`
+# you can also set the minumum rust version by passing an optional additional argument:
+# `./bump_versions.sh "1.2.9" "1.67"`
 
 # array of project dirs
 declare -a projects=("docs" "dev-portal" "operators")
 
 # check number of args passed
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ];
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ];
 then
-    echo "failure: please pass at least 2 and at most 3 args: "
-    echo "./bump_version.sh <new platform_release_version> <new wallet_release_version> [OPTIONAL]<new minimum_rust_version>"
+    echo "failure: please pass at least 1 and at most 2 args: "
+    echo "./bump_version.sh <new wallet_release_version> [OPTIONAL]<new minimum_rust_version>"
     exit 0
 fi
 
@@ -30,7 +30,6 @@ else
   do
     # sed the vars in the book.toml file for each project
     echo "setting platform and wallet versions in $i/"
-    sed -i 's/platform_release_version =.*/platform_release_version = "'$1'"/' "$i"/book.toml
     sed -i 's/wallet_release_version =.*/wallet_release_version = "'$2'"/' "$i"/book.toml
     if [ "$3" ]
     then

77a56600f0d14d0dc73b8933db6db8f1d4f5ded3
pierre <dommerc.pierre@gmail.com>
2023-09-07 16:34:37 +0200

    fix build, add icon launcher

--- nym-vpn/android/build.gradle.kts
+++ nym-vpn/android/build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.2.0-beta01" apply false
+    id("com.android.application") version "8.2.0-beta02" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22"  apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     kotlin("plugin.serialization") version "1.8.22" apply false

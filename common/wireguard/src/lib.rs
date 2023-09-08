0400b6553e5830a962c3641f6c8f4929d555495c
Jon Häggblad <jon.haggblad@gmail.com>
2023-09-08 15:19:17 +0200

    Most channels are in place

--- common/wireguard/src/lib.rs
+++ common/wireguard/src/lib.rs
@@ -1,27 +1,85 @@
-use std::sync::Arc;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{Display, Formatter},
+    sync::Arc,
+    time::Duration,
+};
 
+use bytes::Bytes;
+use futures::StreamExt;
 use nym_task::TaskClient;
-use tokio::net::UdpSocket;
+use tap::TapFallible;
+use tokio::{net::UdpSocket, sync::oneshot};
 
 //const WG_ADDRESS = "0.0.0.0:51820";
 const WG_ADDRESS: &str = "0.0.0.0:51822";
 
-pub async fn start_wg_listener(mut task_client: TaskClient) -> Result<(), Box<dyn std::error::Error>>{
-    log::info!("Starting Wireguard listener on {}", WG_ADDRESS);
+const PRIVATE_KEY: &str = "AEqXrLFT4qjYq3wmX0456iv94uM6nDj5ugp6Jedcflg=";
 
+const PEERS: &[&str; 1] = &["mxV/mw7WZTe+0Msa0kvJHMHERDA/cSskiZWQce+TdEs="];
+
+pub async fn start_wg_listener(
+    mut task_client: TaskClient,
+) -> Result<(), Box<dyn std::error::Error>> {
+    log::info!("Starting Wireguard listener on {}", WG_ADDRESS);
     let udp4_socket = Arc::new(UdpSocket::bind(WG_ADDRESS).await?);
 
-    let mut buf = [0u8; 1024];
     tokio::spawn(async move {
+        let mut active_peers: HashMap<
+            std::net::SocketAddr,
+            tokio::sync::mpsc::UnboundedSender<Event>,
+        > = HashMap::new();
+        let mut active_peers_rx = futures::stream::FuturesUnordered::new();
+        // let mut active_peers_incoming = tokio::sync::mpsc::unbounded_channel();
+        // let (bus_tx, _) = tokio::sync::broadcast::channel(128);
+        let mut buf = [0u8; 1024];
+
         while !task_client.is_shutdown() {
             tokio::select! {
                 _ = task_client.recv() => {
                     log::trace!("WireGuard listener: received shutdown");
                 }
+                addr = active_peers_rx.next() => {
+                    log::info!("WireGuard listener: received shutdown from {:?}", addr);
+                    match addr {
+                        Some(Ok(addr)) => {
+                            active_peers.remove(&addr);
+                        }
+                        Some(Err(err)) => {
+                            log::error!("WireGuard listener: error receiving shutdown from peer: {}", err);
+                        }
+                        None => {}
+                    }
+                }
                 Ok((len, addr)) = udp4_socket.recv_from(&mut buf) => {
                     log::info!("Received {} bytes from {}", len, addr);
-                    handle_incoming_packet().await;
+
+                    if let Some(tx) = active_peers.get_mut(&addr) {
+                        log::info!("WireGuard listener: received packet from known peer");
+                        tx.send(Event::WgPacket(buf[..len].to_vec().into()))
+                            .tap_err(|err| log::error!("{err}"))
+                            .unwrap();
+                        // bus_tx.send(Event::WgPacket(buf[..len].to_vec().into()))
+                        //     .tap_err(|err| log::error!("{err}"))
+                        //     .unwrap();
+                    } else {
+                        log::info!("WireGuard listener: received packet from unknown peer");
+                        let (peer_tx, peer_rx) = tokio::sync::mpsc::unbounded_channel();
+                        active_peers.insert(addr, peer_tx);
+                        let tunnel = WireGuardTunnel::new(peer_rx);
+                        let join_handle = tokio::spawn(async move {
+                            tunnel.spin_off().await;
+                            addr
+                        });
+                        active_peers_rx.push(join_handle);
+                        // bus_tx.send(Event::WgPacket(buf[..len].to_vec().into()))
+                        //     .tap_err(|err| log::error!("{err}"))
+                        //     .unwrap();
+                    }
                 }
+                // oneshot channel from when we should remove the addr from active_peers
+                // (i.e. when the peer disconnects)
+
             }
         }
         log::info!("WireGuard listener: shutting down");
@@ -30,9 +88,50 @@ pub async fn start_wg_listener(mut task_client: TaskClient) -> Result<(), Box<dy
     Ok(())
 }
 
-async fn handle_incoming_packet(packet: &[u8]) {
+struct WireGuardTunnel {}
 
+impl WireGuardTunnel {
+    fn new(peer_rx: tokio::sync::mpsc::UnboundedReceiver<Event>) -> Self {
+        Self {}
+    }
+
+    async fn spin_off(self) {
+        loop {
+            tokio::select! {
+                _ = tokio::time::sleep(Duration::from_millis(2000)) => {
+                    log::info!("WireGuard tunnel: shutting down");
+                    break;
+                }
+            }
+        }
+    }
 }
 
-struct WireGuardTunnel {
+#[derive(Debug, Clone)]
+pub enum Event {
+    /// Dumb event with no data.
+    Dumb,
+    /// IP packet received from the WireGuard tunnel that should be passed through to the corresponding virtual device/internet.
+    /// Original implementation also has protocol here since it understands it, but we'll have to infer it downstream
+    WgPacket(Bytes),
+    /// IP packet to be sent through the WireGuard tunnel as crafted by the virtual device.
+    IpPacket(Bytes),
+}
+
+impl Display for Event {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Event::Dumb => {
+                write!(f, "Dumb{{}}")
+            }
+            Event::WgPacket(data) => {
+                let size = data.len();
+                write!(f, "WgPacket{{ size={size} }}")
+            }
+            Event::IpPacket(data) => {
+                let size = data.len();
+                write!(f, "IpPacket{{ size={size} }}")
+            }
+        }
+    }
 }

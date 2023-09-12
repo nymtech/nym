c1794815bd41571f622196ad2f2de9cc4f70c0e0
Mark Sinclair <mmsinclair@users.noreply.github.com>
2023-09-12 01:43:53 +0100

    Batch integration tests for mixFetch

---
+++ sdk/typescript/tests/integration-tests/mix-fetch/template/webpack/webpack.base.js
@@ -0,0 +1,91 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const WebpackFavicons = require('webpack-favicons');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const os = require('os');
+
+/**
+ * Creates the default Webpack config
+ * @param baseDir The base directory path, e.g. pass `__dirname` of the webpack config file using this method
+ */
+const webpackCommon = (baseDir, htmlPath) => ({
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'thread-loader',
+            options: { workers: Math.max(2, os.cpus().length - 1) },
+          },
+          { loader: 'ts-loader', options: { happyPackMode: true } },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|md|webp)$/i,
+        // More information here https://webpack.js.org/guides/asset-modules/
+        type: 'asset',
+      },
+      {
+        // See https://webpack.js.org/guides/asset-management/#loading-fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ya?ml$/,
+        type: 'json',
+        use: 'yaml-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+    alias: {
+      'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+    },
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+
+    ...(Array.isArray(htmlPath)
+      ? htmlPath.map((item) => new HtmlWebpackPlugin(item))
+      : [
+          new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(baseDir, htmlPath || 'src/index.html'),
+          }),
+        ]),
+
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        mode: 'write-references',
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
+    }),
+  ],
+  output: {
+    path: path.resolve(baseDir, 'dist'),
+    publicPath: '/',
+  },
+});
+
+module.exports = {
+  webpackCommon,
+};
---
+++ sdk/typescript/tests/integration-tests/mix-fetch/template/webpack/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { mergeWithRules } = require('webpack-merge');
+const { webpackCommon } = require('./webpack.base');
+
+module.exports = mergeWithRules({
+  module: {
+    rules: {
+      test: 'match',
+      use: 'replace',
+    },
+  },
+})(
+  webpackCommon(__dirname, [
+    {
+      inject: true,
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+      chunks: ['index'],
+    },
+  ]),
+  {
+    entry: {
+      index: path.resolve(__dirname, 'src/index.ts'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+  },
+);

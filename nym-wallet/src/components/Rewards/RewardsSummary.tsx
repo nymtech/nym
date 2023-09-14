ba0b5e212086516b342e07a74b1a2f50f13c89ec
fmtabbara <fmtabbara@hotmail.co.uk>
2023-09-14 11:29:06 +0100

    add total delegations to rewards summary

--- nym-wallet/src/components/Rewards/RewardsSummary.tsx
+++ nym-wallet/src/components/Rewards/RewardsSummary.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { CircularProgress, Stack, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { InfoTooltip } from '../InfoToolTip';
+import { useDelegationContext } from 'src/context/delegations';
 
 export const RewardsSummary: FCWithChildren<{
   isLoading?: boolean;
@@ -9,19 +10,28 @@ export const RewardsSummary: FCWithChildren<{
   totalRewards?: string;
 }> = ({ isLoading, totalDelegation, totalRewards }) => {
   const theme = useTheme();
+
+  const { totalDelegationsAndRewards } = useDelegationContext();
   return (
-    <Stack direction="row" justifyContent="space-between">
-      <Stack direction="row" spacing={4}>
+    <Stack direction="row" justifyContent="space-between" marginTop={3}>
+      <Stack direction="row" spacing={5}>
         <Stack direction="row" spacing={1} alignItems="center">
           <InfoTooltip title="This is the total amount you have delegated to node(s) in the network" />
           <Typography>Total delegations:</Typography>
+          <Typography fontWeight={600} fontSize={16} textTransform="uppercase">
+            {isLoading ? <CircularProgress size={theme.typography.fontSize} /> : totalDelegationsAndRewards || '-'}
+          </Typography>
+        </Stack>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <InfoTooltip title="This is the total amount you have delegated to node(s) in the network" />
+          <Typography>Original delegations:</Typography>
           <Typography fontWeight={600} fontSize={16} textTransform="uppercase">
             {isLoading ? <CircularProgress size={theme.typography.fontSize} /> : totalDelegation || '-'}
           </Typography>
         </Stack>
         <Stack direction="row" spacing={1} alignItems="center">
-          <InfoTooltip title="This is the rewards you have accrued since the last time you redeemed your rewards. Rewards are automatically compounded. You can redeem your rewards at any time" />
-          <Typography>New rewards:</Typography>
+          <InfoTooltip title="The total amount you have delegated to node(s) in the network. The amount also includes the rewards you have accrued since last time you claimed your rewards" />
+          <Typography>Total rewards:</Typography>
           <Typography fontWeight={600} fontSize={16} textTransform="uppercase">
             {isLoading ? <CircularProgress size={theme.typography.fontSize} /> : totalRewards || '-'}
           </Typography>

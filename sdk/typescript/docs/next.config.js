cd3831a98d33c5870d0d0f077713464294edb414
Gala <calero.vg@gmail.com>
2023-09-14 10:27:37 +0200

    cleaninig a bit

--- sdk/typescript/docs/next.config.js
+++ sdk/typescript/docs/next.config.js
@@ -7,31 +7,6 @@ const { merge } = require('webpack-merge');
 console.dir(withNextra(), { depth: 30 });
 console.dir(withNextra().rewrites, { depth: 30 });
 
-// webpack: (config, options) => {
-//   const nextraConfig = withNextra({ webpack: (config) => config });
-//   const nextraWebpack = nextraConfig.webpack(config, options);
-//   return Object.assign({}, nextraWebpack, {
-//     externals: Object.assign({}, config.externals, {
-//       fs: 'fs',
-//     }),
-//     module: Object.assign({}, config.module, {
-//       rules: config.module.rules.concat([
-//         {
-//           test: /\.txt$/,
-//           loader: 'emit-file-loader',
-//           options: {
-//             name: 'dist/[path][name].[ext]',
-//           },
-//         },
-//         {
-//           test: /\.txt$/,
-//           loader: 'raw-loader',
-//         },
-//       ]),
-//     }),
-//   });
-// },
-
 const config = {
   ...withNextra(),
   webpack: (config, options) => {

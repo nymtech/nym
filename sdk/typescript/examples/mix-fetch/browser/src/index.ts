59933a430c791e9b093b7558ad23f6b600b12acf
Tommy Verrall <tommy@nymtech.net>
2023-10-11 17:09:23 +0200

    testing

--- sdk/typescript/examples/mix-fetch/browser/src/index.ts
+++ sdk/typescript/examples/mix-fetch/browser/src/index.ts
@@ -1,9 +1,37 @@
-import { mixFetch } from '@nymproject/mix-fetch';
+import { SetupMixFetchOps, mixFetch } from '@nymproject/mix-fetch';
 import { appendOutput, appendImageOutput } from './utils';
 
 async function main() {
   // options for mixFetch (you can also set these with the `createMixFetch` function
-  const mixFetchOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const extra = {
+    hiddenGateways: [
+      {
+        owner: 'n1ns3v70ul9gnl9l9fkyz8cyxfq75vjcmx8el0t3',
+        host: 'sandbox-gateway1.nymtech.net',
+        explicitIp: '35.158.238.80',
+        identityKey: 'HjNEDJuotWV8VD4ufeA1jeheTnfNJ7Jorevp57hgaZua',
+        sphinxKey: 'BoXeUD7ERGmzRauMjJD3itVNnQiH42ncUb6kcVLrb3dy',
+      },
+    ],
+  };
+
+
+  const mixFetchOptionsForSandbox: SetupMixFetchOps = {
+    preferredGateway: 'HjNEDJuotWV8VD4ufeA1jeheTnfNJ7Jorevp57hgaZua', // with WSS
+    preferredNetworkRequester:
+      'AzGdJ4MU78Ex22NEWfeycbN7bt3PFZr1MtKstAdhfELG.GSxnKnvKPjjQm3FdtsgG5KyhP6adGbPHRmFWDH4XfUpP@HjNEDJuotWV8VD4ufeA1jeheTnfNJ7Jorevp57hgaZua',
+    mixFetchOverride: {
+      requestTimeoutMs: 60_000,
+    },
+    nymApiUrl: 'https://sandbox-nym-api1.nymtech.net/api',
+    extra,
+    forceTls: true
+  };
+
+  // options for mixFetch (you can also set these with the `createMixFetch` function
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const mixFetchOptionsMainnet = {
     preferredGateway: 'E3mvZTHQCdBvhfr178Swx9g4QG3kkRUun7YnToLMcMbM', // with WSS
     preferredNetworkRequester:
       'GiRjFWrMxt58pEMuusm4yT3RxoMD1MMPrR9M2N4VWRJP.3CNZBPq4vg7v7qozjGjdPMXcvDmkbWPCgbGCjQVw9n6Z@2xU4CBE6QiiYt6EyBXSALwxkNvM7gqJfjHXaMkjiFmYW',
@@ -11,6 +39,9 @@ async function main() {
       requestTimeoutMs: 60_000,
     },
   };
+
+  const mixFetchOptions = mixFetchOptionsForSandbox;
+
 
   // disable CORS (in your app, you probably don't want to disable CORS, it is a good thing to leave it enabled)
   const args = { mode: 'unsafe-ignore-cors' };

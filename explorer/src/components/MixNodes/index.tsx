393d348306ff17fb839e82ca9c1a8f52312ec46c
fmtabbara <fmtabbara@hotmail.co.uk>
2023-10-12 14:28:46 +0100

    fix sorting for mixnodes and gateways

--- explorer/src/components/MixNodes/index.tsx
+++ explorer/src/components/MixNodes/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
-import { MixNodeResponse, MixNodeResponseItem, MixnodeStatus, NodePerformance } from '../../typeDefs/explorer-api';
-import { toPercentIntegerString } from '../../utils';
+import { MixNodeResponse, MixNodeResponseItem, MixnodeStatus } from '../../typeDefs/explorer-api';
+import { toPercentInteger, toPercentIntegerString } from '../../utils';
 import { unymToNym } from '../../utils/currency';
 
 export type MixnodeRowType = {
@@ -15,11 +15,11 @@ export type MixnodeRowType = {
   pledge_amount: number;
   host: string;
   layer: string;
-  profit_percentage: string;
+  profit_percentage: number;
   avg_uptime: string;
   stake_saturation: React.ReactNode;
-  operating_cost: string;
-  node_performance: NodePerformance['most_recent'];
+  operating_cost: number;
+  node_performance: number;
   blacklisted: boolean;
 };
 
@@ -32,7 +32,7 @@ export function mixNodeResponseItemToMixnodeRowType(item: MixNodeResponseItem): 
   const delegations = Number(item.total_delegation.amount) || 0;
   const totalBond = pledge + delegations;
   const selfPercentage = ((pledge * 100) / totalBond).toFixed(2);
-  const profitPercentage = toPercentIntegerString(item.profit_margin_percent) || 0;
+  const profitPercentage = toPercentInteger(item.profit_margin_percent) || 0;
   const uncappedSaturation = typeof item.uncapped_saturation === 'number' ? item.uncapped_saturation * 100 : 0;
 
   return {
@@ -47,11 +47,11 @@ export function mixNodeResponseItemToMixnodeRowType(item: MixNodeResponseItem): 
     pledge_amount: pledge,
     host: item?.mix_node?.host || '',
     layer: item?.layer || '',
-    profit_percentage: `${profitPercentage}%`,
+    profit_percentage: profitPercentage,
     avg_uptime: `${toPercentIntegerString(item.node_performance.last_24h)}%`,
     stake_saturation: Number(uncappedSaturation.toFixed(2)),
-    operating_cost: `${unymToNym(item.operating_cost?.amount, 6)} NYM`,
-    node_performance: `${toPercentIntegerString(item.node_performance.most_recent)}%`,
+    operating_cost: Number(unymToNym(item.operating_cost?.amount, 6)) || 0,
+    node_performance: toPercentInteger(item.node_performance.most_recent),
     blacklisted: item.blacklisted,
   };
 }

52d5eb444b9e0332f9d5dcb2a8a599eed9a7d6f9
Mark Sinclair <mmsinclair@users.noreply.github.com>
2023-10-13 18:40:47 +0100

    Fix up Typescript CI and linting

---
+++ sdk/typescript/scripts/dev-mode-add.mjs
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
+
+const devWorkspace = ['sdk/typescript/packages/**', 'sdk/typescript/examples/**'];
+if (!packageJson.workspaces.includes(devWorkspace)) {
+  // add
+  packageJson.workspaces.push(...devWorkspace);
+
+  // write out modified file
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+}

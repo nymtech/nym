f36fc21bc1f2a078313e165593ba6e089046b29e
Simon Wicky <simon@nymtech.net>
2023-10-27 09:33:16 +0200

    wip double spending detection

--- gateway/src/node/client_handling/websocket/connection_handler/coconut.rs
+++ gateway/src/node/client_handling/websocket/connection_handler/coconut.rs
@@ -3,8 +3,9 @@
 
 use super::authenticated::RequestHandlingError;
 use log::*;
-use nym_coconut_interface::Credential;
-use nym_validator_client::coconut::all_coconut_api_clients;
+use nym_compact_ecash::scheme::EcashCredential;
+use nym_compact_ecash::PayInfo;
+use nym_validator_client::coconut::all_ecash_api_clients;
 use nym_validator_client::{
     nyxd::{
         contract_traits::{
@@ -21,112 +22,158 @@ use std::time::{Duration, SystemTime};
 const ONE_HOUR_SEC: u64 = 3600;
 const MAX_FEEGRANT_UNYM: u128 = 10000;
 
-pub(crate) struct CoconutVerifier {
+pub(crate) struct EcashVerifier {
     nyxd_client: DirectSigningHttpRpcNyxdClient,
     mix_denom_base: String,
 }
 
-impl CoconutVerifier {
+impl EcashVerifier {
     pub fn new(nyxd_client: DirectSigningHttpRpcNyxdClient) -> Self {
         let mix_denom_base = nyxd_client.current_chain_details().mix_denom.base.clone();
 
-        CoconutVerifier {
+        EcashVerifier {
             nyxd_client,
             mix_denom_base,
         }
     }
 
-    pub async fn all_current_coconut_api_clients(
+    pub async fn all_current_ecash_api_clients(
         &self,
     ) -> Result<Vec<CoconutApiClient>, RequestHandlingError> {
         let epoch_id = self.nyxd_client.get_current_epoch().await?.epoch_id;
-        self.all_coconut_api_clients(epoch_id).await
+        self.all_ecash_api_clients(epoch_id).await
     }
 
-    pub async fn all_coconut_api_clients(
+    pub async fn all_ecash_api_clients(
         &self,
         epoch_id: u64,
     ) -> Result<Vec<CoconutApiClient>, RequestHandlingError> {
-        Ok(all_coconut_api_clients(&self.nyxd_client, epoch_id).await?)
+        Ok(all_ecash_api_clients(&self.nyxd_client, epoch_id).await?)
     }
 
-    pub async fn release_funds(
+    pub fn verify_pay_info(&self, pay_info: &PayInfo) -> Result<(), RequestHandlingError> {
+        //SW : TODO implement Payinfo checking here.
+        //SW : Centralized component with respect to clients handler
+        // RequestHandlingError::InvalidBandwidthCredential(String::from(
+        //     "credential failed to verify on gateway",
+        // ))
+        Ok(())
+    }
+
+    pub async fn post_and_store_credential(
         &self,
         api_clients: Vec<CoconutApiClient>,
-        credential: &Credential,
+        credential: EcashCredential,
     ) -> Result<(), RequestHandlingError> {
-        // Use a custom multiplier for revoke, as the default one (1.3)
-        // isn't enough
-        let revoke_fee = Some(Fee::Auto(Some(1.5)));
-
-        let res = self
-            .nyxd_client
-            .spend_credential(
-                Coin::new(
-                    credential.voucher_value().into(),
-                    self.mix_denom_base.clone(),
-                ),
-                credential.blinded_serial_number(),
-                self.nyxd_client.address().to_string(),
-                None,
-            )
-            .await?;
-        let proposal_id = find_attribute(&res.logs, "wasm", BANDWIDTH_PROPOSAL_ID)
-            .ok_or(RequestHandlingError::ProposalIdError {
-                reason: String::from("proposal id not found"),
-            })?
-            .value
-            .parse::<u64>()
-            .map_err(|_| RequestHandlingError::ProposalIdError {
-                reason: String::from("proposal id could not be parsed to u64"),
-            })?;
-
-        let proposal = self.nyxd_client.query_proposal(proposal_id).await?;
-        if !credential.has_blinded_serial_number(&proposal.description)? {
-            return Err(RequestHandlingError::ProposalIdError {
-                reason: String::from("proposal has different serial number"),
-            });
-        }
-
         let req = nym_api_requests::coconut::VerifyCredentialBody::new(
             credential.clone(),
-            proposal_id,
             self.nyxd_client.address().clone(),
         );
+
         for client in api_clients {
-            self.nyxd_client
-                .grant_allowance(
-                    &client.cosmos_address,
-                    vec![Coin::new(MAX_FEEGRANT_UNYM, self.mix_denom_base.clone())],
-                    SystemTime::now().checked_add(Duration::from_secs(ONE_HOUR_SEC)),
-                    // It would be nice to be able to filter deeper, but for now only the msg type filter is avaialable
-                    vec![String::from("/cosmwasm.wasm.v1.MsgExecuteContract")],
-                    "Create allowance to vote the release of funds".to_string(),
-                    None,
-                )
-                .await?;
-            let ret = client.api_client.verify_bandwidth_credential(&req).await;
-            self.nyxd_client
-                .revoke_allowance(
-                    &client.cosmos_address,
-                    "Cleanup the previous allowance for releasing funds".to_string(),
-                    revoke_fee.clone(),
-                )
-                .await?;
-            match ret {
-                Ok(res) => {
-                    if !res.verification_result {
-                        debug!("Validator {} didn't accept the credential. It will probably vote No on the spending proposal", client.api_client.nym_api.current_url());
-                    }
-                }
-                Err(e) => {
-                    warn!("Validator {} could not be reached. There might be a problem with the coconut endpoint - {:?}", client.api_client.nym_api.current_url(), e);
-                }
-            }
+            let ret = client.api_client.verify_bandwidth_credential(&req).await; //SW PUT that in a new threads, ensuring it eventually gets sent
+            warn!(
+                "Return from {} : {:?}",
+                client.api_client.nym_api.current_url(),
+                ret
+            );
+            // match ret {
+            //     Ok(res) => {
+            //         if !res.verification_result {
+            //             debug!(
+            //                 "Validator {} didn't accept the credential.",
+            //                 client.api_client.nym_api.current_url()
+            //             );
+            //         }
+            //     }
+            //     Err(e) => {
+            //         warn!("Validator {} could not be reached. There might be a problem with the coconut endpoint - {:?}", client.api_client.nym_api.current_url(), e);
+            //     }
+            // }
         }
 
-        self.nyxd_client.execute_proposal(proposal_id, None).await?;
+        //store credential
 
         Ok(())
     }
+
+    // pub async fn release_funds(
+    //     &self,
+    //     api_clients: Vec<CoconutApiClient>,
+    //     credential: &nym_coconut_interface::Credential,
+    // ) -> Result<(), RequestHandlingError> {
+    //     // Use a custom multiplier for revoke, as the default one (1.3)
+    //     // isn't enough
+    //     let revoke_fee = Some(Fee::Auto(Some(1.5)));
+
+    //     let res = self
+    //         .nyxd_client
+    //         .spend_credential(
+    //             Coin::new(
+    //                 credential.voucher_value().into(),
+    //                 self.mix_denom_base.clone(),
+    //             ),
+    //             credential.blinded_serial_number(),
+    //             self.nyxd_client.address().to_string(),
+    //             None,
+    //         )
+    //         .await?;
+    //     let proposal_id = find_attribute(&res.logs, "wasm", BANDWIDTH_PROPOSAL_ID)
+    //         .ok_or(RequestHandlingError::ProposalIdError {
+    //             reason: String::from("proposal id not found"),
+    //         })?
+    //         .value
+    //         .parse::<u64>()
+    //         .map_err(|_| RequestHandlingError::ProposalIdError {
+    //             reason: String::from("proposal id could not be parsed to u64"),
+    //         })?;
+
+    //     let proposal = self.nyxd_client.query_proposal(proposal_id).await?;
+    //     if !credential.has_blinded_serial_number(&proposal.description)? {
+    //         return Err(RequestHandlingError::ProposalIdError {
+    //             reason: String::from("proposal has different serial number"),
+    //         });
+    //     }
+
+    //     let req = nym_api_requests::coconut::VerifyCredentialBody::new(
+    //         credential.clone(),
+    //         //proposal_id,
+    //         self.nyxd_client.address().clone(),
+    //     );
+    //     for client in api_clients {
+    //         self.nyxd_client
+    //             .grant_allowance(
+    //                 &client.cosmos_address,
+    //                 vec![Coin::new(MAX_FEEGRANT_UNYM, self.mix_denom_base.clone())],
+    //                 SystemTime::now().checked_add(Duration::from_secs(ONE_HOUR_SEC)),
+    //                 // It would be nice to be able to filter deeper, but for now only the msg type filter is avaialable
+    //                 vec![String::from("/cosmwasm.wasm.v1.MsgExecuteContract")],
+    //                 "Create allowance to vote the release of funds".to_string(),
+    //                 None,
+    //             )
+    //             .await?;
+    //         let ret = client.api_client.verify_bandwidth_credential(&req).await;
+    //         self.nyxd_client
+    //             .revoke_allowance(
+    //                 &client.cosmos_address,
+    //                 "Cleanup the previous allowance for releasing funds".to_string(),
+    //                 revoke_fee.clone(),
+    //             )
+    //             .await?;
+    //         match ret {
+    //             Ok(res) => {
+    //                 if !res.verification_result {
+    //                     debug!("Validator {} didn't accept the credential. It will probably vote No on the spending proposal", client.api_client.nym_api.current_url());
+    //                 }
+    //             }
+    //             Err(e) => {
+    //                 warn!("Validator {} could not be reached. There might be a problem with the coconut endpoint - {:?}", client.api_client.nym_api.current_url(), e);
+    //             }
+    //         }
+    //     }
+
+    //     self.nyxd_client.execute_proposal(proposal_id, None).await?;
+
+    //     Ok(())
+    // }
 }

60912ff8efd017a9ddba0797f82ee114dcc0c099
Sebastian Martinez <me@sebastinez.dev>
2023-11-02 18:19:34 +0100

    Fix sdk publish action (#4088)
    * fix: sdk publish script
    
    The bash implementation used in the GitHub CI runner seems to have an
    issue with incrementing a variable with `(( COUNTER++ ))` while `((
    COUNTER=+1 ))` works fine.
    
    * Add more sdk packages to workspace and normalize build sdk scripts
    
    * Disable workspaces in npm publish sdk
    
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>
    
    * Continue publishing even when it fails
    
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>
    
    ---------
    
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- sdk/typescript/scripts/publish.sh
+++ sdk/typescript/scripts/publish.sh
@@ -10,9 +10,46 @@ set -o pipefail
 
 cd dist
 
+#packages=(
+#chat-app/parcel
+#chat-app/plain-html
+#chat-app/react-webpack-with-theme-example
+#chrome-extension
+#firefox-extension
+#node-tester/parcel
+#node-tester/plain-html
+#node-tester/react
+#react/mui-theme
+#react/sdk-react
+#)
 packages=(
+"wasm/client"
+"wasm/mix-fetch"
+"wasm/node-tester"
+"wasm/extension-storage"
+
+"node/wasm/client"
+"node/wasm/mix-fetch"
+
+"ts/sdk/mix-fetch/cjs"
+"ts/sdk/mix-fetch/cjs-full-fat"
+"ts/sdk/mix-fetch/esm"
+"ts/sdk/mix-fetch/esm-full-fat"
+
 "ts/sdk/nodejs-client/cjs"
 "ts/sdk/mix-fetch-node/cjs"
+
+"ts/sdk/node-tester/cjs"
+"ts/sdk/node-tester/cjs-full-fat"
+"ts/sdk/node-tester/esm"
+"ts/sdk/node-tester/esm-full-fat"
+
+"ts/sdk/sdk/cjs"
+"ts/sdk/sdk/cjs-full-fat"
+"ts/sdk/sdk/esm"
+"ts/sdk/sdk/esm-full-fat"
+
+"ts/sdk/contract-clients"
 )
 
 pushd () {
@@ -39,14 +76,13 @@ COUNTER=0
 
 for item in "${packages[@]}"
 do
-  (( COUNTER++ ))
+  (( COUNTER+=1 ))
   pushd "$item"
   echo "🚀 Publishing $item... (${COUNTER} of ${#packages[@]})"
-  jq -r '. | .name + " " +.version' < package.json
-  npm publish --access=public --verbose
+  cat package.json | jq -r '. | .name + " " +.version'
+  npm publish --access=public --verbose --workspaces false || true
   popd
   echo ""
 done
 echo ""
 echo "✅ Done"
-

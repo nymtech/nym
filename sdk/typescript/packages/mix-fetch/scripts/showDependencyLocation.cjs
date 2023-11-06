f67c6ccefc358ba604b1578284d78ea31661fc69
Mark Sinclair <mmsinclair@users.noreply.github.com>
2023-11-06 15:38:52 +0000

    Show location of WASM package

---
+++ sdk/typescript/packages/mix-fetch/scripts/showDependencyLocation.cjs
@@ -0,0 +1,4 @@
+const packageName = '@nymproject/mix-fetch-wasm';
+const packageJsonPath = require.resolve(packageName + '/package.json');
+
+console.log(`🟢🟢🟢 ${packageName} is at ${packageJsonPath}`);
---
+++ sdk/typescript/packages/sdk/scripts/showDependencyLocation.cjs
@@ -0,0 +1,4 @@
+const packageName = '@nymproject/nym-client-wasm';
+const packageJsonPath = require.resolve(packageName + '/package.json');
+
+console.log(`🟢🟢🟢 ${packageName} is at ${packageJsonPath}`);

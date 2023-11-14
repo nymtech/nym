a1471bb452634a9495915f7a34817ad1c4fca87f
pierre <dommerc.pierre@gmail.com>
2023-11-14 17:21:36 +0100

    setup router

---
+++ nym-vpn/ui/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+  return <div>
+    Home
+  </div>
+}
+
+export default Home;
---
+++ nym-vpn/ui/src/pages/index.ts
@@ -0,0 +1,2 @@
+export { default as Home } from './Home';
+export { default as Settings } from './Settings';

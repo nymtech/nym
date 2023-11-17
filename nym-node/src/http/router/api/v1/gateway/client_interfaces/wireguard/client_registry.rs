a5c1e4abf09d943962115ce337efae08c51c63fd
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2023-11-17 13:04:22 +0000

    Expose the same pub key that's used for wg (#4157)

--- nym-node/src/http/router/api/v1/gateway/client_interfaces/wireguard/client_registry.rs
+++ nym-node/src/http/router/api/v1/gateway/client_interfaces/wireguard/client_registry.rs
@@ -31,10 +31,7 @@ async fn process_final_message(
         }
     };
 
-    if client
-        .verify(state.dh_keypair.private_key(), preshared_nonce)
-        .is_ok()
-    {
+    if client.verify(&state.private_key, preshared_nonce).is_ok() {
         state.registration_in_progress.remove(&client.pub_key());
         state.client_registry.insert(client.pub_key(), client);
 
@@ -104,7 +101,7 @@ pub(crate) async fn register_client(
             // mark it as used, even though it's not final
             *private_ip_ref = false;
             let gateway_data = GatewayClient::new(
-                state.dh_keypair.private_key(),
+                &state.private_key,
                 remote_public,
                 *private_ip_ref.key(),
                 nonce,

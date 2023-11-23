2d899a9684e4a966a2ed1454ef77f5ad6381c5bb
Mark Sinclair <mmsinclair@users.noreply.github.com>
2023-11-23 14:19:31 +0000

    Change mixnode, gateway, network and nym-api binary licenses to GPLv3

--- gateway/gateway-requests/src/iv.rs
+++ gateway/gateway-requests/src/iv.rs
@@ -1,5 +1,5 @@
 // Copyright 2020 - Nym Technologies SA <contact@nymtech.net>
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: GPL-3.0-only
 
 use nym_crypto::generic_array::{typenum::Unsigned, GenericArray};
 use nym_crypto::symmetric::stream_cipher::{random_iv, IvSizeUser, IV as CryptoIV};
--- service-providers/network-requester/src/request_filter/mod.rs
+++ service-providers/network-requester/src/request_filter/mod.rs
@@ -1,5 +1,5 @@
 // Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: GPL-3.0-only
 
 use crate::config::{self, Config};
 use crate::error::NetworkRequesterError;

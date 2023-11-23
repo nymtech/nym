028b7ef2754dbf70a840d01680edaddfc303cb05
pierre <dommerc.pierre@gmail.com>
2023-11-23 15:05:09 +0100

    init tailwind theme

--- nym-vpn/ui/tailwind.config.ts
+++ nym-vpn/ui/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,15 +8,65 @@ export default {
       transparent: 'transparent',
       current: 'currentColor',
       'baltic-sea': {
-        light: '#2B2831',
-        DEFAULT: '#1C1B1F',
+        jaguar: '#2A2831', // [D] bg for network modes
+        // [L] status-line title text + icon
+        // [L] connection timer text
+        // [L] "Connecting" status text
+        // [L] main titles text
+        // [L] network mode title text + icon
+        // [L] node location select text value + icon + label
+        // [D] button text
+        DEFAULT: '#1C1B1F', // [D] main bg
+        oil: '#1F1F22', // [D] connection status bg
+        quartzite: '#202C25', // [D] "Connected" status bg
       },
-      oil: '#1F1F22',
-      quartzite: '#202C25',
+      'blanc-nacre': {
+        DEFAULT: '#F2F4F6', // [L] main bg
+        platinum: '#E6E8EB', // [L] connection status bg
+        icicle: '#E1EFE7', // [L] "Connected" status bg
+      },
+      // [DL] primary accent
       melon: '#FB6E4E',
+      // [DL] secondary accent
       cornflower: '#7075FF',
+      // [DL] error status text
+      teaberry: '#E33B5A',
+      // [DL] "Connected" status text
+      'vert-menthe': '#2BC761',
+      // [D] main titles text
+      // [D] connection timer text
+      // [D] "Connecting" status text
+      // [L] bg for network modes
+      // [L] button text
+      white: '#FFF',
+      black: '#000',
+      mercury: {
+        // [D] status-line title text + icon
+        // [D] network mode title text + icon
+        // [D] node location select text value + icon + label
+        pinkish: '#E6E1E5',
+        DEFAULT: '#E1EFE7',
+        // [D] network mode desc text
+        // [D] "Connection time"
+        // [D] main status desc text
+        mist: '#938F99',
+      },
+      // [DL] "Disconnected" status text
+      'coal-mine': { dark: '#56545A', light: '#A4A4A4' },
+      // [L] "Connection time"
+      // [L] main status desc text
+      'dim-dray': '#696571',
+      // [L] network mode desc text
+      // [L] node location select outline
+      'ciment-feet': '#79747E',
+      // [D] node location select outline
+      'gun-powder': '#49454F',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
   // Toggling dark mode manually

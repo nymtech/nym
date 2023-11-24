acf9de0f74202b334411243cf71a8bec91dee152
Zane Schepke <zanecschepke@gmail.com>
2023-11-24 06:56:13 -0500

    Add protoc optionals arg

--- nym-vpn/desktop/nymvpn-controller/build.rs
+++ nym-vpn/desktop/nymvpn-controller/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     const PROTO_PATH: &str = "proto/nymvpn-controller.proto";
-    tonic_build::configure().compile(&[PROTO_PATH], &["proto"])?;
+    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&[PROTO_PATH], &["proto"])?;
     println!("cargo:rerun-if-changed=proto");
     Ok(())
 }
--- nym-vpn/desktop/nymvpn-server/build.rs
+++ nym-vpn/desktop/nymvpn-server/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
+        .protoc_arg(("--experimental_allow_proto3_optional"))
         .compile(&["proto/nymvpn-server.proto"], &["proto"])?;
     println!("cargo:rerun-if-changed=proto");
     Ok(())

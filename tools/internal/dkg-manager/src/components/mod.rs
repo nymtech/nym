8602b751c7489c48a4f6c884643972ae823e89e7
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2023-11-30 17:47:17 +0000

    removed dkg-manager tool
    it was moved to a different branch

--- tools/internal/dkg-manager/src/components/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
-// SPDX-License-Identifier: Apache-2.0
-
-use ratatui::Frame;
-
-pub mod basic_contract_info;
-pub mod chain_history;
-pub mod home;
-pub mod logger;

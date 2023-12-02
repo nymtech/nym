c9c710a4e31ea60d2974559dc9265b556d19a16e
Jon Häggblad <jon.haggblad@gmail.com>
2023-12-02 21:17:10 +0100

    also set zero mix hops in tun listener

--- service-providers/ip-packet-router/src/tun_listener.rs
+++ service-providers/ip-packet-router/src/tun_listener.rs
@@ -62,7 +62,13 @@ impl TunListener {
                                 log::error!("Failed to serialize response packet");
                                 continue;
                             };
-                            let input_message = InputMessage::new_regular(recipient, response_packet, lane, packet_type);
+
+                            let lane = TransmissionLane::General;
+                            let packet_type = None;
+                            let mix_hops = Some(0);
+                            let input_message = InputMessage::new_regular_custom_hop(*recipient, response_packet, lane, packet_type, mix_hops);
+
+                            // let input_message = InputMessage::new_regular(recipient, response_packet, lane, packet_type);
 
                             if let Err(err) = self.mixnet_client_sender.send(input_message).await {
                                 log::error!("TunListener: failed to send packet to mixnet: {err}");

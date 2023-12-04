1855ee6575815db1da26e7104dcb06c1b46d6276
pierre <dommerc.pierre@gmail.com>
2023-12-04 12:50:05 +0100

    add theme switch into settings

--- nym-vpn/ui/src-tauri/src/commands/app_data.rs
+++ nym-vpn/ui/src-tauri/src/commands/app_data.rs
@@ -3,7 +3,7 @@ use tracing::{debug, instrument};
 
 use crate::{
     error::{CmdError, CmdErrorSource},
-    fs::data::AppData,
+    fs::data::{AppData, UiTheme},
     states::SharedAppData,
 };
 
@@ -43,4 +43,27 @@ pub async fn get_app_data(
         .map_err(|e| CmdError::new(CmdErrorSource::InternalError, e.to_string()))?;
 
     Ok(data)
+}
+
+#[instrument(skip_all)]
+#[tauri::command]
+pub async fn set_ui_theme(
+    data_state: State<'_, SharedAppData>,
+    theme: UiTheme,
+) -> Result<(), CmdError> {
+    debug!("set_ui_theme");
+
+    // save the selected UI theme to disk
+    let mut app_data_store = data_state.lock().await;
+    let mut app_data = app_data_store
+        .read()
+        .await
+        .map_err(|e| CmdError::new(CmdErrorSource::InternalError, e.to_string()))?;
+    app_data.ui_theme = Some(theme);
+    app_data_store.data = app_data;
+    app_data_store
+        .write()
+        .await
+        .map_err(|e| CmdError::new(CmdErrorSource::InternalError, e.to_string()))?;
+    Ok(())
 }
--- nym-vpn/ui/src/pages/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-function Settings() {
-  const { t } = useTranslation();
-
-  return <div>{t('settings')}</div>;
-}
-
-export default Settings;

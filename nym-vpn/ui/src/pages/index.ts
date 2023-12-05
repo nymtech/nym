dc44013f214198c1fb8ba78f29ee835979549c6e
Zane Schepke <zanecschepke@gmail.com>
2023-12-05 09:54:16 -0500

    fix indexing

--- nym-vpn/ui/src/pages/index.ts
+++ nym-vpn/ui/src/pages/index.ts
@@ -1,5 +1,5 @@
 export * from './home';
 export * from './settings';
 export { default as NavLayout } from './NavLayout';
-export { default as NodeLocation } from './location/NodeLocation';
+export * from './location';
 export { default as Error } from './Error';

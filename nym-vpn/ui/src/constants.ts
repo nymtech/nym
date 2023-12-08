31a345ddad63453f38a7e4b77323fff05c87662f
Zane Schepke <zanecschepke@gmail.com>
2023-12-08 06:23:27 -0500

    update quick prefix to fastest

--- nym-vpn/ui/src/constants.ts
+++ nym-vpn/ui/src/constants.ts
@@ -11,7 +11,7 @@ export const AppName = 'NymVPN';
 export const ConnectionEvent = 'connection-state';
 export const ProgressEvent = 'connection-progress';
 //putting this here for now until decided how default country is determined
-export const QuickConnectPrefix = 'Recommended';
+export const QuickConnectPrefix = 'Fastest';
 export const QuickConnectCountry: Country = {
   name: `${QuickConnectPrefix} (Germany)`,
   code: 'DE',

ea6f009c01ce13f3f69de810ac81b869c4895413
Gala <calero.vg@gmail.com>
2023-12-13 14:11:14 +0100

    wip credential generation example

--- sdk/typescript/examples/credentials/browser/src/index.ts
+++ sdk/typescript/examples/credentials/browser/src/index.ts
@@ -4,10 +4,17 @@ import { appendOutput } from './utils';
 async function main() {
   const mnemonic = document.getElementById('mnemonic') as HTMLInputElement;
   const coin = document.getElementById('coin') as HTMLInputElement;
+  const button = document.getElementById('button') as HTMLButtonElement;
   const client = await createNymCredentialsClient({ isSandbox: true }); // options: {isSandbox?: boolean; networkDetails?: {}}
 
-  const credential = await client.comlink.acquireCredential(coin.value, mnemonic.value, {});
-  appendOutput(JSON.stringify(credential, null, 2));
+  const generateCredential = async () => {
+    const credential = await client.comlink.acquireCredential(coin.value, mnemonic.value, {});
+    appendOutput(JSON.stringify(credential, null, 2));
+  };
+
+  if (button) {
+    button.addEventListener('click', () => generateCredential());
+  }
 }
 
 // wait for the html to load
@@ -15,3 +22,4 @@ window.addEventListener('DOMContentLoaded', () => {
   // let's do this!
   main();
 });
+

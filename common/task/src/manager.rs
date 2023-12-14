a81e7e6c53370f3cc06432dec28ba382c110ecaf
Jon Häggblad <jon.haggblad@gmail.com>
2023-12-14 10:26:09 +0100

    Tweak task dropped log verbosity (#4249)

--- common/task/src/manager.rs
+++ common/task/src/manager.rs
@@ -494,13 +494,13 @@ impl Drop for TaskClient {
     fn drop(&mut self) {
         if !self.mode.should_signal_on_drop() {
             self.log(
-                Level::Debug,
-                "the task client is getting dropped: this is expected during client shutdown",
+                Level::Trace,
+                "the task client is getting dropped but inststructed to not signal: this is expected during client shutdown",
             );
             return;
         } else {
             self.log(
-                Level::Info,
+                Level::Debug,
                 "the task client is getting dropped: this is expected during client shutdown",
             );
         }

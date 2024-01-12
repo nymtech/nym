0e74f081427eace7607da34f67039f3da14a6cb4
Mark Sinclair <mmsinclair@users.noreply.github.com>
2024-01-12 16:36:18 +0000

    Get ready for publishing Typescript SDK with credentials client

--- sdk/typescript/packages/sdk/rollup-worker-full-fat.config.mjs
+++ sdk/typescript/packages/sdk/rollup-worker-full-fat.config.mjs
@@ -2,6 +2,7 @@ import { getConfig } from './rollup/worker.mjs';
 
 export default {
   ...getConfig('src/mixnet/wasm/worker.ts', 'nym_client_wasm_bg.wasm', {
-    inlineWasm: process.env.SDK_DEV_MODE === 'true',
+    inlineWasm: true,
+    format: 'cjs',
   }),
 };

bd3de1e9f91ae3d0da1e833803605fce3b08f4c8
Simon Wicky <simon@nymtech.net>
2024-01-16 10:34:27 +0100

    use LengthDelimitedCodec to fix desync bug

--- common/nymnoise/src/lib.rs
+++ common/nymnoise/src/lib.rs
@@ -1,6 +1,11 @@
 // Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use bytes::BytesMut;
+use futures::Sink;
+use futures::SinkExt;
+use futures::Stream;
+use futures::StreamExt;
 use log::*;
 use nym_topology::NymTopology;
 use pin_project::pin_project;
@@ -10,25 +15,21 @@ use snow::Builder;
 use snow::Error;
 use snow::HandshakeState;
 use snow::TransportState;
-use std::cmp::min;
-use std::collections::VecDeque;
 use std::io;
 use std::io::ErrorKind;
 use std::num::TryFromIntError;
 use std::pin::Pin;
 use std::task::Poll;
 use thiserror::Error;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
 use tokio::io::ReadBuf;
 use tokio::{
     io::{AsyncRead, AsyncWrite},
     net::TcpStream,
 };
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
 const MAXMSGLEN: usize = 65535;
 const TAGLEN: usize = 16;
-const HEADER_SIZE: usize = 2;
 
 #[derive(Error, Debug)]
 pub enum NoiseError {
@@ -45,6 +46,9 @@ pub enum NoiseError {
 
     #[error("Handshake timeout")]
     HandshakeTimeoutError(#[from] tokio::time::error::Elapsed),
+
+    #[error("Handshake did not complete")]
+    HandshakeError,
 
     #[error(transparent)]
     IntConversionError(#[from] TryFromIntError),
@@ -91,21 +95,19 @@ impl NoisePattern {
 #[pin_project]
 pub struct NoiseStream {
     #[pin]
-    inner_stream: TcpStream,
+    inner_stream: Framed<TcpStream, LengthDelimitedCodec>,
     handshake: Option<HandshakeState>,
     noise: Option<TransportState>,
-    enc_storage: VecDeque<u8>,
-    dec_storage: VecDeque<u8>,
 }
 
 impl NoiseStream {
     fn new(inner_stream: TcpStream, handshake: HandshakeState) -> NoiseStream {
         NoiseStream {
-            inner_stream,
+            inner_stream: LengthDelimitedCodec::builder()
+                .length_field_type::<u16>()
+                .new_framed(inner_stream),
             handshake: Some(handshake),
             noise: None,
-            enc_storage: VecDeque::with_capacity(MAXMSGLEN + TAGLEN + HEADER_SIZE), //At least one message
-            dec_storage: VecDeque::with_capacity(MAXMSGLEN),
         }
     }
 
@@ -132,11 +134,10 @@ impl NoiseStream {
         &mut self,
         handshake: &mut HandshakeState,
     ) -> Result<(), NoiseError> {
-        let mut buf = vec![0u8; MAXMSGLEN];
-        let len = handshake.write_message(&[], &mut buf)?;
+        let mut buf = BytesMut::with_capacity(MAXMSGLEN);
+        handshake.write_message(&[], &mut buf)?;
 
-        self.inner_stream.write_u16(len.try_into()?).await?; //len is always < 2^16, so it shouldn't fail
-        self.inner_stream.write_all(&buf[..len]).await?;
+        self.inner_stream.send(buf.into()).await?;
         Ok(())
     }
 
@@ -144,17 +145,19 @@ impl NoiseStream {
         &mut self,
         handshake: &mut HandshakeState,
     ) -> Result<(), NoiseError> {
-        let msg_len = self.inner_stream.read_u16().await?;
-        let mut msg = vec![0u8; msg_len.into()];
-        self.inner_stream.read_exact(&mut msg[..]).await?;
-
-        let mut buf = vec![0u8; MAXMSGLEN];
-        handshake.read_message(&msg, &mut buf)?;
-        Ok(())
+        match self.inner_stream.next().await {
+            Some(Ok(msg)) => {
+                let mut buf = vec![0u8; MAXMSGLEN];
+                handshake.read_message(&msg, &mut buf)?;
+                Ok(())
+            }
+            Some(Err(err)) => Err(NoiseError::IoError(err)),
+            None => Err(NoiseError::HandshakeError),
+        }
     }
 
     pub fn peer_addr(&self) -> Result<std::net::SocketAddr, io::Error> {
-        self.inner_stream.peer_addr()
+        self.inner_stream.get_ref().peer_addr()
     }
 }
 
@@ -165,113 +168,75 @@ impl AsyncRead for NoiseStream {
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
         let projected_self = self.project();
-        let enc_storage = projected_self.enc_storage;
-        let ready_to_read = projected_self.inner_stream.poll_read_ready(cx);
 
-        match ready_to_read {
-            Poll::Pending => {
-                //no new data, waking is already scheduled.
-                //Nothing new to decrypt, only check if we can return something from dec_storage, happens after
-            }
+        match projected_self.inner_stream.poll_next(cx) {
+            Poll::Pending => Poll::Pending, //no new data, wake up already scheduled
 
-            Poll::Ready(Ok(())) => {
-                //Read what we can into enc_storage, decrypt what we can into dec_storage
-                let mut tcp_buf = vec![0u8; MAXMSGLEN + HEADER_SIZE + TAGLEN];
-                if let Ok(tcp_len) = projected_self.inner_stream.try_read(&mut tcp_buf) {
-                    if tcp_len == 0 && projected_self.dec_storage.is_empty() {
-                        //EOF
-                        return Poll::Ready(Ok(()));
-                    }
-                    enc_storage.extend(&tcp_buf[..tcp_len]);
-                    //we can at least read the length
-                    while enc_storage.len() >= HEADER_SIZE {
-                        let msg_len = ((enc_storage[0] as usize) << 8) + (enc_storage[1] as usize);
-
-                        //no more messages to read
-                        if enc_storage.len() < HEADER_SIZE + msg_len {
-                            break;
-                        }
-                        //we have a full message to decrypt
-                        //remove size
-                        enc_storage.pop_front();
-                        enc_storage.pop_front();
-
-                        let noise_msg = enc_storage.drain(..msg_len).collect::<Vec<u8>>();
-                        let mut dec_msg = vec![0u8; MAXMSGLEN];
-
-                        let Ok(len) = (match projected_self.noise {
-                            Some(transport_state) => {
-                                transport_state.read_message(&noise_msg, &mut dec_msg)
+            Poll::Ready(Some(Ok(noise_msg))) => {
+                //We have a new moise msg
+                let mut dec_msg = vec![0u8; MAXMSGLEN];
+                let len = match projected_self.noise {
+                    Some(transport_state) => {
+                        warn!(
+                            "Noise msg read : {:?} - {:?}",
+                            &noise_msg[..10],
+                            transport_state.receiving_nonce()
+                        );
+                        match transport_state.read_message(&noise_msg, &mut dec_msg) {
+                            Ok(len) => len,
+                            Err(err) => {
+                                println!("Error : {:?}", err);
+                                return Poll::Ready(Err(ErrorKind::InvalidInput.into()));
                             }
-                            None => return Poll::Ready(Err(ErrorKind::Other.into())),
-                        }) else {
-                            return Poll::Ready(Err(ErrorKind::InvalidInput.into()));
-                        };
-                        projected_self.dec_storage.extend(&dec_msg[..len]);
+                        }
                     }
-                }
+                    None => return Poll::Ready(Err(ErrorKind::Other.into())),
+                };
+                buf.put_slice(&dec_msg[..len]);
+                Poll::Ready(Ok(()))
             }
 
-            //an error occured, let's return it right away
-            Poll::Ready(Err(err)) => return Poll::Ready(Err(err)),
-        }
+            Poll::Ready(Some(Err(err))) => Poll::Ready(Err(err)),
 
-        //check if we can return something
-        let read_len = min(buf.remaining(), projected_self.dec_storage.len());
-        if read_len > 0 {
-            buf.put_slice(
-                &projected_self
-                    .dec_storage
-                    .drain(..read_len)
-                    .collect::<Vec<u8>>(),
-            );
-            return Poll::Ready(Ok(()));
+            //Stream is done, return Ok with nothing in buf
+            Poll::Ready(None) => Poll::Ready(Ok(())),
         }
-
-        //can't return anything, schedule the wakeup and return pending
-        if let Poll::Ready(Ok(())) = projected_self.inner_stream.poll_read_ready(cx) {
-            //we got data in the meantime, we can wake up immediately
-            cx.waker().wake_by_ref();
-        }
-        Poll::Pending
     }
 }
+
 impl AsyncWrite for NoiseStream {
     fn poll_write(
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, std::io::Error>> {
-        let projected_self = self.project();
-        let mut noise_buf = vec![0u8; MAXMSGLEN];
+        let mut projected_self = self.project();
 
-        let Ok(len) = (match projected_self.noise {
-            Some(transport_state) => transport_state.write_message(buf, &mut noise_buf),
-            None => return Poll::Ready(Err(ErrorKind::Other.into())),
-        }) else {
-            return Poll::Ready(Err(ErrorKind::InvalidInput.into()));
-        };
-        let to_send = [&[(len >> 8) as u8, (len & 0xff) as u8], &noise_buf[..len]].concat();
-
-        match projected_self.inner_stream.poll_write(cx, &to_send) {
+        match projected_self.inner_stream.as_mut().poll_ready(cx) {
             Poll::Pending => Poll::Pending,
+
             Poll::Ready(Err(err)) => Poll::Ready(Err(err)),
-            Poll::Ready(Ok(n)) => {
-                //didn't send a thing, no problem for the underlying stream
-                if n == 0 {
-                    return Poll::Ready(Ok(0));
+
+            Poll::Ready(Ok(())) => {
+                let mut noise_buf = BytesMut::with_capacity(MAXMSGLEN + TAGLEN);
+
+                let Ok(_) = (match projected_self.noise {
+                    Some(transport_state) => {
+                        warn!(
+                            "Noise sending nonce : {:?}",
+                            transport_state.sending_nonce()
+                        );
+                        transport_state.write_message(buf, &mut noise_buf)
+                    }
+                    None => return Poll::Ready(Err(ErrorKind::Other.into())),
+                }) else {
+                    return Poll::Ready(Err(ErrorKind::InvalidInput.into()));
+                };
+
+                match projected_self.inner_stream.start_send(noise_buf.into()) {
+                    Ok(()) => Poll::Ready(Ok(buf.len())),
+                    Err(e) => Poll::Ready(Err(e)),
                 }
-                //we sent the whole thing, no problem for the underlying stream
-                //We must guarantee that the return number is <= buf.len()
-                if n == to_send.len() {
-                    return Poll::Ready(Ok(n - HEADER_SIZE - TAGLEN));
-                }
-                //We didn't write the whole message, the stream will be corrupted
-                error!(
-                    "Partial write on Noise Stream, it will be corrupted - {}",
-                    n
-                );
-                Poll::Ready(Err(ErrorKind::WriteZero.into()))
             }
         }
     }
@@ -287,7 +252,7 @@ impl AsyncWrite for NoiseStream {
         self: Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Result<(), std::io::Error>> {
-        self.project().inner_stream.poll_shutdown(cx)
+        self.project().inner_stream.poll_close(cx)
     }
 }
 
@@ -404,6 +369,8 @@ pub async fn upgrade_noise_responder_with_topology(
             return Err(Error::Prereq(Prerequisite::RemotePublicKey).into());
         }
     };
+
+    //SW : for private gateway, we could try to perform the handshake without that key?
     let remote_pub_key = match topology.find_node_key_by_mix_host(initiator_addr) {
         Some(pub_key) => pub_key.to_bytes(),
         None => {

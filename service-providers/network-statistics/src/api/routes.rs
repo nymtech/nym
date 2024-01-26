5e98c14a98a2f1d3b031c147b04fe5cb6f76b9f8
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2024-01-26 17:27:42 +0000

    updated rocket in network statistics

--- service-providers/network-statistics/src/api/routes.rs
+++ service-providers/network-statistics/src/api/routes.rs
@@ -1,9 +1,6 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-// due to the macro expansion of rather old rocket macros...
-#![allow(unused_imports)]
-
 use crate::api::error::Result;
 use crate::storage::NetworkStatisticsStorage;
 use nym_statistics_common::StatsMessage;

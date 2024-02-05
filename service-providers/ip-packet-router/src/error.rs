d7da6ed1abf3bdff561a768b185705f280af2176
Jon Häggblad <jon.haggblad@gmail.com>
2024-02-05 09:01:11 +0100

    Remove unused error case

--- service-providers/ip-packet-router/src/error.rs
+++ service-providers/ip-packet-router/src/error.rs
@@ -57,9 +57,6 @@ pub enum IpPacketRouterError {
     #[error("the provided socket address, '{addr}' is not covered by the exit policy!")]
     AddressNotCoveredByExitPolicy { addr: SocketAddr },
 
-    #[error("failed filter check: '{addr}'")]
-    AddressFailedFilterCheck { addr: SocketAddr },
-
     #[error("failed to apply the exit policy: {source}")]
     ExitPolicyFailure {
         #[from]

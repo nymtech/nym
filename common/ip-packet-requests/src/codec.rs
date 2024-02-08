d0937d64c1cb4222d327581d9223f4c75231a5d5
Jon Häggblad <jon.haggblad@gmail.com>
2024-02-08 09:31:02 +0100

    Extract out magic numbers

--- common/ip-packet-requests/src/codec.rs
+++ common/ip-packet-requests/src/codec.rs
@@ -11,6 +11,13 @@ pub enum Error {
 
 pub const BUFFER_TIMEOUT: Duration = Duration::from_millis(20);
 
+// TODO: increase this to make max out effective sphinx payload size. Sphinx packets also carry the
+// MixAck so that's why we can't just use 2kb.
+pub const MAX_PACKET_SIZE: usize = 1500;
+
+// Each IP packet is prefixed by a 2 byte length prefix
+const LENGTH_PREFIX_SIZE: usize = 2;
+
 // Tokio codec for bundling multiple IP packets into one buffer that is at most 1500 bytes long.
 // These packets are separated by a 2 byte length prefix. We need a timer so that we don't wait too
 // long for the buffer to fill up, since this kills latency.
@@ -34,7 +41,7 @@ impl MultiIpPacketCodec {
         if bundled_packets.is_empty() {
             None
         } else {
-            // log::info!("Sphinx packet utilization: {:.2}", self.buffer.len() as f64 / 1500.0);
+            // log::info!("Sphinx packet utilization: {:.2}", self.buffer.len() as f64 / MAX_PACKET_SIZE as f64);
             Some(bundled_packets.freeze())
         }
     }
@@ -71,7 +78,7 @@ impl Encoder<Bytes> for MultiIpPacketCodec {
         }
         let packet_size = packet.len();
 
-        if self.buffer.len() + packet_size + 2 > 1500 {
+        if self.buffer.len() + packet_size + LENGTH_PREFIX_SIZE > MAX_PACKET_SIZE {
             // If the packet doesn't fit in the buffer, send the buffer and then add it to the buffer
             dst.extend_from_slice(&self.buffer);
             self.buffer = BytesMut::new();
@@ -93,20 +100,20 @@ impl Decoder for MultiIpPacketCodec {
     type Error = Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if src.len() < 2 {
+        if src.len() < LENGTH_PREFIX_SIZE {
             // Not enough bytes to read the length prefix
             return Ok(None);
         }
 
         let packet_size = u16::from_be_bytes([src[0], src[1]]) as usize;
 
-        if src.len() < packet_size + 2 {
+        if src.len() < packet_size + LENGTH_PREFIX_SIZE {
             // Not enough bytes to read the packet
             return Ok(None);
         }
 
         // Remove the length prefix
-        src.advance(2);
+        src.advance(LENGTH_PREFIX_SIZE);
 
         // Read the packet
         let packet = src.split_to(packet_size);

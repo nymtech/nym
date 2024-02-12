22979ce245444451de0cc8b78c812f1e22a1254f
fmtabbara <fmtabbara@hotmail.co.uk>
2024-02-12 16:06:31 +0000

    add ledger intergration

--- explorer/src/context/cosmos-kit.tsx
+++ explorer/src/context/cosmos-kit.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ChainProvider } from '@cosmos-kit/react';
 import { wallets as keplr } from '@cosmos-kit/keplr';
+import { wallets as ledger } from '@cosmos-kit/ledger';
 import { assets, chains } from 'chain-registry';
 
 const CosmosKitProvider = ({ children }: { children: React.ReactNode }) => (
-  <ChainProvider chains={chains} assetLists={assets} wallets={[...keplr]}>
+  <ChainProvider chains={chains} assetLists={assets} wallets={[...keplr, ...ledger]}>
     {children}
   </ChainProvider>
 );

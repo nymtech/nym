f5a8c9e886ee1483c77f70fc8785ed288d25c58f
mfahampshire <mfahampshire@pm.me>
2024-02-13 14:44:56 +0100

    * renamed binding outer directory for nicer path * moved example file from ffi/main.go -> ./example.go

--- sdk/ffi/go/build.sh
+++ sdk/ffi/go/build.sh
@@ -5,8 +5,7 @@ PROJECT_NAME="go"
 set -eu
 MODE="--release"
 
-GO_DIR="./ffi"
-GO_PATH="${GO_DIR}/bindings"
+GO_DIR="./go-nym"
 UDL_PATH="./src/bindings.udl"
 
 build_artifacts() {
@@ -18,20 +17,17 @@ build_artifacts() {
   printf "bindings built \n\n"
 
   # something not right with these - having to add it manually to bindings.go for the moment
-  #pushd $GO_DIR/bindings
-  #echo $(pwd)
-  #LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:../../target/release" \
+  # pushd $GO_DIR/bindings
+  # echo $(pwd)
+  # LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:../../target/release" \
   #	CGO_LDFLAGS="-L../target/release -lnym_go_ffi -lm -ldl" \
   #	CGO_ENABLED=1 \
   #	go run ../main.go
 }
 
 clean_artifacts() {
-  # the path to the new folder we are including
-  GO_DIR="./ffi"
-  GO_PATH="${GO_DIR}/bindings"
   # clean up existing things
-  rm -rf $GO_PATH
+  rm -rf $GO_DIR
   cargo clean
 }
 
--- sdk/ffi/go/example.go
+++ sdk/ffi/go/example.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"nymffi/ffi/bindings"
+	"nymffi/go-nym/bindings"
 	"time"
 )
 

f7edc223e9bca83871566036a7800862eb05ce7a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2024-03-04 13:04:35 +0000

    initial db schema

---
+++ common/client-core/gateways-storage/fs_gateways_migrations/20240304120000_create_initial_tables.sql
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+CREATE TABLE active_gateway
+(
+    id                     INTEGER PRIMARY KEY CHECK (id = 0),
+    active_gateway_id_bs58 TEXT REFERENCES registered_gateway (gateway_id_bs58)
+);
+
+CREATE TABLE registered_gateway
+(
+    gateway_id_bs58 TEXT                                                NOT NULL UNIQUE PRIMARY KEY,
+    gateway_type    TEXT CHECK ( gateway_type IN ('remote', 'custom') ) NOT NULL DEFAULT 'remote'
+);
+
+CREATE TABLE remote_gateway_details
+(
+    gateway_id_bs58                          TEXT NOT NULL UNIQUE PRIMARY KEY REFERENCES registered_gateway (gateway_id_bs58),
+    derived_aes128_ctr_blake3_hmac_keys_bs58 TEXT NOT NULL,
+    gateway_owner_address                    TEXT NOT NULL,
+    gateway_listener                         TEXT NOT NULL
+);
+
+CREATE TABLE custom_gateway_details
+(
+    gateway_id_bs58 TEXT NOT NULL UNIQUE PRIMARY KEY REFERENCES registered_gateway (gateway_id_bs58),
+    data            BLOB
+);
+

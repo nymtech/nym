601478dfb54b4f865487582c2930b4c069dad9cb
Mark Sinclair <mmsinclair@users.noreply.github.com>
2024-03-11 14:37:37 +0000

    Add released GitHub Action bundle

--- .github/actions/nym-hash-releases/index.js
+++ .github/actions/nym-hash-releases/index.js
@@ -5,11 +5,15 @@ import { createHashesFromReleaseTagOrNameOrId } from './create-hashes.mjs';
 const algorithm = core.getInput('hash-type');
 const filename = core.getInput("file-name");
 
+async function main() {
 // use the release id from the payload if it is set
-const releaseTagOrNameOrId = core.getInput("release-tag-or-name-or-id") || github.context.payload.release?.id;
+    const releaseTagOrNameOrId = core.getInput("release-tag-or-name-or-id") || github.context.payload.release?.id;
 
-try {
-    await createHashesFromReleaseTagOrNameOrId({ releaseTagOrNameOrId, algorithm, filename  })
-} catch (error) {
-    core.setFailed(error.message);
+    try {
+        await createHashesFromReleaseTagOrNameOrId({releaseTagOrNameOrId, algorithm, filename})
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+main().catch(error => core.setFailed(error.message));

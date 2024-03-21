3e0735dbd818ce378e708af07b9db05cf8a851a5
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-03-21 11:19:29 +0200

    Add unit test

--- service-providers/ip-packet-router/src/util/generate_new_ip.rs
+++ service-providers/ip-packet-router/src/util/generate_new_ip.rs
@@ -6,10 +6,9 @@ use crate::constants::{TUN_DEVICE_ADDRESS_V4, TUN_DEVICE_ADDRESS_V6};
 
 // Find an available IP address in self.connected_clients
 // TODO: make this nicer
-fn generate_random_ips_within_subnet() -> IpPair {
-    let mut rng = rand::thread_rng();
+fn generate_random_ips_within_subnet<R: rand::Rng>(rng: &mut R) -> IpPair {
     // Generate a random number in the range 2-65535
-    let last_bytes: u16 = rand::Rng::gen_range(&mut rng, 1..=65534);
+    let last_bytes: u16 = rand::Rng::gen_range(rng, 2..=65534);
     let before_last_byte = (last_bytes >> 8) as u8;
     let last_byte = (last_bytes & 255) as u8;
     let ipv4 = Ipv4Addr::new(10, 0, before_last_byte, last_byte);
@@ -34,7 +33,8 @@ pub(crate) fn find_new_ips<T>(
     connected_clients_ipv4: &HashMap<Ipv4Addr, T>,
     connected_clients_ipv6: &HashMap<Ipv6Addr, T>,
 ) -> Option<IpPair> {
-    let mut new_ips = generate_random_ips_within_subnet();
+    let mut rng = rand::thread_rng();
+    let mut new_ips = generate_random_ips_within_subnet(&mut rng);
     let mut tries = 0;
     let tun_ips = IpPair::new(TUN_DEVICE_ADDRESS_V4, TUN_DEVICE_ADDRESS_V6);
 
@@ -44,11 +44,31 @@ pub(crate) fn find_new_ips<T>(
         tun_ips,
         new_ips,
     ) {
-        new_ips = generate_random_ips_within_subnet();
+        new_ips = generate_random_ips_within_subnet(&mut rng);
         tries += 1;
         if tries > 100 {
             return None;
         }
     }
     Some(new_ips)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn verify_ip_generation() {
+        let mut map = HashSet::with_capacity(65533);
+        let mut rng = rand::rngs::mock::StepRng::new(0, 65540);
+        for _ in 2..65535 {
+            let pair = generate_random_ips_within_subnet(&mut rng);
+            println!("{:?}", pair);
+            assert!(!map.contains(&pair));
+            map.insert(pair);
+        }
+        let pair = generate_random_ips_within_subnet(&mut rng);
+        assert!(map.contains(&pair));
+    }
 }

f012a1a069462e387b6f9109b7f6bead39f92f19
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2024-03-27 16:07:07 +0000

    fixed add gateway commands for nr and ipr

--- service-providers/network-requester/src/cli/add_gateway.rs
+++ service-providers/network-requester/src/cli/add_gateway.rs
@@ -4,28 +4,27 @@
 use crate::cli::CliNetworkRequesterClient;
 use crate::error::NetworkRequesterError;
 use nym_bin_common::output_format::OutputFormat;
-use nym_client_core::cli_helpers::client_list_gateways::{
-    list_gateways, CommonClientListGatewaysArgs,
-};
+use nym_client_core::cli_helpers::client_add_gateway::{add_gateway, CommonClientAddGatewayArgs};
+
 
 #[derive(clap::Args)]
 pub(crate) struct Args {
     #[command(flatten)]
-    common_args: CommonClientListGatewaysArgs,
+    common_args: CommonClientAddGatewayArgs,
 
     #[arg(short, long, default_value_t = OutputFormat::default())]
     output: OutputFormat,
 }
 
-impl AsRef<CommonClientListGatewaysArgs> for Args {
-    fn as_ref(&self) -> &CommonClientListGatewaysArgs {
+impl AsRef<CommonClientAddGatewayArgs> for Args {
+    fn as_ref(&self) -> &CommonClientAddGatewayArgs {
         &self.common_args
     }
 }
 
 pub(crate) async fn execute(args: Args) -> Result<(), NetworkRequesterError> {
     let output = args.output;
-    let res = list_gateways::<CliNetworkRequesterClient, _>(args).await?;
+    let res = add_gateway::<CliNetworkRequesterClient, _>(args).await?;
 
     println!("{}", output.format(&res));
     Ok(())

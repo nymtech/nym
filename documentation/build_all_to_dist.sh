1cfbc1d64a66dd068892157ddc2961284e6450b7
mfahampshire <mfahampshire@pm.me>
2024-04-25 16:10:44 +0200

    test

--- documentation/build_all_to_dist.sh
+++ documentation/build_all_to_dist.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-
 # this is a script called by the github CI and CD workflows to build all 3 docs projects
 # and move them to /dist/ in the root of the monorepo. They are rsynced to various servers
 # from there by subsequent workflow tasks.
@@ -18,34 +14,9 @@ then
 else
    for i in "${projects[@]}"
    do
-      # cd to project dir
-      cd "./$i" &&
-      # little sanity checks
-      echo $(pwd) && echo $(mdbook --version) &&
-      # clean old book
-      echo "cleaning old book"
-      rm -rf ./book/
-      # build book
-      # mdbook test || true
-      mdbook build
-      # check for destination, if ! then mkdir & check again else echo thumbs up
-      if [ ! -d ../../dist/docs/$i ]; then
-        echo "dest doesn't exist: creating dir"
-        mkdir -p ../../dist/docs/$i
-      fi
-      if [ -d ../../dist/docs/$i ]; then
-        echo "cp destination exists, all good"
-      fi
-      # clean old dist/$i
-      rm -rf ../../dist/docs/$i
-      # move newly rendered book/ to dist
-      rsync -r ./book/html/ ../../dist/docs/$i
-      # sanity check
-      ls -laF ../../dist/docs/
-      # cd back to ../documentation/
-      cd ../
+      cd "./$i" && mdbook build --dest-dir ../../dist/$i/ && cd ../
    done
    # rename for server paths
-   rm -rf ../dist/docs/developers
-   mv ../dist/docs/dev-portal ../dist/docs/developers
+   rm -rf ../dist/developers
+   mv ../dist/dev-portal ../dist/developers
 fi

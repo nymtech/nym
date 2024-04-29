f34c9d5d283e73d059e5e6cf5cf15c1749854530
import this <97586125+serinko@users.noreply.github.com>
2024-04-29 15:07:51 +0000

    [DOCs]: Get nym-vpn guides ready for launch (#4563)
    * update nymvpn cli guide
    
    * update nymvpn landing page
    
    * update nymvpn landing page
    
    * syntax edit
    
    * final version for review
    
    * final version for review
    
    * fix on feedback
    
    * fix on feedback

---
+++ documentation/scripts/cmdrun/nym_vpn_cli_version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+release_url="https://api.github.com/repos/nymtech/nym-vpn-client/releases"
+current_cli_version=$(curl -s $release_url | jq -r '.[].tag_name' | grep '^nym-vpn-cli-' | sort -Vr | head -n 1 | awk -F'-v' '{print $NF}')
+
+echo "${current_cli_version}"

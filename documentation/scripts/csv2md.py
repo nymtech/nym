df288620c6b6703e25c76d3ab67abf11634686a2
serinko <97586125+serinko@users.noreply.github.com>
2024-05-13 13:14:06 +0200

    initialise nym versin of good-bad isps by tor community

---
+++ documentation/scripts/csv2md.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""CLI to display .csv files as markdown"""
+
+import argparse
+import pandas as pd
+import sys
+import pathlib
+import csv
+
+def create_table(args):
+    """Imports csv and creates a table"""
+    file = args.file
+    csv = pd.read_csv(file)
+    if args.index:
+        if args.table:
+            table = csv.to_markdown(tablefmt="grid")
+        else:
+            table = csv.to_markdown()
+    else:
+        if args.table:
+            table = csv.to_markdown(tablefmt="grid", index=False)
+        else:
+            table = csv.to_markdown(index = False)
+    return table
+
+
+def display_file(args):
+    """Display csv file as a table"""
+    table = create_table(args)
+    print(table)
+
+
+def panic(msg):
+    """Error message print"""
+    print(f"error: {msg}", file=sys.stderr)
+    sys.exit(-1)
+
+
+def parser_main():
+    """Main function initializing ArgumentParser, storing arguments and executing commands."""
+    # Top level parser
+    parser = argparse.ArgumentParser(
+            prog='CSV TERMINAL READER',
+            description='''Quickly prints .csv files in terminal''',
+            epilog='''Code is power!'''
+        )
+
+    # Parser arguments
+    parser.add_argument("-V","--version", action="version", version='%(prog)s 1.1.0')
+    parser.add_argument("file", help="csv file name")
+    parser.add_argument("-t","--table", default=False, action="store_true", help="output with a tabulate option for terminal reading - does not parse for mdbook render!")
+    parser.add_argument("-i","--index", default=False, action="store_true", help="output without an index column")
+
+    parser.set_defaults(func=display_file)
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except AttributeError as e:
+        msg = f"{e}.\nPlease run with --help or read the error message in case your .csv file is corrupted."
+        panic(msg)
+
+if __name__ == '__main__':
+    parser_main()

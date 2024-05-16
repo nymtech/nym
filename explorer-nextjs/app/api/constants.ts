0ed8423db56ad7ab7d58f75ac0f628de6fb5a2b8
Mark Sinclair <mmsinclair@users.noreply.github.com>
2024-05-16 16:00:18 +0100

    Add defaults for constants

--- explorer-nextjs/app/api/constants.ts
+++ explorer-nextjs/app/api/constants.ts
@@ -1,8 +1,11 @@
 // master APIs
-export const API_BASE_URL = process.env.NEXT_PUBLIC_EXPLORER_API_URL;
-export const NYM_API_BASE_URL = process.env.NEXT_PUBLIC_NYM_API_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_EXPLORER_API_URL || 'https://explorer.nymtech.net/api/v1';
+export const NYM_API_BASE_URL = process.env.NEXT_PUBLIC_NYM_API_URL || 'https://validator.nymtech.net';
+
+export const NYX_RPC_BASE_URL = process.env.NEXT_PUBLIC_NYX_RPC_BASE_URL || 'https://rpc.nymtech.net';
+
 export const VALIDATOR_BASE_URL = process.env.NEXT_PUBLIC_VALIDATOR_URL || 'https://rpc.nymtech.net';
-export const BIG_DIPPER = process.env.NEXT_PUBLIC_BIG_DIPPER_URL;
+export const BIG_DIPPER = process.env.NEXT_PUBLIC_BIG_DIPPER_URL || 'https://nym.explorers.guru';
 
 // specific API routes
 export const OVERVIEW_API = `${API_BASE_URL}/overview`;
@@ -11,8 +14,8 @@ export const MIXNODES_API = `${API_BASE_URL}/mix-nodes`;
 export const MIXNODE_API = `${API_BASE_URL}/mix-node`;
 export const GATEWAYS_EXPLORER_API = `${API_BASE_URL}/gateways`;
 export const GATEWAYS_API = `${NYM_API_BASE_URL}/api/v1/status/gateways/detailed`;
-export const VALIDATORS_API = `${VALIDATOR_BASE_URL}/validators`;
-export const BLOCK_API = `${NYM_API_BASE_URL}/block`;
+export const VALIDATORS_API = `${NYX_RPC_BASE_URL}/validators`;
+export const BLOCK_API = `${NYX_RPC_BASE_URL}/block`;
 export const COUNTRY_DATA_API = `${API_BASE_URL}/countries`;
 export const UPTIME_STORY_API = `${NYM_API_BASE_URL}/api/v1/status/mixnode`; // add ID then '/history' to this.
 export const UPTIME_STORY_API_GATEWAY = `${NYM_API_BASE_URL}/api/v1/status/gateway`; // add ID then '/history' or '/report' to this

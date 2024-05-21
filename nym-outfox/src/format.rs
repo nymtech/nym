2a1d37dd22b8739c0919c1b097bcd4f6ed84c9b5
Simon Wicky <simon@nymtech.net>
2024-05-21 16:31:19 +0200

    update crypto and rand crates

--- nym-outfox/src/format.rs
+++ nym-outfox/src/format.rs
@@ -241,7 +241,7 @@ impl MixStageParameters {
             });
         }
 
-        let user_public_key = (&ED25519_BASEPOINT_TABLE * &user_secret_key).to_montgomery();
+        let user_public_key = (ED25519_BASEPOINT_TABLE * &user_secret_key).to_montgomery();
         let shared_key = user_secret_key * mix_public_key;
 
         // Copy rounting data into buffer

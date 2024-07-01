a6c79f9f37aee631945edcb0a35b91d7b44dba83
fmtabbara <fmtabbara@hotmail.co.uk>
2024-07-01 11:02:46 +0100

    finish next-explorer build

--- explorer-nextjs/app/utils/index.ts
+++ explorer-nextjs/app/utils/index.ts
@@ -1,19 +1,21 @@
 /* eslint-disable camelcase */
-import { MutableRefObject } from 'react';
-import { Theme } from '@mui/material/styles';
-import { registerLocale, getName } from 'i18n-iso-countries';
-import Big from 'big.js';
-import { CountryData } from '@/app/typeDefs/explorer-api';
-import { EconomicsRowsType } from '@/app/components/MixNodes/Economics/types';
-import { Network } from '../typeDefs/network';
+import { MutableRefObject } from "react";
+import { Theme } from "@mui/material/styles";
+import { registerLocale, getName } from "i18n-iso-countries";
+import Big from "big.js";
+import { CountryData } from "@/app/typeDefs/explorer-api";
+import { EconomicsRowsType } from "@/app/components/MixNodes/Economics/types";
+import { Network } from "../typeDefs/network";
 
-registerLocale(require('i18n-iso-countries/langs/en.json'));
+registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 export function formatNumber(num: number): string {
   return new Intl.NumberFormat().format(num);
 }
 
-export function scrollToRef(ref: MutableRefObject<HTMLDivElement | undefined>): void {
+export function scrollToRef(
+  ref: MutableRefObject<HTMLDivElement | undefined>
+): void {
   if (ref?.current) ref.current.scrollIntoView();
 }
 
@@ -25,13 +27,15 @@ export type CountryDataRowType = {
   percentage: string;
 };
 
-export function countryDataToGridRow(countriesData: CountryData[]): CountryDataRowType[] {
+export function countryDataToGridRow(
+  countriesData: CountryData[]
+): CountryDataRowType[] {
   const totalNodes = countriesData.reduce((acc, obj) => acc + obj.nodes, 0);
   const formatted = countriesData.map((each: CountryData, index: number) => {
     const updatedCountryRecord: CountryDataRowType = {
       ...each,
       id: index,
-      countryName: getName(each.ISO3, 'en', { select: 'alias' }),
+      countryName: getName(each.ISO3, "en", { select: "alias" }) || "",
       percentage: ((each.nodes * 100) / totalNodes).toFixed(1),
     };
     return updatedCountryRecord;
@@ -41,16 +45,21 @@ export function countryDataToGridRow(countriesData: CountryData[]): CountryDataR
   return sorted;
 }
 
-export const splice = (start: number, deleteCount: number, address?: string): string => {
+export const splice = (
+  start: number,
+  deleteCount: number,
+  address?: string
+): string => {
   if (address) {
-    const array = address.split('');
-    array.splice(start, deleteCount, '...');
-    return array.join('');
+    const array = address.split("");
+    array.splice(start, deleteCount, "...");
+    return array.join("");
   }
-  return '';
+  return "";
 };
 
-export const trimAddress = (address = '', trimBy = 6) => `${address.slice(0, trimBy)}...${address.slice(-trimBy)}`;
+export const trimAddress = (address = "", trimBy = 6) =>
+  `${address.slice(0, trimBy)}...${address.slice(-trimBy)}`;
 
 /**
  * Converts a stringified percentage float (0.0-1.0) to a stringified integer (0-100).
@@ -58,27 +67,33 @@ export const trimAddress = (address = '', trimBy = 6) => `${address.slice(0, tri
  * @param value - the percentage to convert
  * @returns A stringified integer
  */
-export const toPercentIntegerString = (value: string) => Math.round(Number(value) * 100).toString();
-export const toPercentInteger = (value: string) => Math.round(Number(value) * 100);
+export const toPercentIntegerString = (value: string) =>
+  Math.round(Number(value) * 100).toString();
+export const toPercentInteger = (value: string) =>
+  Math.round(Number(value) * 100);
 
-export const textColour = (value: EconomicsRowsType, field: string, theme: Theme) => {
+export const textColour = (
+  value: EconomicsRowsType,
+  field: string,
+  theme: Theme
+) => {
   const progressBarValue = value?.progressBarValue || 0;
   const fieldValue = value.value;
 
   if (progressBarValue > 100) {
     return theme.palette.warning.main;
   }
-  if (field === 'selectionChance') {
+  if (field === "selectionChance") {
     // TODO: when v2 will be deployed, remove cases: VeryHigh, Moderate and VeryLow
     switch (fieldValue) {
-      case 'High':
-      case 'VeryHigh':
+      case "High":
+      case "VeryHigh":
         return theme.palette.nym.networkExplorer.selectionChance.overModerate;
-      case 'Good':
-      case 'Moderate':
+      case "Good":
+      case "Moderate":
         return theme.palette.nym.networkExplorer.selectionChance.moderate;
-      case 'Low':
-      case 'VeryLow':
+      case "Low":
+      case "VeryLow":
         return theme.palette.nym.networkExplorer.selectionChance.underModerate;
       default:
         return theme.palette.nym.wallet.fee;
@@ -101,8 +116,12 @@ export const isLessThan = (a: number, b: number) => a < b;
  *
  */
 
-export const isBalanceEnough = (fee: string, tx: string = '0', balance: string = '0') => {
-  console.log('balance', balance, fee, tx);
+export const isBalanceEnough = (
+  fee: string,
+  tx: string = "0",
+  balance: string = "0"
+) => {
+  console.log("balance", balance, fee, tx);
   try {
     return Big(balance).gte(Big(fee).plus(Big(tx)));
   } catch (e) {
@@ -112,13 +131,13 @@ export const isBalanceEnough = (fee: string, tx: string = '0', balance: string =
 };
 
 export const urls = (networkName?: Network) =>
-  networkName === 'MAINNET'
+  networkName === "MAINNET"
     ? {
-      mixnetExplorer: 'https://mixnet.explorers.guru/',
-      blockExplorer: 'https://blocks.nymtech.net',
-      networkExplorer: 'https://explorer.nymtech.net',
-    }
+        mixnetExplorer: "https://mixnet.explorers.guru/",
+        blockExplorer: "https://blocks.nymtech.net",
+        networkExplorer: "https://explorer.nymtech.net",
+      }
     : {
-      blockExplorer: `https://${networkName}-blocks.nymtech.net`,
-      networkExplorer: `https://${networkName}-explorer.nymtech.net`,
-    };
+        blockExplorer: `https://${networkName}-blocks.nymtech.net`,
+        networkExplorer: `https://${networkName}-explorer.nymtech.net`,
+      };

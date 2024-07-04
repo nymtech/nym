70599b97b985d6a5f0be478fcdedb559d14bbe47
Jon Häggblad <jon.haggblad@gmail.com>
2024-07-04 13:10:16 +0200

    Send bandwidth status messages when connecting (#4691)
    * Send bandwidth status messages when connecting
    
    * Rename to task_client
    
    * Move status message type to bandwidth controller

---
+++ common/bandwidth-controller/src/event.rs
@@ -0,0 +1,13 @@
+// Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+// See other comments for other TaskStatus message enumds about abusing the Error trait when we
+// should have a new trait for TaskStatus messages
+#[derive(Debug, thiserror::Error)]
+pub enum BandwidthStatusMessage {
+    #[error("remaining bandwidth: {0}")]
+    RemainingBandwidth(i64),
+
+    #[error("no bandwidth left")]
+    NoBandwidth,
+}

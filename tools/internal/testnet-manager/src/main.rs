fa8e5a9656de07ea7f5b679e2974d8d25e214368
Jon Häggblad <jon.haggblad@gmail.com>
2024-07-31 17:10:21 +0200

    Disable testnet-manager on non-unix

--- tools/internal/testnet-manager/src/main.rs
+++ tools/internal/testnet-manager/src/main.rs
@@ -1,6 +1,9 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+// Allow dead code for not(unix)
+#![cfg_attr(not(unix), allow(dead_code))]
+
 use crate::cli::Cli;
 use clap::Parser;
 use nym_bin_common::logging::setup_tracing_logger;
@@ -10,6 +13,7 @@ pub(crate) mod error;
 mod helpers;
 mod manager;
 
+#[cfg(unix)]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // std::env::set_var(
@@ -23,4 +27,9 @@ async fn main() -> anyhow::Result<()> {
     cli.execute().await?;
 
     Ok(())
+}
+
+#[cfg(not(unix))]
+fn main() {
+    println!("This binary is only supported on Unix systems");
 }

8aa55553d77e28444859c37012a1ca20ed34f99b
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-01 09:40:44 +0200

    Move mod behind cfg too

--- tools/internal/testnet-manager/src/main.rs
+++ tools/internal/testnet-manager/src/main.rs
@@ -4,18 +4,22 @@
 // Allow dead code for not(unix)
 #![cfg_attr(not(unix), allow(dead_code))]
 
-use crate::cli::Cli;
-use clap::Parser;
-use nym_bin_common::logging::setup_tracing_logger;
-
+#[cfg(unix)]
 pub(crate) mod cli;
+#[cfg(unix)]
 pub(crate) mod error;
+#[cfg(unix)]
 mod helpers;
+#[cfg(unix)]
 mod manager;
 
 #[cfg(unix)]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    use crate::cli::Cli;
+    use clap::Parser;
+    use nym_bin_common::logging::setup_tracing_logger;
+
     // std::env::set_var(
     //     "RUST_LOG",
     //     "trace,handlebars=warn,tendermint_rpc=warn,h2=warn,hyper=warn,rustls=warn,reqwest=warn,tungstenite=warn,async_tungstenite=warn,tokio_util=warn,tokio_tungstenite=warn,tokio-util=warn",

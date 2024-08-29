33b85a3ca188bccb393957b514d098f8ff547b50
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2024-08-29 08:47:08 +0200

    Fix additional one

--- common/commands/src/validator/mixnet/operators/mixnode/keys/decode_mixnode_key.rs
+++ common/commands/src/validator/mixnet/operators/mixnode/keys/decode_mixnode_key.rs
@@ -1,6 +1,7 @@
 // Copyright 2021 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use base64::Engine;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -10,7 +11,9 @@ pub struct Args {
 }
 
 pub fn decode_mixnode_key(args: Args) {
-    let b64_decoded = base64::decode(args.key).expect("failed to decode base64 string");
+    let b64_decoded = base64::prelude::BASE64_STANDARD
+        .decode(args.key)
+        .expect("failed to decode base64 string");
     let b58_encoded = bs58::encode(&b64_decoded).into_string();
 
     println!("{b58_encoded}")

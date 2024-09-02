9c88f254a47cb3a13066d1b75628d3708eefe1ae
benedettadavico <benedetta.davico@gmail.com>
2024-09-02 07:32:41 +0200

    10 mins test

--- common/wireguard/src/peer_controller.rs
+++ common/wireguard/src/peer_controller.rs
@@ -133,8 +133,7 @@ impl<St: Storage> PeerController<St> {
     }
 
     async fn check_peers(&mut self) -> Result<(), Error> {
-        let reset = Utc::now().num_seconds_from_midnight() as u64
-            <= DEFAULT_PEER_TIMEOUT_CHECK.as_secs() + 10;
+        let reset = Utc::now().num_seconds_from_midnight() % 600 == 0;
 
         if reset {
             for (_, peer) in self.suspended_peers.drain() {

8c5698f8cfe2e62a7360b453941fb547ff98348c
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-09-04 17:44:37 +0200

    Fix sdk example default value

--- common/network-defaults/src/ecash.rs
+++ common/network-defaults/src/ecash.rs
@@ -19,10 +19,10 @@ pub enum TicketTypeRepr {
 }
 
 impl TicketTypeRepr {
-    pub const WIREGUARD_ENTRY_TICKET_SIZE: u64 = 500 * 1024 * 1024; // 500 MB
-    pub const WIREGUARD_EXIT_TICKET_SIZE: u64 = 500 * 1024 * 1024; // 500 MB
-    pub const MIXNET_ENTRY_TICKET_SIZE: u64 = 100 * 1024 * 1024; // 100 MB
-    pub const MIXNET_EXIT_TICKET_SIZE: u64 = 100 * 1024 * 1024; // 100 MB
+    pub const WIREGUARD_ENTRY_TICKET_SIZE: u64 = 512 * 1024 * 1024; // 512 MB
+    pub const WIREGUARD_EXIT_TICKET_SIZE: u64 = 512 * 1024 * 1024; // 512 MB
+    pub const MIXNET_ENTRY_TICKET_SIZE: u64 = 128 * 1024 * 1024; // 128 MB
+    pub const MIXNET_EXIT_TICKET_SIZE: u64 = 128 * 1024 * 1024; // 128 MB
 
     /// How much bandwidth (in bytes) one ticket can grant
     pub const fn bandwidth_value(&self) -> u64 {

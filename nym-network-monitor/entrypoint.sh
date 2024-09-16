a51221738221eb6ce013c7883882e8acb1e415d4
Drazen Urch <drazen@urch.eu>
2024-09-16 17:15:40 +0200

    Few fixes (#4883)

--- nym-network-monitor/entrypoint.sh
+++ nym-network-monitor/entrypoint.sh
@@ -4,15 +4,17 @@
 
 set -ex
 
-users=${2:-10}
-timeout=${1:-600}
+private_key=${1}
+network=${2:-mainnet}
+timeout=${3:-600}
+users=${4:-10}
 
-RUST_LOG=info nym-network-monitor --env mainnet.env --host 127.0.0.1 --port 8080 &
+RUST_LOG=info nym-network-monitor --env envs/"${network}".env --host 127.0.0.1 --port 8080 --private-key "${private_key}" &
 nnm_pid=$!
 
 sleep 10
 
-python -m locust -H http://127.0.0.1:8080 --processes 4 --autostart --autoquit 60 -u "$users" -t "$timeout"s &
+python -m locust -H http://127.0.0.1:8080 --processes 4 --autostart --autoquit 60 -u "${users}" -t "${timeout}"s &
 locust_pid=$!
 
 wait $locust_pid

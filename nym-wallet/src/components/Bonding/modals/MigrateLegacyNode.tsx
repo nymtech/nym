e967b0b82d194b200fea1d5132407b664db96e40
fmtabbara <fmtabbara@hotmail.co.uk>
2024-09-22 19:14:23 +0100

    use migrate node modal

---
+++ nym-wallet/src/components/Bonding/modals/MigrateLegacyNode.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { SimpleModal } from 'src/components/Modals/SimpleModal';
+
+const MigrateLegacyNode = ({
+  open,
+  onClose,
+  handleMigrate,
+}: {
+  open: boolean;
+  onClose: () => void;
+  handleMigrate: () => Promise<void>;
+}) => {
+  return (
+    <SimpleModal
+      open={open}
+      header={<Typography sx={{ fontWeight: 700 }}>Migrate Legacy Node</Typography>}
+      onOk={handleMigrate}
+      okLabel="Migrate"
+      onClose={onClose}
+      sx={{ maxWidth: 500 }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Typography>You have a legacy node that needs to be migrated to the new system.</Typography>
+      </Box>
+    </SimpleModal>
+  );
+};
+
+export default MigrateLegacyNode;

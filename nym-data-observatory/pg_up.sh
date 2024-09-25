c08ba5c0c011227f8b2ea375568449951ea3efa3
Dinko Zdravac <173912580+dynco-nym@users.noreply.github.com>
2024-09-25 13:28:36 +0200

    Assume offline mode in sqlx (#4926)
    * Assume offline mode
    
    * PR feedback

--- nym-data-observatory/pg_up.sh
+++ nym-data-observatory/pg_up.sh
@@ -3,7 +3,10 @@
 source .env
 
 # Launching a container in such a way that it's destroyed after you detach from the terminal:
-docker compose up --abort-on-container-exit --remove-orphans
+docker compose up
 
 # docker exec -it nym-data-observatory-pg /bin/bash
 # psql -U youruser -d yourdb
+
+echo "Tearing down containers to have a clean slate"
+docker compose down -v

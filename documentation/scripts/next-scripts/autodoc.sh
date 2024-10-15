63f86f088cf96f33f398b697d6107572c0e10189
mfahampshire <maxhampshire@pm.me>
2024-10-15 15:50:34 +0200

    updated autodoc for committing changing else exit

--- documentation/scripts/next-scripts/autodoc.sh
+++ documentation/scripts/next-scripts/autodoc.sh
@@ -12,6 +12,13 @@ mv autodoc-generated-markdown/nym-client-commands.md ../docs/pages/developers/cl
 mv autodoc-generated-markdown/nym-socks5-client-commands.md ../docs/pages/developers/clients/socks5/commands.mdx &&
 
 # commit files to git: needed for remote deployment from branch
-
+if ! git diff --quiet -- "../docs/pages/developers/tools" "../docs/pages/developers/clients/websocket" "../docs/pages/developers/clients/socks5"; then
+    printf "commiting changes"
+    git add ../docs/pages/developers/
+    git commit -m "auto commit generated command files"
+    git push origin HEAD
+else
+    printf "nothing to commit"
+fi
 
 cd ../docs

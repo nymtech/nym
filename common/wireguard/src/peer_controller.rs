6318ebb97348786d388af2b168918974baff2d2f
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-10-18 09:13:51 +0300

    Remove mismatch possibilities

--- common/wireguard/src/peer_controller.rs
+++ common/wireguard/src/peer_controller.rs
@@ -225,14 +225,10 @@ impl<St: Storage + Clone + 'static> PeerController<St> {
                 .available_bandwidth()
                 .await
         } else {
-            let peer = self
-                .host_information
-                .read()
-                .await
-                .peers
-                .get(key)
-                .ok_or(Error::PeerMismatch)?
-                .clone();
+            let Some(peer) = self.host_information.read().await.peers.get(key).cloned() else {
+                // host information not updated yet
+                return Ok(None);
+            };
             BANDWIDTH_CAP_PER_DAY.saturating_sub((peer.rx_bytes + peer.tx_bytes) as i64)
         };
 

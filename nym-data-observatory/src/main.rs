a6bdd32fb11898151577a3dd09d363df943c23fb
Fran Arbanas <arbanasfran@gmail.com>
2024-10-24 13:52:09 +0200

    feat: split the postgresql connection string into multiple variables

--- nym-data-observatory/src/main.rs
+++ nym-data-observatory/src/main.rs
@@ -18,9 +18,25 @@ struct Args {
     #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_ENV_FILE")]
     env_file: Option<String>,
 
-    /// DB connection url
-    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_URL")]
-    connection_url: String,
+    /// DB connection username
+    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_USERNAME")]
+    connection_username: String,
+
+    /// DB connection password
+    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_PASSWORD")]
+    connection_password: String,
+
+    /// DB connection host
+    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_HOST")]
+    connection_host: String,
+
+    /// DB connection port
+    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_PORT")]
+    connection_port: String,
+
+    /// DB connection database name
+    #[arg(short, long, default_value = None, env = "NYM_DATA_OBSERVATORY_CONNECTION_DB")]
+    connection_db: String,
 }
 
 #[tokio::main]
@@ -31,7 +47,15 @@ async fn main() -> anyhow::Result<()> {
 
     setup_env(args.env_file); // Defaults to mainnet if empty
 
-    let storage = db::Storage::init(args.connection_url).await?;
+    let connection_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        args.connection_username,
+        args.connection_password,
+        args.connection_host,
+        args.connection_port,
+        args.connection_db
+    );
+    let storage = db::Storage::init(connection_url).await?;
     let db_pool = storage.pool_owned().await;
     tokio::spawn(async move {
         background_task::spawn_in_background(db_pool).await;

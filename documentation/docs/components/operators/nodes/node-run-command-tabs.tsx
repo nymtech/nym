48b893e578ecb7800d6883529f85c49dad22c523
mfahampshire <maxhampshire@pm.me>
2024-10-24 16:42:28 +0200

    pushing build attempt changes

--- documentation/docs/components/operators/nodes/node-run-command-tabs.tsx
+++ documentation/docs/components/operators/nodes/node-run-command-tabs.tsx
@@ -5,17 +5,17 @@ import ExitGateway from 'components/operators/snippets/exit-gateway-run-tab-snip
 
 export const RunTabs = () => {
 
-return (
-<div>
-  <Tabs items={[
-    <code>mixnode</code>,
-    <code>exit-gateway</code>,
-    <code>entry-gateway</code>
-  ]} defaultIndex="1">
-      <Tabs.Tab><Mixnodes/></Tabs.Tab>
-      <Tabs.Tab><ExitGateway/></Tabs.Tab>
-      <Tabs.Tab><EntryGateway/></Tabs.Tab>
-  </Tabs>
-</div>
+  return (
+    <div>
+      <Tabs items={[
+        <code>mixnode</code>,
+        <code>exit-gateway</code>,
+        <code>entry-gateway</code>
+      ]} defaultIndex={1}>
+        <Tabs.Tab><Mixnodes /></Tabs.Tab>
+        <Tabs.Tab><ExitGateway /></Tabs.Tab>
+        <Tabs.Tab><EntryGateway /></Tabs.Tab>
+      </Tabs>
+    </div>
   )
 }
--- documentation/docs/components/outputs/api-scraping-outputs/time-now.md
+++ documentation/docs/components/outputs/api-scraping-outputs/time-now.md
@@ -1,1 +1,1 @@
-Thursday, October 24th 2024, 10:50:29 UTC
+Thursday, October 24th 2024, 14:37:25 UTC

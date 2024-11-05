f036ad2147f593f7b05858b12aaab1d7726234c7
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-11-05 17:35:10 +0200

    Backport magura fixes to aero (#5095)
    * Backport 250GB/30 days to aero
    
    * Pass the Poisson flag on authenticator config (#5037)
    
    * Fix bw type

--- common/wireguard/src/peer_controller.rs
+++ common/wireguard/src/peer_controller.rs
@@ -7,9 +7,7 @@ use defguard_wireguard_rs::{
     WireguardInterfaceApi,
 };
 use futures::channel::oneshot;
-use nym_authenticator_requests::{
-    v1::registration::BANDWIDTH_CAP_PER_DAY, v2::registration::RemainingBandwidthData,
-};
+use nym_authenticator_requests::v2::registration::{RemainingBandwidthData, BANDWIDTH_CAP_PER_DAY};
 use nym_credential_verification::{
     bandwidth_storage_manager::BandwidthStorageManager, BandwidthFlushingBehaviourConfig,
     ClientBandwidth,
@@ -229,7 +227,7 @@ impl<St: Storage + Clone + 'static> PeerController<St> {
                 // host information not updated yet
                 return Ok(None);
             };
-            BANDWIDTH_CAP_PER_DAY.saturating_sub((peer.rx_bytes + peer.tx_bytes) as i64)
+            BANDWIDTH_CAP_PER_DAY.saturating_sub(peer.rx_bytes + peer.tx_bytes) as i64
         };
 
         Ok(Some(RemainingBandwidthData {

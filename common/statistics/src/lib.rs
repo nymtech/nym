7cc3b90627bbf4779e88e377e76eda04e8f9c2d7
Simon Wicky <simon@nymtech.net>
2024-11-05 14:44:56 +0100

    basic os reporting

--- common/statistics/src/lib.rs
+++ common/statistics/src/lib.rs
@@ -1,5 +1,12 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+#![warn(clippy::expect_used)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::todo)]
+#![warn(clippy::dbg_macro)]
+
 pub mod clients;
+pub mod error;
 pub mod gateways;
+pub mod report;

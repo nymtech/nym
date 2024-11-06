632418b3d4e5526ae8c9a88c6984c8a9101b41eb
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2024-11-06 17:43:24 +0100

    PR feedback

--- common/network-defaults/build.rs
+++ common/network-defaults/build.rs
@@ -1,11 +1,15 @@
+use cargo_metadata::MetadataCommand;
 use regex::Regex;
-use std::{collections::HashMap, fs};
-
-const TARGET_ENV_FILE: &str = "../../envs/mainnet.env";
+use std::{collections::HashMap, fs, path::PathBuf};
 
 /// Sync variable values defined in code with .env file
 fn main() {
-    let file = include_str!("src/mainnet.rs");
+    let source_of_truth = include_str!("src/mainnet.rs");
+    let mut output_path = workspace_root();
+    output_path.push("envs");
+    output_path.push("mainnet.env");
+
+    println!("{}", output_path.display());
 
     let variables_to_track = [
         "NETWORK_NAME",
@@ -35,30 +39,47 @@ fn main() {
 
         let re = Regex::new(&pattern).unwrap();
         let value = re
-            .captures(file)
+            .captures(source_of_truth)
             .and_then(|caps| caps.get(1).map(|match_| match_.as_str().to_string()))
             .expect("Couldn't find var in source file");
         println!("Storing {}={}", var, value);
         replace_with.insert(var, value);
     }
 
-    let mut contents = fs::read_to_string(TARGET_ENV_FILE).unwrap();
+    let mut contents = fs::read_to_string(&output_path).unwrap();
 
     for (var, value) in replace_with {
         // match a pattern that looks like:
-        // <var> = "<value>"
+        // <var> = <value>
         // where `<var>` is a variable name inserted into search pattern
-        let pattern = format!(r#"{}\s*=\s*"([^"]*)""#, regex::escape(var));
+        let pattern = format!(r#"{}\s*=\s*([^\n]*)"#, regex::escape(var));
 
         // replace matched pattern with
-        // <var>="<value>"
+        // <var>=<value>
         let re = Regex::new(&pattern).unwrap();
         contents = re
             .replace(&contents, |_: &regex::Captures| {
-                format!(r#"{}="{}""#, var, value)
+                format!(r#"{}={}"#, var, value)
             })
             .to_string();
     }
 
-    fs::write(TARGET_ENV_FILE, contents).unwrap();
+    println!("File contents to write:\n{}", contents);
+    if output_path.exists() {
+        fs::write(output_path, contents).unwrap();
+    } else {
+        panic!("{} doesn't exist", output_path.display());
+    }
+}
+
+fn workspace_root() -> PathBuf {
+    let metadata = MetadataCommand::new()
+        .exec()
+        .expect("Failed to get cargo metadata");
+
+    metadata
+        .workspace_root
+        .into_std_path_buf()
+        .canonicalize()
+        .expect("Failed to canonicalize path")
 }

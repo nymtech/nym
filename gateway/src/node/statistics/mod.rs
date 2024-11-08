46de259ce1880cbc677bc2d6fdaae2868471693e
Simon Wicky <simon@nymtech.net>
2024-11-08 11:34:03 +0100

    refactor gateway stats collection to fit client stats collection in same common crate

--- gateway/src/node/statistics/mod.rs
+++ gateway/src/node/statistics/mod.rs
@@ -1,10 +1,11 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use futures::{channel::mpsc, StreamExt};
 use nym_gateway_stats_storage::PersistentStatsStorage;
 use nym_node_http_api::state::metrics::SharedSessionStats;
-use nym_statistics_common::events::{StatsEvent, StatsEventReceiver, StatsEventSender};
+use nym_statistics_common::gateways::{
+    GatewayStatsEvent, GatewayStatsReceiver, GatewayStatsReporter,
+};
 use nym_task::TaskClient;
 use sessions::SessionStatsHandler;
 use std::time::Duration;
@@ -16,7 +17,7 @@ pub mod sessions;
 const STATISTICS_UPDATE_TIMER_INTERVAL: Duration = Duration::from_secs(3600); //update timer, no need to check everytime
 
 pub(crate) struct GatewayStatisticsCollector {
-    stats_event_rx: StatsEventReceiver,
+    stats_event_rx: GatewayStatsReceiver,
     session_stats: SessionStatsHandler,
     //here goes additionnal stats handler
 }
@@ -25,15 +26,16 @@ impl GatewayStatisticsCollector {
     pub fn new(
         shared_session_stats: SharedSessionStats,
         stats_storage: PersistentStatsStorage,
-    ) -> (GatewayStatisticsCollector, StatsEventSender) {
-        let (stats_event_tx, stats_event_rx) = mpsc::unbounded();
+    ) -> (GatewayStatisticsCollector, GatewayStatsReporter) {
+        let (stats_event_tx, stats_event_rx) = tokio::sync::mpsc::unbounded_channel();
 
         let session_stats = SessionStatsHandler::new(shared_session_stats, stats_storage);
         let collector = GatewayStatisticsCollector {
             stats_event_rx,
             session_stats,
         };
-        (collector, stats_event_tx)
+        let reporter = GatewayStatsReporter::new(stats_event_tx);
+        (collector, reporter)
     }
 
     async fn update_shared_state(&mut self, update_time: OffsetDateTime) {
@@ -68,10 +70,10 @@ impl GatewayStatisticsCollector {
                         self.update_shared_state(now).await;
                 },
 
-                Some(stat_event) = self.stats_event_rx.next() => {
+                Some(stat_event) = self.stats_event_rx.recv() => {
                     //dispatching event to proper handler
                     match stat_event {
-                        StatsEvent::SessionStatsEvent(event) => {
+                        GatewayStatsEvent::SessionStatsEvent(event) => {
                             if let Err(e) = self.session_stats.handle_event(event).await{
                             warn!("Session event handling error - {e}");
                         }},

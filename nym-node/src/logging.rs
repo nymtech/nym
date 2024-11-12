8532a647876b2d64d5be5c711992de214a3235f0
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2024-11-12 09:01:20 +0000

    clippy

--- nym-node/src/logging.rs
+++ nym-node/src/logging.rs
@@ -4,9 +4,9 @@
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{filter::Directive, EnvFilter};
 
-pub(crate) fn setup_tracing_logger() {
-    fn directive_checked(directive: String) -> Directive {
-        directive.parse().expect("Failed to parse log directive")
+pub(crate) fn setup_tracing_logger() -> anyhow::Result<()> {
+    fn directive_checked(directive: impl Into<String>) -> anyhow::Result<Directive> {
+        directive.into().parse().map_err(From::from)
     }
 
     let log_builder = tracing_subscriber::fmt()
@@ -26,8 +26,10 @@ pub(crate) fn setup_tracing_logger() {
     // these crates are more granularly filtered
     let filter_crates = ["defguard_wireguard_rs"];
     for crate_name in filter_crates {
-        filter = filter.add_directive(directive_checked(format!("{}=warn", crate_name)));
+        filter = filter.add_directive(directive_checked(format!("{}=warn", crate_name))?);
     }
 
     log_builder.with_env_filter(filter).init();
+
+    Ok(())
 }

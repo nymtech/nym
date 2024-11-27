eb3cf389cc7e85d352240f28f2e032695078d9a9
mfahampshire <maxhampshire@pm.me>
2024-11-27 22:02:26 +0100

    first pass spin out client_pool

--- sdk/rust/nym-sdk/src/lib.rs
+++ sdk/rust/nym-sdk/src/lib.rs
@@ -2,10 +2,12 @@
 //!
 //! The main component currently is [`mixnet`].
 //! [`tcp_proxy`] is probably a good place to start for anyone wanting to integrate with existing app code and read/write from a socket.
+//! [`client_pool`] is a configurable client pool.
 
 mod error;
 
 pub mod bandwidth;
+pub mod client_pool;
 pub mod mixnet;
 pub mod tcp_proxy;
 

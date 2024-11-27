706766283c52dda97470b35b8c76ba7a6b173a2d
mfahampshire <maxhampshire@pm.me>
2024-11-27 22:02:26 +0100

    first pass spin out client_pool

--- sdk/rust/nym-sdk/src/tcp_proxy.rs
+++ sdk/rust/nym-sdk/src/tcp_proxy.rs
@@ -214,11 +214,8 @@
 //! }
 
 // TODO UPDATE EXAMPLE ONCE STABLE
-
-// mod connection_tracker;
 mod tcp_proxy_client;
 mod tcp_proxy_server;
 
-// pub use connection_tracker::ConnectionTracker;
 pub use tcp_proxy_client::NymProxyClient;
 pub use tcp_proxy_server::NymProxyServer;

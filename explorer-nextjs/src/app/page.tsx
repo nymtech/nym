3415d7925b3818802685ec4735fc19ef8e40ec08
Yana <yanok87@users.noreply.github.com>
2024-12-09 19:03:04 +0700

    wip

--- explorer-nextjs/src/app/page.tsx
+++ explorer-nextjs/src/app/page.tsx
@@ -1,88 +1,115 @@
-import ExplorerCard from "@/components/cards/ExplorerCard";
-import ExplorerHeroCard from "@/components/cards/ExplorerHeroCard";
 import CopyToClipboard from "@/components/copyToClipboard/CopyToClipboard";
 import Gateway from "@/components/icons/Gateway";
-import ExplorerListItem from "@/components/list/ListItem";
-import ProgressBar from "@/components/progressBar/ProgressBar";
-import StarRarating from "@/components/starRating/StarRating";
 import ExplorerButtonGroup from "@/components/toggleButton/ToggleButton";
+"use client";
+import TwoSidedSwitch from "@/components/twoSidedSwitchButton";
 import { Wrapper } from "@/components/wrapper";
-import { Container, Grid2, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import React from "react";
+import { type ContentCardProps, MonoCard } from "../components/cards/MonoCard";
+
+const explorerCard: ContentCardProps = {
+  overTitle: "SINGLE",
+  profileImage: {},
+  title: "SINGLE",
+  profileCountry: {
+    countryCode: "NO",
+    countryName: "Norway",
+  },
+  upDownLine: {
+    percentage: 10,
+    numberWentUp: true,
+  },
+  titlePrice: {
+    price: 1.15,
+    upDownLine: {
+      percentage: 10,
+      numberWentUp: true,
+    },
+  },
+  dataRows: {
+    rows: [
+      { key: "Market cap", value: "$ 1000000" },
+      { key: "24H VOL", value: "$ 1000000" },
+    ],
+  },
+  graph: {
+    data: [
+      {
+        date_utc: "2024-11-20",
+        numericData: 10,
+      },
+      {
+        date_utc: "2024-11-21",
+        numericData: 12,
+      },
+      {
+        date_utc: "2024-11-22",
+        numericData: 9,
+      },
+      {
+        date_utc: "2024-11-23",
+        numericData: 11,
+      },
+    ],
+    color: "#00CA33",
+    label: "Label",
+  },
+  nymAddress: {
+    address: "n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su",
+    title: "Nym address",
+  },
+  identityKey: {
+    address: "n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su",
+    title: "Nym address",
+  },
+  qrCode: {
+    url: "https://nymtech.net",
+  },
+  ratings: {
+    ratings: [
+      { title: "Rating-1", numberOfStars: 4 },
+      { title: "Rating-2", numberOfStars: 2 },
+      { title: "Rating-3", numberOfStars: 3 },
+    ],
+  },
+  progressBar: {
+    title: "Current NGM epoch",
+    start: "2024-12-08T12:26:19Z",
+    showEpoch: true,
+  },
+  comments: true,
+  paragraph: "Additional line",
+  stakeButton: {
+    label: "Stake on node",
+    identityKey: "n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su",
+  },
+};
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Wrapper>
-          <Container maxWidth="md">
-            <Stack spacing={4}>
-              <ExplorerCard title="Explorer Card" subtitle="Cryptosailors">
-                <ExplorerListItem
-                  label="Identity Key"
-                  value="n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su"
+        <Box sx={{ p: 5 }}>
+          <Wrapper>
+            <Typography fontWeight="light">
+              🚀 EXPLORER 2.0, Let&apos;s go! 🚀
+            </Typography>
+            <Grid container>
+              <Grid size={{ xs: 12, md: 6 }}>
+                <MonoCard {...explorerCard} />
+              </Grid>
+              <Grid size={{ xs: 4 }}>
+                <TwoSidedSwitch
+                  leftLabel="Account"
+                  rightLabel="Mixnode"
+                  // onSwitch={() => console.log("object :>> ")}
                 />
-                <ExplorerListItem
-                  label="Nym Address"
-                  value={
-                    <Stack direction="row" gap={0.5} alignItems="center">
-                      <Typography variant="body4">
-                        n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su
-                      </Typography>
-                      <CopyToClipboard text="n1w7tfthyfkhh3au3mqpy294p4dk65dzal2h04su" />
-                    </Stack>
-                  }
-                />
-                <ExplorerListItem
-                  label="Star Rating"
-                  value={<StarRarating value={3} />}
-                />
-                <ExplorerListItem
-                  label="Progress bar"
-                  value={<ProgressBar value={50} color="secondary" />}
-                />
-                <ExplorerListItem
-                  label="Button group"
-                  value={
-                    <ExplorerButtonGroup
-                      options={[
-                        {
-                          label: "Node",
-                          link: "/node",
-                          isSelected: true,
-                        },
-                        {
-                          label: "Account",
-                          link: "/account",
-                          isSelected: false,
-                        },
-                      ]}
-                    />
-                  }
-                />
-              </ExplorerCard>
-              <Grid2 container spacing={4}>
-                <Grid2 size={6}>
-                  <ExplorerHeroCard
-                    label="Onboarding"
-                    title="How to select Nym vpn gateway?"
-                    description="Stake your tokens to well performing mix nodes, and earn a share of operator rewards!"
-                    image={<Gateway />}
-                    link={"/onboarding"}
-                  />
-                </Grid2>
-                <Grid2 size={6}>
-                  <ExplorerHeroCard
-                    label="Onboarding"
-                    title="How to select Nym vpn gateway?"
-                    description="Stake your tokens to well performing mix nodes, and earn a share of operator rewards!"
-                    image={<Gateway />}
-                    link={"/onboarding"}
-                  />
-                </Grid2>
-              </Grid2>
-            </Stack>
-          </Container>
-        </Wrapper>
+              </Grid>
+            </Grid>
+          </Wrapper>
+        </Box>
       </main>
     </div>
   );
---
+++ explorer-nextjs/src/components/icons/NymTokenSVG.tsx
@@ -0,0 +1,38 @@
+import { useTheme } from "@mui/material/styles";
+import * as React from "react";
+
+export const NymTokenSVG = () => {
+  const theme = useTheme();
+  const color = theme.palette.text.primary;
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title id="nymTokenTitle">Nym Token Icon</title>
+      <g clipPath="url(#clip0_7134_15888)">
+        <path
+          d="M17.07 3.43C13.17 -0.480002 6.83 -0.480002 2.93 3.43C-0.980002 7.34 -0.980002 13.67 2.93 17.57C6.84 21.48 13.17 21.48 17.07 17.57C20.98 13.67 20.98 7.33 17.07 3.43ZM16.21 16.71C12.78 20.14 7.21 20.14 3.78 16.71C0.349997 13.28 0.349997 7.71 3.78 4.28C7.21 0.849997 12.78 0.849997 16.21 4.28C19.65 7.72 19.65 13.28 16.21 16.71Z"
+          fill={color}
+        />
+        <path
+          d="M15.4 16.33V4.66999C14.89 4.18999 14.32 3.76999 13.71 3.43999V14.59L6.35001 3.39999C5.71001 3.73999 5.12001 4.15999 4.60001 4.65999V16.33C5.11001 16.81 5.68001 17.23 6.29001 17.56V6.40999L13.65 17.6C14.29 17.26 14.88 16.83 15.4 16.33Z"
+          fill={color}
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_7134_15888">
+          <rect
+            width="20"
+            height="20"
+            fill={color}
+            transform="translate(0 0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};

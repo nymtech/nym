f94f3b1a1d67a499848f28fe286d9c0bb1bd8e18
fmtabbara <fmtabbara@hotmail.co.uk>
2024-12-11 00:44:26 +0000

    create some base components

---
+++ explorer-nextjs/src/components/Cards/ExplorerHeroCard.tsx
@@ -0,0 +1,62 @@
+import ArrowUpRight from "@/components/icons/ArrowUpRight";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Link } from "../muiLink";
+
+const cardStyles = {
+  p: 3,
+  cursor: "pointer",
+  "&:hover": {
+    bgcolor: "accent.main",
+  },
+};
+const cardContentStyles = {
+  mt: 10,
+};
+const titleStyles = {
+  letterSpacing: 0.7,
+};
+
+const ExplorerHeroCard = ({
+  title,
+  label,
+  description,
+  image,
+  link,
+}: {
+  title: string;
+  label: string;
+  description: string;
+  image: React.ReactNode;
+  link: string;
+}) => {
+  return (
+    <Link href={link} sx={{ textDecoration: "none" }}>
+      <Card sx={cardStyles} elevation={0}>
+        <CardHeader
+          title={
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="body4" sx={titleStyles}>
+                {label}
+              </Typography>
+              <ArrowUpRight />
+            </Stack>
+          }
+        />
+        <CardContent sx={cardContentStyles}>
+          <Stack spacing={4}>
+            {image}
+            <Typography variant="h2">{title}</Typography>
+            <Typography variant="body3">{description}</Typography>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+export default ExplorerHeroCard;
---
+++ explorer-nextjs/src/components/List/ListItem.tsx
@@ -0,0 +1,48 @@
+import { Divider, Stack, type SxProps, Typography } from "@mui/material";
+
+const listItemLabelStyle: SxProps = {
+  color: "pine.600",
+  letterSpacing: 0.7,
+  flexGrow: 1,
+};
+
+const listItemValueStyle: SxProps = {
+  fontSize: "small",
+  fontWeight: 300,
+  color: "pine.950",
+};
+
+const ExplorerListItem = ({
+  label,
+  value,
+  row,
+}: {
+  label: string;
+  value: string | React.ReactNode;
+  row?: boolean;
+}) => {
+  return (
+    <>
+      <Stack
+        direction={row ? "row" : "column"}
+        alignItems={row ? "center" : "flex-start"}
+        justifyContent="space-between"
+        gap={1}
+      >
+        <Typography variant="h6" sx={listItemLabelStyle}>
+          {label}
+        </Typography>
+        {typeof value === "string" ? (
+          <Typography variant="body3" sx={listItemValueStyle}>
+            {value}
+          </Typography>
+        ) : (
+          value
+        )}
+      </Stack>
+      <Divider variant="fullWidth" sx={{ my: 2 }} />
+    </>
+  );
+};
+
+export default ExplorerListItem;

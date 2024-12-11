280283e41a20e0c8ec2ae180a4878dbbf58d82d9
fmtabbara <fmtabbara@hotmail.co.uk>
2024-12-11 15:26:53 +0000

    use another clipboard package

--- explorer-nextjs/src/components/copyToClipboard/CopyToClipboard.tsx
+++ explorer-nextjs/src/components/copyToClipboard/CopyToClipboard.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { IconButton } from "@mui/material";
-import { CopyToClipboard as ReactCopyToClipboard } from "react-copy-to-clipboard";
+import { IconButton, Typography } from "@mui/material";
+import { useCopyToClipboard } from "@uidotdev/usehooks";
+import { useEffect } from "react";
 import CopyFile from "../icons/CopyFile";
+
+const CLEAR_AFTER_MS = 10_000;
 
 const CopyToClipboard = ({
   text,
@@ -13,10 +16,31 @@ const CopyToClipboard = ({
   Icon?: React.ReactNode;
   size?: "small" | "medium" | "large";
 }) => {
+  const [copiedText, setCopiedText] = useCopyToClipboard();
+  const hasCopied = Boolean(copiedText);
+
+  useEffect(() => {
+    if (hasCopied) {
+      const timeout = setTimeout(() => {
+        setCopiedText("");
+      }, CLEAR_AFTER_MS);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [hasCopied, setCopiedText]);
+
+  if (hasCopied) {
+    return (
+      <Typography sx={{ color: "pine.950" }} variant="h6" color="textSecondary">
+        Copied
+      </Typography>
+    );
+  }
+
   return (
-    <ReactCopyToClipboard text={text}>
-      <IconButton size={size}>{Icon || <CopyFile />}</IconButton>
-    </ReactCopyToClipboard>
+    <IconButton size={size} onClick={() => setCopiedText(text)}>
+      {Icon || <CopyFile />}
+    </IconButton>
   );
 };
 

56c604112cb4a230fa2c97e0caed7f59ce2dca7f
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2024-12-12 16:53:57 +0000

    bugfix: make sure to apply gateway score filtering when choosing initial node

--- common/wasm/client-core/src/helpers.rs
+++ common/wasm/client-core/src/helpers.rs
@@ -121,9 +121,10 @@ pub async fn setup_gateway_from_api(
     force_tls: bool,
     chosen_gateway: Option<IdentityKey>,
     nym_apis: &[Url],
+    minimum_performance: u8,
 ) -> Result<InitialisationResult, WasmCoreError> {
     let mut rng = thread_rng();
-    let gateways = current_gateways(&mut rng, nym_apis, None).await?;
+    let gateways = current_gateways(&mut rng, nym_apis, None, minimum_performance).await?;
     setup_gateway_wasm(client_store, force_tls, chosen_gateway, &gateways).await
 }
 

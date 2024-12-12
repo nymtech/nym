5e3259c7873dc1bf551d834ca16701c5e7e15a11
fmtabbara <fmtabbara@hotmail.co.uk>
2024-12-12 23:25:23 +0000

    create layout and heading components

---
+++ explorer-nextjs/src/components/headings/SectionHeading.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const SectionHeading = ({ title }: { title: string }) => {
+  return <Typography variant="h2">{title}</Typography>;
+};
+
+export default SectionHeading;

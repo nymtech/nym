103523d20af7bf293840cdf6d8fc769e1189ce0d
fmtabbara <fmtabbara@hotmail.co.uk>
2024-12-12 23:27:52 +0000

    country flag and layout components

---
+++ explorer-nextjs/src/components/countryFlag/CountryFlag.tsx
@@ -0,0 +1,21 @@
+import { Stack, Typography } from "@mui/material";
+import Flag from "react-world-flags";
+
+interface ICountryFlag {
+  countryCode: string;
+  countryName?: string;
+}
+
+const CountryFlag = ({ countryCode, countryName }: ICountryFlag) => {
+  return (
+    <Stack direction="row" gap={1}>
+      <Flag code={countryCode} width="19" />
+      <Typography variant="subtitle2" sx={{ color: "pine.950" }}>
+        {countryName}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default CountryFlag;
+export type { ICountryFlag };

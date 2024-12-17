4f07343efdec27c97b2a3b5ee083aa84f3026022
Sachin Kamath <github@skamath.me>
2024-12-17 15:05:24 +0530

    api: fetch addresses from config.

--- nyx-chain-watcher/src/http/api/mod.rs
+++ nyx-chain-watcher/src/http/api/mod.rs
@@ -8,6 +8,7 @@ use utoipa_swagger_ui::SwaggerUi;
 use crate::http::{api_docs, server::HttpServer, state::AppState};
 
 pub(crate) mod price;
+pub(crate) mod watcher;
 
 pub(crate) struct RouterBuilder {
     unfinished_router: Router<AppState>,
@@ -24,7 +25,8 @@ impl RouterBuilder {
                 "/",
                 axum::routing::get(|| async { Redirect::permanent("/swagger") }),
             )
-            .nest("/v1", Router::new().nest("/price", price::routes()));
+            .nest("/v1", Router::new().nest("/price", price::routes()))
+            .nest("/v1", Router::new().nest("/watcher", watcher::routes()));
 
         Self {
             unfinished_router: router,

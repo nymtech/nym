a7f46081d985249edf7e0fbf268c37b4df7d41dc
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2024-12-18 13:23:17 +0100

    Move ecash schema out of ecash crate

--- common/nym_offline_compact_ecash/src/scheme/expiration_date_signatures.rs
+++ common/nym_offline_compact_ecash/src/scheme/expiration_date_signatures.rs
@@ -12,19 +12,15 @@ use bls12_381::{G1Projective, Scalar};
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
-use utoipa::ToSchema;
 
 /// A structure representing an expiration date signature.
 pub type ExpirationDateSignature = Signature;
 pub type PartialExpirationDateSignature = ExpirationDateSignature;
 
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize, ToSchema)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct AnnotatedExpirationDateSignature {
-    #[schema(value_type = Signature)]
     pub signature: ExpirationDateSignature,
-    #[schema(value_type = u32)]
     pub expiration_timestamp: EncodedDate,
-    #[schema(value_type = u32)]
     pub spending_timestamp: EncodedDate,
 }
 

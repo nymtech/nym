9120020c759f1d7e6f22a61a2c3e11e1d28ac66b
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2024-12-19 10:37:12 +0100

    Move redocly config to nym-api/

--- nym-api/redocly/redocly.sh
+++ nym-api/redocly/redocly.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
 curl -s http://localhost:8000/api-docs/openapi.json | jq . > formatted-openapi.json
 npx @redocly/cli@latest lint --config .redocly.yaml \
-    --skip-rule=operation-2xx-response \
     # --generate-ignore-file

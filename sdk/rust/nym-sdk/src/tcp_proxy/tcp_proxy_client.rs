3ef03105517ef176de02f74b9b54fa3969d3fd59
mfahampshire <maxhampshire@pm.me>
2025-01-08 16:20:54 +0100

    license

--- sdk/rust/nym-sdk/src/tcp_proxy/tcp_proxy_client.rs
+++ sdk/rust/nym-sdk/src/tcp_proxy/tcp_proxy_client.rs
@@ -1,3 +1,6 @@
+// Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
 use crate::client_pool::ClientPool;
 use crate::mixnet::{IncludedSurbs, MixnetClientBuilder, MixnetMessageSender, NymNetworkDetails};
 use std::{sync::Arc, time::Duration};

05f8f4b847c5660a3eb6a0fa6bdf34902ab4b056
RadekSabacky <radek@nymtech.net>
2025-01-13 14:57:44 +0100

    / change alert message explorer 2.0

--- explorer/src/pages/Gateways/index.tsx
+++ explorer/src/pages/Gateways/index.tsx
@@ -236,8 +236,7 @@ export const PageGateways: FCWithChildren = () => {
           <Alert severity="warning" sx={{ mb: 3, fontSize: 'medium', width: '100%' }}>
             <Box>
               <Typography>
-                You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon, in the first quarter of
-                2025.
+                You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon.
               </Typography>
             </Box>
           </Alert>
--- explorer/src/pages/MixnodesMap/index.tsx
+++ explorer/src/pages/MixnodesMap/index.tsx
@@ -79,8 +79,7 @@ export const PageMixnodesMap: FCWithChildren = () => {
             <Alert severity="warning" sx={{ mb: 3, fontSize: 'medium', width: '100%' }}>
               <Box>
                 <Typography>
-                  You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon, in the first quarter
-                  of 2025.
+                  You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon.
                 </Typography>
               </Box>
             </Alert>

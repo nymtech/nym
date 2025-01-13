33931122814538bd7ce8cc8cc7f564fb4a75f4a7
RadekSabacky <radek@nymtech.net>
2025-01-13 15:30:05 +0100

    / prettier on releaseAlert

--- explorer/src/components/ReleaseAlert.tsx
+++ explorer/src/components/ReleaseAlert.tsx
@@ -4,10 +4,8 @@ export const ReleaseAlert = () => {
   return (
     <Alert severity="warning" sx={{ mb: 3, fontSize: 'medium', width: '100%' }}>
       <Box>
-        <Typography>
-          You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon.
-        </Typography>
+        <Typography>You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon.</Typography>
       </Box>
     </Alert>
   );
-}
+};

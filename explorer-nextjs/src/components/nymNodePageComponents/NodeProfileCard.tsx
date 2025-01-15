d2c42ab7c6b2e47ec44fdd797a5e2a530edb0166
Yana <yanok87@users.noreply.github.com>
2025-01-15 13:16:08 +0200

    Add location label to NodoProfileCard

--- explorer-nextjs/src/components/nymNodePageComponents/NodeProfileCard.tsx
+++ explorer-nextjs/src/components/nymNodePageComponents/NodeProfileCard.tsx
@@ -115,10 +115,13 @@ export const NodeProfileCard = (props: INodeProfileCardProps) => {
           {nodeInfo?.self_description.moniker || "Moniker"}
         </Typography>
         {nodeInfo.description.auxiliary_details.location && (
-          <CountryFlag
-            countryCode={nodeInfo.description.auxiliary_details.location}
-            countryName={nodeInfo.description.auxiliary_details.location}
-          />
+          <Box display={"flex"} gap={1}>
+            <Typography variant="h6">Location:</Typography>
+            <CountryFlag
+              countryCode={nodeInfo.description.auxiliary_details.location}
+              countryName={nodeInfo.description.auxiliary_details.location}
+            />
+          </Box>
         )}
         {nodeInfo && (
           <Typography variant="body4" sx={{ color: "pine.950" }} mt={2}>

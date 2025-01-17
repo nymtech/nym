2f5aed0354b01c2b2896521b202bac7ea8ec5fb7
Yana <yanok87@users.noreply.github.com>
2025-01-17 19:14:49 +0200

    Add spectreDao tokenomics to landing page

--- explorer-nextjs/src/app/api/types.ts
+++ explorer-nextjs/src/app/api/types.ts
@@ -384,3 +384,40 @@ export type ObservatoryBalance = {
   self_bonded: BalanceDetails;
   spendable: BalanceDetails;
 };
+
+export type Quote = {
+  ath_date: string;
+  ath_price: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_12h: number;
+  percent_change_15m: number;
+  percent_change_1h: number;
+  percent_change_1y: number;
+  percent_change_24h: number;
+  percent_change_30d: number;
+  percent_change_30m: number;
+  percent_change_6h: number;
+  percent_change_7d: number;
+  percent_from_price_ath: number;
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+};
+
+export type Quotes = {
+  USD: Quote;
+};
+
+export type NymTokenomics = {
+  beta_value: number;
+  first_data_at: string;
+  id: string;
+  last_updated: string;
+  max_supply: number;
+  name: string;
+  quotes: Quotes;
+  rank: number;
+  symbol: string;
+  total_supply: number;
+};

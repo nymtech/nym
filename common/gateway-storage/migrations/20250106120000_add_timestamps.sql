9f8f19dc335a44e52d1acbb397776fc0905c4c90
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-01-21 09:24:41 +0000

    fixed sql migration for adding default message timestamp

--- common/gateway-storage/migrations/20250106120000_add_timestamps.sql
+++ common/gateway-storage/migrations/20250106120000_add_timestamps.sql
@@ -3,5 +3,22 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+
 ALTER TABLE message_store
-    ADD COLUMN timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
+    RENAME TO message_store_old;
+
+-- add new column with message timestamp. 
+-- note: we can't simply alter existing table to add it since the default value is non-constant
+CREATE TABLE message_store
+(
+    id                  INTEGER                     NOT NULL PRIMARY KEY AUTOINCREMENT,
+    client_address_bs58 TEXT                        NOT NULL,
+    content             BLOB                        NOT NULL,
+    timestamp           TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO message_store(id, client_address_bs58, content)
+SELECT id, client_address_bs58, content
+FROM message_store_old;
+DROP TABLE message_store_old;
+

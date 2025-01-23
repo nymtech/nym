e6ead5e0c14b8282387adbec8264385a6ef29432
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-01-23 12:05:27 +0000

    initialised nym-pool contract and updated all bls12_381 to make it possible

---
+++ contracts/nym-pool/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+#![warn(clippy::expect_used)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::todo)]
+#![warn(clippy::dbg_macro)]
+
+mod contract;

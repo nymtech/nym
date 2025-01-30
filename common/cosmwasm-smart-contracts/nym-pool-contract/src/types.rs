9c779f2420d644dac6818bee368d33b6502e0779
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-01-30 08:46:12 +0000

    grant type validation

--- common/cosmwasm-smart-contracts/nym-pool-contract/src/types.rs
+++ common/cosmwasm-smart-contracts/nym-pool-contract/src/types.rs
@@ -54,7 +54,8 @@ pub mod grants {
             expiration_unix.map(Timestamp::from_seconds)
         }
 
-        pub fn validate(&self, env: &Env, denom: &str) -> Result<(), NymPoolContractError> {
+        /// Perform validation of a new grant that's to be created
+        pub fn validate_new(&self, env: &Env, denom: &str) -> Result<(), NymPoolContractError> {
             // 1. perform validation on the inner, basic, allowance
             self.basic().validate(env, denom)?;
 
@@ -62,9 +63,9 @@ pub mod grants {
             match self {
                 // we already validated basic allowance
                 Allowance::Basic(_) => Ok(()),
-                Allowance::ClassicPeriodic(allowance) => allowance.validate(env, denom),
-                Allowance::CumulativePeriodic(allowance) => allowance.validate(env, denom),
-                Allowance::Delayed(allowance) => allowance.validate(env, denom),
+                Allowance::ClassicPeriodic(allowance) => allowance.validate_new_inner(denom),
+                Allowance::CumulativePeriodic(allowance) => allowance.validate_new_inner(denom),
+                Allowance::Delayed(allowance) => allowance.validate_new_inner(env),
             }
         }
     }
@@ -116,20 +117,47 @@ pub mod grants {
 
         /// period_spend_limit specifies the maximum number of coins that can be spent
         /// in the period
-        pub period_spend_limit: Option<Coin>,
+        pub period_spend_limit: Coin,
 
         /// period_can_spend is the number of coins left to be spent before the period_reset time
+        // set by the contract during initialisation of the grant
+        #[serde(default)]
         pub period_can_spend: Option<Coin>,
 
         /// period_reset is the time at which this period resets and a new one begins,
         /// it is calculated from the start time of the first transaction after the
         /// last period ended
+        // set by the contract during initialisation of the grant
+        #[serde(default)]
         pub period_reset_unix_timestamp: u64,
     }
 
     impl ClassicPeriodicAllowance {
-        pub fn validate(&self, env: &Env, denom: &str) -> Result<(), NymPoolContractError> {
-            todo!()
+        pub(super) fn validate_new_inner(&self, denom: &str) -> Result<(), NymPoolContractError> {
+            // period duration shouldn't be zero
+            if self.period_duration_secs == 0 {
+                return Err(NymPoolContractError::ZeroAllowancePeriod);
+            }
+
+            // the denom for period spend limit must match the expected value
+            if self.period_spend_limit.denom != denom {
+                return Err(NymPoolContractError::InvalidDenom {
+                    expected: denom.to_string(),
+                    got: self.period_spend_limit.denom.to_string(),
+                });
+            }
+
+            // if the basic spend limit is set, the period spend limit cannot be larger than it
+            if let Some(ref basic_limit) = self.basic.spend_limit {
+                if basic_limit.amount < self.period_spend_limit.amount {
+                    return Err(NymPoolContractError::PeriodicGrantOverSpendLimit {
+                        periodic: self.period_spend_limit.clone(),
+                        total_limit: basic_limit.clone(),
+                    });
+                }
+            }
+
+            Ok(())
         }
     }
 
@@ -145,20 +173,75 @@ pub mod grants {
         /// period_grant specifies the maximum number of coins that is granted per period
         pub period_grant: Coin,
 
-        /// spendable_limit is the maximum value the grants and accumulate to
-        pub spendable_limit: Option<Coin>,
+        /// accumulation_limit is the maximum value the grants and accumulate to
+        pub accumulation_limit: Option<Coin>,
 
         /// spendable is the number of coins left to be spent before additional grant is applied
-        pub spendable: Coin,
+        // set by the contract during initialisation of the grant
+        #[serde(default)]
+        pub spendable: Option<Coin>,
 
         /// last_grant_applied is the time at which last transaction associated with this allowance
         /// has been sent and `spendable` value has been adjusted
+        // set by the contract during initialisation of the grant
+        #[serde(default)]
         pub last_grant_applied_unix_timestamp: u64,
     }
 
     impl CumulativePeriodicAllowance {
-        pub fn validate(&self, env: &Env, denom: &str) -> Result<(), NymPoolContractError> {
-            todo!()
+        pub(super) fn validate_new_inner(&self, denom: &str) -> Result<(), NymPoolContractError> {
+            // period duration shouldn't be zero
+            if self.period_duration_secs == 0 {
+                return Err(NymPoolContractError::ZeroAllowancePeriod);
+            }
+
+            // the denom for period grant must match the expected value
+            if self.period_grant.denom != denom {
+                return Err(NymPoolContractError::InvalidDenom {
+                    expected: denom.to_string(),
+                    got: self.period_grant.denom.to_string(),
+                });
+            }
+
+            // the period grant must not be larger than the total spend limit, if set
+            if let Some(ref basic_limit) = self.basic.spend_limit {
+                if basic_limit.amount < self.period_grant.amount {
+                    return Err(NymPoolContractError::PeriodicGrantOverSpendLimit {
+                        periodic: self.period_grant.clone(),
+                        total_limit: basic_limit.clone(),
+                    });
+                }
+            }
+
+            if let Some(ref accumulation_limit) = self.accumulation_limit {
+                // if set, the accumulation limit must not be smaller than the period grant
+                if accumulation_limit.amount < self.period_grant.amount {
+                    return Err(NymPoolContractError::AccumulationBelowGrantAmount {
+                        accumulation: accumulation_limit.clone(),
+                        periodic_grant: self.period_grant.clone(),
+                    });
+                }
+
+                // if set, the denom for accumulation limit must match the expected value
+                if accumulation_limit.denom != denom {
+                    return Err(NymPoolContractError::InvalidDenom {
+                        expected: denom.to_string(),
+                        got: accumulation_limit.denom.to_string(),
+                    });
+                }
+
+                // if set, the accumulation limit must not be larger than the total spend limit
+                if let Some(ref basic_limit) = self.basic.spend_limit {
+                    if basic_limit.amount < accumulation_limit.amount {
+                        return Err(NymPoolContractError::AccumulationOverSpendLimit {
+                            accumulation: accumulation_limit.clone(),
+                            total_limit: basic_limit.clone(),
+                        });
+                    }
+                }
+            }
+
+            Ok(())
         }
     }
 
@@ -175,8 +258,21 @@ pub mod grants {
     }
 
     impl DelayedAllowance {
-        pub fn validate(&self, env: &Env, denom: &str) -> Result<(), NymPoolContractError> {
-            todo!()
+        pub(super) fn validate_new_inner(&self, env: &Env) -> Result<(), NymPoolContractError> {
+            // available at must be set in the future
+            ensure_unix_timestamp_not_in_the_past(self.available_at_unix_timestamp, env)?;
+
+            // and it must become available before the underlying allowance expires
+            if let Some(expiration) = self.basic.expiration_unix_timestamp {
+                if expiration < self.available_at_unix_timestamp {
+                    return Err(NymPoolContractError::UnattainableDelayedAllowance {
+                        expiration_timestamp: expiration,
+                        available_timestamp: self.available_at_unix_timestamp,
+                    });
+                }
+            }
+
+            Ok(())
         }
     }
 }
@@ -225,13 +321,41 @@ mod tests {
 
     fn mock_basic_allowance() -> BasicAllowance {
         BasicAllowance {
-            spend_limit: Some(coin(12345, TEST_DENOM)),
+            spend_limit: Some(coin(100000, TEST_DENOM)),
             expiration_unix_timestamp: Some(1643652000),
         }
     }
 
+    fn mock_classic_periodic_allowance() -> ClassicPeriodicAllowance {
+        ClassicPeriodicAllowance {
+            basic: mock_basic_allowance(),
+            period_duration_secs: 10,
+            period_spend_limit: coin(1000, TEST_DENOM),
+            period_can_spend: None,
+            period_reset_unix_timestamp: 0,
+        }
+    }
+
+    fn mock_cumulative_periodic_allowance() -> CumulativePeriodicAllowance {
+        CumulativePeriodicAllowance {
+            basic: mock_basic_allowance(),
+            period_duration_secs: 10,
+            period_grant: coin(1000, TEST_DENOM),
+            accumulation_limit: Some(coin(10000, TEST_DENOM)),
+            spendable: None,
+            last_grant_applied_unix_timestamp: 0,
+        }
+    }
+
+    fn mock_delayed_allowance() -> DelayedAllowance {
+        DelayedAllowance {
+            basic: mock_basic_allowance(),
+            available_at_unix_timestamp: 1643650000,
+        }
+    }
+
     #[cfg(test)]
-    mod validating_allowances {
+    mod validating_new_allowances {
         use super::*;
 
         #[cfg(test)]
@@ -287,16 +411,244 @@ mod tests {
         #[cfg(test)]
         mod classic_periodic_allowance {
             use super::*;
+            use crate::NymPoolContractError;
+
+            #[test]
+            fn period_duration_must_be_nonzero() {
+                let mut allowance = mock_classic_periodic_allowance();
+
+                allowance.period_duration_secs = 0;
+                assert_eq!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::ZeroAllowancePeriod
+                );
+
+                allowance.period_duration_secs = 1;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn spend_limit_must_match_expected_denom() {
+                let allowance = mock_classic_periodic_allowance();
+
+                // mismatched denom
+                assert!(allowance.validate_new_inner("baddenom").is_err());
+
+                // matched denom
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn period_spend_limit_must_be_smaller_than_total_limit() {
+                let mut allowance = mock_classic_periodic_allowance();
+
+                let total_limit = coin(1000, TEST_DENOM);
+                allowance.basic.spend_limit = Some(total_limit);
+
+                // above total spend limit
+                allowance.period_spend_limit = coin(1001, TEST_DENOM);
+                assert!(matches!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::PeriodicGrantOverSpendLimit { .. }
+                ));
+
+                // below total spend limit
+                allowance.period_spend_limit = coin(999, TEST_DENOM);
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // equal to total spend limit
+                allowance.period_spend_limit = coin(1000, TEST_DENOM);
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no total spend limit
+                allowance.basic.spend_limit = None;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
         }
 
         #[cfg(test)]
         mod cumulative_periodic_allowance {
             use super::*;
+            use crate::NymPoolContractError;
+
+            #[test]
+            fn period_duration_must_be_nonzero() {
+                let mut allowance = mock_cumulative_periodic_allowance();
+
+                allowance.period_duration_secs = 0;
+                assert_eq!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::ZeroAllowancePeriod
+                );
+
+                allowance.period_duration_secs = 1;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn grant_must_match_expected_denom() {
+                let allowance = mock_cumulative_periodic_allowance();
+
+                // mismatched denom
+                assert!(allowance.validate_new_inner("baddenom").is_err());
+
+                // matched denom
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn grant_amount_must_be_smaller_than_total_limit() {
+                let mut allowance = mock_cumulative_periodic_allowance();
+
+                let total_limit = coin(1000, TEST_DENOM);
+                allowance.basic.spend_limit = Some(total_limit);
+                allowance.accumulation_limit = None;
+
+                // above total spend limit
+                allowance.period_grant = coin(1001, TEST_DENOM);
+                assert!(matches!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::PeriodicGrantOverSpendLimit { .. }
+                ));
+
+                // below total spend limit
+                allowance.period_grant = coin(999, TEST_DENOM);
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // equal to total spend limit
+                allowance.period_grant = coin(1000, TEST_DENOM);
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no total spend limit
+                allowance.basic.spend_limit = None;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn accumulation_limit_must_be_smaller_than_total_limit() {
+                let mut allowance = mock_cumulative_periodic_allowance();
+
+                let total_limit = coin(1000, TEST_DENOM);
+                allowance.basic.spend_limit = Some(total_limit.clone());
+                allowance.period_grant = coin(500, TEST_DENOM);
+
+                // above total spend limit
+                allowance.accumulation_limit = Some(coin(1001, TEST_DENOM));
+                assert!(matches!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::AccumulationOverSpendLimit { .. }
+                ));
+
+                // below total spend limit
+                allowance.accumulation_limit = Some(coin(999, TEST_DENOM));
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // equal to total spend limit
+                allowance.accumulation_limit = Some(coin(1000, TEST_DENOM));
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no total spend limit
+                allowance.basic.spend_limit = None;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no accumulation limit
+                allowance.accumulation_limit = None;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no accumulation limit but with spend limit
+                allowance.basic.spend_limit = Some(total_limit);
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn accumulation_limit_must_not_be_smaller_than_grant_amount() {
+                let mut allowance = mock_cumulative_periodic_allowance();
+
+                let total_limit = coin(1000, TEST_DENOM);
+                allowance.basic.spend_limit = Some(total_limit);
+                allowance.period_grant = coin(500, TEST_DENOM);
+
+                // below grant amount
+                allowance.accumulation_limit = Some(coin(499, TEST_DENOM));
+                assert!(matches!(
+                    allowance.validate_new_inner(TEST_DENOM).unwrap_err(),
+                    NymPoolContractError::AccumulationBelowGrantAmount { .. }
+                ));
+
+                // above grant amount
+                allowance.accumulation_limit = Some(coin(501, TEST_DENOM));
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // equal to grant amount
+                allowance.accumulation_limit = Some(coin(500, TEST_DENOM));
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+
+                // no accumulation limit
+                allowance.accumulation_limit = None;
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
+
+            #[test]
+            fn accumulation_limit_must_match_expected_denom() {
+                let mut allowance = mock_cumulative_periodic_allowance();
+                allowance.accumulation_limit = Some(coin(1000, "baddenom"));
+
+                // mismatched denom
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_err());
+
+                // matched denom
+                allowance.accumulation_limit = Some(coin(1000, TEST_DENOM));
+                assert!(allowance.validate_new_inner(TEST_DENOM).is_ok());
+            }
         }
 
         #[cfg(test)]
         mod delayed_allowance {
             use super::*;
+            use cosmwasm_std::testing::mock_env;
+            use cosmwasm_std::Timestamp;
+
+            #[test]
+            fn doesnt_allow_availability_in_the_past() {
+                let allowance = mock_delayed_allowance();
+                let mut env = mock_env();
+
+                // availability is in the past
+                env.block.time = Timestamp::from_seconds(allowance.available_at_unix_timestamp + 1);
+                assert!(allowance.validate_new_inner(&env).is_err());
+
+                // availability is equal to the current block time
+                env.block.time = Timestamp::from_seconds(allowance.available_at_unix_timestamp);
+                assert!(allowance.validate_new_inner(&env).is_ok());
+
+                // availability is in the future
+                env.block.time = Timestamp::from_seconds(allowance.available_at_unix_timestamp - 1);
+                assert!(allowance.validate_new_inner(&env).is_ok());
+            }
+
+            #[test]
+            fn must_have_available_before_allowance_expiration() {
+                let mut allowance = mock_delayed_allowance();
+                let mut env = mock_env();
+                env.block.time = Timestamp::from_seconds(100);
+                allowance.basic.expiration_unix_timestamp = Some(1000);
+
+                // after expiration
+                allowance.available_at_unix_timestamp = 1001;
+                assert!(allowance.validate_new_inner(&env).is_err());
+
+                // equal to expiration
+                allowance.available_at_unix_timestamp = 1000;
+                assert!(allowance.validate_new_inner(&env).is_ok());
+
+                // before expiration
+                allowance.available_at_unix_timestamp = 999;
+                assert!(allowance.validate_new_inner(&env).is_ok());
+
+                // with no explicit expiration
+                allowance.basic.expiration_unix_timestamp = None;
+                assert!(allowance.validate_new_inner(&env).is_ok());
+            }
         }
     }
 }

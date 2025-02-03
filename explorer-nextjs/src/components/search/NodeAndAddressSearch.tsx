352fd12505c5a57fc7a2cc80b64ef5fad4986043
Yana <yanok87@users.noreply.github.com>
2025-02-03 17:24:21 +0200

    fix build

--- explorer-nextjs/src/components/search/NodeAndAddressSearch.tsx
+++ explorer-nextjs/src/components/search/NodeAndAddressSearch.tsx
@@ -1,9 +1,9 @@
 "use client";
+import type { NodeData } from "@/app/api/types";
 import { Search } from "@mui/icons-material";
 import { Button, CircularProgress, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import type NodeData from "../../app/api/types";
 import { NYM_NODE_BONDED } from "../../app/api/urls";
 import Input from "../input/Input";
 

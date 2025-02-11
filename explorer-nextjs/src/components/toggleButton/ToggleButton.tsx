120ad25a89a3c8c325a3d31187d7eb93e819db74
fmtabbara <fmtabbara@hotmail.co.uk>
2025-02-11 19:32:48 +0000

    remove logic from togglebutton component

--- explorer-nextjs/src/components/toggleButton/ToggleButton.tsx
+++ explorer-nextjs/src/components/toggleButton/ToggleButton.tsx
@@ -1,10 +1,4 @@
-"use client";
-import { fetchCurrentEpoch } from "@/app/api";
-import { Button, ButtonGroup, CircularProgress } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import { useQueryClient } from "@tanstack/react-query";
-import { subSeconds } from "date-fns";
-import { useCallback, useEffect, useState } from "react";
+import { Button, ButtonGroup } from "@mui/material";
 import { Link } from "../muiLink";
 
 type Option = {
@@ -18,56 +12,10 @@ type Options = [Option, Option];
 const ExplorerButtonGroup = ({
   size = "small",
   options,
-  onPage,
 }: {
   size?: "small" | "medium" | "large";
   options: Options;
-  onPage: string;
 }) => {
-  const [hasEpochStarted, setHasEpochStarted] = useState(false);
-  const [loading, setLoading] = useState<string | null>(null);
-
-  const { data } = useQuery({
-    enabled: true,
-    queryKey: ["currentEpoch"],
-    queryFn: fetchCurrentEpoch,
-    refetchInterval: 30000,
-    staleTime: 30000,
-    refetchOnMount: true,
-    keepPreviousData: false,
-  });
-
-  const queryClient = useQueryClient();
-  // check epoch update
-  useEffect(() => {
-    const checkEpochStatus = () => {
-      if (!data?.dateTime) return;
-      const oneHourLater = subSeconds(new Date(data.dateTime), 30).getTime();
-      const now = Date.now();
-      setHasEpochStarted(now >= oneHourLater);
-    };
-
-    checkEpochStatus();
-    const interval = setInterval(checkEpochStatus, 60000);
-    return () => clearInterval(interval);
-  });
-
-  const handleRefetch = useCallback(() => {
-    queryClient.invalidateQueries();
-  }, [queryClient]);
-
-  useEffect(() => {
-    if (!hasEpochStarted) return;
-    handleRefetch();
-    const interval = setInterval(handleRefetch, 30000);
-    return () => clearInterval(interval);
-  }, [hasEpochStarted, handleRefetch]);
-
-  const handleClick = (label: string) => {
-    if (onPage === label) return;
-    setLoading(label);
-  };
-
   return (
     <ButtonGroup size={size}>
       {options.map((option) => (
@@ -75,7 +23,6 @@ const ExplorerButtonGroup = ({
           href={option.link}
           key={option.label}
           sx={{ textDecoration: "none" }}
-          onClick={() => handleClick(option.label)}
         >
           <Button
             sx={{
@@ -88,13 +35,8 @@ const ExplorerButtonGroup = ({
               bgcolor: option.isSelected ? "primary.main" : "transparent",
             }}
             variant="outlined"
-            // disabled={loading === option.label}
           >
-            {loading === option.label ? (
-              <CircularProgress size={18} color="inherit" />
-            ) : (
-              option.label
-            )}
+            {option.label}
           </Button>
         </Link>
       ))}

345df90d2e077d378a8b9fd065deeb09e173b5f3
Yana <yanok87@users.noreply.github.com>
2025-02-12 18:21:58 +0200

    Add staking article to staking page

--- explorer-nextjs/src/app/(pages)/stake/page.tsx
+++ explorer-nextjs/src/app/(pages)/stake/page.tsx
@@ -1,3 +1,5 @@
+import BlogArticlesCards from "@/components/blogs/BlogArticleCards";
+import Grid from "@mui/material/Grid2";
 import { ContentLayout } from "../../../components/contentLayout/ContentLayout";
 import SectionHeading from "../../../components/headings/SectionHeading";
 import OverviewCards from "../../../components/staking/OverviewCards";
@@ -11,6 +13,12 @@ export default async function StakingPage() {
       <SubHeaderRow />
       <OverviewCards />
       <StakeTableWithAction />
+      <Grid container columnSpacing={5} rowSpacing={5}>
+        <Grid size={12}>
+          <SectionHeading title="Onboarding" />
+        </Grid>
+        <BlogArticlesCards ids={[2]} />
+      </Grid>
     </ContentLayout>
   );
 }

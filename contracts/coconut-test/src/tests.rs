3f4b09251f837cf0f1c96b76f076ea43c9f048e7
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-02-14 14:37:38 +0000

    removed old coconut contract code + additional dkg fixes

--- contracts/coconut-test/src/tests.rs
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
-// SPDX-License-Identifier: Apache-2.0
-
-mod deposit_and_release;
-mod helpers;
-mod spend_credential_creates_proposal;
-mod submit_vk_creates_proposal;
-mod test_wrapper;

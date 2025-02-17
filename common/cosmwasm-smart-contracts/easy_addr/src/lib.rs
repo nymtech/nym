f27499ca60055fa6292d8f478c3f71b94f99ee04
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-02-17 12:21:53 +0000

    fixed easy_addr

--- common/cosmwasm-smart-contracts/easy_addr/src/lib.rs
+++ common/cosmwasm-smart-contracts/easy_addr/src/lib.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::testing::mock_dependencies;
+use cosmwasm_std::testing::MockApi;
 
 use proc_macro::TokenStream;
 use quote::quote;
@@ -7,9 +7,6 @@ use syn::parse_macro_input;
 #[proc_macro]
 pub fn addr(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as syn::LitStr).value();
-    let addr = mock_dependencies()
-        .api
-        .addr_make(input.as_str())
-        .to_string();
+    let addr = MockApi::default().addr_make(input.as_str()).to_string();
     TokenStream::from(quote! {#addr})
 }

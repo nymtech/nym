a95db25ce3d3026633ec5176598cdcd5da955453
jmwample <jmwample@users.noreply.github.com>
2025-02-18 11:39:40 -0700

    exclude wasm

--- common/http-api-client/src/lib.rs
+++ common/http-api-client/src/lib.rs
@@ -147,6 +147,7 @@ use thiserror::Error;
 use tracing::{instrument, warn};
 use url::Url;
 
+#[cfg(not(target_arch = "wasm32"))]
 use std::net::SocketAddr;
 #[cfg(not(target_arch = "wasm32"))]
 use std::sync::Arc;
@@ -297,6 +298,7 @@ impl ClientBuilder {
     ///
     /// Set the port to `0` to use the conventional port for the given scheme (e.g. 80 for http).
     /// Ports in the URL itself will always be used instead of the port in the overridden addr.
+    #[cfg(not(target_arch = "wasm32"))]
     pub fn resolve_to_addrs(mut self, domain: &str, addrs: &[SocketAddr]) -> ClientBuilder {
         self.reqwest_client_builder = self.reqwest_client_builder.resolve_to_addrs(domain, addrs);
         self

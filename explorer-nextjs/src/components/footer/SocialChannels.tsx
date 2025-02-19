231d94b6d4fb44b16e7ee0f920c4df8803c55945
Yana <yanok87@users.noreply.github.com>
2025-02-19 16:44:09 +0200

    remove turbopack from package.json

--- explorer-nextjs/src/components/footer/SocialChannels.tsx
+++ explorer-nextjs/src/components/footer/SocialChannels.tsx
@@ -31,6 +31,7 @@ export const SocialChannels = () => {
               }}
             >
               <SocialIcon channel={channel?.name} />
+              {/* {channel.name} */}
             </Link>
           );
         },

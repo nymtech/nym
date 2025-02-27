25eceb4eacce5254cca80f46ee9d0e65fea12040
RadekSabacky <radek@nymtech.net>
2025-02-27 11:47:11 +0100

    + ribbon text update

--- explorer-nextjs/app/components/ReleaseAlert.tsx
+++ explorer-nextjs/app/components/ReleaseAlert.tsx
@@ -1,9 +1,16 @@
-import { Alert, Box, Typography } from '@mui/material';
+import { Alert, Box, Link, Typography } from '@mui/material';
+import StyledLink from './StyledLink';
 
 export const ReleaseAlert = () => (
   <Alert severity="warning" sx={{ mb: 3, fontSize: 'medium', width: '100%' }}>
     <Box>
-      <Typography>You are now viewing the legacy Nym mixnet explorer. Explorer 2.0 is coming soon.</Typography>
+      <Typography>
+        "You are now viewing the legacy Nym mixnet explorer. Check out Explorer 2.0 here link
+        <Link
+            href="https://nym.com/explorer"
+            style={{ textDecoration: 'none' }}
+        />
+      </Typography>
     </Box>
   </Alert>
 );

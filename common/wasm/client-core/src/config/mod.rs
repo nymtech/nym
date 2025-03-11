2d00fcd9346e5964984c44a2ba7ae0345d37b511
Jon Häggblad <jon.haggblad@gmail.com>
2025-03-11 08:35:40 +0100

    Allow resetting all SURB sender tags (#5600)
    * Allow resetting all SURB sender tags
    
    * wasm fixes
    
    * More wasm fixes

--- common/wasm/client-core/src/config/mod.rs
+++ common/wasm/client-core/src/config/mod.rs
@@ -494,6 +494,9 @@ pub struct ReplySurbsWasm {
     /// Defines how many mix nodes the reply surb should go through.
     /// If not set, the default value is going to be used.
     pub surb_mix_hops: Option<u8>,
+
+    /// Specifies if we should reset all the sender tags on startup
+    pub fresh_sender_tags: bool,
 }
 
 impl Default for ReplySurbsWasm {
@@ -525,6 +528,7 @@ impl From<ReplySurbsWasm> for ConfigReplySurbs {
                 reply_surbs.maximum_reply_key_age_ms as u64,
             ),
             surb_mix_hops: reply_surbs.surb_mix_hops,
+            fresh_sender_tags: reply_surbs.fresh_sender_tags,
         }
     }
 }
@@ -548,6 +552,7 @@ impl From<ConfigReplySurbs> for ReplySurbsWasm {
             maximum_reply_surb_age_ms: reply_surbs.maximum_reply_surb_age.as_millis() as u32,
             maximum_reply_key_age_ms: reply_surbs.maximum_reply_key_age.as_millis() as u32,
             surb_mix_hops: reply_surbs.surb_mix_hops,
+            fresh_sender_tags: reply_surbs.fresh_sender_tags,
         }
     }
 }

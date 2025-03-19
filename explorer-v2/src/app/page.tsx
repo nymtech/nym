52ffd2e7982d5d761f47682c01da042530c92212
Yana <iana.matrosova@gmail.com>
2025-03-19 15:30:39 +0200

    fix build

--- explorer-v2/src/app/page.tsx
+++ explorer-v2/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import BlogArticlesCards from "../components/blogs/BlogArticleCards";
 import { ContentLayout } from "../components/contentLayout/ContentLayout";

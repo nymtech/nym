e0ff823211ebad12645efd27d69a8a2575574713
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-03-20 14:58:27 +0100

    db::scraper -> db::node_description

--- nym-node-status-api/nym-node-status-api/src/db/queries/mod.rs
+++ nym-node-status-api/nym-node-status-api/src/db/queries/mod.rs
@@ -2,9 +2,9 @@ mod gateways;
 mod gateways_stats;
 mod misc;
 mod mixnodes;
+pub(crate) mod node_description;
 mod nym_nodes;
 mod packet_stats;
-pub(crate) mod scraper;
 mod summary;
 pub(crate) mod testruns;
 
@@ -14,6 +14,7 @@ pub(crate) use gateways::{
 pub(crate) use gateways_stats::{delete_old_records, get_sessions_stats, insert_session_records};
 pub(crate) use misc::insert_summaries;
 pub(crate) use mixnodes::{get_all_mixnodes, get_bonded_mix_ids, get_daily_stats, update_mixnodes};
+pub(crate) use node_description::{get_nodes_for_scraping, insert_scraped_node_description};
 pub(crate) use nym_nodes::{
     get_active_nym_nodes, get_all_nym_nodes, get_described_node_bond_info, get_node_descriptions,
     update_nym_nodes,
@@ -21,5 +22,4 @@ pub(crate) use nym_nodes::{
 pub(crate) use packet_stats::{
     get_raw_node_stats, insert_daily_node_stats, insert_node_packet_stats,
 };
-pub(crate) use scraper::{get_nodes_for_scraping, insert_scraped_node_description};
 pub(crate) use summary::{get_summary, get_summary_history};

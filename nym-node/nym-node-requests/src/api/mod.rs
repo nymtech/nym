96b296f54f6b320893d1c44154c129713dca0b89
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-04-09 11:41:31 +0200

    additional Polish; missing features, extra test, etc

--- nym-node/nym-node-requests/src/api/mod.rs
+++ nym-node/nym-node-requests/src/api/mod.rs
@@ -1,14 +1,13 @@
-// Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
+// Copyright 2023-2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::api::v1::node::models::{LegacyHostInformation, LegacyHostInformationV2};
 use crate::error::Error;
-use nym_crypto::asymmetric::identity;
+use nym_crypto::asymmetric::ed25519;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
 use std::ops::Deref;
-use utoipa::ToSchema;
 
 #[cfg(feature = "client")]
 pub mod client;
@@ -20,7 +19,7 @@ pub use client::Client;
 // create the type alias manually if openapi is not enabled
 pub type SignedHostInformation = SignedData<crate::api::v1::node::models::HostInformation>;
 
-#[derive(ToSchema)]
+#[cfg_attr(feature = "openapi", derive(utoipa::ToSchema))]
 pub struct SignedDataHostInfo {
     // #[serde(flatten)]
     pub data: crate::api::v1::node::models::HostInformation,
@@ -35,16 +34,17 @@ pub struct SignedData<T> {
 }
 
 impl<T> SignedData<T> {
-    pub fn new(data: T, key: &identity::PrivateKey) -> Result<Self, Error>
+    pub fn new(data: T, key: &ed25519::PrivateKey) -> Result<Self, Error>
     where
         T: Serialize,
     {
         let plaintext = serde_json::to_string(&data)?;
+
         let signature = key.sign(plaintext).to_base58_string();
         Ok(SignedData { data, signature })
     }
 
-    pub fn verify(&self, key: &identity::PublicKey) -> bool
+    pub fn verify(&self, key: &ed25519::PublicKey) -> bool
     where
         T: Serialize,
     {
@@ -52,7 +52,7 @@ impl<T> SignedData<T> {
             return false;
         };
 
-        let Ok(signature) = identity::Signature::from_base58_string(&self.signature) else {
+        let Ok(signature) = ed25519::Signature::from_base58_string(&self.signature) else {
             return false;
         };
 
@@ -65,6 +65,8 @@ impl SignedHostInformation {
         if self.verify(&self.keys.ed25519_identity) {
             return true;
         }
+
+        // TODO: @JS: to remove downgrade support in future release(s)
 
         // attempt to verify legacy signatures
         let legacy_v2 = SignedData {
@@ -107,6 +109,7 @@ impl Display for ErrorResponse {
 mod tests {
 
     use super::*;
+    use crate::api::v1::node::models::HostInformation;
     use nym_crypto::asymmetric::{ed25519, x25519};
     use nym_noise_keys::{NoiseVersion, VersionedNoiseKey};
     use rand_chacha::rand_core::SeedableRng;
@@ -222,6 +225,48 @@ mod tests {
         };
 
         assert!(!current_struct_no_noise.verify(ed22519.public_key()));
+        assert!(current_struct_no_noise.verify_host_information());
+
+        assert!(!current_struct_noise.verify(ed22519.public_key()));
+        assert!(current_struct_noise.verify_host_information())
+    }
+
+    #[test]
+    fn dummy_current_signed_host_verification() {
+        let mut rng = rand_chacha::ChaCha20Rng::from_seed([0u8; 32]);
+        let ed22519 = ed25519::KeyPair::new(&mut rng);
+        let x25519_sphinx = x25519::KeyPair::new(&mut rng);
+        let x25519_noise = x25519::KeyPair::new(&mut rng);
+
+        let host_info_no_noise = HostInformation {
+            ip_address: vec!["1.1.1.1".parse().unwrap()],
+            hostname: Some("foomp.com".to_string()),
+            keys: crate::api::v1::node::models::HostKeys {
+                ed25519_identity: *ed22519.public_key(),
+                x25519_sphinx: *x25519_sphinx.public_key(),
+                x25519_noise: None,
+            },
+        };
+
+        let host_info_noise = crate::api::v1::node::models::HostInformation {
+            ip_address: vec!["1.1.1.1".parse().unwrap()],
+            hostname: Some("foomp.com".to_string()),
+            keys: crate::api::v1::node::models::HostKeys {
+                ed25519_identity: *ed22519.public_key(),
+                x25519_sphinx: *x25519_sphinx.public_key(),
+                x25519_noise: Some(VersionedNoiseKey {
+                    version: NoiseVersion::V1,
+                    x25519_pubkey: *x25519_noise.public_key(),
+                }),
+            },
+        };
+
+        // signature on legacy data
+        let current_struct_no_noise =
+            SignedData::new(host_info_no_noise, ed22519.private_key()).unwrap();
+        let current_struct_noise = SignedData::new(host_info_noise, ed22519.private_key()).unwrap();
+
+        assert!(current_struct_no_noise.verify(ed22519.public_key()));
         assert!(current_struct_no_noise.verify_host_information());
 
         // if noise key is present, the signature is actually valid

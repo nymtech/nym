951b7df5d73a4e0ddec6649fd36c099ad9b1462a
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-04-10 17:15:26 +0200

    Don't query rewards

--- nym-api/src/unstable_routes/account.rs
+++ nym-api/src/unstable_routes/account.rs
@@ -14,12 +14,12 @@ use axum::{
     routing::get,
     Json, Router,
 };
-use cosmwasm_std::Coin;
+use cosmwasm_std::{Coin, Decimal};
 use nym_topology::NodeId;
 use nym_validator_client::nyxd::AccountId;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, str::FromStr};
-use tracing::{error, instrument};
+use tracing::{error, instrument, warn};
 use utoipa::ToSchema;
 
 pub(crate) fn routes() -> Router<AppState> {
@@ -47,17 +47,25 @@ async fn address(
     Path(AddressQueryParam { address }): Path<AddressQueryParam>,
     State(state): State<AppState>,
 ) -> AxumResult<Json<NyxAccountDetails>> {
-    let address = AccountId::from_str(&address).map_err(|err| {
+    let account_id = AccountId::from_str(&address).map_err(|err| {
         error!("{err}");
-        AxumErrorResponse::not_found(address)
+        AxumErrorResponse::not_found(&address)
     })?;
+
+    let node_cache = state.nym_contract_cache();
+    let nym_nodes = node_cache
+        .nym_nodes()
+        .await
+        .into_iter()
+        .map(|node| (node.node_id(), node))
+        .collect::<HashMap<_, _>>();
 
     let state_client = state.nyxd_client;
     let mut total_value = 0u128;
 
     // 1. get balances of chain tokens
     let balances = state_client
-        .get_all_balances(&address)
+        .get_all_balances(&account_id)
         .await?
         .into_iter()
         .map(|c| {
@@ -69,43 +77,47 @@ async fn address(
         .collect::<Vec<Coin>>();
 
     // 2. get list of delegations (history)
-    let delegations: Vec<NyxAccountDelegationDetails> = state_client
-        .get_all_delegator_delegations(&address)
-        .await?
-        .into_iter()
+    let og_delegations = state_client
+        .get_all_delegator_delegations(&account_id)
+        .await?;
+    let delegations: Vec<NyxAccountDelegationDetails> = og_delegations
+        .iter()
         .map(|d| NyxAccountDelegationDetails {
-            delegated: d.amount,
+            delegated: d.amount.clone(),
             height: d.height,
             node_id: d.node_id,
-            proxy: d.proxy,
+            proxy: d.proxy.clone(),
         })
         .collect();
 
     // 3. get the current reward for each active delegation
+    // calculate rewards from nodes this delegator delegated to
     let mut rewards_map: HashMap<&NodeId, NyxAccountDelegationRewardDetails> = HashMap::new();
-    for d in &delegations {
-        if rewards_map.contains_key(&d.node_id) {
-            continue;
-        }
+    for delegation in og_delegations.iter() {
+        let node_id = &delegation.node_id;
 
-        if let Ok(r) = state_client
-            .get_pending_delegator_reward(
-                &address,
-                d.node_id,
-                d.proxy.clone().map(|d| d.to_string()),
-            )
-            .await
-        {
-            if let Some(rewards) = r.amount_earned {
-                rewards_map.insert(
-                    &d.node_id,
-                    NyxAccountDelegationRewardDetails {
-                        node_id: d.node_id,
-                        rewards,
-                        amount_staked: r.amount_staked.unwrap_or_default(),
-                        node_still_fully_bonded: r.node_still_fully_bonded,
-                    },
-                );
+        if let Some(nym_node_details) = nym_nodes.get(&node_id) {
+            match nym_node_details
+                .rewarding_details
+                .determine_delegation_reward(&delegation)
+            {
+                Ok(delegation_reward) => {
+                    rewards_map.insert(
+                        node_id,
+                        NyxAccountDelegationRewardDetails {
+                            node_id: delegation.node_id,
+                            rewards: decimal_to_coin(delegation_reward, "unym"),
+                            amount_staked: nym_node_details.original_pledge().clone(),
+                            node_still_fully_bonded: !nym_node_details.is_unbonding(),
+                        },
+                    );
+                }
+                Err(err) => {
+                    warn!(
+                        "Couldn't determine delegations for {} on node {}: {}",
+                        &account_id, node_id, err
+                    )
+                }
             }
         }
     }
@@ -123,8 +135,7 @@ async fn address(
         total_value += r.amount_staked.amount.u128();
     }
 
-    // 5. get vesting account details (if present)
-    // everyone has already fully vested
+    // 5. get vesting account details (if any): none because everyone has already fully vested
     let vesting_account: Option<NymVestingAccount> = None;
 
     if let Some(vesting_account) = vesting_account.clone() {
@@ -132,26 +143,33 @@ async fn address(
         total_value += vesting_account.spendable.amount.u128();
     }
 
-    // 6. get operator rewards
-    let operator_rewards: Option<Coin> = if let Ok(operator_rewards_res) =
-        state_client.get_pending_operator_reward(&address).await
-    {
-        if let Some(operator_reward_amount) = &operator_rewards_res.amount_earned {
-            total_value += operator_reward_amount.amount.u128();
-        }
+    // 6. get operator rewards: is this an operator?
+    let mut operator_rewards = 0;
 
-        operator_rewards_res.amount_earned
-    } else {
-        None
-    };
+    for (_, node_details) in nym_nodes.iter() {
+        if address == node_details.bond_information.owner.as_str() {
+            let pending_operator_reward = node_details.pending_operator_reward().amount.u128();
+
+            // add operator rewards
+            operator_rewards += pending_operator_reward;
+
+            // add to totals
+            total_value += pending_operator_reward;
+        }
+    }
 
     // 7. convert totals
     let claimable_rewards = Coin::new(claimable_rewards, "unym");
     let total_delegations = Coin::new(total_delegations, "unym");
     let total_value = Coin::new(total_value, "unym");
+    let operator_rewards = if operator_rewards > 0 {
+        Some(Coin::new(operator_rewards, "unym"))
+    } else {
+        None
+    };
 
     Ok(Json(NyxAccountDetails {
-        address: address.to_string(),
+        address: account_id.to_string(),
         balances,
         delegations,
         accumulated_rewards,
@@ -161,4 +179,29 @@ async fn address(
         vesting_account,
         operator_rewards,
     }))
+}
+
+fn decimal_to_coin(decimal: Decimal, denom: impl Into<String>) -> Coin {
+    Coin::new(decimal.to_uint_floor(), denom)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn decimal_to_coin_test() {
+        let test_values = [
+            (1234, 0, 1234),
+            (1234, 2, 12),
+            (1_234_000_000_000_000u128, 6, 1_234_000_000u128),
+        ];
+
+        for (amount, decimal_places, coin_amount) in test_values {
+            let decimal =
+                Decimal::from_atomics(cosmwasm_std::Uint128::new(amount), decimal_places).unwrap();
+            let coin_from_decimal = decimal_to_coin(decimal, "unym");
+            assert_eq!(coin_from_decimal, Coin::new(coin_amount, "unym"));
+        }
+    }
 }

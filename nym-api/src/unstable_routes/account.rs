4ad36b7f2a323d70757e9a81d0ba1422eb2e2efb
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-04-15 15:55:45 +0200

    Addressing PR feedback - not cloning nym nodes from cache - reduced number of nym nodes kept in memory - reduced number of iterations to read all data - removed some fields

--- nym-api/src/unstable_routes/account.rs
+++ nym-api/src/unstable_routes/account.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::{
@@ -15,10 +15,12 @@ use axum::{
     Json, Router,
 };
 use cosmwasm_std::{Coin, Decimal};
-use nym_topology::NodeId;
 use nym_validator_client::nyxd::AccountId;
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 use tracing::{error, instrument, warn};
 use utoipa::ToSchema;
 
@@ -51,48 +53,59 @@ async fn address(
         error!("{err}");
         AxumErrorResponse::not_found(&address)
     })?;
+    let base_denom = &state.network_details().network.chain_details.mix_denom.base;
 
-    let node_cache = state.nym_contract_cache();
-    let nym_nodes = node_cache
-        .nym_nodes()
-        .await
-        .into_iter()
-        .map(|node| (node.node_id(), node))
-        .collect::<HashMap<_, _>>();
-
-    let state_client = state.nyxd_client;
+    let state_client = &state.nyxd_client;
     let mut total_value = 0u128;
 
     // 1. get balances of chain tokens
-    let balances = state_client
-        .get_all_balances(&account_id)
+    let balance = state_client
+        .get_address_balance(&account_id, base_denom)
         .await?
-        .into_iter()
-        .map(|c| {
-            if c.denom == "unym" {
-                total_value += c.amount;
-            }
-            c.into()
-        })
-        .collect::<Vec<Coin>>();
+        .unwrap_or_else(|| nym_validator_client::nyxd::Coin::new(0u128, base_denom));
+    total_value += balance.amount;
 
     // 2. get list of delegations (history)
     let og_delegations = state_client
         .get_all_delegator_delegations(&account_id)
         .await?;
-    let delegations: Vec<NyxAccountDelegationDetails> = og_delegations
+
+    let delegated_to_nodes = og_delegations
         .iter()
-        .map(|d| NyxAccountDelegationDetails {
-            delegated: d.amount.clone(),
-            height: d.height,
-            node_id: d.node_id,
-            proxy: d.proxy.clone(),
+        .map(|d| d.node_id)
+        .collect::<HashSet<_>>();
+
+    let mut operator_rewards = 0;
+    let nym_nodes = state
+        .nym_contract_cache()
+        .all_cached_nym_nodes()
+        .await
+        .ok_or_else(AxumErrorResponse::service_unavailable)?
+        .iter()
+        .filter_map(|node_details| {
+            // is this an operator of this node?
+            if account_id.to_string() == node_details.bond_information.owner.as_str() {
+                let pending_operator_reward = node_details.pending_operator_reward().amount.u128();
+
+                // add operator rewards
+                operator_rewards += pending_operator_reward;
+
+                // add to totals
+                total_value += pending_operator_reward;
+            }
+            if delegated_to_nodes.contains(&node_details.node_id()) {
+                Some((node_details.node_id(), node_details.clone()))
+            } else {
+                None
+            }
         })
-        .collect();
+        .collect::<HashMap<_, _>>();
 
     // 3. get the current reward for each active delegation
     // calculate rewards from nodes this delegator delegated to
-    let mut rewards_map: HashMap<&NodeId, NyxAccountDelegationRewardDetails> = HashMap::new();
+    let mut claimable_rewards = 0u128;
+
+    let mut accumulated_rewards = Vec::new();
     for delegation in og_delegations.iter() {
         let node_id = &delegation.node_id;
 
@@ -102,15 +115,16 @@ async fn address(
                 .determine_delegation_reward(delegation)
             {
                 Ok(delegation_reward) => {
-                    rewards_map.insert(
-                        node_id,
-                        NyxAccountDelegationRewardDetails {
-                            node_id: delegation.node_id,
-                            rewards: decimal_to_coin(delegation_reward, "unym"),
-                            amount_staked: nym_node_details.original_pledge().clone(),
-                            node_still_fully_bonded: !nym_node_details.is_unbonding(),
-                        },
-                    );
+                    let reward = NyxAccountDelegationRewardDetails {
+                        node_id: delegation.node_id,
+                        rewards: decimal_to_coin(delegation_reward, base_denom),
+                        node_still_fully_bonded: !nym_node_details.is_unbonding(),
+                    };
+                    // 4. sum the rewards and delegations
+                    claimable_rewards += reward.rewards.amount.u128();
+                    total_value += reward.rewards.amount.u128();
+
+                    accumulated_rewards.push(reward);
                 }
                 Err(err) => {
                     warn!(
@@ -122,59 +136,37 @@ async fn address(
         }
     }
 
-    // 4. make the map of rewards into a vec and sum the rewards and delegations
-    let accumulated_rewards: Vec<NyxAccountDelegationRewardDetails> =
-        rewards_map.values().cloned().collect();
-
-    let mut claimable_rewards = 0u128;
-    let mut total_delegations = 0u128;
-    for r in &accumulated_rewards {
-        claimable_rewards += r.rewards.amount.u128();
-        total_delegations += r.amount_staked.amount.u128();
-        total_value += r.rewards.amount.u128();
-        total_value += r.amount_staked.amount.u128();
-    }
-
     // 5. get vesting account details (if any): none because everyone has already fully vested
     let vesting_account: Option<NymVestingAccount> = None;
 
-    if let Some(vesting_account) = vesting_account.clone() {
+    if let Some(vesting_account) = &vesting_account {
         total_value += vesting_account.locked.amount.u128();
         total_value += vesting_account.spendable.amount.u128();
     }
 
-    // 6. get operator rewards: is this an operator?
-    let mut operator_rewards = 0;
-
-    for (_, node_details) in nym_nodes.iter() {
-        if address == node_details.bond_information.owner.as_str() {
-            let pending_operator_reward = node_details.pending_operator_reward().amount.u128();
-
-            // add operator rewards
-            operator_rewards += pending_operator_reward;
-
-            // add to totals
-            total_value += pending_operator_reward;
-        }
-    }
-
-    // 7. convert totals
-    let claimable_rewards = Coin::new(claimable_rewards, "unym");
-    let total_delegations = Coin::new(total_delegations, "unym");
-    let total_value = Coin::new(total_value, "unym");
+    // 6. convert totals
+    let claimable_rewards = Coin::new(claimable_rewards, base_denom);
+    let total_value = Coin::new(total_value, base_denom);
     let operator_rewards = if operator_rewards > 0 {
-        Some(Coin::new(operator_rewards, "unym"))
+        Some(Coin::new(operator_rewards, base_denom))
     } else {
         None
     };
 
     Ok(Json(NyxAccountDetails {
         address: account_id.to_string(),
-        balances,
-        delegations,
+        balance: balance.into(),
+        delegations: og_delegations
+            .into_iter()
+            .map(|d| NyxAccountDelegationDetails {
+                delegated: d.amount,
+                height: d.height,
+                node_id: d.node_id,
+                proxy: d.proxy,
+            })
+            .collect(),
         accumulated_rewards,
         claimable_rewards,
-        total_delegations,
         total_value,
         vesting_account,
         operator_rewards,
--- nym-api/src/unstable_routes/models.rs
+++ nym-api/src/unstable_routes/models.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
 use cosmwasm_std::{Addr, Coin};
@@ -7,51 +7,54 @@ use serde::{Deserialize, Serialize};
 use utoipa::schema;
 
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
+#[schema(title = "Coin")]
+pub struct CoinSchema {
+    pub denom: String,
+    pub amount: u128,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
 pub struct NyxAccountDelegationDetails {
     pub node_id: NodeId,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub delegated: Coin,
     pub height: u64,
-    #[schema(value_type = String)]
+    #[schema(value_type = Option<String>)]
     pub proxy: Option<Addr>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
 pub struct NyxAccountDelegationRewardDetails {
     pub node_id: NodeId,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub rewards: Coin,
-    #[schema(value_type = String)]
-    pub amount_staked: Coin,
     pub node_still_fully_bonded: bool,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
 pub struct NymVestingAccount {
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub locked: Coin,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub vested: Coin,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub vesting: Coin,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub spendable: Coin,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
 pub struct NyxAccountDetails {
     pub address: String,
-    #[schema(value_type = Vec<String>)]
-    pub balances: Vec<Coin>,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
+    pub balance: Coin,
+    #[schema(value_type = CoinSchema)]
     pub total_value: Coin,
     pub delegations: Vec<NyxAccountDelegationDetails>,
     pub accumulated_rewards: Vec<NyxAccountDelegationRewardDetails>,
-    #[schema(value_type = String)]
-    pub total_delegations: Coin,
-    #[schema(value_type = String)]
+    #[schema(value_type = CoinSchema)]
     pub claimable_rewards: Coin,
     pub vesting_account: Option<NymVestingAccount>,
-    #[schema(value_type = Option<String>)]
+    #[schema(value_type = Option<CoinSchema>)]
     pub operator_rewards: Option<Coin>,
 }

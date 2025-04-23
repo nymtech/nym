518fe020810f682de3fdfcc9a594d73fd42b4a01
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-04-23 16:08:36 +0200

    WIP

--- nym-node-status-api/nym-node-status-api/src/http/api/nym_nodes.rs
+++ nym-node-status-api/nym-node-status-api/src/http/api/nym_nodes.rs
@@ -1,8 +1,11 @@
 use axum::{
-    extract::{Query, State},
+    extract::{Path, Query, State},
     Json, Router,
 };
+use nym_validator_client::client::NodeId;
+use serde::Deserialize;
 use tracing::instrument;
+use utoipa::IntoParams;
 
 use crate::http::{
     error::{HttpError, HttpResult},
@@ -12,7 +15,12 @@ use crate::http::{
 };
 
 pub(crate) fn routes() -> Router<AppState> {
-    Router::new().route("/", axum::routing::get(nym_nodes))
+    Router::new()
+        .route("/", axum::routing::get(nym_nodes))
+        .route(
+            "/:node_id/delegations",
+            axum::routing::get(node_delegations),
+        )
 }
 
 #[utoipa::path(
@@ -45,4 +53,44 @@ async fn nym_nodes(
         })?;
 
     Ok(Json(PagedResult::paginate(pagination, nodes)))
+}
+
+#[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Path)]
+struct NodeIdParam {
+    #[param(minimum = 0)]
+    node_id: NodeId,
+}
+
+#[utoipa::path(
+    tag = "Nym Nodes",
+    get,
+    params(
+        NodeIdParam
+    ),
+    path = "/{node_id}/delegations",
+    context_path = "/v3/nym-nodes",
+    responses(
+        (status = 200, body = PagedResult<ExtendedNymNode>)
+    )
+)]
+#[instrument(level = tracing::Level::DEBUG, skip(state))]
+async fn node_delegations(
+    Path(node_id): Path<NodeId>,
+    State(state): State<AppState>,
+) -> HttpResult<Json<PagedResult<ExtendedNymNode>>> {
+    let db = state.db_pool();
+    let node_geocache = state.node_geocache();
+
+    let nodes = state
+        .cache()
+        .get_nym_nodes_list(db, node_geocache)
+        .await
+        .map_err(|e| {
+            tracing::error!("{e}");
+            HttpError::internal()
+        })?;
+
+    todo!()
+    // Ok(Json())
 }

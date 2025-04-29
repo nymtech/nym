fbcf44eeb91145edc55e528c5945f71a83c1d9cf
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-04-29 13:23:14 +0200

    Add /account/{address} (#5673)
    * Add /account/{address}
    
    * Don't query vesting info
    
    * Don't query rewards
    
    * Remove unused code
    
    * Fix clippy
    
    * Fix build.rs build on Windows
    
    * Addressing PR feedback
    - not cloning nym nodes from cache
    - reduced number of nym nodes kept in memory
    - reduced number of iterations to read all data
    - removed some fields
    
    * Fix total_delegations
    
    * Optimize nym_nodes hashmap
    
    * Split flow into functions
    
    * Remove vesting info
    
    * Add caching for endpoint
    
    * Cache optimizations
    
    * Return early if balance is 0
    
    * Refactor state cloning shenanigans

--- nym-api/build.rs
+++ nym-api/build.rs
@@ -1,13 +1,20 @@
 use sqlx::{Connection, FromRow, SqliteConnection};
 use std::env;
 
+const SQLITE_DB_FILENAME: &str = "nym-api-example.sqlite";
+
 // it's fine if compilation fails
 #[allow(clippy::unwrap_used)]
 #[allow(clippy::expect_used)]
 #[tokio::main]
 async fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let database_path = format!("{}/nym-api-example.sqlite", out_dir);
+    let database_path = format!("{}/{}", out_dir, SQLITE_DB_FILENAME);
+
+    #[cfg(target_family = "unix")]
+    write_db_path_to_file(&out_dir, SQLITE_DB_FILENAME)
+        .await
+        .ok();
 
     let mut conn = SqliteConnection::connect(&format!("sqlite://{}?mode=rwc", database_path))
         .await
@@ -61,4 +68,33 @@ async fn main() {
     // for some strange reason we need to add a leading `/` to the windows path even though it's
     // not a valid windows path... but hey, it works...
     println!("cargo:rustc-env=DATABASE_URL=sqlite:///{}", &database_path);
+}
+
+/// use `./enter_db.sh` to inspect DB
+#[cfg(target_family = "unix")]
+async fn write_db_path_to_file(out_dir: &str, db_filename: &str) -> anyhow::Result<()> {
+    use std::os::unix::fs::PermissionsExt;
+    use tokio::{fs::File, io::AsyncWriteExt};
+
+    if env::var("CI").is_ok() {
+        return Ok(());
+    }
+    let mut file = File::create("settings.sql").await?;
+    let settings = ".mode columns
+.headers on";
+    file.write_all(settings.as_bytes()).await?;
+
+    let mut file = File::create("enter_db.sh").await?;
+    let contents = format!(
+        "#!/bin/sh\n\
+        sqlite3 -init settings.sql {}/{}",
+        out_dir, db_filename,
+    );
+    file.write_all(contents.as_bytes()).await?;
+
+    file.set_permissions(std::fs::Permissions::from_mode(0o755))
+        .await
+        .map_err(anyhow::Error::from)?;
+
+    Ok(())
 }
---
+++ nym-api/src/unstable_routes/models.rs
@@ -0,0 +1,51 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use cosmwasm_std::{Addr, Coin};
+use nym_topology::NodeId;
+use serde::{Deserialize, Serialize};
+use utoipa::schema;
+
+#[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
+#[schema(title = "Coin")]
+pub struct CoinSchema {
+    pub denom: String,
+    pub amount: u128,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
+pub struct NyxAccountDelegationDetails {
+    pub node_id: NodeId,
+    #[schema(value_type = CoinSchema)]
+    pub delegated: Coin,
+    pub height: u64,
+    #[schema(value_type = Option<String>)]
+    pub proxy: Option<Addr>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
+pub struct NyxAccountDelegationRewardDetails {
+    pub node_id: NodeId,
+    #[schema(value_type = CoinSchema)]
+    pub rewards: Coin,
+    #[schema(value_type = String)]
+    pub amount_staked: Coin,
+    pub node_still_fully_bonded: bool,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, utoipa::ToSchema, utoipa::ToResponse)]
+pub struct NyxAccountDetails {
+    pub address: String,
+    #[schema(value_type = CoinSchema)]
+    pub balance: Coin,
+    #[schema(value_type = CoinSchema)]
+    pub total_value: Coin,
+    pub delegations: Vec<NyxAccountDelegationDetails>,
+    pub accumulated_rewards: Vec<NyxAccountDelegationRewardDetails>,
+    #[schema(value_type = String)]
+    pub total_delegations: Coin,
+    #[schema(value_type = CoinSchema)]
+    pub claimable_rewards: Coin,
+    #[schema(value_type = Option<CoinSchema>)]
+    pub operator_rewards: Option<Coin>,
+}

21c3510b0fddb50722c66c7c5e12b4cdc5364ead
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-05-07 15:30:42 +0100

    introduce 'Bincode' variant for FormattedResponse

--- common/http-api-common/src/lib.rs
+++ common/http-api-common/src/lib.rs
@@ -1,9 +1,10 @@
-// Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
+// Copyright 2023-2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
 use axum::http::{header, HeaderValue, StatusCode};
 use axum::response::{IntoResponse, Response};
 use axum::Json;
+use bincode::Options;
 use bytes::{BufMut, BytesMut};
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +14,8 @@ pub mod middleware;
 pub enum FormattedResponse<T> {
     Json(Json<T>),
     Yaml(Yaml<T>),
+    #[cfg(feature = "bincode")]
+    Bincode(Bincode<T>),
 }
 
 impl<T> IntoResponse for FormattedResponse<T>
@@ -23,6 +26,8 @@ where
         match self {
             FormattedResponse::Json(json_response) => json_response.into_response(),
             FormattedResponse::Yaml(yaml_response) => yaml_response.into_response(),
+            #[cfg(feature = "bincode")]
+            FormattedResponse::Bincode(bincode_response) => bincode_response.into_response(),
         }
     }
 }
@@ -34,6 +39,8 @@ pub enum Output {
     #[default]
     Json,
     Yaml,
+    #[cfg(feature = "bincode")]
+    Bincode,
 }
 
 #[derive(Default, Debug, Serialize, Deserialize, Copy, Clone)]
@@ -48,6 +55,8 @@ impl Output {
         match self {
             Output::Json => FormattedResponse::Json(Json(data)),
             Output::Yaml => FormattedResponse::Yaml(Yaml(data)),
+            #[cfg(feature = "bincode")]
+            Output::Bincode => FormattedResponse::Bincode(Bincode(data)),
         }
     }
 }
@@ -74,6 +83,51 @@ where
                 [(
                     header::CONTENT_TYPE,
                     HeaderValue::from_static("application/yaml"),
+                )],
+                buf.into_inner().freeze(),
+            )
+                .into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_static(mime::TEXT_PLAIN_UTF_8.as_ref()),
+                )],
+                err.to_string(),
+            )
+                .into_response(),
+        }
+    }
+}
+
+// be explicit about those values because bincode uses different defaults in different places
+#[cfg(feature = "bincode")]
+pub fn make_bincode_serializer() -> impl bincode::Options {
+    use bincode::Options;
+    bincode::DefaultOptions::new()
+        .with_little_endian()
+        .with_varint_encoding()
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+#[must_use]
+#[cfg(feature = "bincode")]
+pub struct Bincode<T>(pub T);
+
+#[cfg(feature = "bincode")]
+impl<T> IntoResponse for Bincode<T>
+where
+    T: Serialize,
+{
+    // replicates axum's Json
+    fn into_response(self) -> Response {
+        let mut buf = BytesMut::with_capacity(128).writer();
+
+        match make_bincode_serializer().serialize_into(&mut buf, &self.0) {
+            Ok(()) => (
+                [(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_static("application/bincode"),
                 )],
                 buf.into_inner().freeze(),
             )

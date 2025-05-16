552f4a6572e3a966e02dbbf960df9597fe0e1739
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-05-16 11:05:13 +0100

    v2 nym-api endpoints to retrieve nodes with additional metadata information

--- common/http-api-common/src/response/yaml.rs
+++ common/http-api-common/src/response/yaml.rs
@@ -30,6 +30,10 @@ impl<T> Yaml<T> {
         self.0.headers.insert(name, value.into());
         self
     }
+
+    pub(crate) fn map<U, F: FnOnce(T) -> U>(self, op: F) -> Yaml<U> {
+        Yaml(self.0.map(op))
+    }
 }
 
 impl<T> IntoResponse for Yaml<T>

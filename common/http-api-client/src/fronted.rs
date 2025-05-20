8cf637e4bacab942f85b23809990d641aac91ed0
jmwample <jmwample@users.noreply.github.com>
2025-05-20 12:06:02 -0600

    updates to nym HTTP api client with multiple features relating to request domains

---
+++ common/http-api-client/src/fronted.rs
@@ -0,0 +1,208 @@
+// Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+//! Utilities for and implementation of request tunneling
+
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+
+use crate::ClientBuilder;
+
+use url::Url;
+
+// #[cfg(feature = "tunneling")]
+#[derive(Debug)]
+pub(crate) struct Front {
+    pub(crate) opts: FrontOptions,
+    pub(crate) fronts: Vec<Url>,
+
+    current_front_idx: AtomicUsize,
+    next_front_idx: AtomicUsize,
+    enabled: AtomicBool,
+}
+
+#[cfg(feature = "tunneling")]
+impl Clone for Front {
+    fn clone(&self) -> Self {
+        Self {
+            opts: self.opts.clone(),
+            fronts: self.fronts.clone(),
+            current_front_idx: AtomicUsize::new(self.current_front_idx.load(Ordering::Relaxed)),
+            next_front_idx: AtomicUsize::new(self.next_front_idx.load(Ordering::Relaxed)),
+            enabled: AtomicBool::new(self.enabled.load(Ordering::Relaxed)),
+        }
+    }
+}
+
+#[cfg(feature = "tunneling")]
+impl Front {
+    #[cfg(feature = "tunneling")]
+    pub(crate) fn host_str(&self) -> Option<&str> {
+        self.fronts
+            .get(self.current_front_idx.load(Ordering::Relaxed))
+            .and_then(|url| url.host_str())
+    }
+
+    #[cfg(feature = "tunneling")]
+    pub(crate) fn is_enabled(&self) -> bool {
+        match self.opts.policy {
+            FrontPolicy::Off => false,
+            FrontPolicy::OnRetry => self.enabled.load(Ordering::Relaxed),
+            FrontPolicy::Always => true,
+        }
+    }
+
+    // Used to indicate that the client hit an error that should trigger the retry policy
+    // to enable fronting.
+    pub(crate) fn retry_enable(&self) {
+        if self.is_enabled() {
+            return;
+        }
+        if matches!(self.opts.policy, FrontPolicy::OnRetry) {
+            self.enabled.store(true, Ordering::Relaxed);
+        }
+    }
+
+    #[cfg(feature = "tunneling")]
+    pub(crate) fn update_front(&self) {
+        match self.opts.strategy {
+            FrontUrlStrategy::RoundRobin => {
+                let current = self.next_front_idx.load(Ordering::Relaxed);
+                self.current_front_idx.store(current, Ordering::Relaxed);
+                let next = current + 1 % self.fronts.len();
+                self.next_front_idx.store(next, Ordering::Relaxed);
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct FrontOptions {
+    pub policy: FrontPolicy,
+    pub strategy: FrontUrlStrategy,
+}
+
+impl Default for FrontOptions {
+    fn default() -> Self {
+        Self {
+            policy: Default::default(),
+            strategy: Default::default(),
+        }
+    }
+}
+
+impl FrontOptions {
+    pub fn on_retry() -> Self {
+        Self {
+            policy: FrontPolicy::OnRetry,
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+#[cfg(feature = "tunneling")]
+pub enum FrontPolicy {
+    Always,
+    OnRetry,
+    #[default]
+    Off,
+}
+
+#[derive(Debug, Default, PartialEq, Clone)]
+#[cfg(feature = "tunneling")]
+pub enum FrontUrlStrategy {
+    #[default]
+    RoundRobin,
+}
+
+impl ClientBuilder {
+    /// Enable and configure request tunneling for API requests.
+    #[cfg(feature = "tunneling")]
+    pub fn with_fronting(mut self, fronts: Vec<Url>, opts: FrontOptions) -> Self {
+        let pre_enable = opts.policy == FrontPolicy::Always;
+        let front = Front {
+            opts,
+            fronts,
+            current_front_idx: AtomicUsize::new(0_usize),
+            next_front_idx: AtomicUsize::new(0_usize), // Fine to start as 0, as we update it immediately
+            enabled: AtomicBool::new(pre_enable),
+        };
+
+        front.update_front();
+
+        self.front = Some(front);
+
+        self
+    }
+
+    // pub fn enable(self) -> Self {
+    //     if self.front.is_none() {
+    //         return self;
+    //     }
+
+    //     // it should no be possible for this to be panic
+    //     let front = self.front.as_ref().unwrap();
+    //     front.enabled.store(true, Ordering::Relaxed);
+    //     self
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{ApiClientCore, NO_PARAMS};
+
+    // #[tokio::test]
+    // async fn vpn_api_works() {
+    //     let opts = FrontOptions::default();
+    //     let fronts = vec!["https://www.as.com".parse().unwrap()]; // fastly
+    //     // let fronts = vec!["https://www.cdn77.com".parse().unwrap()]; // cdn77??
+
+    //     let client = ClientBuilder::new::<&str, &str>("https://nymvpn.com")
+    //     // let client = ClientBuilder::new::<&str, &str>("https://1287599755.rsc.cdn77.org")
+    //         .expect("bad url")
+    //         .with_fronting(fronts, opts)
+    //         .build::<&str>()
+    //         .expect("failed to build client");
+
+    //     let response = client
+    //         .send_request::<(), &str, &str, &str>(
+    //             reqwest::Method::GET,
+    //             &["api", "public", "v1", "directory", "gateways"],
+    //             NO_PARAMS,
+    //             None,
+    //         )
+    //         .await
+    //         .expect("failed get request");
+
+    //     println!("{response:?}");
+    //     assert_eq!(response.status(), 200);
+    // }
+
+    #[tokio::test]
+    async fn nym_api_works() {
+        let opts = FrontOptions::default();
+        let fronts = vec!["https://yelp.global.ssl.fastly.net".parse().unwrap()]; // fastly
+                                                                                  // let fronts = vec!["https://cdn77.com".parse().unwrap()]; // cdn77
+
+        // let client = ClientBuilder::new::<&str, &str>("https://validator.nymtech.net")
+        let client = ClientBuilder::new::<&str, &str>("https://validator.global.ssl.fastly.net")
+            .expect("bad url")
+            .with_fronting(fronts, opts)
+            .build::<&str>()
+            .expect("failed to build client");
+
+        let response = client
+            .send_request::<(), &str, &str, &str>(
+                reqwest::Method::GET,
+                &["api", "v1", "network", "details"],
+                NO_PARAMS,
+                None,
+            )
+            .await
+            .expect("failed get request");
+
+        println!("{response:?}");
+        assert_eq!(response.status(), 200);
+    }
+}
---
+++ common/http-api-client/src/tests.rs
@@ -0,0 +1,109 @@
+use super::*;
+
+#[test]
+fn sanitizing_urls() {
+    let base_url: Url = "http://foomp.com".parse().unwrap();
+
+    // works with a full string
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, "/foo//bar/", NO_PARAMS).as_str()
+    );
+
+    // (and leading slash doesn't matter)
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, "foo//bar/", NO_PARAMS).as_str()
+    );
+
+    // works with 1 segment
+    assert_eq!(
+        "http://foomp.com/foo",
+        sanitize_url(&base_url, &["foo"], NO_PARAMS).as_str()
+    );
+
+    // works with 2 segments
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["foo", "bar"], NO_PARAMS).as_str()
+    );
+
+    // works with leading slash
+    assert_eq!(
+        "http://foomp.com/foo",
+        sanitize_url(&base_url, &["/foo"], NO_PARAMS).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["/foo", "bar"], NO_PARAMS).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["foo", "/bar"], NO_PARAMS).as_str()
+    );
+
+    // works with trailing slash
+    assert_eq!(
+        "http://foomp.com/foo",
+        sanitize_url(&base_url, &["foo/"], NO_PARAMS).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["foo/", "bar"], NO_PARAMS).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["foo", "bar/"], NO_PARAMS).as_str()
+    );
+
+    // works with both leading and trailing slash
+    assert_eq!(
+        "http://foomp.com/foo",
+        sanitize_url(&base_url, &["/foo/"], NO_PARAMS).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar",
+        sanitize_url(&base_url, &["/foo/", "/bar/"], NO_PARAMS).as_str()
+    );
+
+    // adds params
+    assert_eq!(
+        "http://foomp.com/foo/bar?foomp=baz",
+        sanitize_url(&base_url, &["foo", "bar"], &[("foomp", "baz")]).as_str()
+    );
+    assert_eq!(
+        "http://foomp.com/foo/bar?arg1=val1&arg2=val2",
+        sanitize_url(
+            &base_url,
+            &["/foo/", "/bar/"],
+            &[("arg1", "val1"), ("arg2", "val2")]
+        )
+        .as_str()
+    );
+}
+
+// - Do the retries work
+// - Do we use fallback urls on retry if multiple are provided
+// - Do we use the next front on retry if multiple are provided
+// - If we have more retries than urls, do we wrap back to the first one again
+// - on error without retries is where we have multiple urls, is the url updated?
+
+#[tokio::test]
+async fn api_client_retry() -> Result<(), Box<dyn std::error::Error>> {
+    let client =
+        ClientBuilder::new_with_urls(vec!["http://broken.nym.badurl".parse()?, "http://example.com/".parse()?])
+            .with_retries(3)
+            .build::<HttpClientError>()?;
+
+	let req = client.create_get_request(&["/"], NO_PARAMS);
+	let resp = client
+		.send::<HttpClientError>(req)
+		.await?;
+
+	assert_eq!(resp.status(), 200);
+
+	// check that the url was updated
+	assert_eq!(client.current_url().as_str(), "http://example.com/");
+
+	Ok(())
+}

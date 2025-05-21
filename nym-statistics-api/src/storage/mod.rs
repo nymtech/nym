be050c0587a8780feff75e97bd5107d49ad60d85
Simon Wicky <simon@nymtech.net>
2025-05-21 15:05:33 +0200

    change storage schema

--- nym-statistics-api/src/storage/mod.rs
+++ nym-statistics-api/src/storage/mod.rs
@@ -1,10 +1,9 @@
 use anyhow::{anyhow, Result};
-use nym_statistics_common::report::vpn_client::{
-    StaticInformationReport, UsageReport, VpnClientStatsReport,
-};
+use models::{ConnectionInfoDto, DailyActiveDeviceDto};
 use sqlx::{migrate::Migrator, postgres::PgConnectOptions};
-use std::{net::SocketAddr, str::FromStr};
-use time::{Date, OffsetDateTime};
+use std::str::FromStr;
+
+pub(crate) mod models;
 
 pub(crate) type DbPool = sqlx::PgPool;
 static MIGRATOR: Migrator = sqlx::migrate!("./migrations");
@@ -40,19 +39,12 @@ impl StatisticsStorage {
 
     pub(crate) async fn store_vpn_client_report(
         &mut self,
-        report: VpnClientStatsReport,
-        datetime: OffsetDateTime,
-        origin: SocketAddr,
+        active_device: DailyActiveDeviceDto,
+        connection_info: Option<ConnectionInfoDto>,
     ) -> Result<()> {
-        self.store_device(
-            report.stats_id.clone(),
-            &report.static_information,
-            datetime.date(),
-        )
-        .await?;
-        if let Some(usage_report) = report.basic_usage {
-            self.store_connection_stats(report.stats_id.clone(), origin, datetime, &usage_report)
-                .await?;
+        self.store_device(active_device).await?;
+        if let Some(connection_info) = connection_info {
+            self.store_connection_stats(connection_info).await?;
         }
         Ok(())
     }
@@ -60,12 +52,7 @@ impl StatisticsStorage {
     // Interestingly enough, because gateway-storage is using the `chrono` feature of sqlx and in 0.7.4 it takes priority over the `time` one, we cannot use the query! macro here.
     // Due to features unification, the binary will not compile when built from the workspace root because it will expect `chrono` types.
     // As a consequence, there is no compile time verification of these queries.
-    async fn store_device(
-        &self,
-        stats_id: String,
-        report: &StaticInformationReport,
-        day: Date,
-    ) -> Result<()> {
+    async fn store_device(&self, active_device: DailyActiveDeviceDto) -> Result<()> {
         sqlx::query(
             "INSERT INTO active_device (
                 day, 
@@ -73,43 +60,36 @@ impl StatisticsStorage {
                 os_type, 
                 os_version, 
                 architecture, 
-                app_version) 
-                VALUES ($1, $2, $3, $4, $5, $6)
+                app_version,
+                user_agent) 
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
             ON CONFLICT (device_id, day) DO NOTHING",
         )
-        .bind(day)
-        .bind(stats_id)
-        .bind(report.os_type.clone())
-        .bind(report.os_version.clone())
-        .bind(report.os_arch.clone())
-        .bind(report.app_version.clone())
+        .bind(active_device.day)
+        .bind(active_device.stats_id)
+        .bind(active_device.os_type)
+        .bind(active_device.os_version)
+        .bind(active_device.os_arch)
+        .bind(active_device.app_version)
+        .bind(active_device.user_agent)
         .execute(&self.connection_pool)
         .await?;
         Ok(())
     }
 
     // We're cannot use the query! macro because of the above comment
-    async fn store_connection_stats(
-        &self,
-        stats_id: String,
-        received_from: SocketAddr,
-        received_at: OffsetDateTime,
-        report: &UsageReport,
-    ) -> Result<()> {
+    async fn store_connection_stats(&self, connection_info: ConnectionInfoDto) -> Result<()> {
         sqlx::query(
             "INSERT INTO connection_stats (
                 received_at, 
-                device_id, 
                 connection_time_ms, 
                 two_hop, 
-                gateway_ip) VALUES ($1, $2, $3, $4, $5)
-                ON CONFLICT (device_id, received_at) DO NOTHING",
+                gateway_ip) VALUES ($1, $2, $3, $4)",
         )
-        .bind(received_at)
-        .bind(stats_id)
-        .bind(report.connection_time_ms)
-        .bind(report.two_hop)
-        .bind(received_from.to_string())
+        .bind(connection_info.received_at)
+        .bind(connection_info.connection_time_ms)
+        .bind(connection_info.two_hop)
+        .bind(connection_info.received_from)
         .execute(&self.connection_pool)
         .await?;
         Ok(())
---
+++ nym-statistics-api/src/storage/models.rs
@@ -0,0 +1,59 @@
+use std::net::SocketAddr;
+
+use axum_extra::headers::UserAgent;
+use nym_statistics_common::report::vpn_client::VpnClientStatsReport;
+use time::{Date, OffsetDateTime};
+
+pub type StatsId = String;
+
+#[derive(Debug, Clone, sqlx::FromRow)]
+pub(crate) struct DailyActiveDeviceDto {
+    pub(crate) day: Date,
+    pub(crate) stats_id: StatsId,
+    pub(crate) os_type: String,
+    pub(crate) os_version: Option<String>,
+    pub(crate) os_arch: String,
+    pub(crate) app_version: String,
+    pub(crate) user_agent: String,
+}
+
+impl DailyActiveDeviceDto {
+    pub(crate) fn new(
+        received_at: OffsetDateTime,
+        stats_report: &VpnClientStatsReport,
+        user_agent: UserAgent,
+    ) -> Self {
+        Self {
+            day: received_at.date(),
+            stats_id: stats_report.stats_id.clone(),
+            os_type: stats_report.static_information.os_type.clone(),
+            os_version: stats_report.static_information.os_version.clone(),
+            os_arch: stats_report.static_information.os_arch.clone(),
+            app_version: stats_report.static_information.app_version.clone(),
+            user_agent: user_agent.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, sqlx::FromRow)]
+pub(crate) struct ConnectionInfoDto {
+    pub(crate) received_at: OffsetDateTime,
+    pub(crate) received_from: String,
+    pub(crate) connection_time_ms: Option<i32>,
+    pub(crate) two_hop: bool,
+}
+
+impl ConnectionInfoDto {
+    pub(crate) fn maybe_new(
+        received_at: OffsetDateTime,
+        stats_report: &VpnClientStatsReport,
+        received_from: SocketAddr,
+    ) -> Option<Self> {
+        stats_report.basic_usage.as_ref().map(|usage_report| Self {
+            received_at,
+            received_from: received_from.ip().to_string(),
+            connection_time_ms: usage_report.connection_time_ms,
+            two_hop: usage_report.two_hop,
+        })
+    }
+}

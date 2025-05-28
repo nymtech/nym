8e1274b86e323c2de66407bc15169fcc7293d53f
Simon Wicky <simon@nymtech.net>
2025-05-28 08:57:47 +0200

    base stats api

---
+++ nym-statistics-api/migrations/001_init.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE active_device (
+    day          DATE NOT NULL,
+    device_id    TEXT NOT NULL,
+    os_type      TEXT,
+    os_version   TEXT,
+    architecture TEXT,
+    app_version  TEXT,
+    PRIMARY KEY (device_id, day)
+); 
+
+CREATE TABLE connection_stats (
+    received_at           TIMESTAMP WITH TIME ZONE  NOT NULL,
+    device_id             TEXT                      NOT NULL,
+    connection_time_ms    INTEGER,
+    two_hop               BOOLEAN,
+    gateway_ip            TEXT,
+    PRIMARY KEY (device_id, received_at)
+);
+
+
+CREATE INDEX idx_connection_stats_id ON connection_stats (device_id);
+CREATE INDEX idx_active_device_day ON active_device (day);
---
+++ nym-statistics-api/src/http/api/mod.rs
@@ -0,0 +1,74 @@
+use anyhow::anyhow;
+use axum::{response::Redirect, Router};
+use nym_http_api_common::middleware::logging::log_request_debug;
+use tokio::net::ToSocketAddrs;
+use tower_http::cors::CorsLayer;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::http::{server::HttpServer, state::AppState};
+
+pub(crate) mod stats;
+
+pub(crate) struct RouterBuilder {
+    unfinished_router: Router<AppState>,
+}
+
+impl RouterBuilder {
+    pub(crate) fn with_default_routes() -> Self {
+        let router = Router::new()
+            .merge(
+                SwaggerUi::new("/swagger")
+                    .url("/api-docs/openapi.json", super::api_docs::ApiDoc::openapi()),
+            )
+            .route(
+                "/",
+                axum::routing::get(|| async { Redirect::permanent("/swagger") }),
+            )
+            .nest("/v1", Router::new().nest("/stats", stats::routes()));
+
+        Self {
+            unfinished_router: router,
+        }
+    }
+
+    pub(crate) fn with_state(self, state: AppState) -> RouterWithState {
+        RouterWithState {
+            router: self.finalize_routes().with_state(state),
+        }
+    }
+
+    fn finalize_routes(self) -> Router<AppState> {
+        // layers added later wrap earlier layers
+        self.unfinished_router
+            // CORS layer needs to wrap other API layers
+            .layer(setup_cors())
+            // logger should be outermost layer
+            .layer(axum::middleware::from_fn(log_request_debug))
+    }
+}
+
+pub(crate) struct RouterWithState {
+    router: Router,
+}
+
+impl RouterWithState {
+    pub(crate) async fn build_server<A: ToSocketAddrs>(
+        self,
+        bind_address: A,
+    ) -> anyhow::Result<HttpServer> {
+        tokio::net::TcpListener::bind(bind_address)
+            .await
+            .map(|listener| HttpServer::new(self.router, listener))
+            .map_err(|err| anyhow!("Couldn't bind to address due to {}", err))
+    }
+}
+
+fn setup_cors() -> CorsLayer {
+    use axum::http::Method;
+    CorsLayer::new()
+        .allow_origin(tower_http::cors::Any)
+        .allow_methods([Method::POST, Method::GET, Method::PATCH, Method::OPTIONS])
+        .allow_headers(tower_http::cors::Any)
+        .allow_credentials(false)
+}

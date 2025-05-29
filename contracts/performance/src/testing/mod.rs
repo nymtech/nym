5dd641dfe6e737646e5f1d9542f98bf4112b3530
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-05-29 14:01:22 +0100

    shared code for contract testing

--- contracts/performance/src/testing/mod.rs
+++ contracts/performance/src/testing/mod.rs
@@ -1,4 +1,57 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-pub const TEST_DENOM: &str = "unym";
+use crate::contract::{execute, instantiate, migrate, query};
+use cosmwasm_std::Addr;
+use nym_contracts_common_testing::{
+    AdminExt, ContractFn, ContractTester, PermissionedFn, QueryFn, TestableNymContract,
+};
+use nym_performance_contract_common::constants::storage_keys;
+use nym_performance_contract_common::{
+    ExecuteMsg, InstantiateMsg, MigrateMsg, NymPerformanceContractError, QueryMsg,
+};
+
+pub struct PerformanceContract;
+
+impl TestableNymContract for PerformanceContract {
+    const NAME: &'static str = "performance-contract";
+    type InitMsg = InstantiateMsg;
+    type ExecuteMsg = ExecuteMsg;
+    type QueryMsg = QueryMsg;
+    type MigrateMsg = MigrateMsg;
+    type ContractError = NymPerformanceContractError;
+
+    fn instantiate() -> ContractFn<Self::InitMsg, Self::ContractError> {
+        instantiate
+    }
+
+    fn execute() -> ContractFn<Self::ExecuteMsg, Self::ContractError> {
+        execute
+    }
+
+    fn query() -> QueryFn<Self::QueryMsg, Self::ContractError> {
+        query
+    }
+
+    fn migrate() -> PermissionedFn<Self::MigrateMsg, Self::ContractError> {
+        migrate
+    }
+
+    fn base_init_msg() -> Self::InitMsg {
+        InstantiateMsg {}
+    }
+}
+
+pub fn init_contract_tester() -> ContractTester<PerformanceContract> {
+    PerformanceContract::init()
+}
+
+pub(crate) trait PerformanceContractTesterExt {
+    fn admin_unchecked(&self) -> Addr;
+}
+
+impl PerformanceContractTesterExt for ContractTester<PerformanceContract> {
+    fn admin_unchecked(&self) -> Addr {
+        <Self as AdminExt>::admin_unchecked(self, storage_keys::CONTRACT_ADMIN)
+    }
+}

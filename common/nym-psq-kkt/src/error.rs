0b22f959c595c963b0efe9fca088f4016c74f330
Georgio Nicolas <me@georgio.xyz>
2025-05-29 02:59:27 +0200

    PSQ poc

--- common/nym-psq-kkt/src/error.rs
+++ common/nym-psq-kkt/src/error.rs
@@ -1,7 +1,52 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
+use std::io;
 use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PSQError {
+    #[error("encountered a decryption error")]
+    DecryptionError,
+
+    #[error("encountered a KEM error")]
+    KEMError,
+
+    #[error("encountered a PSQ error")]
+    PSQError,
+
+    #[error("encountered an IO error: {0}")]
+    IoError(#[from] io::Error),
+
+    #[error("Incorrect state")]
+    IncorrectStateError,
+
+    #[error("Handshake did not complete")]
+    HandshakeError,
+
+    #[error("Unknown noise version")]
+    UnknownVersion,
+
+    #[error("Handshake timeout")]
+    HandshakeTimeout(#[from] tokio::time::error::Elapsed),
+}
+
+impl From<libcrux_kem::Error> for PSQError {
+    fn from(err: libcrux_kem::Error) -> Self {
+        match err {
+            // Error::Decrypt => PSQError::DecryptionError,
+            err => PSQError::KEMError,
+        }
+    }
+}
+impl From<libcrux_psq::Error> for PSQError {
+    fn from(err: libcrux_psq::Error) -> Self {
+        match err {
+            // Error::Decrypt => PSQError::DecryptionError,
+            err => PSQError::PSQError,
+        }
+    }
+}
 
 #[derive(Error, Debug)]
 pub enum KKTError {

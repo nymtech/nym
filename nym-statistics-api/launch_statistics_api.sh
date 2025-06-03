e311cf5ec2f4af0c8f49a4531360f549e7e03e56
Simon Wicky <simon@nymtech.net>
2025-06-03 09:55:36 +0200

    apply review comments

--- nym-statistics-api/launch_statistics_api.sh
+++ nym-statistics-api/launch_statistics_api.sh
@@ -8,7 +8,7 @@ user_rust_log_preference=$RUST_LOG
 function run_bare() {
     # export necessary env vars
     set -a
-    source .env.dev
+    source .env
     set +a
     export RUST_LOG=${user_rust_log_preference:-debug}
     echo "RUST_LOG=${RUST_LOG}"
@@ -16,4 +16,6 @@ function run_bare() {
     cargo run --package nym-statistics-api
 }
 
+
+# Requires pg_up.sh, or a running postres instance, with the correct parameters in an .env file
 run_bare

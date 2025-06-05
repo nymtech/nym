28b4fe7e7ecc4ed998535c42ec164db3b7dd5349
Yana <iana.matrosova@gmail.com>
2025-06-05 12:33:43 +0300

    add 10 recommended nodes

--- explorer-v2/src/app/constants.ts
+++ explorer-v2/src/app/constants.ts
@@ -1,3 +1,5 @@
 export const TABLET_WIDTH = "(min-width:700px)";
 
-export const RECOMMENDED_NODES = [587, 1048, 1863];
+export const RECOMMENDED_NODES = [
+  1362, 291, 1719, 1768, 1772, 1512, 896, 1415, 2114, 2010,
+];

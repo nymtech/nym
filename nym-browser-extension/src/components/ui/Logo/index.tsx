95cac8f786e26ef0125f07ddc4529c41b02a04fd
Tommy Verrall <tommyvez@protonmail.com>
2025-06-11 17:15:20 +0200

    updated browser extension piece
    - keep all the internal-dev wasm pieces as future examples
    - everything previously was going to be removed
    - shows functioning wasm interaction with the js

--- nym-browser-extension/src/components/ui/Logo/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { NymLogoBW } from '@nymproject/react/logo/NymLogoBW';
-
-export const Logo = ({ small }: { small?: boolean }) => (
-  <NymLogoBW width={small ? '37.5px' : '75px'} height={small ? '37.5px' : '75px'} />
-);

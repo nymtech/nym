154f86b21f647135e63a4c6a9d944eaaccc5315f
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-06-26 14:32:41 +0200

    Move stuff around

--- nym-node-status-api/nym-node-status-api/src/main.rs
+++ nym-node-status-api/nym-node-status-api/src/main.rs
@@ -9,7 +9,7 @@ mod cli;
 mod db;
 mod http;
 mod logging;
-mod mixnet_scraper;
+mod metrics_scraper;
 mod monitor;
 mod node_scraper;
 mod testruns;
@@ -35,7 +35,11 @@ async fn main() -> anyhow::Result<()> {
     let db_pool = storage.pool_owned();
 
     // Start the node scraper
-    let scraper = mixnet_scraper::Scraper::new(storage.pool_owned());
+    let scraper = node_scraper::DescriptionScraper::new(storage.pool_owned());
+    tokio::spawn(async move {
+        scraper.start().await;
+    });
+    let scraper = node_scraper::PacketScraper::new(storage.pool_owned());
     tokio::spawn(async move {
         scraper.start().await;
     });
@@ -74,7 +78,8 @@ async fn main() -> anyhow::Result<()> {
 
     let db_pool_scraper = storage.pool_owned();
     tokio::spawn(async move {
-        node_scraper::spawn_in_background(db_pool_scraper, args_clone.nym_api_client_timeout).await;
+        metrics_scraper::spawn_in_background(db_pool_scraper, args_clone.nym_api_client_timeout)
+            .await;
         tracing::info!("Started metrics scraper task");
     });
 

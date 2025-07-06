c4fec04fb084c3933cafafbee39f7e51080c2f2c
Mark Sinclair <mmsinclair@users.noreply.github.com>
2025-07-06 13:19:53 +0100

    wip

--- nyx-chain-watcher/sqlite/src/http/api_docs.rs
+++ nyx-chain-watcher/sqlite/src/http/api_docs.rs
@@ -4,7 +4,7 @@ use utoipauto::utoipauto;
 // manually import external structs which are behind feature flags because they
 // can't be automatically discovered
 // https://github.com/ProbablyClem/utoipauto/issues/13#issuecomment-1974911829
-#[utoipauto(paths = "./nyx-chain-watcher/src")]
+#[utoipauto(paths = "./nyx-chain-watcher/sqlite/src")]
 #[derive(OpenApi)]
 #[openapi(info(title = "Nyx Chain Watcher API"), tags(), components(schemas()))]
 pub(super) struct ApiDoc;

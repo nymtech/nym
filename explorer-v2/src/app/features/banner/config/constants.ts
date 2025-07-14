9f920e1e9c4525821838fc262bd5c293add579e0
Yana <iana.matrosova@gmail.com>
2025-07-14 16:31:36 +0300

    banner

---
+++ explorer-v2/src/app/features/banner/config/constants.ts
@@ -0,0 +1,1 @@
+export const bannerApiPath = "/explorer-banner";
--- explorer-v2/src/components/header/index.tsx
+++ explorer-v2/src/components/header/index.tsx
@@ -1,12 +1,18 @@
+import { Box } from "@mui/material";
 import { DesktopHeader } from "./DesktopHeader";
 import { MobileHeader } from "./MobileHeader";
 
 export const Header = async () => {
   return (
-    <header>
+    <Box
+      component="header"
+      sx={{
+        backgroundColor: "background.default",
+      }}
+    >
       <DesktopHeader />
       <MobileHeader />
       {/* Mobile header will go here */}
-    </header>
+    </Box>
   );
 };

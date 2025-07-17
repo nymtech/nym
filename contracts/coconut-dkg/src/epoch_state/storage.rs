e206484a3cf99351ebad44a694feba5fbca3d01b
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-07-17 12:46:00 +0100

    define storage item for holding historical DKG state

--- contracts/coconut-dkg/src/epoch_state/storage.rs
+++ contracts/coconut-dkg/src/epoch_state/storage.rs
@@ -1,11 +1,18 @@
 // Copyright 2022 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use cw_storage_plus::{Item, Map};
+use cw_storage_plus::{Item, Map, SnapshotItem, Strategy};
 use nym_coconut_dkg_common::types::{Epoch, EpochId};
 
 pub(crate) const CURRENT_EPOCH: Item<Epoch> = Item::new("current_epoch");
+pub const HISTORICAL_EPOCH: SnapshotItem<Epoch> = SnapshotItem::new(
+    "historical_epoch",
+    "historical_epoch__checkpoints",
+    "historical_epoch__changelog",
+    Strategy::EveryBlock,
+);
 
 pub const THRESHOLD: Item<u64> = Item::new("threshold");
 
 pub const EPOCH_THRESHOLDS: Map<EpochId, u64> = Map::new("epoch_thresholds");
+

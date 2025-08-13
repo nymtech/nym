979485c5826157b2bfeeec2c454645e1d9c6aba5
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-08-13 12:52:16 +0100

    http api client adjustment (#5953)
    * missing feature lock for attempting to clone client
    
    * added helper macro to generate user agent without additional imports

--- common/http-api-client/src/lib.rs
+++ common/http-api-client/src/lib.rs
@@ -173,6 +173,10 @@ mod path;
 #[cfg(not(target_arch = "wasm32"))]
 pub use dns::{HickoryDnsError, HickoryDnsResolver};
 
+// helper for generating user agent based on binary information
+#[doc(hidden)]
+pub use nym_bin_common::bin_info;
+
 /// Default HTTP request connection timeout.
 ///
 /// The timeout is relatively high as we are often making requests over the mixnet, where latency is
@@ -608,6 +612,7 @@ impl Client {
             current_idx: Arc::new(Default::default()),
             reqwest_client: self.reqwest_client.clone(),
 
+            #[cfg(feature = "tunneling")]
             front: self.front.clone(),
             retry_limit: self.retry_limit,
 

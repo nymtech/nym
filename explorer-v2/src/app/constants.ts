0003bf1a8bc3e2851dfb5a95599d7da1f08c6374
serinko <97586125+serinko@users.noreply.github.com>
2025-09-05 17:47:23 +0200

    initialise attempt to list 10 top nodes automatically

--- explorer-v2/src/app/constants.ts
+++ explorer-v2/src/app/constants.ts
@@ -1,1 +1,7 @@
 export const TABLET_WIDTH = "(min-width:700px)";
+
+// import auto-picking function
+import { fetchRecommendedNodes } from "./lib/recommended";
+
+// export a promise that resolves to number[]
+export const RECOMMENDED_NODES_PROMISE = fetchRecommendedNodes();

cdfec82c0782048b5ddeb97c72b6fdbb0e1086d7
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-09-05 17:53:21 +0100

    linux-specific fixes to IpPacketRouter

--- common/wireguard-private-metadata/server/src/http/mod.rs
+++ common/wireguard-private-metadata/server/src/http/mod.rs
@@ -1,12 +1,9 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use std::sync::Arc;
-
-use tokio::task::JoinHandle;
-use tokio_util::sync::CancellationToken;
-
 use nym_wireguard::WgApiWrapper;
+use std::sync::Arc;
+use tokio::task::JoinHandle;
 
 pub(crate) mod openapi;
 pub(crate) mod router;
@@ -20,7 +17,6 @@ pub(crate) mod state;
 /// AFTER you have shut down BG tasks (or past their grace period).
 #[allow(unused)]
 pub struct ShutdownHandles {
-    axum_shutdown_button: CancellationToken,
     /// Tokio JoinHandle for axum server's task
     axum_join_handle: AxumJoinHandle,
     /// Wireguard API for kernel interactions
@@ -30,13 +26,8 @@ pub struct ShutdownHandles {
 impl ShutdownHandles {
     /// Cancellation token is given to Axum server constructor. When the token
     /// receives a shutdown signal, Axum server will shut down gracefully.
-    pub fn new(
-        axum_join_handle: AxumJoinHandle,
-        wg_api: Arc<WgApiWrapper>,
-        axum_shutdown_button: CancellationToken,
-    ) -> Self {
+    pub fn new(axum_join_handle: AxumJoinHandle, wg_api: Arc<WgApiWrapper>) -> Self {
         Self {
-            axum_shutdown_button,
             axum_join_handle,
             wg_api,
         }
--- service-providers/ip-packet-router/src/cli/run.rs
+++ service-providers/ip-packet-router/src/cli/run.rs
@@ -28,7 +28,7 @@ pub(crate) async fn execute(args: &Run) -> Result<(), IpPacketRouterError> {
     log::debug!("Using config: {config:#?}");
 
     log::info!("Starting ip packet router service provider");
-    let mut shutdown_manager = ShutdownManager::build_new_default?;
+    let mut shutdown_manager = ShutdownManager::build_new_default()?;
     let mut server = nym_ip_packet_router::IpPacketRouter::new(
         config,
         shutdown_manager.shutdown_tracker_owned(),

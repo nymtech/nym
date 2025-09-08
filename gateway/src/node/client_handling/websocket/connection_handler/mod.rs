6ba20e7f5449874a41de7b13f4df957750812481
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2025-09-08 15:34:30 +0200

    context extraction

--- gateway/src/node/client_handling/websocket/connection_handler/mod.rs
+++ gateway/src/node/client_handling/websocket/connection_handler/mod.rs
@@ -2,16 +2,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use crate::config::Config;
+use nym_bin_common::opentelemetry::context::ContextCarrier;
 use nym_credential_verification::BandwidthFlushingBehaviourConfig;
 use nym_gateway_requests::shared_key::SharedGatewayKey;
 use nym_gateway_requests::ServerResponse;
 use nym_sphinx::DestinationAddressBytes;
+use opentelemetry::propagation::TextMapPropagator;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 use rand::{CryptoRng, Rng};
 use std::time::Duration;
 use time::OffsetDateTime;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_tungstenite::WebSocketStream;
 use tracing::{debug, instrument, trace, warn};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 
 pub(crate) use self::authenticated::AuthenticatedHandler;
 pub(crate) use self::fresh::FreshHandler;

7d087996b45971efcd198a28341d1cd1845b43a9
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2025-09-08 18:27:15 +0200

    wip

--- gateway/src/node/client_handling/websocket/connection_handler/fresh.rs
+++ gateway/src/node/client_handling/websocket/connection_handler/fresh.rs
@@ -40,6 +40,7 @@ use opentelemetry::Context;
 use opentelemetry_sdk::logs::TraceContext;
 use opentelemetry_sdk::trace::{IdGenerator, RandomIdGenerator};
 use opentelemetry::propagation::TextMapPropagator;
+use opentelemetry::trace::TraceContextExt;
 use opentelemetry_sdk::propagation::TraceContextPropagator;
 use rand::CryptoRng;
 use std::collections::HashMap;
@@ -909,6 +910,9 @@ impl<R, S> FreshHandler<R, S> {
         info!("handle_initial_client_request: Extracted trace id after child_span: {:?}", extracted_trace_id);
         let gw_carrier = carrier.into_map();
 
+        let context = opentelemetry::Context::current();
+        let trace_id = context.span().span_context().trace_id();
+        error!("=== [DEBUG SPAN] Current trace id in handle_initial_client_request: {:?}", trace_id);
 
         let auth_result = match request {
             ClientControlRequest::Authenticate {
@@ -1003,12 +1007,10 @@ impl<R, S> FreshHandler<R, S> {
                 let new_carrier  = ContextCarrier::from_map(context_carrier);
                 let propagator = TraceContextPropagator::new();
                 let extracted_context = propagator.extract(&new_carrier);
-                let extracted_trace_id = new_carrier.extract_trace_id();
-                error!("Extracted trace id: {:?}", extracted_trace_id);
                 tracing::Span::current().set_parent(extracted_context);
                 let span = tracing::info_span!("handle_authenticated_client");
                 let _entered_span = span.enter();
-                warn!("==== Context propagation successful to handle until authenticated client ====");
+                warn!("==== Context propagation successful to handle until authenticated client handler ====");
 
                 let (mix_sender, mix_receiver) = mpsc::unbounded();
                 // Channel for handlers to ask other handlers if they are still active.

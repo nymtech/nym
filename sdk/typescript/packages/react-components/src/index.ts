e5dfd9ad02ac0c8567af7eb432ce12816f26e9e8
benedettadavico <benedetta.davico@gmail.com>
2025-09-15 12:29:00 +0200

    .

--- sdk/typescript/packages/react-components/src/index.ts
+++ sdk/typescript/packages/react-components/src/index.ts
@@ -1,1 +1,4 @@
-// no-op
+import '@nymproject/mui-theme';
+
+export { CurrencyFormField } from './components/currency/CurrencyFormField';
+export { IdentityKeyFormField } from './components/mixnodes/IdentityKeyFormField';

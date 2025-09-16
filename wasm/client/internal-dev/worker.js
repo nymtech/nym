66f614c6416b75ef8dac97da19bde6952375d1fb
mfahampshire <maxhampshire@pm.me>
2025-09-16 11:54:58 +0200

    a little cleanup in worker file

--- wasm/client/internal-dev/worker.js
+++ wasm/client/internal-dev/worker.js
@@ -212,18 +212,6 @@ async function normalNymClientUsage() {
     nymApiUrl: validator,
   });
 
-  // try {
-  //   let localClient = await NymClient.newWithConfig(
-  //     config,
-  //     onMessageHandler,
-  //     {}
-  //   );
-  //   console.log(">>>>>>>> POST PROMISE");
-  //   // console.log(localClient.selfAddress);
-  // } catch (e) {
-  //   console.log("local client creation error: ", e);
-  // }
-
   let localClient;
   try {
     localClient = await NymClient.newWithConfig(config, onMessageHandler, {});
@@ -281,17 +269,3 @@ async function main() {
 
 // Let's get started!
 main();
-
-// Copyright 2020-2023 Nym Technologies SA
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.

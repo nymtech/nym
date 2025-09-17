953d7d22474a8679d53a2654821d1e9b901e4e5e
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2025-09-17 16:12:25 +0200

    Improve run scripts

--- nym-node-status-api/nym-node-status-api/build.rs
+++ nym-node-status-api/nym-node-status-api/build.rs
@@ -5,8 +5,22 @@ use anyhow::Result;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     #[cfg(feature = "pg")]
-    if let Ok(database_url) = std::env::var("DATABASE_URL") {
+    let database_url = if let Ok(database_url) = std::env::var("DATABASE_URL") {
         println!("cargo::rustc-env=DATABASE_URL={database_url}");
+        database_url
+    } else {
+        let database_url = "postgres://testuser:testpass@localhost:5433/nym_node_status_api_test";
+        println!("cargo::rustc-env=DATABASE_URL={database_url}");
+        database_url.to_string()
+    };
+
+    #[cfg(feature = "pg")]
+    {
+        // run migrations at build time to allow sqlx to fetch up-to-date DB schema
+        let pool = sqlx::postgres::PgPool::connect(&database_url).await?;
+        sqlx::migrate!("./migrations_pg").run(&pool).await?;
+        pool.close().await;
+        println!("cargo::rerun-if-changed=migrations");
     }
 
     Ok(())

0b4bb013eaf1413a7bfabb9d8ebbbdadd649808d
Mark Sinclair <14054343+mmsinclair@users.noreply.github.com>
2025-09-17 18:32:24 +0100

    Update entrypoint.sh

--- nym-node-status-api/nym-node-status-agent/entrypoint.sh
+++ nym-node-status-api/nym-node-status-agent/entrypoint.sh
@@ -7,6 +7,6 @@ trap "echo 'Stopping...'; exit 0" SIGTERM
 
 # Run probe in an infinite loop
 while true; do
-    /nym/nym-node-status-agent run-probe
+    /nym/nym-node-status-agent run-probe --server "${NODE_STATUS_AGENT_SERVER_ADDRESS}|${NODE_STATUS_AGENT_SERVER_PORT}" --mnemonic "${NYM_NODE_MNEMONICS}"
     sleep "$SLEEP_TIME"
 done

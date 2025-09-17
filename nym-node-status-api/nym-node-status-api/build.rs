1a1fc1e426881ccc61b27224833b795621199c85
Mark Sinclair <mmsinclair@users.noreply.github.com>
2025-09-17 16:55:30 +0100

    Fix build.rs

--- nym-node-status-api/nym-node-status-api/build.rs
+++ nym-node-status-api/nym-node-status-api/build.rs
@@ -4,16 +4,8 @@ use anyhow::Result;
 /// cargo clean -p nym-node-status-api
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let database_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
-        String::from("postgres://testuser:testpass@localhost:5433/nym_node_status_api_test")
-    });
-    println!("cargo::rustc-env=DATABASE_URL={database_url}");
-
-    // run migrations at build time to allow sqlx to fetch up-to-date DB schema
-    let pool = sqlx::postgres::PgPool::connect(&database_url).await?;
-    sqlx::migrate!("./migrations_pg").run(&pool).await?;
-    pool.close().await;
-    println!("cargo::rerun-if-changed=migrations");
-
+    if let Ok(database_url) = std::env::var("DATABASE_URL") {
+        println!("cargo::rustc-env=DATABASE_URL={database_url}");
+    }
     Ok(())
 }

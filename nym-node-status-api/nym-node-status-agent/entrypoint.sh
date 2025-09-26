97b8dd15717f935928f22cd3c6b8c3fa4492dbb5
benedettadavico <benedetta.davico@gmail.com>
2025-09-26 11:50:22 +0200

    typo

--- nym-node-status-api/nym-node-status-agent/entrypoint.sh
+++ nym-node-status-api/nym-node-status-agent/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Starting agent loop with sleep interval: ${SLEEP_TIME}s"
 # Trap SIGTERM to allow graceful shutdown
 trap "echo 'Stopping...'; exit 0" SIGTERM
 
-DEFAULT_ARGS="run-probe --server \"${NODE_STATUS_AGENT_SERVER_ADDRESS}|${NODE_STATUS_AGENT_SERVER_PORT}\" 
+DEFAULT_ARGS="run-probe --server \"${NODE_STATUS_AGENT_SERVER_ADDRESS}|${NODE_STATUS_AGENT_SERVER_PORT}\""
 COMMAND="/nym/nym-node-status-agent ${ARGS}"
 
 echo "default_args = '${DEFAULT_ARGS}'"

72e8180abe8e87dca0a45d3c590d9383c20142ca
mfahampshire <maxhampshire@pm.me>
2025-09-26 17:25:56 +0200

    TLS first version

--- smolmix/src/error.rs
+++ smolmix/src/error.rs
@@ -13,4 +13,16 @@ pub enum SmolmixError {
 
     #[error("Nym SDK error: {0}")]
     NymSdk(#[from] nym_sdk::Error),
+
+    #[error("TLS handshake failed")]
+    TlsHandshakeFailed,
+
+    #[error("TLS encrypt/decrypt error")]
+    TlsCrypto,
+
+    #[error("DNS err placeholder")]
+    InvalidDnsName,
+
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
 }

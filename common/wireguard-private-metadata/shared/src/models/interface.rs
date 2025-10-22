bc40a4607c3161c0f9b97d82163ee158fb96a422
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2025-10-22 08:51:49 +0100

    v2 wireguard private metadata

--- common/wireguard-private-metadata/shared/src/models/interface.rs
+++ common/wireguard-private-metadata/shared/src/models/interface.rs
@@ -1,25 +1,25 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use nym_credentials_interface::CredentialSpendingData;
+use nym_credentials_interface::BandwidthCredential;
 
 #[cfg(feature = "testing")]
 use crate::models::v0;
-use crate::models::{Construct, Extract, Request, Response, Version, v1};
+use crate::models::{Construct, Extract, Request, Response, Version, v1, v2};
 
 pub enum RequestData {
-    AvailableBandwidth(()),
-    TopUpBandwidth(Box<CredentialSpendingData>),
+    AvailableBandwidth,
+    TopUpBandwidth {
+        credential: Box<BandwidthCredential>,
+    },
 }
 
 impl From<super::latest::interface::RequestData> for RequestData {
     fn from(value: super::latest::interface::RequestData) -> Self {
         match value {
-            super::latest::interface::RequestData::AvailableBandwidth(inner) => {
-                Self::AvailableBandwidth(inner)
-            }
-            super::latest::interface::RequestData::TopUpBandwidth(credential_spending_data) => {
-                Self::TopUpBandwidth(credential_spending_data)
+            super::latest::interface::RequestData::AvailableBandwidth => Self::AvailableBandwidth,
+            super::latest::interface::RequestData::TopUpBandwidth { credential } => {
+                Self::TopUpBandwidth { credential }
             }
         }
     }
@@ -28,10 +28,8 @@ impl From<super::latest::interface::RequestData> for RequestData {
 impl From<RequestData> for super::latest::interface::RequestData {
     fn from(value: RequestData) -> Self {
         match value {
-            RequestData::AvailableBandwidth(inner) => Self::AvailableBandwidth(inner),
-            RequestData::TopUpBandwidth(credential_spending_data) => {
-                Self::TopUpBandwidth(credential_spending_data)
-            }
+            RequestData::AvailableBandwidth => Self::AvailableBandwidth,
+            RequestData::TopUpBandwidth { credential } => Self::TopUpBandwidth { credential },
         }
     }
 }
@@ -39,12 +37,18 @@ impl From<RequestData> for super::latest::interface::RequestData {
 impl From<super::latest::interface::ResponseData> for ResponseData {
     fn from(value: super::latest::interface::ResponseData) -> Self {
         match value {
-            super::latest::interface::ResponseData::AvailableBandwidth(inner) => {
-                Self::AvailableBandwidth(inner)
-            }
-            super::latest::interface::ResponseData::TopUpBandwidth(credential_spending_data) => {
-                Self::TopUpBandwidth(credential_spending_data)
-            }
+            super::latest::interface::ResponseData::AvailableBandwidth {
+                amount,
+                upgrade_mode,
+            } => Self::AvailableBandwidth {
+                amount,
+                upgrade_mode,
+            },
+            super::latest::interface::ResponseData::TopUpBandwidth {
+                available_bandwidth,
+            } => Self::TopUpBandwidth {
+                available_bandwidth,
+            },
         }
     }
 }
@@ -52,10 +56,18 @@ impl From<super::latest::interface::ResponseData> for ResponseData {
 impl From<ResponseData> for super::latest::interface::ResponseData {
     fn from(value: ResponseData) -> Self {
         match value {
-            ResponseData::AvailableBandwidth(inner) => Self::AvailableBandwidth(inner),
-            ResponseData::TopUpBandwidth(credential_spending_data) => {
-                Self::TopUpBandwidth(credential_spending_data)
-            }
+            ResponseData::AvailableBandwidth {
+                amount,
+                upgrade_mode,
+            } => Self::AvailableBandwidth {
+                amount,
+                upgrade_mode,
+            },
+            ResponseData::TopUpBandwidth {
+                available_bandwidth,
+            } => Self::TopUpBandwidth {
+                available_bandwidth,
+            },
         }
     }
 }
@@ -65,13 +77,26 @@ impl Construct<RequestData> for Request {
         match version {
             #[cfg(feature = "testing")]
             Version::V0 => {
-                let translate_info = super::latest::interface::RequestData::from(info);
-                let downgrade_info = v0::interface::RequestData::try_from(translate_info)?;
-                let versioned_request = v0::VersionedRequest::construct(downgrade_info, version)?;
+                // attempt to go through conversion chain for `info`: v2 => v1 => v0
+                let v2_info = v2::interface::RequestData::from(info);
+                let v1_info = v1::interface::RequestData::try_from(v2_info)?;
+                let v0_info = v0::interface::RequestData::try_from(v1_info)?;
+
+                let versioned_request = v0::VersionedRequest::construct(v0_info, version)?;
                 Ok(versioned_request.try_into()?)
             }
             Version::V1 => {
-                let versioned_request = v1::VersionedRequest::construct(info.into(), version)?;
+                // attempt to go through conversion chain for `info`: v2 => v1
+                let v2_info = v2::interface::RequestData::from(info);
+                let v1_info = v1::interface::RequestData::try_from(v2_info)?;
+
+                let versioned_request = v1::VersionedRequest::construct(v1_info, version)?;
+                Ok(versioned_request.try_into()?)
+            }
+            Version::V2 => {
+                let v2_info = v2::interface::RequestData::from(info);
+
+                let versioned_request = v2::VersionedRequest::construct(v2_info, version)?;
                 Ok(versioned_request.try_into()?)
             }
         }
@@ -84,24 +109,36 @@ impl Extract<RequestData> for Request {
             #[cfg(feature = "testing")]
             super::Version::V0 => {
                 let versioned_request = v0::VersionedRequest::try_from(self.clone())?;
-                let (request, version) = versioned_request.extract()?;
+                let (extracted_v0_info, version) = versioned_request.extract()?;
 
-                let upgrade_request = super::latest::interface::RequestData::try_from(request)?;
+                let v1_info = v1::interface::RequestData::try_from(extracted_v0_info)?;
+                let v2_info = v2::interface::RequestData::try_from(v1_info)?;
 
-                Ok((upgrade_request.into(), version))
+                let request_data = RequestData::from(v2_info);
+                Ok((request_data, version))
             }
             super::Version::V1 => {
-                let versioned_request = v1::VersionedRequest::try_from(self.clone())?;
-                let (extracted, version) = versioned_request.extract()?;
-                Ok((extracted.into(), version))
+                let versioned_request = v1::VersionedRequest::try_from(self)?;
+                let (extracted_v1_info, version) = versioned_request.extract()?;
+                let v2_info = v2::interface::RequestData::try_from(extracted_v1_info)?;
+
+                let request_data = RequestData::from(v2_info);
+                Ok((request_data, version))
+            }
+            super::Version::V2 => {
+                let versioned_request = v2::VersionedRequest::try_from(self)?;
+                let (extracted_v2_info, version) = versioned_request.extract()?;
+
+                let request_data = RequestData::from(extracted_v2_info);
+                Ok((request_data, version))
             }
         }
     }
 }
 
 pub enum ResponseData {
-    AvailableBandwidth(i64),
-    TopUpBandwidth(i64),
+    AvailableBandwidth { amount: i64, upgrade_mode: bool },
+    TopUpBandwidth { available_bandwidth: i64 },
 }
 
 impl Construct<ResponseData> for Response {
@@ -109,14 +146,26 @@ impl Construct<ResponseData> for Response {
         match version {
             #[cfg(feature = "testing")]
             super::Version::V0 => {
-                let translate_response = super::latest::interface::ResponseData::from(info);
-                let downgrade_response = v0::interface::ResponseData::try_from(translate_response)?;
-                let versioned_response =
-                    v0::VersionedResponse::construct(downgrade_response, version)?;
+                // attempt to go through conversion chain for `info`: v2 => v1 => v0
+                let v2_info = v2::interface::ResponseData::from(info);
+                let v1_info = v1::interface::ResponseData::try_from(v2_info)?;
+                let v0_info = v0::interface::ResponseData::try_from(v1_info)?;
+
+                let versioned_response = v0::VersionedResponse::construct(v0_info, version)?;
                 Ok(versioned_response.try_into()?)
             }
             Version::V1 => {
-                let versioned_response = v1::VersionedResponse::construct(info.into(), version)?;
+                // attempt to go through conversion chain for `info`: v2 => v1
+                let v2_info = v2::interface::ResponseData::from(info);
+                let v1_info = v1::interface::ResponseData::try_from(v2_info)?;
+
+                let versioned_response = v1::VersionedResponse::construct(v1_info, version)?;
+                Ok(versioned_response.try_into()?)
+            }
+            Version::V2 => {
+                let v2_info = v2::interface::ResponseData::from(info);
+
+                let versioned_response = v2::VersionedResponse::construct(v2_info, version)?;
                 Ok(versioned_response.try_into()?)
             }
         }
@@ -129,16 +178,27 @@ impl Extract<ResponseData> for Response {
             #[cfg(feature = "testing")]
             super::Version::V0 => {
                 let versioned_response = v0::VersionedResponse::try_from(self.clone())?;
-                let (response, version) = versioned_response.extract()?;
+                let (extracted_v0_info, version) = versioned_response.extract()?;
+                let v1_info = v1::interface::ResponseData::try_from(extracted_v0_info)?;
+                let v2_info = v2::interface::ResponseData::try_from(v1_info)?;
 
-                let upgrade_response = super::latest::interface::ResponseData::try_from(response)?;
-
-                Ok((upgrade_response.into(), version))
+                let response_data = ResponseData::from(v2_info);
+                Ok((response_data, version))
             }
             super::Version::V1 => {
                 let versioned_response = v1::VersionedResponse::try_from(self.clone())?;
-                let (extracted, version) = versioned_response.extract()?;
-                Ok((extracted.into(), version))
+                let (extracted_v1_info, version) = versioned_response.extract()?;
+                let v2_info = v2::interface::ResponseData::try_from(extracted_v1_info)?;
+
+                let response_data = ResponseData::from(v2_info);
+                Ok((response_data, version))
+            }
+            super::Version::V2 => {
+                let versioned_response = v2::VersionedResponse::try_from(self.clone())?;
+                let (extracted_v2_info, version) = versioned_response.extract()?;
+
+                let response_data = ResponseData::from(extracted_v2_info);
+                Ok((response_data, version))
             }
         }
     }

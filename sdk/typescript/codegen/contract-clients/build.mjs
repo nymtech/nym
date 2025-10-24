c3dad4231d044bd387a6bc1345106afd6ecc79e5
Mark Sinclair <mmsinclair@users.noreply.github.com>
2025-10-24 23:23:41 +0100

    ts-sdk: contract clients: update and re-run autogen

--- sdk/typescript/codegen/contract-clients/build.mjs
+++ sdk/typescript/codegen/contract-clients/build.mjs
@@ -3,13 +3,11 @@ import codegen from '@cosmwasm/ts-codegen';
 codegen
   .default({
     contracts: [
-      { name: 'coconut-bandwidth', dir: '../../../../contracts/coconut-bandwidth' },
+      { name: 'ecash', dir: '../../../../contracts/ecash' },
       { name: 'coconut-dkg', dir: '../../../../contracts/coconut-dkg' },
       { name: 'mixnet', dir: '../../../../contracts/mixnet' },
       { name: 'cw3-flex-multisig', dir: '../../../../contracts/multisig/cw3-flex-multisig' },
       { name: 'cw4-group', dir: '../../../../contracts/multisig/cw4-group' },
-      { name: 'name-service', dir: '../../../../contracts/name-service' },
-      { name: 'service-provider-directory', dir: '../../../../contracts/service-provider-directory' },
       { name: 'vesting', dir: '../../../../contracts/vesting' },
     ],
     outPath: './src',

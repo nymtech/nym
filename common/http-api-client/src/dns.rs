f9232e62df964cdc8642634c461bb30ccdf29928
jmwample <jmwample@users.noreply.github.com>
2025-11-05 15:15:34 -0700

    initial implementation of a static DNS resolver for fallback

--- common/http-api-client/src/dns.rs
+++ common/http-api-client/src/dns.rs
@@ -30,9 +30,9 @@
 use crate::ClientBuilder;
 
 use std::{
-    net::SocketAddr,
-    sync::{Arc, LazyLock},
-    time::Duration,
+    collections::HashMap,
+    net::{IpAddr, SocketAddr},
+    sync::{Arc, LazyLock, Mutex},
 };
 
 use hickory_resolver::{
@@ -77,9 +77,11 @@ static SHARED_RESOLVER: LazyLock<HickoryDnsResolver> = LazyLock::new(|| {
 /// Error occurring while resolving a hostname into an IP address.
 pub enum ResolveError {
     #[error("hickory-dns resolver error: {0}")]
-    HickoryDnsError(#[from] hickory_resolver::ResolveError),
+    ResolveError(#[from] hickory_resolver::ResolveError),
     #[error("high level lookup timed out")]
     Timeout,
+    #[error("hostname not found in static lookup table")]
+    StaticLookupMiss,
 }
 
 /// Wrapper around an `AsyncResolver`, which implements the `Resolve` trait.
@@ -301,9 +303,46 @@ fn new_resolver_system() -> Result<TokioResolver, ResolveError> {
     Ok(resolver_builder.build())
 }
 
+struct StaticResolver {
+    static_addr_map: Arc<Mutex<HashMap<String, Vec<IpAddr>>>>,
+}
+
+impl StaticResolver {
+    fn new(static_entries: HashMap<String, Vec<IpAddr>>) -> StaticResolver {
+        Self {
+            static_addr_map: Arc::new(Mutex::new(static_entries)),
+        }
+    }
+}
+
+impl Resolve for StaticResolver {
+    fn resolve(&self, name: Name) -> Resolving {
+        let addr_map = self.static_addr_map.clone();
+        Box::pin(async move {
+            let addr_map = addr_map.lock().unwrap();
+            let lookup = match addr_map.get(name.as_str()) {
+                None => return Err(ResolveError::StaticLookupMiss.into()),
+                Some(addrs) => addrs,
+            };
+            let addrs: Addrs = Box::new(
+                lookup
+                    .clone()
+                    .into_iter()
+                    .map(|ip_addr| SocketAddr::new(ip_addr, 0)),
+            );
+
+            Ok(addrs)
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use itertools::Itertools;
+
     use super::*;
+    use std::error::Error as StdError;
+    use std::str::FromStr;
 
     #[tokio::test]
     async fn reqwest_with_custom_dns() {
@@ -334,6 +373,39 @@ mod test {
         let addrs = resolver.resolve_str(domain).await?;
 
         assert!(addrs.into_iter().next().is_some());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn lookup_using_static_resolver() -> Result<(), Box<dyn StdError + Send + Sync>> {
+        let example_domain = String::from("static.nymvpn.com");
+
+        // lookup for domain for which there is no entry
+        let resolver = StaticResolver::new(HashMap::new());
+
+        let url = reqwest::dns::Name::from_str(&example_domain).unwrap();
+        let result = resolver.resolve(url.into()).await;
+        assert!(result.is_err());
+        match result {
+            Ok(_) => panic!("lookup with empty map should fail"),
+            Err(e) => assert_eq!(e.to_string(), ResolveError::StaticLookupMiss.to_string()),
+        }
+
+        // Successful lookup
+        let mut addr_map = HashMap::new();
+        let example_ip4: IpAddr = "10.10.10.10".parse().unwrap();
+        let example_ip6: IpAddr = "dead::beef".parse().unwrap();
+        addr_map.insert(
+            example_domain.clone(),
+            vec![example_ip4.clone(), example_ip6.clone()],
+        );
+
+        let url = reqwest::dns::Name::from_str(&example_domain).unwrap();
+        let resolver = StaticResolver::new(addr_map);
+        let mut addrs = resolver.resolve(url.into()).await?;
+        assert!(addrs.contains(&SocketAddr::new(example_ip4, 0)));
+        assert!(addrs.contains(&SocketAddr::new(example_ip6, 0)));
 
         Ok(())
     }

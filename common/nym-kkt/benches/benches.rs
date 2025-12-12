539216d5854454adf96d5ad8551cc5116465f0a7
Georgio Nicolas <me@georgio.xyz>
2025-12-12 16:30:15 +0100

    Add KKT cryptographic primitives
    Post-quantum Key Encapsulation Mechanism (KEM) Key Transfer protocol.
    Enables efficient distribution of post-quantum KEM public keys.
    
    Squashed from georgio/noise-psq branch.

---
+++ common/nym-kkt/benches/benches.rs
@@ -0,0 +1,518 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use nym_crypto::asymmetric::ed25519;
+use nym_kkt::{
+    ciphersuite::{Ciphersuite, EncapsulationKey, HashFunction, SignatureScheme, KEM},
+    context::KKTMode,
+    frame::KKTFrame,
+    key_utils::{generate_keypair_libcrux, generate_keypair_mceliece, hash_encapsulation_key},
+    session::{
+        anonymous_initiator_process, initiator_ingest_response, initiator_process,
+        responder_ingest_message, responder_process,
+    },
+};
+use rand::prelude::*;
+
+pub fn gen_ed25519_keypair(c: &mut Criterion) {
+    c.bench_function("Generate Ed25519 Keypair", |b| {
+        b.iter(|| {
+            let mut s: [u8; 32] = [0u8; 32];
+            rand::rng().fill_bytes(&mut s);
+            ed25519::KeyPair::from_secret(s, 0)
+        });
+    });
+}
+
+pub fn gen_mlkem768_keypair(c: &mut Criterion) {
+    c.bench_function("Generate MlKem768 Keypair", |b| {
+        b.iter(|| {
+            libcrux_kem::key_gen(libcrux_kem::Algorithm::MlKem768, &mut rand::rng()).unwrap()
+        });
+    });
+}
+
+pub fn kkt_benchmark(c: &mut Criterion) {
+    let mut rng = rand::rng();
+
+    // generate ed25519 keys
+    let mut secret_initiator: [u8; 32] = [0u8; 32];
+    rng.fill_bytes(&mut secret_initiator);
+    let initiator_ed25519_keypair = ed25519::KeyPair::from_secret(secret_initiator, 0);
+
+    let mut secret_responder: [u8; 32] = [0u8; 32];
+    rng.fill_bytes(&mut secret_responder);
+    let responder_ed25519_keypair = ed25519::KeyPair::from_secret(secret_responder, 1);
+    for kem in [KEM::MlKem768, KEM::XWing, KEM::X25519, KEM::McEliece] {
+        for hash_function in [
+            HashFunction::Blake3,
+            HashFunction::SHA256,
+            HashFunction::SHAKE128,
+            HashFunction::SHAKE256,
+        ] {
+            let ciphersuite = Ciphersuite::resolve_ciphersuite(
+                kem,
+                hash_function,
+                SignatureScheme::Ed25519,
+                None,
+            )
+            .unwrap();
+
+            // generate kem public keys
+
+            let (responder_kem_public_key, initiator_kem_public_key) = match kem {
+                KEM::MlKem768 => (
+                    EncapsulationKey::MlKem768(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                    EncapsulationKey::MlKem768(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                ),
+                KEM::XWing => (
+                    EncapsulationKey::XWing(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                    EncapsulationKey::XWing(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                ),
+                KEM::X25519 => (
+                    EncapsulationKey::X25519(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                    EncapsulationKey::X25519(generate_keypair_libcrux(&mut rng, kem).unwrap().1),
+                ),
+                KEM::McEliece => (
+                    EncapsulationKey::McEliece(generate_keypair_mceliece(&mut rng).1),
+                    EncapsulationKey::McEliece(generate_keypair_mceliece(&mut rng).1),
+                ),
+            };
+
+            let i_kem_key_bytes = initiator_kem_public_key.encode();
+
+            let r_kem_key_bytes = responder_kem_public_key.encode();
+
+            let i_dir_hash = hash_encapsulation_key(
+                &ciphersuite.hash_function(),
+                ciphersuite.hash_len(),
+                &i_kem_key_bytes,
+            );
+
+            let r_dir_hash = hash_encapsulation_key(
+                &ciphersuite.hash_function(),
+                ciphersuite.hash_len(),
+                &r_kem_key_bytes,
+            );
+
+            // Anonymous Initiator, OneWay
+            {
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Generate Request",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| anonymous_initiator_process(&mut rng, ciphersuite).unwrap());
+                    },
+                );
+
+                let (mut i_context, i_frame) =
+                    anonymous_initiator_process(&mut rng, ciphersuite).unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Encode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| b.iter(|| i_frame.to_bytes()),
+                );
+
+                let i_frame_bytes = i_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Decode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| b.iter(|| KKTFrame::from_bytes(&i_frame_bytes).unwrap()),
+                );
+
+                let (i_frame_r, r_context) = KKTFrame::from_bytes(&i_frame_bytes).unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Responder Ingest Frame",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_ingest_message(&r_context, None, None, &i_frame_r).unwrap()
+                        });
+                    },
+                );
+
+                let (mut r_context, _) =
+                    responder_ingest_message(&r_context, None, None, &i_frame_r).unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Responder Generate Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_process(
+                                &mut r_context,
+                                i_frame_r.session_id_ref(),
+                                responder_ed25519_keypair.private_key(),
+                                &responder_kem_public_key,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+                let r_frame = responder_process(
+                    &mut r_context,
+                    i_frame_r.session_id_ref(),
+                    responder_ed25519_keypair.private_key(),
+                    &responder_kem_public_key,
+                )
+                .unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Responder Encode Frame",
+                        kem, hash_function
+                    ),
+                    |b| b.iter(|| r_frame.to_bytes()),
+                );
+
+                let r_bytes = r_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Anonymous Initiator: Initiator Ingest Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            initiator_ingest_response(
+                                &mut i_context,
+                                responder_ed25519_keypair.public_key(),
+                                &r_dir_hash,
+                                &r_bytes,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let obtained_key = initiator_ingest_response(
+                    &mut i_context,
+                    responder_ed25519_keypair.public_key(),
+                    &r_dir_hash,
+                    &r_bytes,
+                )
+                .unwrap();
+
+                assert_eq!(obtained_key.encode(), r_kem_key_bytes)
+            }
+            // Initiator, OneWay
+            {
+                let (mut i_context, i_frame) = initiator_process(
+                    &mut rng,
+                    KKTMode::OneWay,
+                    ciphersuite,
+                    initiator_ed25519_keypair.private_key(),
+                    None,
+                )
+                .unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Generate Request",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            initiator_process(
+                                &mut rng,
+                                KKTMode::OneWay,
+                                ciphersuite,
+                                initiator_ed25519_keypair.private_key(),
+                                None,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Encode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| b.iter(|| i_frame.to_bytes()),
+                );
+
+                let i_frame_bytes = i_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Decode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| b.iter(|| KKTFrame::from_bytes(&i_frame_bytes).unwrap()),
+                );
+
+                let (i_frame_r, r_context) = KKTFrame::from_bytes(&i_frame_bytes).unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Responder Ingest Frame",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_ingest_message(
+                                &r_context,
+                                Some(initiator_ed25519_keypair.public_key()),
+                                None,
+                                &i_frame_r,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let (mut r_context, r_obtained_key) = responder_ingest_message(
+                    &r_context,
+                    Some(initiator_ed25519_keypair.public_key()),
+                    None,
+                    &i_frame_r,
+                )
+                .unwrap();
+
+                assert!(r_obtained_key.is_none());
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Responder Generate Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_process(
+                                &mut r_context,
+                                i_frame_r.session_id_ref(),
+                                responder_ed25519_keypair.private_key(),
+                                &responder_kem_public_key,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let r_frame = responder_process(
+                    &mut r_context,
+                    i_frame_r.session_id_ref(),
+                    responder_ed25519_keypair.private_key(),
+                    &responder_kem_public_key,
+                )
+                .unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Responder Encode Frame",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| r_frame.to_bytes());
+                    },
+                );
+
+                let r_bytes = r_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator OneWay: Initiator Ingest Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            initiator_ingest_response(
+                                &mut i_context,
+                                responder_ed25519_keypair.public_key(),
+                                &r_dir_hash,
+                                &r_bytes,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let i_obtained_key = initiator_ingest_response(
+                    &mut i_context,
+                    responder_ed25519_keypair.public_key(),
+                    &r_dir_hash,
+                    &r_bytes,
+                )
+                .unwrap();
+
+                assert_eq!(i_obtained_key.encode(), r_kem_key_bytes)
+            }
+
+            // Initiator, Mutual
+            {
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Generate Request",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            initiator_process(
+                                &mut rng,
+                                KKTMode::Mutual,
+                                ciphersuite,
+                                initiator_ed25519_keypair.private_key(),
+                                Some(&initiator_kem_public_key),
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let (mut i_context, i_frame) = initiator_process(
+                    &mut rng,
+                    KKTMode::Mutual,
+                    ciphersuite,
+                    initiator_ed25519_keypair.private_key(),
+                    Some(&initiator_kem_public_key),
+                )
+                .unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Encode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| i_frame.to_bytes());
+                    },
+                );
+
+                let i_frame_bytes = i_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Decode Frame - Request",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| KKTFrame::from_bytes(&i_frame_bytes).unwrap());
+                    },
+                );
+
+                let (i_frame_r, r_context) = KKTFrame::from_bytes(&i_frame_bytes).unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Responder Ingest Frame",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_ingest_message(
+                                &r_context,
+                                Some(initiator_ed25519_keypair.public_key()),
+                                Some(&i_dir_hash),
+                                &i_frame_r,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let (mut r_context, r_obtained_key) = responder_ingest_message(
+                    &r_context,
+                    Some(initiator_ed25519_keypair.public_key()),
+                    Some(&i_dir_hash),
+                    &i_frame_r,
+                )
+                .unwrap();
+
+                assert_eq!(r_obtained_key.unwrap().encode(), i_kem_key_bytes);
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Responder Generate Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            responder_process(
+                                &mut r_context,
+                                i_frame_r.session_id_ref(),
+                                responder_ed25519_keypair.private_key(),
+                                &responder_kem_public_key,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let r_frame = responder_process(
+                    &mut r_context,
+                    i_frame_r.session_id_ref(),
+                    responder_ed25519_keypair.private_key(),
+                    &responder_kem_public_key,
+                )
+                .unwrap();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Responder Encode Frame",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            r_frame.to_bytes();
+                        });
+                    },
+                );
+
+                let r_bytes = r_frame.to_bytes();
+
+                c.bench_function(
+                    &format!(
+                        "{}, {} | Initiator Mutual: Initiator Ingest Response",
+                        kem, hash_function
+                    ),
+                    |b| {
+                        b.iter(|| {
+                            initiator_ingest_response(
+                                &mut i_context,
+                                responder_ed25519_keypair.public_key(),
+                                &r_dir_hash,
+                                &r_bytes,
+                            )
+                            .unwrap()
+                        });
+                    },
+                );
+
+                let obtained_key = initiator_ingest_response(
+                    &mut i_context,
+                    responder_ed25519_keypair.public_key(),
+                    &r_dir_hash,
+                    &r_bytes,
+                )
+                .unwrap();
+
+                assert_eq!(obtained_key.encode(), r_kem_key_bytes)
+            }
+        }
+    }
+}
+
+criterion_group!(
+    benches,
+    gen_ed25519_keypair,
+    gen_mlkem768_keypair,
+    kkt_benchmark
+);
+criterion_main!(benches);
---
+++ common/nym-kkt/src/error.rs
@@ -0,0 +1,90 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+use thiserror::Error;
+
+use crate::context::KKTStatus;
+
+#[derive(Error, Debug)]
+pub enum KKTError {
+    #[error("Signature constructor error")]
+    SigConstructorError,
+    #[error("Signature verification error")]
+    SigVerifError,
+    #[error("Ciphersuite Decoding Error: {}", info)]
+    CiphersuiteDecodingError { info: String },
+    #[error("Insecure Encapsulation Key Hash Length")]
+    InsecureHashLen,
+
+    #[error("KKT Frame Decoding Error: {}", info)]
+    FrameDecodingError { info: String },
+
+    #[error("KKT Frame Encoding Error: {}", info)]
+    FrameEncodingError { info: String },
+
+    #[error("KKT Incompatibility Error: {}", info)]
+    IncompatibilityError { info: &'static str },
+
+    #[error("KKT Responder Flagged Error: {}", status)]
+    ResponderFlaggedError { status: KKTStatus },
+
+    #[error("KKT Message Count Limit Reached")]
+    MessageCountLimitReached,
+
+    #[error("PSQ KEM Error: {}", info)]
+    KEMError { info: &'static str },
+
+    #[error("Local Function Input Error: {}", info)]
+    FunctionInputError { info: &'static str },
+
+    #[error("{}", info)]
+    X25519Error { info: &'static str },
+    // #[error("Protocol did not complete")]
+    // ProtocolError,
+
+    // #[error("encountered an IO error: {0}")]
+    // IoError(#[from] io::Error),s
+
+    // #[error("Handshake timeout")]
+    // HandshakeTimeout(#[from] tokio::time::error::Elapsed),
+}
+
+impl From<libcrux_kem::Error> for KKTError {
+    fn from(err: libcrux_kem::Error) -> Self {
+        match err {
+            libcrux_kem::Error::EcDhError(_) => KKTError::KEMError { info: "ECDH Error" },
+            libcrux_kem::Error::KeyGen => KKTError::KEMError {
+                info: "Key Generation Error",
+            },
+            libcrux_kem::Error::Encapsulate => KKTError::KEMError {
+                info: "Encapsulation Error",
+            },
+            libcrux_kem::Error::Decapsulate => KKTError::KEMError {
+                info: "Decapsulation Error",
+            },
+            libcrux_kem::Error::UnsupportedAlgorithm => KKTError::KEMError {
+                info: "libcrux Unsupported Algorithm",
+            },
+            libcrux_kem::Error::InvalidPrivateKey => KKTError::KEMError {
+                info: "Invalid Private Key",
+            },
+
+            libcrux_kem::Error::InvalidPublicKey => KKTError::KEMError {
+                info: "Invalid Public Key",
+            },
+            libcrux_kem::Error::InvalidCiphertext => KKTError::KEMError {
+                info: "Invalid Ciphertext",
+            },
+        }
+    }
+}
+impl From<libcrux_ecdh::Error> for KKTError {
+    fn from(err: libcrux_ecdh::Error) -> Self {
+        match err {
+            libcrux_ecdh::Error::InvalidPoint => KKTError::KEMError {
+                info: "Invalid Remote Public Key",
+            },
+            _ => todo!(),
+        }
+    }
+}

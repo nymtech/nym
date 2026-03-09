820e499a5fbd1f1dffe4f1736cf9356b6e2ff163
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-03-09 10:47:42 +0000

    create initial structure for network monitors contract

---
+++ common/cosmwasm-smart-contracts/network-monitors-contract/src/constants.rs
@@ -0,0 +1,6 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod storage_keys {
+    pub const CONTRACT_ADMIN: &str = "contract-admin";
+}

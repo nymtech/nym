960358308689b304e56c211b2cb72540ad268857
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-03-09 13:13:24 +0000

    implemented logic of the network monitors contract

--- contracts/network-monitors/src/queries.rs
+++ contracts/network-monitors/src/queries.rs
@@ -1,16 +1,60 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::storage::NETWORK_MONITORS_CONTRACT_STORAGE;
-use cosmwasm_std::Deps;
+use crate::storage::{retrieval_limits, NETWORK_MONITORS_CONTRACT_STORAGE};
+use cosmwasm_std::{Deps, StdResult};
 use cw_controllers::AdminResponse;
-use nym_network_monitors_contract_common::NetworkMonitorsContractError;
+use cw_storage_plus::Bound;
+use nym_network_monitors_contract_common::{
+    AuthorisedNetworkMonitorOrchestratorsResponse, AuthorisedNetworkMonitorsPagedResponse,
+    NetworkMonitorsContractError,
+};
+use std::net::IpAddr;
 
 pub fn query_admin(deps: Deps) -> Result<AdminResponse, NetworkMonitorsContractError> {
     NETWORK_MONITORS_CONTRACT_STORAGE
         .contract_admin
         .query_admin(deps)
         .map_err(Into::into)
+}
+
+// no need for pagination as we don't expect even a double digit of those
+pub fn query_network_monitor_orchestrators(
+    deps: Deps,
+) -> Result<AuthorisedNetworkMonitorOrchestratorsResponse, NetworkMonitorsContractError> {
+    let authorised = NETWORK_MONITORS_CONTRACT_STORAGE
+        .authorised_orchestrators
+        .range(deps.storage, None, None, cosmwasm_std::Order::Ascending)
+        .map(|record| record.map(|(_, details)| details))
+        .collect::<StdResult<Vec<_>>>()?;
+
+    Ok(AuthorisedNetworkMonitorOrchestratorsResponse { authorised })
+}
+
+pub fn query_network_monitor_agents(
+    deps: Deps,
+    start_after: Option<IpAddr>,
+    limit: Option<u32>,
+) -> Result<AuthorisedNetworkMonitorsPagedResponse, NetworkMonitorsContractError> {
+    let limit = limit
+        .unwrap_or(retrieval_limits::AGENTS_DEFAULT_LIMIT)
+        .min(retrieval_limits::AGENTS_MAX_LIMIT) as usize;
+
+    let start = start_after.map(|addr| Bound::exclusive(addr.to_string()));
+
+    let authorised = NETWORK_MONITORS_CONTRACT_STORAGE
+        .authorised_agents
+        .range(deps.storage, start, None, cosmwasm_std::Order::Ascending)
+        .take(limit)
+        .map(|record| record.map(|(_, details)| details))
+        .collect::<StdResult<Vec<_>>>()?;
+
+    let start_next_after = authorised.last().map(|last| last.address);
+
+    Ok(AuthorisedNetworkMonitorsPagedResponse {
+        authorised,
+        start_next_after,
+    })
 }
 
 #[cfg(test)]

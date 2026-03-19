11c09a15aeafe3e79ec66e4d0e70b7618c2c5db6
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-03-19 11:04:46 +0000

    additional structure for receiving ingress packets

--- nym-network-monitor-v3/nym-network-monitor-agent/src/cli/run_agent.rs
+++ nym-network-monitor-v3/nym-network-monitor-agent/src/cli/run_agent.rs
@@ -1,6 +1,7 @@
 // Copyright 2026 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use super::env::vars::*;
 use crate::cli::common::CommonArgs;
 use url::Url;
 
@@ -10,8 +11,13 @@ pub(crate) struct Args {
     common_args: CommonArgs,
 
     /// Address of the orchestrator for requesting work assignments
-    #[clap(long)]
+    #[clap(long, env = NYM_NETWORK_MONITOR_AGENT_ORCHESTRATOR_ADDRESS_ARG)]
     orchestrator_address: Url,
+
+    /// Bearer token required for requesting work assignments
+    /// and submitting the results
+    #[clap(long, env = NYM_NETWORK_MONITOR_AGENT_ORCHESTRATOR_TOKEN_ARG)]
+    orchestrator_token: String,
 }
 
 pub(crate) async fn execute(args: Args) -> anyhow::Result<()> {

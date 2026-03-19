bc3df3151821e1e948236318d9516077bf563952
Simon Wicky <simon@nymtech.net>
2026-03-19 15:09:20 +0100

    move format_debug_bytes in common crate (#6580)
    * move format_debug_bytes in common crate
    
    * license change

--- common/nym-common/src/debug.rs
+++ common/nym-common/src/debug.rs
@@ -1,3 +1,6 @@
+// Copyright 2026 Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
 use std::fmt::{self, Write};
 
 pub fn format_debug_bytes(bytes: &[u8]) -> Result<String, fmt::Error> {

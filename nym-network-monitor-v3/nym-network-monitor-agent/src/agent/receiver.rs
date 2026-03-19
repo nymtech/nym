73078ea26d814e90225ed4879727b9ce3f80bc61
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-03-19 14:48:45 +0000

    initial sending/receiving test loop

--- nym-network-monitor-v3/nym-network-monitor-agent/src/agent/receiver.rs
+++ nym-network-monitor-v3/nym-network-monitor-agent/src/agent/receiver.rs
@@ -1,13 +1,13 @@
 // Copyright 2026 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use futures::channel::mpsc::{UnboundedReceiver, UnboundedSender};
 use nym_sphinx_framing::packet::FramedNymPacket;
 use time::OffsetDateTime;
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
 pub(crate) struct ReceivedPacket {
-    received_at: OffsetDateTime,
-    received: FramedNymPacket,
+    pub(crate) received_at: OffsetDateTime,
+    pub(crate) received: FramedNymPacket,
 }
 
 impl ReceivedPacket {
@@ -20,13 +20,4 @@ impl ReceivedPacket {
 }
 
 pub(crate) type MixnetPacketsSender = UnboundedSender<ReceivedPacket>;
-
-pub(crate) struct MixnetPacketsReceiver {
-    inner: UnboundedReceiver<ReceivedPacket>,
-}
-
-impl MixnetPacketsReceiver {
-    pub(crate) fn new(inner: UnboundedReceiver<ReceivedPacket>) -> Self {
-        Self { inner }
-    }
-}
+pub(crate) type MixnetPacketsReceiver = UnboundedReceiver<ReceivedPacket>;

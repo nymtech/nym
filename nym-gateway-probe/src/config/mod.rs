e803fa3e5c3c800c6a9da4c9050920c0186109ba
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2026-04-07 12:28:12 +0200

    PR feedback

--- nym-gateway-probe/src/config/mod.rs
+++ nym-gateway-probe/src/config/mod.rs
@@ -29,7 +29,7 @@ pub struct ProbeConfig {
     ///   socks5-only - Socks5 network requester test
     ///   all         - Mixnet, wireguard over authenticator and LP registration
     ///
-    #[arg(long, default_value_t = ProbeConfig::default().test_mode, verbatim_doc_comment)]
+    #[arg(long, default_value_t = TestMode::default(), verbatim_doc_comment)]
     pub test_mode: TestMode,
 
     #[arg(long, global = true)]

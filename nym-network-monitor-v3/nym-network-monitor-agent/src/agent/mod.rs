fd8abf434748c32ffe9c621259c04f9a984428ee
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-04-14 17:35:24 +0100

    stub the initial structure for the agent

--- nym-network-monitor-v3/nym-network-monitor-agent/src/agent/mod.rs
+++ nym-network-monitor-v3/nym-network-monitor-agent/src/agent/mod.rs
@@ -4,11 +4,12 @@
 use crate::agent::config::NodeTesterConfig;
 use crate::agent::helpers::load_noise_key;
 use nym_crypto::asymmetric::x25519;
-use nym_pemstore::load_key;
+use nym_network_monitor_orchestrator_requests::client::OrchestratorClient;
+use nym_network_monitor_orchestrator_requests::models::AgentAnnounceRequest;
+use nym_noise::LATEST_NOISE_VERSION;
 use std::path::Path;
 use std::sync::Arc;
-use url::Url;
-use zeroize::Zeroizing;
+use tracing::info;
 
 pub(crate) mod config;
 pub(crate) mod helpers;
@@ -22,15 +23,10 @@ pub(crate) struct NetworkMonitorAgent {
     /// Tester configuration controlling rates, timeouts, and addressing.
     tester_config: NodeTesterConfig,
 
-    /// Address of the orchestrator for requesting work assignments
-    orchestrator_address: Url,
+    orchestrator_client: OrchestratorClient,
 
     /// The tester's own Noise key pair, used to authenticate the egress connection.
     noise_key: Arc<x25519::KeyPair>,
-
-    /// Bearer token required for requesting work assignments
-    /// and submitting the results
-    orchestrator_token: Zeroizing<String>,
 }
 
 impl NetworkMonitorAgent {
@@ -39,14 +35,48 @@ impl NetworkMonitorAgent {
     pub(crate) fn new<P: AsRef<Path>>(
         tester_config: NodeTesterConfig,
         noise_key_path: P,
-        orchestrator_address: Url,
-        orchestrator_token: String,
+        orchestrator_client: OrchestratorClient,
     ) -> anyhow::Result<Self> {
         Ok(NetworkMonitorAgent {
             tester_config,
-            orchestrator_address,
+            orchestrator_client,
             noise_key: load_noise_key(noise_key_path)?,
-            orchestrator_token: Zeroizing::new(orchestrator_token),
         })
+    }
+
+    // TODO: orchestrator will have to check if this combination of key/address already exists
+    pub(crate) async fn announce_agent(&self) -> anyhow::Result<()> {
+        self.orchestrator_client
+            .announce_agent(&AgentAnnounceRequest {
+                agent_mix_socket_address: self.tester_config.mixnet_address,
+                x25519_noise_key: *self.noise_key.public_key(),
+                // we're always using the latest noise version available
+                noise_version: LATEST_NOISE_VERSION.into(),
+            })
+            .await?;
+        Ok(())
+    }
+
+    pub(crate) async fn run_stress_test(&self) -> anyhow::Result<()> {
+        // 1. query the orchestrator for a work assignment
+        let Some(work_assignment) = self
+            .orchestrator_client
+            .request_work_assignment()
+            .await?
+            .assignment
+        else {
+            // 2. if no work is available - exit immediately
+            info!("no work available, exiting...");
+            return Ok(());
+        };
+
+        info!("retrieved the following work assignment: {work_assignment:?}");
+
+        // TODO:
+        // 3. otherwise construct the tester and attempt to perform the measurements
+        let _ = &self.tester_config;
+
+        // 4. after that has concluded - submit the results back to the orchestrator
+        Ok(())
     }
 }

7f898d41dfb1a71602d0a1c64e85017518ab2a85
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2026-04-22 13:25:02 +0100

    stubs for submitting results

--- nym-api/src/nym_nodes/handlers/v3.rs
+++ nym-api/src/nym_nodes/handlers/v3.rs
@@ -7,17 +7,9 @@ use axum::extract::{Path, State};
 use axum::routing::{get, post};
 use axum::{Json, Router};
 use nym_api_requests::models::network_monitor::KnownNetworkMonitorResponse;
+use nym_api_requests::models::StressTestBatchSubmission;
 use nym_crypto::asymmetric::ed25519;
-
-pub(crate) fn routes() -> Router<AppState> {
-    Router::new().nest("/stress-testing", stress_testing_routes())
-}
-
-fn stress_testing_routes() -> Router<AppState> {
-    Router::new()
-        .route("/batch-submit", post(batch_submit_stress_testing_results))
-        .route("/known-monitors/:identity_key", get(known_network_monitor))
-}
+use std::time::Duration;
 
 #[utoipa::path(
     tag = "Nym Nodes",
@@ -28,7 +20,50 @@ fn stress_testing_routes() -> Router<AppState> {
         (status = 501, description = "the endpoint has not been implemented yet"),
     ),
 )]
-async fn batch_submit_stress_testing_results(State(state): State<AppState>) -> ApiResult<()> {
+async fn batch_submit_stress_testing_results(
+    State(state): State<AppState>,
+    Json(body): Json<StressTestBatchSubmission>,
+) -> ApiResult<()> {
+    // 1. check if the request is not stale
+    // TODO: make the timeout configurable. currently we have an issue of no easy way of
+    // propagating config values, but hardcoding it to 30s is fine for now
+    if body.body.is_stale(Duration::from_secs(30)) {
+        return Err(AxumErrorResponse::bad_request(
+            "request is stale, please resubmit it with a fresh timestamp",
+        ));
+    }
+
+    // 2. check if the sent public key is even in the authorised set
+    if !state
+        .network_monitors()
+        .is_authorised(&state.nyxd_client)
+        .await?
+    {
+        return Err(AxumErrorResponse::unauthorised(
+            "the provided public key does not correspond to any known network monitor",
+        ));
+    }
+
+    // 3. check if the request is not replayed (i.e. timestamp is not smaller than the latest known submission)
+    let last_request = state
+        .network_monitor_submissions
+        .submitted(body.body.signer)
+        .await;
+
+    if body.body.timestamp <= last_request {
+        return Err(AxumErrorResponse::bad_request(
+            "each request must have an explicitly greater timestamp than the previous one",
+        ));
+    }
+
+    // 4. verify the signature on the request
+    if !body.verify_signature(&body.body.signer) {
+        return Err(AxumErrorResponse::unauthorised(
+            "the provided request failed integrity check",
+        ));
+    }
+
+    // 5. process received results
     Err(AxumErrorResponse::not_implemented())
 }
 
@@ -63,4 +98,14 @@ async fn known_network_monitor(
         identity_key,
         authorised,
     }))
+}
+
+fn stress_testing_routes() -> Router<AppState> {
+    Router::new()
+        .route("/batch-submit", post(batch_submit_stress_testing_results))
+        .route("/known-monitors/:identity_key", get(known_network_monitor))
+}
+
+pub(crate) fn routes() -> Router<AppState> {
+    Router::new().nest("/stress-testing", stress_testing_routes())
 }

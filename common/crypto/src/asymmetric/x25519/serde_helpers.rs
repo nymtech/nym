10cdf0c83424d44e311fdd38eb2c8b2d38f79e63
Simon Wicky <simon@nymtech.net>
2026-04-27 11:54:32 +0200

    helper changes

--- common/crypto/src/asymmetric/x25519/serde_helpers.rs
+++ common/crypto/src/asymmetric/x25519/serde_helpers.rs
@@ -1,7 +1,23 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use super::PublicKey;
+use super::{PrivateKey, PublicKey};
+
+pub mod bs58_x25519_private_key {
+    use super::*;
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S: Serializer>(key: &PrivateKey, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_str(&key.to_base58_string())
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<PrivateKey, D::Error> {
+        let s = String::deserialize(deserializer)?;
+        PrivateKey::from_base58_string(s).map_err(serde::de::Error::custom)
+    }
+}
 
 pub mod bs58_x25519_pubkey {
     use super::*;

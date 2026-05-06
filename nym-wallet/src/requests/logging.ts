7b30c83f9a74409a4c024b1dbb46f118ee6cf7a9
Tommy Verrall <tommyvez@protonmail.com>
2026-05-06 17:03:25 +0200

    Linting
     - Add changelog for future release notes

--- nym-wallet/src/requests/logging.ts
+++ nym-wallet/src/requests/logging.ts
@@ -2,5 +2,4 @@ import { invokeWrapper } from './wrapper';
 
 export const helpLogToggleWindow = async () => invokeWrapper<void>('help_log_toggle_window', {});
 
-export const logViewerWindowSupported = async () =>
-  invokeWrapper<boolean>('log_viewer_window_supported', {});
+export const logViewerWindowSupported = async () => invokeWrapper<boolean>('log_viewer_window_supported', {});

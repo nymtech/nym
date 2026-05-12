8d13307e0beca41999ac07970e124a6ccd74825d
jmwample <jmwample@users.noreply.github.com>
2026-05-12 14:57:24 -0600

    re-order default API urls for network details

--- common/network-defaults/src/mainnet.rs
+++ common/network-defaults/src/mainnet.rs
@@ -44,12 +44,12 @@ pub const NYM_APIS: &[ApiUrlConst] = &[
         front_hosts: None,
     },
     ApiUrlConst {
-        url: "https://nym-frontdoor.vercel.app/api/",
-        front_hosts: Some(&["vercel.app", "vercel.com"]),
-    },
-    ApiUrlConst {
         url: "https://nym-frontdoor.global.ssl.fastly.net/api/",
         front_hosts: Some(&["yelp.global.ssl.fastly.net"]),
+    },
+    ApiUrlConst {
+        url: "https://nym-frontdoor.vercel.app/api/",
+        front_hosts: Some(&["vercel.app", "vercel.com"]),
     },
     ApiUrlConst {
         url: "https://cdn1.media-platform.net/api/",
@@ -68,11 +68,15 @@ pub const UPGRADE_MODE_ATTESTER_ED25519_BS58_PUBKEY: &str =
 pub const NYM_VPN_APIS: &[ApiUrlConst] = &[
     ApiUrlConst {
         url: NYM_VPN_API,
-        front_hosts: Some(&["vercel.app", "vercel.com"]),
+        front_hosts: None,
     },
     ApiUrlConst {
         url: "https://nymvpn-frontdoor.global.ssl.fastly.net/api/",
         front_hosts: Some(&["yelp.global.ssl.fastly.net"]),
+    },
+    ApiUrlConst {
+        url: NYM_VPN_API,
+        front_hosts: Some(&["vercel.app", "vercel.com"]),
     },
 ];
 

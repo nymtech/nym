64c68d7b7686e8da07b10212d5367ea8e6b7da48
Andy Duplain <trojanfoe@gmail.com>
2026-05-21 09:13:31 +0100

    Fix tests.

--- nym-sqlx-pool-guard/src/lib.rs
+++ nym-sqlx-pool-guard/src/lib.rs
@@ -184,12 +184,9 @@ mod tests {
 
     use super::*;
 
+    #[traced_test]
     #[tokio::test]
     async fn test_wait_close() {
-        tracing_subscriber::fmt()
-            .with_max_level(tracing::Level::TRACE)
-            .init();
-
         let temp_dir = tempfile::tempdir().unwrap();
         let database_path = temp_dir.path().join("storage.db");
 

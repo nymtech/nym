59d5a13f40ebbff394e9f0d3b361b73f89f6023e
Tommy Verrall <tommyvez@protonmail.com>
2026-08-10 14:31:37 +0200

    Fix clippy

--- common/client-core/gateways-storage/build.rs
+++ common/client-core/gateways-storage/build.rs
@@ -28,12 +28,12 @@ async fn main() -> anyhow::Result<()> {
             .context("Failed to perform SQLx migrations")?;
 
         #[cfg(target_family = "unix")]
-        println!("cargo:rustc-env=DATABASE_URL=sqlite://{}", &database_path);
+        println!("cargo:rustc-env=DATABASE_URL=sqlite://{}", database_path);
 
         #[cfg(target_family = "windows")]
         // for some strange reason we need to add a leading `/` to the windows path even though it's
         // not a valid windows path... but hey, it works...
-        println!("cargo:rustc-env=DATABASE_URL=sqlite:///{}", &database_path);
+        println!("cargo:rustc-env=DATABASE_URL=sqlite:///{}", database_path);
     }
 
     Ok(())
